6442f82a0d2af667e748ddcc76b9c10539225fb3
Tobias Doerffel <tobias.doerffel@gmail.com>
2007-11-12 00:04:07 +0000

    several small fixes
    git-svn-id: https://lmms.svn.sf.net/svnroot/lmms/trunk/lmms@583 0778d3d1-df1d-0410-868b-ea421aaaa00d

--- plugins/ladspa_effect/caps/basics.h
+++ plugins/ladspa_effect/caps/basics.h
@@ -84,6 +84,7 @@
 #define MIN_GAIN .000001 /* -120 dB */
 #define NOISE_FLOOR .00000000000005 /* -266 dB */
 
+#ifdef BUILD_WIN32
 typedef int8_t			int8;
 typedef uint8_t			uint8;
 typedef int16_t			int16;
@@ -92,6 +93,16 @@ typedef int32_t			int32;
 typedef uint32_t		uint32;
 typedef int64_t			int64;
 typedef uint64_t		uint64;
+#else
+typedef __int8_t			int8;
+typedef __uint8_t			uint8;
+typedef __int16_t			int16;
+typedef __uint16_t		uint16;
+typedef __int32_t			int32;
+typedef __uint32_t		uint32;
+typedef __int64_t			int64;
+typedef __uint64_t		uint64;
+#endif
 
 typedef struct {
 	char * name;
