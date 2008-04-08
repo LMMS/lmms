4787071752253fddb88718a174496af3128d0342
Paul Giblock <drfaygo@gmail.com>
2008-04-08 08:35:26 +0000

    Added missing file
    git-svn-id: https://lmms.svn.sf.net/svnroot/lmms/trunk/lmms@904 0778d3d1-df1d-0410-868b-ea421aaaa00d

---
+++ include/controller_dialog.h
@@ -0,0 +1,62 @@
+/*
+ * controller_dialog.h - per-controller-specific view for changing a
+ * controller's settings
+ *
+ * Copyright (c) 2008 Paul Giblock <drfaygo/at/gmail.com>
+ * 
+ * This file is part of Linux MultiMedia Studio - http://lmms.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef _CONTROLLER_DIALOG_H
+#define _CONTROLLER_DIALOG_H
+
+#include <QtGui/QWidget>
+
+#include "automatable_model.h"
+#include "controller.h"
+#include "mv_base.h"
+
+class controllerDialog : public QWidget, public modelView
+{
+public:
+	controllerDialog( controller * _controller, QWidget * _parent ) :
+		modelView( _controller ),
+		QWidget( _parent )
+	{};
+
+	virtual ~controllerDialog()	{};
+
+public slots:
+	//void editControls( void );
+	//void deletePlugin( void );
+	//void displayHelp( void );
+	//void closeEffects( void );
+
+	
+signals:
+
+
+protected:
+	virtual void contextMenuEvent( QContextMenuEvent * _me ) {};
+	virtual void paintEvent( QPaintEvent * _pe ) {};
+	virtual void modelChanged( void ) {};
+
+} ;
+
+#endif
