1464d03b74565fb7789840e04829b03a148c8443
Tobias Doerffel <tobias.doerffel@gmail.com>
2008-06-30 21:47:47 +0000

    again fixed worker-thread destruction
    git-svn-id: https://lmms.svn.sf.net/svnroot/lmms/trunk/lmms@1250 0778d3d1-df1d-0410-868b-ea421aaaa00d

--- src/core/mixer.cpp
+++ src/core/mixer.cpp
@@ -354,11 +354,11 @@ mixer::~mixer()
 		for( int w = 0; w < m_numWorkers; ++w )
 		{
 			m_workers[w]->quit();
-			while( m_workers[w]->isRunning() )
-			{
-				DISTRIBUTE_JOB_QUEUE(jq);
-				m_workers[w]->wait( 50 );
-			}
+		}
+		DISTRIBUTE_JOB_QUEUE(jq);
+		for( int w = 0; w < m_numWorkers; ++w )
+		{
+			m_workers[w]->wait( 500 );
 		}
 	}
 
