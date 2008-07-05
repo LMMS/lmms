c678dbb49551ad1449ef3efc6191b12979a4212e
Tobias Doerffel <tobias.doerffel@gmail.com>
2008-07-05 23:21:28 +0000

    fixed tempo-sync-models (closes #2010776)
    git-svn-id: https://lmms.svn.sf.net/svnroot/lmms/trunk/lmms@1276 0778d3d1-df1d-0410-868b-ea421aaaa00d

--- src/core/instrument_functions.cpp
+++ src/core/instrument_functions.cpp
@@ -268,7 +268,7 @@ arpeggiator::arpeggiator( model * _parent ) :
 	m_arpEnabledModel( FALSE ),
 	m_arpModel( this, tr( "Arpeggio type" ) ),
 	m_arpRangeModel( 1.0f, 1.0f, 9.0f, 1.0f, this, tr( "Arpeggio range" ) ),
-	m_arpTimeModel( 100.0f, 25.0f, 2000.0f, 1.0f, 1.0, this,
+	m_arpTimeModel( 100.0f, 25.0f, 2000.0f, 1.0f, 2000, this,
 							tr( "Arpeggio time" ) ),
 	m_arpGateModel( 100.0f, 1.0f, 200.0f, 1.0f, this,
 							tr( "Arpeggio gate" ) ),
