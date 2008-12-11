4b9fb4f41f56c11e1984e870d27f2f80f89b8b40
Tobias Doerffel <tobias.doerffel@gmail.com>
2008-12-11 00:05:45 +0000

    splitted view component out of automation_pattern.*
    git-svn-id: https://lmms.svn.sf.net/svnroot/lmms/trunk/lmms@1905 0778d3d1-df1d-0410-868b-ea421aaaa00d

---
+++ include/automation_pattern_view.h
@@ -0,0 +1,73 @@
+/*
+ * automation_pattern_view.h - declaration of class automationPatternView
+ *
+ * Copyright (c) 2008 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ * 
+ * This file is part of Linux MultiMedia Studio - http://lmms.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+
+#ifndef _AUTOMATION_PATTERN_VIEW_H
+#define _AUTOMATION_PATTERN_VIEW_H
+
+#include "track.h"
+
+class automationPattern;
+
+
+class automationPatternView : public trackContentObjectView
+{
+	Q_OBJECT
+public:
+	automationPatternView( automationPattern * _pat, trackView * _parent );
+	virtual ~automationPatternView();
+
+
+public slots:
+	virtual void update( void );
+
+
+protected slots:
+	void resetName( void );
+	void changeName( void );
+	void disconnectObject( QAction * _a );
+
+
+protected:
+	virtual void constructContextMenu( QMenu * );
+	virtual void mouseDoubleClickEvent( QMouseEvent * _me );
+	virtual void paintEvent( QPaintEvent * _pe );
+	virtual void resizeEvent( QResizeEvent * _re )
+	{
+		m_needsUpdate = true;
+		trackContentObjectView::resizeEvent( _re );
+	}
+	virtual void dragEnterEvent( QDragEnterEvent * _dee );
+	virtual void dropEvent( QDropEvent * _de );
+
+
+private:
+	automationPattern * m_pat;
+	QPixmap m_paintPixmap;
+	bool m_needsUpdate;
+
+} ;
+
+
+#endif
