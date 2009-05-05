af284e980f7eba8a361935fb80af9f9c34731fc0
Frederik <freggy@gmail.com>
2009-05-05 23:13:32 +0200

    Allow compilation with -Werror=format-security
    This commit fixes some calls to functions that are taking a format
    string and an optional set of parameters as arguments. At some places
    no format string was specified if only a simple C string was to be
    sprintf()ed.  However for security reasons this is bad and was replaced
    by code like
    
    	sprintf( dest, "%s", str );

--- plugins/flp_import/unrtf/attr.c
+++ plugins/flp_import/unrtf/attr.c
@@ -108,10 +108,10 @@ attr_express_begin (int attr, const char* param) {
 	switch(attr) 
 	{
 	case ATTR_BOLD: 
-		outstring+=QString().sprintf(op->bold_begin); 
+		outstring+=QString().sprintf("%s", op->bold_begin); 
 		break;
 	case ATTR_ITALIC: 
-		outstring+=QString().sprintf(op->italic_begin); 
+		outstring+=QString().sprintf("%s", op->italic_begin); 
 		break;
 
 	/* Various underlines, they all resolve to HTML's <u> */
@@ -123,11 +123,11 @@ attr_express_begin (int attr, const char* param) {
 	case ATTR_2DOT_DASH_UL:
 	case ATTR_WORD_UL: 
 	case ATTR_UNDERLINE: 
-		outstring+=QString().sprintf(op->underline_begin); 
+		outstring+=QString().sprintf("%s", op->underline_begin); 
 		break;
 
 	case ATTR_DOUBLE_UL: 
-		outstring+=QString().sprintf(op->dbl_underline_begin); 
+		outstring+=QString().sprintf("%s", op->dbl_underline_begin); 
 		break;
 
 	case ATTR_FONTSIZE: 
@@ -148,18 +148,18 @@ attr_express_begin (int attr, const char* param) {
 		break;
 
 	case ATTR_SUPER: 
-		outstring+=QString().sprintf(op->superscript_begin); 
+		outstring+=QString().sprintf("%s", op->superscript_begin); 
 		break;
 	case ATTR_SUB: 
-		outstring+=QString().sprintf(op->subscript_begin); 
+		outstring+=QString().sprintf("%s", op->subscript_begin); 
 		break;
 
 	case ATTR_STRIKE: 
-		outstring+=QString().sprintf(op->strikethru_begin); 
+		outstring+=QString().sprintf("%s", op->strikethru_begin); 
 		break;
 
 	case ATTR_DBL_STRIKE: 
-		outstring+=QString().sprintf(op->dbl_strikethru_begin); 
+		outstring+=QString().sprintf("%s", op->dbl_strikethru_begin); 
 		break;
 
 	case ATTR_EXPAND: 
@@ -167,16 +167,16 @@ attr_express_begin (int attr, const char* param) {
 		break;
 
 	case ATTR_OUTLINE: 
-		outstring+=QString().sprintf(op->outline_begin); 
+		outstring+=QString().sprintf("%s", op->outline_begin); 
 		break;
 	case ATTR_SHADOW: 
-		outstring+=QString().sprintf(op->shadow_begin); 
+		outstring+=QString().sprintf("%s", op->shadow_begin); 
 		break;
 	case ATTR_EMBOSS: 
-		outstring+=QString().sprintf(op->emboss_begin); 
+		outstring+=QString().sprintf("%s", op->emboss_begin); 
 		break;
 	case ATTR_ENGRAVE: 
-		outstring+=QString().sprintf(op->engrave_begin); 
+		outstring+=QString().sprintf("%s", op->engrave_begin); 
 		break;
 
 	case ATTR_CAPS:
@@ -189,7 +189,7 @@ attr_express_begin (int attr, const char* param) {
 			simulate_smallcaps = TRUE;
 		else {
 			if (op->small_caps_begin)
-				outstring+=QString().sprintf(op->small_caps_begin); 
+				outstring+=QString().sprintf("%s", op->small_caps_begin); 
 		}
 		break;
 	}
@@ -209,10 +209,10 @@ attr_express_end (int attr, char *param)
 	switch(attr) 
 	{
 	case ATTR_BOLD: 
-		outstring+=QString().sprintf(op->bold_end); 
+		outstring+=QString().sprintf("%s", op->bold_end); 
 		break;
 	case ATTR_ITALIC: 
-		outstring+=QString().sprintf(op->italic_end); 
+		outstring+=QString().sprintf("%s", op->italic_end); 
 		break;
 
 	/* Various underlines, they all resolve to HTML's </u> */
@@ -224,11 +224,11 @@ attr_express_end (int attr, char *param)
 	case ATTR_2DOT_DASH_UL: 
 	case ATTR_WORD_UL: 
 	case ATTR_UNDERLINE: 
-		outstring+=QString().sprintf(op->underline_end); 
+		outstring+=QString().sprintf("%s", op->underline_end); 
 		break;
 
 	case ATTR_DOUBLE_UL: 
-		outstring+=QString().sprintf(op->dbl_underline_end); 
+		outstring+=QString().sprintf("%s", op->dbl_underline_end); 
 		break;
 
 	case ATTR_FONTSIZE: 
@@ -236,47 +236,47 @@ attr_express_end (int attr, char *param)
 		break;
 
 	case ATTR_FONTFACE: 
-		outstring+=QString().sprintf(op->font_end); 
+		outstring+=QString().sprintf("%s", op->font_end); 
 		break;
 
 	case ATTR_FOREGROUND: 
-		outstring+=QString().sprintf(op->foreground_end); 
+		outstring+=QString().sprintf("%s", op->foreground_end); 
 		break;
 	case ATTR_BACKGROUND: 
 		if (!simple_mode)
-		  outstring+=QString().sprintf(op->background_end);
+		  outstring+=QString().sprintf("%s", op->background_end);
 		break;
 
 	case ATTR_SUPER: 
-		outstring+=QString().sprintf(op->superscript_end); 
+		outstring+=QString().sprintf("%s", op->superscript_end); 
 		break;
 	case ATTR_SUB: 
-		outstring+=QString().sprintf(op->subscript_end); 
+		outstring+=QString().sprintf("%s", op->subscript_end); 
 		break;
 
 	case ATTR_STRIKE: 
-		outstring+=QString().sprintf(op->strikethru_end); 
+		outstring+=QString().sprintf("%s", op->strikethru_end); 
 		break;
 
 	case ATTR_DBL_STRIKE: 
-		outstring+=QString().sprintf(op->dbl_strikethru_end); 
+		outstring+=QString().sprintf("%s", op->dbl_strikethru_end); 
 		break;
 
 	case ATTR_OUTLINE: 
-		outstring+=QString().sprintf(op->outline_end); 
+		outstring+=QString().sprintf("%s", op->outline_end); 
 		break;
 	case ATTR_SHADOW: 
-		outstring+=QString().sprintf(op->shadow_end); 
+		outstring+=QString().sprintf("%s", op->shadow_end); 
 		break;
 	case ATTR_EMBOSS: 
-		outstring+=QString().sprintf(op->emboss_end); 
+		outstring+=QString().sprintf("%s", op->emboss_end); 
 		break;
 	case ATTR_ENGRAVE: 
-		outstring+=QString().sprintf(op->engrave_end); 
+		outstring+=QString().sprintf("%s", op->engrave_end); 
 		break;
 
 	case ATTR_EXPAND: 
-		outstring+=QString().sprintf(op->expand_end); 
+		outstring+=QString().sprintf("%s", op->expand_end); 
 		break;
 
 	case ATTR_CAPS:
@@ -289,7 +289,7 @@ attr_express_end (int attr, char *param)
 			simulate_smallcaps = FALSE;
 		else {
 			if (op->small_caps_end)
-				outstring+=QString().sprintf(op->small_caps_end); 
+				outstring+=QString().sprintf("%s", op->small_caps_end); 
 		}
 		break;
 	}
--- plugins/flp_import/unrtf/output.c
+++ plugins/flp_import/unrtf/output.c
@@ -238,49 +238,49 @@ op_begin_std_fontsize (OutputPersonality *op, int size)
 	switch (size) {
 	case 8:
 		if (op->fontsize8_begin) {
-			outstring+=QString().sprintf(op->fontsize8_begin);
+			outstring+=QString().sprintf("%s", op->fontsize8_begin);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 10:
 		if (op->fontsize10_begin) {
-			outstring+=QString().sprintf(op->fontsize10_begin);
+			outstring+=QString().sprintf("%s", op->fontsize10_begin);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 12:
 		if (op->fontsize12_begin) {
-			outstring+=QString().sprintf(op->fontsize12_begin);
+			outstring+=QString().sprintf("%s", op->fontsize12_begin);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 14:
 		if (op->fontsize14_begin) {
-			outstring+=QString().sprintf(op->fontsize14_begin);
+			outstring+=QString().sprintf("%s", op->fontsize14_begin);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 18:
 		if (op->fontsize18_begin) {
-			outstring+=QString().sprintf(op->fontsize18_begin);
+			outstring+=QString().sprintf("%s", op->fontsize18_begin);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 24:
 		if (op->fontsize24_begin) {
-			outstring+=QString().sprintf(op->fontsize24_begin);
+			outstring+=QString().sprintf("%s", op->fontsize24_begin);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 36:
 		if (op->fontsize36_begin) {
-			outstring+=QString().sprintf(op->fontsize36_begin);
+			outstring+=QString().sprintf("%s", op->fontsize36_begin);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 48:
 		if (op->fontsize48_begin) {
-			outstring+=QString().sprintf(op->fontsize48_begin);
+			outstring+=QString().sprintf("%s", op->fontsize48_begin);
 			found_std_expr = TRUE;
 		}
 		break;
@@ -300,46 +300,46 @@ op_begin_std_fontsize (OutputPersonality *op, int size)
 			 * size.
 			 */
 			if (size<9 && op->fontsize8_begin) {
-				outstring+=QString().sprintf(op->fontsize8_begin);
+				outstring+=QString().sprintf("%s", op->fontsize8_begin);
 			} else 
 			if (size<11 && op->fontsize10_begin) {
-				outstring+=QString().sprintf(op->fontsize10_begin);
+				outstring+=QString().sprintf("%s", op->fontsize10_begin);
 			} else 
 			if (size<13 && op->fontsize12_begin) {
-				outstring+=QString().sprintf(op->fontsize12_begin);
+				outstring+=QString().sprintf("%s", op->fontsize12_begin);
 			} else 
 			if (size<16 && op->fontsize14_begin) {
-				outstring+=QString().sprintf(op->fontsize14_begin);
+				outstring+=QString().sprintf("%s", op->fontsize14_begin);
 			} else 
 			if (size<21 && op->fontsize18_begin) {
-				outstring+=QString().sprintf(op->fontsize18_begin);
+				outstring+=QString().sprintf("%s", op->fontsize18_begin);
 			} else 
 			if (size<30 && op->fontsize24_begin) {
-				outstring+=QString().sprintf(op->fontsize24_begin);
+				outstring+=QString().sprintf("%s", op->fontsize24_begin);
 			} else 
 			if (size<42 && op->fontsize36_begin) {
-				outstring+=QString().sprintf(op->fontsize36_begin);
+				outstring+=QString().sprintf("%s", op->fontsize36_begin);
 			} else 
 			if (size>40 && op->fontsize48_begin) {
-				outstring+=QString().sprintf(op->fontsize48_begin);
+				outstring+=QString().sprintf("%s", op->fontsize48_begin);
 			} else 
 			/* If we can't even produce a good approximation,
 			 * just try to get a font size near 12 point.
 			 */
 			if (op->fontsize12_begin)
-				outstring+=QString().sprintf(op->fontsize12_begin);
+				outstring+=QString().sprintf("%s", op->fontsize12_begin);
 			else
 			if (op->fontsize14_begin)
-				outstring+=QString().sprintf(op->fontsize14_begin);
+				outstring+=QString().sprintf("%s", op->fontsize14_begin);
 			else
 			if (op->fontsize10_begin)
-				outstring+=QString().sprintf(op->fontsize10_begin);
+				outstring+=QString().sprintf("%s", op->fontsize10_begin);
 			else
 			if (op->fontsize18_begin)
-				outstring+=QString().sprintf(op->fontsize18_begin);
+				outstring+=QString().sprintf("%s", op->fontsize18_begin);
 			else
 			if (op->fontsize8_begin)
-				outstring+=QString().sprintf(op->fontsize8_begin);
+				outstring+=QString().sprintf("%s", op->fontsize8_begin);
 			else
 				error_handler ("output personality lacks sufficient font size change capability");
 		}
@@ -367,49 +367,49 @@ op_end_std_fontsize (OutputPersonality *op, int size)
 	switch (size) {
 	case 8:
 		if (op->fontsize8_end) {
-			outstring+=QString().sprintf(op->fontsize8_end);
+			outstring+=QString().sprintf("%s", op->fontsize8_end);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 10:
 		if (op->fontsize10_end) {
-			outstring+=QString().sprintf(op->fontsize10_end);
+			outstring+=QString().sprintf("%s", op->fontsize10_end);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 12:
 		if (op->fontsize12_end) {
-			outstring+=QString().sprintf(op->fontsize12_end);
+			outstring+=QString().sprintf("%s", op->fontsize12_end);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 14:
 		if (op->fontsize14_end) {
-			outstring+=QString().sprintf(op->fontsize14_end);
+			outstring+=QString().sprintf("%s", op->fontsize14_end);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 18:
 		if (op->fontsize18_end) {
-			outstring+=QString().sprintf(op->fontsize18_end);
+			outstring+=QString().sprintf("%s", op->fontsize18_end);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 24:
 		if (op->fontsize24_end) {
-			outstring+=QString().sprintf(op->fontsize24_end);
+			outstring+=QString().sprintf("%s", op->fontsize24_end);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 36:
 		if (op->fontsize36_end) {
-			outstring+=QString().sprintf(op->fontsize36_end);
+			outstring+=QString().sprintf("%s", op->fontsize36_end);
 			found_std_expr = TRUE;
 		}
 		break;
 	case 48:
 		if (op->fontsize48_end) {
-			outstring+=QString().sprintf(op->fontsize48_end);
+			outstring+=QString().sprintf("%s", op->fontsize48_end);
 			found_std_expr = TRUE;
 		}
 		break;
@@ -429,46 +429,46 @@ op_end_std_fontsize (OutputPersonality *op, int size)
 			 * size.
 			 */
 			if (size<9 && op->fontsize8_end) {
-				outstring+=QString().sprintf(op->fontsize8_end);
+				outstring+=QString().sprintf("%s", op->fontsize8_end);
 			} else 
 			if (size<11 && op->fontsize10_end) {
-				outstring+=QString().sprintf(op->fontsize10_end);
+				outstring+=QString().sprintf("%s", op->fontsize10_end);
 			} else 
 			if (size<13 && op->fontsize12_end) {
-				outstring+=QString().sprintf(op->fontsize12_end);
+				outstring+=QString().sprintf("%s", op->fontsize12_end);
 			} else 
 			if (size<16 && op->fontsize14_end) {
-				outstring+=QString().sprintf(op->fontsize14_end);
+				outstring+=QString().sprintf("%s", op->fontsize14_end);
 			} else 
 			if (size<21 && op->fontsize18_end) {
-				outstring+=QString().sprintf(op->fontsize18_end);
+				outstring+=QString().sprintf("%s", op->fontsize18_end);
 			} else 
 			if (size<30 && op->fontsize24_end) {
-				outstring+=QString().sprintf(op->fontsize24_end);
+				outstring+=QString().sprintf("%s", op->fontsize24_end);
 			} else 
 			if (size<42 && op->fontsize36_end) {
-				outstring+=QString().sprintf(op->fontsize36_end);
+				outstring+=QString().sprintf("%s", op->fontsize36_end);
 			} else 
 			if (size>40 && op->fontsize48_end) {
-				outstring+=QString().sprintf(op->fontsize48_end);
+				outstring+=QString().sprintf("%s", op->fontsize48_end);
 			} else 
 			/* If we can't even produce a good approximation,
 			 * just try to get a font size near 12 point.
 			 */
 			if (op->fontsize12_end)
-				outstring+=QString().sprintf(op->fontsize12_end);
+				outstring+=QString().sprintf("%s", op->fontsize12_end);
 			else
 			if (op->fontsize14_end)
-				outstring+=QString().sprintf(op->fontsize14_end);
+				outstring+=QString().sprintf("%s", op->fontsize14_end);
 			else
 			if (op->fontsize10_end)
-				outstring+=QString().sprintf(op->fontsize10_end);
+				outstring+=QString().sprintf("%s", op->fontsize10_end);
 			else
 			if (op->fontsize18_end)
-				outstring+=QString().sprintf(op->fontsize18_end);
+				outstring+=QString().sprintf("%s", op->fontsize18_end);
 			else
 			if (op->fontsize8_end)
-				outstring+=QString().sprintf(op->fontsize8_end);
+				outstring+=QString().sprintf("%s", op->fontsize8_end);
 			else
 				error_handler ("output personality lacks sufficient font size change capability");
 		}
