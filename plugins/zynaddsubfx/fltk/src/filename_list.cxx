dc539d88de79211a32376ba4dfa1e266a88173d7
Tobias Doerffel <tobias.doerffel@gmail.com>
2009-06-01 00:53:48 +0200

    ZynAddSubFX: fixed compilation failure of FLTK if glibc >= 2.10
    Fixed compilation failure of FLTK on systems with glibc newer than
    version 2.9 where the signature of scandir(...) has changed.
    
    Signed-off-by: Tobias Doerffel <tobias.doerffel@gmail.com>

--- plugins/zynaddsubfx/fltk/src/filename_list.cxx
+++ plugins/zynaddsubfx/fltk/src/filename_list.cxx
@@ -77,7 +77,7 @@ int fl_filename_list(const char *d, dirent ***list,
                      Fl_File_Sort_F *sort) {
 #ifndef HAVE_SCANDIR
   int n = scandir(d, list, 0, sort);
-#elif defined(__hpux) || defined(__CYGWIN__)
+#elif defined(__hpux) || defined(__CYGWIN__) || (__GLIBC__ >= 2 && __GLIBC_MINOR__ >= 10 )
   // HP-UX, Cygwin define the comparison function like this:
   int n = scandir(d, list, 0, (int(*)(const dirent **, const dirent **))sort);
 #elif defined(__osf__)
