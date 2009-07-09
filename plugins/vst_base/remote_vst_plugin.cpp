LMMS/lmms

commit 4ad5add74582c587ae97f1e8529e5e1046ff8f70

RemoteVstPlugin: typedef VstMidiEventList
Added a typedef for VstMidiEventList so actual type does not have
to be hard-coded each time in the code.

Signed-off-by: Tobias Doerffel <tobias.doerffel@gmail.com>

plugins/vst_base/remote_vst_plugin.cpp
@@ -268,7 +268,8 @@ private:
 	float * * m_inputs;
 	float * * m_outputs;
 
-	std::vector<VstMidiEvent> m_midiEvents;
+	typedef std::vector<VstMidiEventList> VstMidiEventList;
+	VstMidiEventList m_midiEvents;
 
 	bpm_t m_bpm;
 	double m_currentSamplePos;
@@ -607,8 +608,7 @@ void RemoteVstPlugin::process( const sampleFrame * _in, sampleFrame * _out )
 		events->reserved = 0;
 		events->numEvents = m_midiEvents.size();
 		int idx = 0;
-		for( std::vector<VstMidiEvent>::iterator it =
-							m_midiEvents.begin();
+		for( VstMidiEventList::iterator it = m_midiEvents.begin();
 					it != m_midiEvents.end(); ++it, ++idx )
 		{
 			memcpy( &vme[idx], &*it, sizeof( VstMidiEvent ) );

commit c4c94985a621a2ceb485762f2670463381183b0c

RemoteVstPlugin: fixed typo in 4ad5add74582c587ae97f1e8529e5e1046ff8f70
Commit 4ad5add74582c587ae97f1e8529e5e1046ff8f70 introduced a typo which
lead to compilation failure. This commit fixes it.

Signed-off-by: Tobias Doerffel <tobias.doerffel@gmail.com>

plugins/vst_base/remote_vst_plugin.cpp
@@ -268,7 +268,7 @@ private:
 	float * * m_inputs;
 	float * * m_outputs;
 
-	typedef std::vector<VstMidiEventList> VstMidiEventList;
+	typedef std::vector<VstMidiEvent> VstMidiEventList;
 	VstMidiEventList m_midiEvents;
 
 	bpm_t m_bpm;