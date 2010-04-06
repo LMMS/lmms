c59fe081ee9c21944254f1921d2e8f8641345358
Tobias Doerffel <tobias.doerffel@gmail.com>
2010-04-06 11:47:50 +0200

    Improved support for MinGW cross compile builds
    Cross compile support for Win32 and Win64 has been unified,
    simplified and adapted to the new MinGW64-based build environment.
    This finally allows to produce working Win64 binaries.
    (cherry picked from commit 51103bb6744a8e1bb3f3a75250dd606af5164d59)

--- cmake/modules/Win64Toolchain.cmake
+++ cmake/modules/Win64Toolchain.cmake
@@ -1,39 +1,9 @@
-# this one is important
-SET(CMAKE_SYSTEM_NAME Windows)
-#this one not so much
-SET(CMAKE_SYSTEM_VERSION 1)
+SET(MINGW_PREFIX /opt/mingw64)
 
 SET(CMAKE_SYSTEM_PROCESSOR x86_64)
-SET(CMAKE_INSTALL_PREFIX /opt/mingw64)
 
-SET(CC_PREFIX /opt/mingw64)
+SET(WIN64 TRUE)
 
-# specify the cross compiler
-SET(CMAKE_C_COMPILER   ${CC_PREFIX}/bin/x86_64-pc-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER ${CC_PREFIX}/bin/x86_64-pc-mingw32-g++)
 
-# where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH  /opt/mingw64)
-
-SET(QT_BINARY_DIR   ${CC_PREFIX}/bin)
-SET(QT_LIBRARY_DIR  ${CC_PREFIX}/lib)
-SET(QT_QTCORE_LIBRARY   ${CC_PREFIX}/lib/libQtCore4.a)
-SET(QT_INCLUDE_DIR ${CC_PREFIX}/include/qt4)
-SET(QT_QTCORE_INCLUDE_DIR ${CC_PREFIX}/include/qt4/QtCore)
-SET(QT_MKSPECS_DIR  ${CC_PREFIX}/share/qt4/mkspecs)
-SET(QT_MOC_EXECUTABLE  ${QT_BINARY_DIR}/moc.exe)
-SET(QT_RCC_EXECUTABLE  ${QT_BINARY_DIR}/rcc.exe)
-SET(QT_QMAKE_EXECUTABLE  /usr/bin/qmake)
-SET(QT_UIC_EXECUTABLE  ${QT_BINARY_DIR}/uic.exe)
-
-# search for programs in the build host directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# for libraries and headers in the target directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-SET(PKG_CONFIG_EXECUTABLE ${CC_PREFIX}/bin/pkg-config)
-
-INCLUDE_DIRECTORIES(${CC_PREFIX}/include)
-LINK_DIRECTORIES(${CC_PREFIX}/lib ${CC_PREFIX}/bin)
+INCLUDE(MinGWCrossCompile)
 
