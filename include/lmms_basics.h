LMMS/lmms

commit 4e9ebd09c69deb22d4c76179259bb74de799fae2

Use proper path seperator for LADSPA_PATH
According to http://www.ladspa.org/ladspa_sdk/shared_plugins.html,
LADSPA_PATH is supposed to be delimited by colons.  This patch does so,
except on Windows.  On Windows we use ';' instead of ':' since colon is
a legal character for a path.

include/lmms_basics.h
@@ -119,6 +119,13 @@ inline bool typeInfo<float>::isEqual( float _x, float _y )
 #endif
 
 
+#ifdef LMMS_BUILD_WIN32
+#define PATH_SEPERATOR = ';';
+#else
+#define PATH_SEPERATOR = ':';
+#endif
+
+
 #define DEFAULT_CHANNELS 2
 #define LMMS_DISABLE_SURROUND
 #ifdef LMMS_DISABLE_SURROUND

src/core/ladspa_manager.cpp
@@ -38,7 +38,7 @@
 ladspaManager::ladspaManager()
 {
 	QStringList ladspaDirectories = QString( getenv( "LADSPA_PATH" ) ).
-								split( ',' );
+								split( PATH_SEPERATOR );
 	ladspaDirectories += configManager::inst()->ladspaDir().split( ',' );
 
 	ladspaDirectories.push_back( configManager::inst()->pluginDir() + "ladspa" );

commit 114c1973e2939b9506b48de14b73cf9df010ecbd

Renamed PATH_SEPARATOR to LADSPA_PATH_SEPARATOR and fixed it
Commit 4e9ebd09c69deb22d4c76179259bb74de799fae2 introduced PATH_SEPARATOR
but with wrong syntax. Fixed this and renamed it to LADSPA_PATH_SEPARATOR.

include/lmms_basics.h
@@ -120,9 +120,9 @@ inline bool typeInfo<float>::isEqual( float _x, float _y )
 
 
 #ifdef LMMS_BUILD_WIN32
-#define PATH_SEPERATOR = ';';
+#define LADSPA_PATH_SEPERATOR ';'
 #else
-#define PATH_SEPERATOR = ':';
+#define LADSPA_PATH_SEPERATOR ':'
 #endif
 
 

src/core/ladspa_manager.cpp
@@ -38,7 +38,7 @@
 ladspaManager::ladspaManager()
 {
 	QStringList ladspaDirectories = QString( getenv( "LADSPA_PATH" ) ).
-								split( PATH_SEPERATOR );
+								split( LADSPA_PATH_SEPERATOR );
 	ladspaDirectories += configManager::inst()->ladspaDir().split( ',' );
 
 	ladspaDirectories.push_back( configManager::inst()->pluginDir() + "ladspa" );