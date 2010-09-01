LMMS/lmms

commit 534c4debb519dcc6c33b93212db8648b2c691237

ZynAddSubFX: save and restore settings of new knobs
When adding the new knobs for automatable parameters of ZynAddSubFX,
I forgot to include them in the save/load routines.

plugins/zynaddsubfx/ZynAddSubFx.cpp
@@ -154,6 +154,14 @@ ZynAddSubFxInstrument::~ZynAddSubFxInstrument()
 void ZynAddSubFxInstrument::saveSettings( QDomDocument & _doc,
 	                             QDomElement & _this )
 {
+	m_portamentoModel.saveSettings( _doc, _this, "portamento" );
+	m_filterFreqModel.saveSettings( _doc, _this, "filterfreq" );
+	m_filterQModel.saveSettings( _doc, _this, "filtercut" );
+	m_bandwidthModel.saveSettings( _doc, _this, "bandwidth" );
+	m_fmGainModel.saveSettings( _doc, _this, "fmgain" );
+	m_resCenterFreqModel.saveSettings( _doc, _this, "rescenterfreq" );
+	m_resBandwidthModel.saveSettings( _doc, _this, "resbandwidth" );
+
 	QTemporaryFile tf;
 	if( tf.open() )
 	{
@@ -192,6 +200,14 @@ void ZynAddSubFxInstrument::loadSettings( const QDomElement & _this )
 	{
 		return;
 	}
+
+	m_portamentoModel.loadSettings( _this, "portamento" );
+	m_filterFreqModel.loadSettings( _this, "filterfreq" );
+	m_filterQModel.loadSettings( _this, "filtercut" );
+	m_bandwidthModel.loadSettings( _this, "bandwidth" );
+	m_fmGainModel.loadSettings( _this, "fmgain" );
+	m_resCenterFreqModel.loadSettings( _this, "rescenterfreq" );
+	m_resBandwidthModel.loadSettings( _this, "resbandwidth" );
 
 	QDomDocument doc;
 	doc.appendChild( doc.importNode( _this.firstChild(), true ) );

commit 4e695a4ea81892550c0cd927022109a7150553a6

ZynAddSubFX: properly load preset data if knobs are automated/controlled
Commit 534c4debb519dcc6c33b93212db8648b2c691237 introduced a regression.
If knobs have a song-global automation or controllers attached, they
save these settings in a sub XML node which confused the ZASF preset
loader which assumed the first child node would always be the ZASF
preset data. Fix this by explicitely searching for an XML element named
"ZynAddSubFX-data".

Closes #3057275.

plugins/zynaddsubfx/ZynAddSubFx.cpp
@@ -214,7 +214,13 @@ void ZynAddSubFxInstrument::loadSettings( const QDomElement & _this )
 	m_forwardMidiCcModel.loadSettings( _this, "forwardmidicc" );
 
 	QDomDocument doc;
-	doc.appendChild( doc.importNode( _this.firstChild(), true ) );
+	QDomElement data = _this.firstChildElement( "ZynAddSubFX-data" );
+	if( data.isNull() )
+	{
+		data = _this.firstChildElement();
+	}
+	doc.appendChild( doc.importNode( data, true ) );
+
 	QTemporaryFile tf;
 	tf.setAutoRemove( false );
 	if( tf.open() )