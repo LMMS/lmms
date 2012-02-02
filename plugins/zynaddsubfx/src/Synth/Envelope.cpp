LMMS/lmms

commit 56d7e2e6ecb9eefeef34c29679d53a2620e6286c

ZynAddSubFX: minor fixes from upstream
  * Fixing Envelopes not going to zero
  * FLTK1.3: Applying patch to force code generation
(cherry picked from commit a1006f7a3297f19d7e5052ce90f181cfd130f325)

plugins/zynaddsubfx/src/Synth/Envelope.cpp
@@ -183,7 +183,7 @@ REALTYPE Envelope::envout_dB()
         if(out > 0.001)
             envoutval = rap2dB(out);
         else
-            envoutval = -40.0;
+            envoutval = MIN_ENVELOPE_DB;
     }
     else
         out = dB2rap(envout());

plugins/zynaddsubfx/src/Params/EnvelopeParams.h
@@ -28,7 +28,7 @@
 #include "Presets.h"
 
 #define MAX_ENVELOPE_POINTS 40
-#define MIN_ENVELOPE_DB -40
+#define MIN_ENVELOPE_DB -400
 
 class EnvelopeParams:public Presets
 {

plugins/zynaddsubfx/src/UI/BankUI.fl
@@ -32,7 +32,7 @@ decl {\#include "../Misc/Config.h"} {public
 
 class BankProcess_ {} {
   Function {process()} {open return_type {virtual void}
-  } {}
+  } {code{;}}
   decl {Bank *bank;} {public
   }
 } 

plugins/zynaddsubfx/src/UI/OscilGenUI.fl
@@ -129,7 +129,7 @@ delete [] spc;} {selected
 
 class PSlider {: {public Fl_Slider}
 } {
-  Function {PSlider(int x,int y, int w, int h, const char *label=0):Fl_Slider(x,y,w,h,label)} {} {}
+  Function {PSlider(int x,int y, int w, int h, const char *label=0):Fl_Slider(x,y,w,h,label)} {} {code{;}}
   Function {handle(int event)} {return_type int
   } {
     code {int X=x(),Y=y(),W=w(),H=h();

plugins/zynaddsubfx/src/UI/PresetsUI.fl
@@ -18,9 +18,9 @@ decl {\#include "../Params/Presets.h"} {public
 
 class PresetsUI_ {} {
   Function {refresh()} {open return_type {virtual void}
-  } {}
+  } {code{;}}
   Function {~PresetsUI_()} {open return_type virtual
-  } {}
+  } {code{;}}
 } 
 
 class PresetsUI {} {

commit 376f11a6c09379ee5fbe3a26aa72e778461c8d28

ZynAddSubFX: Env: fixing envelope scaling
Fixing bug introduced in 56d7e2e6ecb9eefeef34c29679d53a2620e6286c

plugins/zynaddsubfx/src/Synth/Envelope.cpp
@@ -56,7 +56,7 @@ Envelope::Envelope(EnvelopeParams *envpars, REALTYPE basefreq)
 
         switch(mode) {
         case 2:
-            envval[i] = (1.0 - envpars->Penvval[i] / 127.0) * MIN_ENVELOPE_DB;
+            envval[i] = (1.0 - envpars->Penvval[i] / 127.0) * -40;
             break;
         case 3:
             envval[i] =