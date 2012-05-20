2e5105be44a8e12a133a54ce29183ae335a8ad80
Raine M. Ekman <raine@iki.fi>
2012-05-20 19:00:29 +0200

    AutomationEditor: show tooltip with exact value
    Added tooltip telling the exact value under the cursor when editing
    automation.
    
    Signed-off-by: Tobias Doerffel <tobias.doerffel@gmail.com>

--- src/gui/AutomationEditor.cpp
+++ src/gui/AutomationEditor.cpp
@@ -37,6 +37,8 @@
 #include <QtGui/QScrollBar>
 #include <QtGui/QStyleOption>
 #include <QtGui/QWheelEvent>
+#include <QToolTip>
+
 
 
 #ifndef __USE_XOPEN
@@ -1227,6 +1229,10 @@ inline void AutomationEditor::drawCross( QPainter & _p )
 	_p.drawLine( ValuesWidth, (int) cross_y, width(), (int) cross_y );
 	_p.drawLine( mouse_pos.x(), TopMargin, mouse_pos.x(),
 						height() - ScrollBarSize );
+	QPoint tt_pos =  QCursor::pos();
+	tt_pos.ry() -= 64;
+	tt_pos.rx() += 32;
+	QToolTip::showText( tt_pos,QString::number( level ),this);
 }
 
 
