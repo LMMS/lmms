b6c1bb00a2fc52bf0907d97f7f1d7572d22836cd
Paul Giblock <pgib@users.sf.net>
2012-05-28 02:37:29 -0400

    Add support for upgrading to standard output
    Useful for me to initially import LMMS presets for use in lmms-lv2.

--- src/core/mmp.cpp
+++ src/core/mmp.cpp
@@ -166,7 +166,7 @@ QString multimediaProject::nameWithExtension( const QString & _fn ) const
 
 
 
-bool multimediaProject::writeFile( const QString & _fn )
+void multimediaProject::write( QTextStream & _strm )
 {
 	if( type() == SongProject || type() == SongProjectTemplate
 					|| type() == InstrumentTrackSettings )
@@ -174,7 +174,14 @@ bool multimediaProject::writeFile( const QString & _fn )
 		cleanMetaNodes( documentElement() );
 	}
 
+	save(_strm, 2);
+}
 
+
+
+
+bool multimediaProject::writeFile( const QString & _fn )
+{
 	QString fn = nameWithExtension( _fn );
 	QFile outfile( fn );
 	if( !outfile.open( QIODevice::WriteOnly | QIODevice::Truncate ) )
@@ -192,14 +199,18 @@ bool multimediaProject::writeFile( const QString & _fn )
 						).arg( fn ) );
 		return false;
 	}
-	QString xml = toString( 2 );
+
 	if( fn.section( '.', -1 ) == "mmpz" )
 	{
+		QString xml;
+		QTextStream ts( &xml );
+		write( ts );
 		outfile.write( qCompress( xml.toUtf8() ) );
 	}
 	else
 	{
-		QTextStream( &outfile ) << xml;
+		QTextStream ts( &outfile );
+		write( ts );
 	}
 	outfile.close();
 
