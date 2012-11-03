2c3e7483aa0b78229c1dbe1f0f936979b2ade459
Tobias Doerffel <tobias.doerffel@gmail.com>
2012-11-03 19:37:28 +0100

    MainWindow: fixed ambiguous hotkey for export operations
    The new export operation had the same hotkey as the classical one. This
    is not a good idea and is now fixed.

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -262,7 +262,7 @@ void MainWindow::finalize( void )
 					tr( "E&xport tracks..." ),
 					engine::getSong(),
 					SLOT( exportProjectTracks() ),
-					Qt::CTRL + Qt::Key_E );
+					Qt::CTRL + Qt::SHIFT + Qt::Key_E );
 
 	project_menu->addSeparator();
 	project_menu->addAction( embed::getIconPixmap( "exit" ), tr( "&Quit" ),
