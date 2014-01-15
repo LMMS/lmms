22ca7acb81ef495c5b4fefb4536aa147544fff38
Lukas W <lukaswhl@googlemail.com>
2014-01-15 17:17:29 +0100

    Add FindWine module and use it in main CMakeLists.txt
    Old CheckLibraryExists implementation didn't work on my system.
    A find module should be better and also easier to maintain.

---
+++ cmake/modules/FindWine.cmake
@@ -0,0 +1,19 @@
+# - Try to find the wine libraries
+# Once done this will define
+#
+#  WINE_FOUND - System has wine
+#  WINE_INCLUDE_DIRS - The wine include directories
+#  WINE_LIBRARIES - The libraries needed to use wine
+#  WINE_DEFINITIONS - Compiler switches required for using wine
+#
+
+FIND_PATH(WINE_INCLUDE_DIR windows/windows.h PATH_SUFFIXES wine)
+FIND_LIBRARY(WINE_LIBRARY NAMES wine)
+
+set(WINE_INCLUDE_DIRS ${WINE_INCLUDE_DIR} )
+set(WINE_LIBRARIES ${WINE_LIBRARY} )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Wine DEFAULT_MSG WINE_LIBRARIES WINE_INCLUDE_DIRS)
+
+mark_as_advanced(WINE_INCLUDE_DIR WINE_LIBRARY)
