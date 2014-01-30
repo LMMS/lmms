f707f3f665fd71be394c84951d6f69f8eb9c3025
wongcc966422 <wongcc966422@users.noreply.github.com>
2014-01-31 03:13:44 +0800

    Update song.cpp

--- src/core/song.cpp
+++ src/core/song.cpp
@@ -648,7 +648,7 @@ void song::playSong()
 	m_playMode = Mode_PlaySong;
 	m_playing = true;
 	m_paused = false;
-	m_actions.push_front( ActionPlaySong );
+	Controller::resetFrameCounter();
 
 	savePos();
 	if(QApplication::type() != QApplication::Tty) {
