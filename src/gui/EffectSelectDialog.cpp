LMMS/lmms

commit 360bfe5089c9fd13847b9b982011305fd63daf3f

EffectSelectDialog: sort plugin names
Sort plugin names and make sure that effects without sub plugins (i.e.
native LMMS effects) are listed first in order to better advertise our
cool plugins.

Closes #331.

src/gui/EffectSelectDialog.cpp
@@ -71,15 +71,23 @@ EffectSelectDialog::EffectSelectDialog( QWidget * _parent ) :
 
 	// and fill our source model
 	QStringList pluginNames;
-	for( EffectKeyList::ConstIterator it = m_effectKeys.begin();
-										it != m_effectKeys.end(); ++it )
+	QStringList subPluginNames;
+	for( EffectKeyList::ConstIterator it = m_effectKeys.begin(); it != m_effectKeys.end(); ++it )
 	{
-		pluginNames += QString( ( *it ).desc->displayName ) +
-			( ( ( *it ).desc->subPluginFeatures != NULL ) ?
-							": " + ( *it ).name
-						:
-							"" );
+		if( ( *it ).desc->subPluginFeatures )
+		{
+			subPluginNames += QString( "%1: %2" ).arg(  ( *it ).desc->displayName, ( *it ).name );
+		}
+		else
+		{
+			pluginNames += ( *it ).desc->displayName;
+		}
 	}
+
+	qSort( pluginNames );
+	qSort( subPluginNames );
+
+	pluginNames += subPluginNames;
 
 	int row = 0;
 	for( QStringList::ConstIterator it = pluginNames.begin();

commit dd1a227963654f4e3af97c5949e0e03e9e24ef90

EffectSelectDialog: sort effect keys instead of plugin names
Commit 360bfe5089c9fd13847b9b982011305fd63daf3f broke mapping between
displayed and internal effect list. Fix this by sorting the effect key
list itself.

src/gui/EffectSelectDialog.cpp
@@ -44,6 +44,8 @@ EffectSelectDialog::EffectSelectDialog( QWidget * _parent ) :
 	// query effects
 	Plugin::getDescriptorsOfAvailPlugins( m_pluginDescriptors );
 
+	EffectKeyList subPluginEffectKeys;
+
 	for( Plugin::DescriptorList::ConstIterator it = m_pluginDescriptors.begin();
 										it != m_pluginDescriptors.end(); ++it )
 	{
@@ -60,7 +62,7 @@ EffectSelectDialog::EffectSelectDialog( QWidget * _parent ) :
 				// so we're on the safe side and the compiler
 				// likely will reduce that to just "it"
 							&( *it ),
-							m_effectKeys );
+							subPluginEffectKeys );
 		}
 		else
 		{
@@ -69,25 +71,21 @@ EffectSelectDialog::EffectSelectDialog( QWidget * _parent ) :
 		}
 	}
 
+	m_effectKeys += subPluginEffectKeys;
+
 	// and fill our source model
 	QStringList pluginNames;
-	QStringList subPluginNames;
 	for( EffectKeyList::ConstIterator it = m_effectKeys.begin(); it != m_effectKeys.end(); ++it )
 	{
 		if( ( *it ).desc->subPluginFeatures )
 		{
-			subPluginNames += QString( "%1: %2" ).arg(  ( *it ).desc->displayName, ( *it ).name );
+			pluginNames += QString( "%1: %2" ).arg(  ( *it ).desc->displayName, ( *it ).name );
 		}
 		else
 		{
 			pluginNames += ( *it ).desc->displayName;
 		}
 	}
-
-	qSort( pluginNames );
-	qSort( subPluginNames );
-
-	pluginNames += subPluginNames;
 
 	int row = 0;
 	for( QStringList::ConstIterator it = pluginNames.begin();