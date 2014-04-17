bb7209b8940c816a8fff8101f532dd27a31f5b70
psyomn <lethaljellybean@gmail.com>
2014-04-17 02:25:07 -0400

    mem-helper.cpp: add license information

--- src/core/MemoryHelper.cpp
+++ src/core/MemoryHelper.cpp
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2014 Simon Symeonidis <lethaljellybean/at/gmail/com>
+ * Copyright (c) 2008-2014 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ *
+ * This file is part of Linux MultiMedia Studio - http://lmms.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
 #include <stdlib.h>
 
 #include "lmms_basics.h"
