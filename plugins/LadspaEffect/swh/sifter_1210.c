3d95f82f30f0a2315b80d6615abcaf29d699181c
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-05-02 14:01:59 -0700

    Remove inline function recursion for Apple
    q_sort is an inline function which recurses upon itself.  Some compilers such as MSVC limit this which can be overridden with a parameter.  Since I could find no such parameter for clang, this #IFDEF allows the sifter plugin to compile successfully.
    
    If this approach has negative side effects, it should only affect apple.  If there's a better approach to this, please reject the pull request and submit your own.

--- plugins/LadspaEffect/swh/sifter_1210.c
+++ plugins/LadspaEffect/swh/sifter_1210.c
@@ -33,7 +33,11 @@ void _init(); // forward declaration
 
 inline int partition(LADSPA_Data array[], int left, int right);
 
+#ifdef LMMS_BUILD_APPLE
+void q_sort(LADSPA_Data array[], int left, int right) {
+#else
 inline void q_sort(LADSPA_Data array[], int left, int right) {
+#endif
         float pivot = partition(array, left, right);
 
         if (left < pivot) {
