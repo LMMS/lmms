d28195f9bc6dca634332013b5d4ca89a4cb9abb0
Stian Jørgensrud <stianahj@gmail.com>
2014-08-30 01:11:15 +0200

    Just updated based on mrBears wonderful work

--- src/gui/PianoRoll.cpp
+++ src/gui/PianoRoll.cpp
@@ -620,7 +620,57 @@ PianoRoll::PianoRoll() :
 	tb_layout->addSpacing( 4 );
 	tb_layout->addWidget( m_chordComboBox );
 	tb_layout->addStretch();
+	
+	m_zoomingComboBox->setWhatsThis(
+		tr( 
+			"This controls the magnification of an axis." 
+			" It can be helpful to choose magnification for a specific"
+			" task. For ordinary editing, the magnification should be" 
+			" fitted to your smallest notes.”
+		) );
+		
+		m_quantizeComboBox->setWhatsThis(
+		tr(
+			"The 'Q' stands for quantization, and controls the grid size” 
+			" notes and control points snap to." 
+“ With smaller quantization values, you can draw shorter notes” 
+“ in Piano Roll, and more exact control points in the” 
+“ Automation Editor.”
 
+		) );	
+		
+		m_noteLenComboBox->setWhatsThis(
+		tr(
+			"This lets you select the length of new notes." 
+			" 'Last Note' means that LMMS will use the note length of" 
+                        " the note you last edited" 
+		) );			
+		
+		m_scaleComboBox->setWhatsThis(
+		tr(			
+			"Click this button and you will become a DJ" 
+			"The feature is directly connected to the context-menu" 
+			" on the virtual keyboard, to the left in Piano Roll." 
+			"After you have chosen the scale you want" 
+			“ in this very drop-down menu," 
+			" you can right click on a desired key in the virtual keyboard," 
+			" and then choose 'Mark current Scale'." 
+			" LMMS will highlight all notes that belongs to the chosen scale,"
+			" and in the key you have selected!"
+					) );
+		
+
+		m_chordComboBox->setWhatsThis(
+		tr(
+			"Let you select a chord which LMMS then can draw or highlight." 
+			" You can find the most common chords in this drop-down menu." 
+			" After you have selected a chord, click anywhere to place the chord, and right" 
+“ click on the virtual keyboard to open context menu and highlight the chord.” 
+			" To return to single note placement, you need to choose 'No chord'" 
+			" in this drop-down menu."
+		) );	  
+
+	
 	// setup our actual window
 	setFocusPolicy( Qt::StrongFocus );
 	setFocus();
