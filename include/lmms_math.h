0916bc073e42bbf490c0b7686505dd893ef4382f
Lukas W <lukaswhl@gmail.com>
2014-11-16 01:51:29 +0100

    Fix compile errors

--- include/lmms_math.h
+++ include/lmms_math.h
@@ -31,7 +31,12 @@
 #include "lmmsconfig.h"
 #include <QtCore/QtGlobal>
 
-#include <math.h>
+#include <cmath>
+using namespace std;
+
+#ifndef M_PI
+#	define M_PI 3.14159265358979323846
+#endif
 
 #if defined (LMMS_BUILD_WIN32) || defined (LMMS_BUILD_APPLE) 
 #ifndef isnanf
