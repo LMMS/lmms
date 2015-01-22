LMMS/lmms

commit aaeb5216ad3f951be89aa2dcad437f0320a4597d

Rename note to Note

src/core/Song.cpp
@@ -112,7 +112,7 @@ Song::Song() :
 /*	connect( &m_masterPitchModel, SIGNAL( dataChanged() ),
 			this, SLOT( masterPitchChanged() ) );*/
 
-	qRegisterMetaType<note>( "note" );
+	qRegisterMetaType<Note>( "note" );
 }
 
 

include/InstrumentTrack.h
@@ -206,8 +206,8 @@ public:
 signals:
 	void instrumentChanged();
 	void newNote();
-	void midiNoteOn( const note& );
-	void midiNoteOff( const note& );
+	void midiNoteOn( const Note& );
+	void midiNoteOff( const Note& );
 	void nameChanged();
 
 

include/Mixer.h
@@ -53,7 +53,7 @@
 
 
 #include "lmms_basics.h"
-#include "note.h"
+#include "Note.h"
 #include "fifo_buffer.h"
 #include "MixerProfiler.h"
 

include/Note.h
@@ -1,5 +1,5 @@
 /*
- * note.h - declaration of class note which contains all informations about a
+ * Note.h - declaration of class note which contains all informations about a
  *          note + definitions of several constants and enums
  *
  * Copyright (c) 2004-2014 Tobias Doerffel <tobydox/at/users.sourceforge.net>
@@ -78,17 +78,17 @@ const float MaxDetuning = 4 * 12.0f;
 
 
 
-class EXPORT note : public SerializingObject
+class EXPORT Note : public SerializingObject
 {
 public:
-	note( const MidiTime & _length = MidiTime( 0 ),
+	Note( const MidiTime & _length = MidiTime( 0 ),
 		const MidiTime & _pos = MidiTime( 0 ),
 		int key = DefaultKey,
 		volume_t _volume = DefaultVolume,
 		panning_t _panning = DefaultPanning,
 		DetuningHelper * _detuning = NULL );
-	note( const note & _note );
-	virtual ~note();
+	Note( const Note & _note );
+	virtual ~Note();
 
 	// used by GUI
 	inline void setSelected( const bool _selected ){ m_selected = _selected; }
@@ -112,7 +112,7 @@ public:
 	void quantizeLength( const int _q_grid );
 	void quantizePos( const int _q_grid );
 
-	static inline bool lessThan( note * &lhs, note * &rhs )
+	static inline bool lessThan( Note * &lhs, Note * &rhs )
 	{
 		// function to compare two notes - must be called explictly when
 		// using qSort
@@ -234,7 +234,7 @@ private:
 } ;
 
 
-typedef QVector<note *> NoteVector;
+typedef QVector<Note *> NoteVector;
 
 
 #endif

include/NotePlayHandle.h
@@ -27,7 +27,7 @@
 #define NOTE_PLAY_HANDLE_H
 
 #include "lmmsconfig.h"
-#include "note.h"
+#include "Note.h"
 #include "PlayHandle.h"
 #include "Track.h"
 #include "MemoryManager.h"
@@ -42,7 +42,7 @@ typedef QList<NotePlayHandle *> NotePlayHandleList;
 typedef QList<const NotePlayHandle *> ConstNotePlayHandleList;
 
 
-class EXPORT NotePlayHandle : public PlayHandle, public note
+class EXPORT NotePlayHandle : public PlayHandle, public Note
 {
 	MM_OPERATORS
 public:
@@ -63,7 +63,7 @@ public:
 	NotePlayHandle( InstrumentTrack* instrumentTrack,
 					const f_cnt_t offset,
 					const f_cnt_t frames,
-					const note& noteToPlay,
+					const Note& noteToPlay,
 					NotePlayHandle* parent = NULL,
 					int midiEventChannel = -1,
 					Origin origin = OriginPattern );
@@ -326,7 +326,7 @@ public:
 	static NotePlayHandle * acquire( InstrumentTrack* instrumentTrack,
 					const f_cnt_t offset,
 					const f_cnt_t frames,
-					const note& noteToPlay,
+					const Note& noteToPlay,
 					NotePlayHandle* parent = NULL,
 					int midiEventChannel = -1,
 					NotePlayHandle::Origin origin = NotePlayHandle::OriginPattern );

include/Pattern.h
@@ -33,7 +33,7 @@
 #include <QPixmap>
 
 
-#include "note.h"
+#include "Note.h"
 #include "Track.h"
 
 
@@ -67,13 +67,13 @@ public:
 	MidiTime beatPatternLength() const;
 
 	// note management
-	note * addNote( const note & _new_note, const bool _quant_pos = true );
+	Note * addNote( const Note & _new_note, const bool _quant_pos = true );
 
-	void removeNote( const note * _note_to_del );
+	void removeNote( const Note * _note_to_del );
 
-	note * noteAtStep( int _step );
+	Note * noteAtStep( int _step );
 
-	note * rearrangeNote( const note * _note_to_proc,
+	Note * rearrangeNote( const Note * _note_to_proc,
 						const bool _quant_pos = true );
 	void rearrangeAllNotes();
 	void clearNotes();

include/Piano.h
@@ -25,7 +25,7 @@
 #ifndef PIANO_H
 #define PIANO_H
 
-#include "note.h"
+#include "Note.h"
 #include "Model.h"
 
 class InstrumentTrack;

include/PianoRoll.h
@@ -32,7 +32,7 @@
 
 #include "ComboBoxModel.h"
 #include "SerializingObject.h"
-#include "note.h"
+#include "Note.h"
 #include "lmms_basics.h"
 #include "Song.h"
 #include "tooltip.h"
@@ -123,7 +123,7 @@ protected:
 
 	int getKey( int _y ) const;
 	static inline void drawNoteRect( QPainter & _p, int _x, int _y,
-					int  _width, note * _n, const QColor & noteCol );
+					int  _width, Note * _n, const QColor & noteCol );
 	void removeSelection();
 	void selectAll();
 	void getSelectedNotes( NoteVector & _selected_notes );
@@ -137,8 +137,8 @@ protected slots:
 	void recordAccompany();
 	void stop();
 
-	void startRecordNote( const note & _n );
-	void finishRecordNote( const note & _n );
+	void startRecordNote( const Note & _n );
+	void finishRecordNote( const Note & _n );
 
 	void horScrolled( int _new_pos );
 	void verScrolled( int _new_pos );
@@ -232,7 +232,7 @@ private:
 	void shiftSemiTone(int amount);
 	bool isSelection() const;
 	int selectionCount() const;
-	void testPlayNote( note * n );
+	void testPlayNote( Note * n );
 	void testPlayKey( int _key, int _vol, int _pan );
 	void pauseTestNotes( bool _pause = true );
 
@@ -300,9 +300,9 @@ private:
 
 	MidiTime m_currentPosition;
 	bool m_recording;
-	QList<note> m_recordingNotes;
+	QList<Note> m_recordingNotes;
 
-	note * m_currentNote;
+	Note * m_currentNote;
 	actions m_action;
 	noteEditMode m_noteEditMode;
 
@@ -355,9 +355,9 @@ private:
 
 	void copy_to_clipboard( const NoteVector & _notes ) const;
 
-	void drawDetuningInfo( QPainter & _p, note * _n, int _x, int _y );
+	void drawDetuningInfo( QPainter & _p, Note * _n, int _x, int _y );
 	bool mouseOverNote();
-	note * noteUnderMouse();
+	Note * noteUnderMouse();
 
 	// turn a selection rectangle into selected notes
 	void computeSelectedNotes( bool shift );

commit 4dd40e17556b09082db14a69bf602fbbd245227a

Fix missed rename note->Note
Fixes #1464
Bug introduced in aaeb5216ad3f951be89aa2dcad437f0320a4597d

src/core/Song.cpp
@@ -116,7 +116,7 @@ Song::Song() :
 /*	connect( &m_masterPitchModel, SIGNAL( dataChanged() ),
 			this, SLOT( masterPitchChanged() ) );*/
 
-	qRegisterMetaType<Note>( "note" );
+	qRegisterMetaType<Note>( "Note" );
 	setType( SongContainer );
 }
 