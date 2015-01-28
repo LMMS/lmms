fa6dd6da8b559b61f57e2f304f37fd3b4a70b905
Dave French <dave.french3@googlemail.com>
2015-01-28 22:12:59 +0000

    Change color of muted activity indicator from red to grey

--- src/tracks/InstrumentTrack.cpp
+++ src/tracks/InstrumentTrack.cpp
@@ -554,7 +554,7 @@ void InstrumentTrack::muteHasChanged()
 {
 	if( m_mutedModel.value() )
 	{
-		m_fb->setActiveColor( QColor( "red" ) );
+		m_fb->setActiveColor( QColor( 0x282828 ) );
 	} else
 	{
 		m_fb->setActiveColor( QApplication::palette().color( QPalette::Active,
