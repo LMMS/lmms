38d825f9fcbbcf152586ab533fb7dd5c0249f85d
Lukas W <lukaswhl@gmail.com>
2015-01-29 17:34:07 +0100

    Fix Qt Resources for CMake < v2.8.9
    Support for generated .rc files was added in CMake v2.8.9 to the
    QT4_ADD_RESOURCES macro. In order to support older versions of CMake, this
    commit includes the source of the new macro from v2.8.9 and uses it if
    necessary.

--- cmake/modules/GenQrc.cmake
+++ cmake/modules/GenQrc.cmake
@@ -2,7 +2,7 @@
 
 # Generates a simple qrc file containing the given resource files ${ARGN}:
 #     GEN_QRC(resources.qrc artwork.png icon.png PREFIX /icons)
-# Files may also be added using a pattern with the GLOB keyword, e.g.: 
+# Files may also be added using a pattern with the GLOB keyword, e.g.:
 #     GEN_QRC(resources.qrc GLOB *.png)
 FUNCTION(GEN_QRC OUT_FILE)
 	CMAKE_PARSE_ARGUMENTS(RC "" "PREFIX;GLOB" "" ${ARGN})
@@ -16,7 +16,7 @@ FUNCTION(GEN_QRC OUT_FILE)
 		SET(RC_PREFIX "/")
 	ENDIF()
 
-	# We need to convert our list to a string in order to pass it to the script 
+	# We need to convert our list to a string in order to pass it to the script
 	# on the command line.
 	STRING(REPLACE ";" "\;" FILES "${RC_UNPARSED_ARGUMENTS};${GLOB_FILES}")
 
@@ -30,10 +30,10 @@ FUNCTION(GEN_QRC OUT_FILE)
 	)
 ENDFUNCTION()
 
-# Generates a qrc file named ${QRC_OUT} from ${ARGN}, rccs it and returns Qt's 
+# Generates a qrc file named ${QRC_OUT} from ${ARGN}, rccs it and returns Qt's
 # output file.
 # Must only be run once per CMakeLists.txt.
-# Usage example: 
+# Usage example:
 #     ADD_GEN_QRC(RCC_OUTPUT resources.qrc icon.png manual.pdf)
 #     ADD_EXECUTABLE(myexe main.cpp ${RCC_OUTPUT})
 MACRO(ADD_GEN_QRC RCCOUT QRC_OUT)
@@ -42,7 +42,61 @@ MACRO(ADD_GEN_QRC RCCOUT QRC_OUT)
 	ELSE()
 		SET(QRC_FILE ${QRC_OUT})
 	ENDIF()
-	
+
 	GEN_QRC(${QRC_FILE} "${ARGN}")
-	QT4_ADD_RESOURCES(${RCCOUT} ${QRC_FILE})
+	IF(${CMAKE_VERSION} VERSION_LESS 2.8.9)
+		QT4_ADD_RESOURCES_2(${RCCOUT} ${QRC_FILE})
+	ELSE()
+		QT4_ADD_RESOURCES(${RCCOUT} ${QRC_FILE})
+	ENDIF()
 ENDMACRO()
+
+
+
+# This macro is taken from CMake v2.8.9 so we can support CMake versions older
+# than that.
+#
+# Copyright 2005-2009 Kitware, Inc.
+# Distributed under the OSI-approved BSD License (the "License");
+MACRO (QT4_ADD_RESOURCES_2 outfiles )
+	QT4_EXTRACT_OPTIONS(rcc_files rcc_options ${ARGN})
+
+	FOREACH (it ${rcc_files})
+		GET_FILENAME_COMPONENT(outfilename ${it} NAME_WE)
+		GET_FILENAME_COMPONENT(infile ${it} ABSOLUTE)
+		GET_FILENAME_COMPONENT(rc_path ${infile} PATH)
+		SET(outfile ${CMAKE_CURRENT_BINARY_DIR}/qrc_${outfilename}.cxx)
+
+		SET(_RC_DEPENDS)
+		IF(EXISTS "${infile}")
+			#  parse file for dependencies
+			#  all files are absolute paths or relative to the location of the qrc file
+			FILE(READ "${infile}" _RC_FILE_CONTENTS)
+			STRING(REGEX MATCHALL "<file[^<]+" _RC_FILES "${_RC_FILE_CONTENTS}")
+			FOREACH(_RC_FILE ${_RC_FILES})
+				STRING(REGEX REPLACE "^<file[^>]*>" "" _RC_FILE "${_RC_FILE}")
+				IF(NOT IS_ABSOLUTE "${_RC_FILE}")
+					SET(_RC_FILE "${rc_path}/${_RC_FILE}")
+				ENDIF(NOT IS_ABSOLUTE "${_RC_FILE}")
+				SET(_RC_DEPENDS ${_RC_DEPENDS} "${_RC_FILE}")
+			ENDFOREACH(_RC_FILE)
+			# Since this cmake macro is doing the dependency scanning for these files,
+			# let's make a configured file and add it as a dependency so cmake is run
+			# again when dependencies need to be recomputed.
+			QT4_MAKE_OUTPUT_FILE("${infile}" "" "qrc.depends" out_depends)
+			CONFIGURE_FILE("${infile}" "${out_depends}" COPY_ONLY)
+		ELSE(EXISTS "${infile}")
+			# The .qrc file does not exist (yet). Let's add a dependency and hope
+			# that it will be generated later
+			SET(out_depends)
+		ENDIF(EXISTS "${infile}")
+
+		ADD_CUSTOM_COMMAND(OUTPUT ${outfile}
+		COMMAND ${QT_RCC_EXECUTABLE}
+		ARGS ${rcc_options} -name ${outfilename} -o ${outfile} ${infile}
+		MAIN_DEPENDENCY ${infile}
+		DEPENDS ${_RC_DEPENDS} "${out_depends}" VERBATIM)
+		SET(${outfiles} ${${outfiles}} ${outfile})
+	ENDFOREACH (it)
+
+ENDMACRO (QT4_ADD_RESOURCES_2)
