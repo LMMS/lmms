2260907285284b8c4b5b7cfee9a217ee3ff57785
mohamed <mohamed@amaksoud.com>
2015-02-04 22:37:43 +0100

    adding exportProjectMidi method, internally uses midiexport plugin

--- include/Song.h
+++ include/Song.h
@@ -268,6 +268,7 @@ public slots:
 	void importProject();
 	void exportProject(bool multiExport=false);
 	void exportProjectTracks();
+	void exportProjectMidi();
 
 	void startExport();
 	void stopExport();
