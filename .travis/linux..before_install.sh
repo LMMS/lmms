3cee26bfa5b3babb788d0aaceb0038e58c0b30b5
Dave French <dave.french3@googlemail.com>
2015-04-22 16:09:39 +0100

    remover liblo-dev and loblo 7 from travis as the ubuntu reo is too old for 12.4
    addded retrevial of source code from sourceforge for liblo to linux..before

--- .travis/linux..before_install.sh
+++ .travis/linux..before_install.sh
@@ -1,2 +1,7 @@
 sudo add-apt-repository ppa:kalakris/cmake -y;
 sudo apt-get update -qq
+wget http://downloads.sourceforge.net/liblo/liblo-0.28.tar.gz
+tar xvf liblo-0.28.tar.gz && cd liblo-0.28
+./configure && make && sudo make install
+sudo ldconfig
+cd ..
