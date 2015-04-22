61b927c19126c6d1640f9d0e7a7ed30cd2f88f69
Dave French <dave.french3@googlemail.com>
2015-04-22 16:47:56 +0100

    added wine1.4-common to travis build for new gcc

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -1,4 +1,4 @@
 sudo apt-get install -y cmake libqt4-dev libsndfile-dev fftw3-dev libvorbis-dev	\
 	libogg-dev libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev 		\
 	libstk0-dev libfluidsynth-dev portaudio19-dev wine-dev g++-multilib 		\
-	libfltk1.3-dev libgig-dev wine1.4-amd64
+	libfltk1.3-dev libgig-dev wine1.4-amd64 wine1.4-common
