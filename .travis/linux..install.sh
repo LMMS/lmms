e115c7c38baf58b3384d81c6274707c467df6a39
Dave French <dave.french3@googlemail.com>
2015-04-22 13:59:17 +0100

    ZASF 2.5 upgrade added RTOSC as git submodule
    added -liblo to linux travis

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -1,4 +1,4 @@
 sudo apt-get install -y cmake libqt4-dev libsndfile-dev fftw3-dev libvorbis-dev	\
 	libogg-dev libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev 		\
 	libstk0-dev libfluidsynth-dev portaudio19-dev wine-dev g++-multilib 		\
-	libfltk1.3-dev libgig-dev
+	libfltk1.3-dev libgig-dev liblo-dev
