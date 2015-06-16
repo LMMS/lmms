583dcd3314f3444a2a7d1ee9c58a099c6dfb6a31
Lukas W <lukaswhl@gmail.com>
2015-06-16 18:23:01 +0200

    Travis: Add Qt5 job

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -1,4 +1,14 @@
-sudo apt-get install -y cmake libqt4-dev libsndfile-dev fftw3-dev libvorbis-dev	\
-	libogg-dev libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev 		\
-	libstk0-dev libfluidsynth-dev portaudio19-dev wine-dev g++-multilib 		\
-	libfltk1.3-dev libgig-dev
+PACKAGES=cmake libsndfile-dev fftw3-dev libvorbis-dev  libogg-dev              \
+	libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev libstk0-dev       \
+	libfluidsynth-dev portaudio19-dev wine-dev g++-multilib libfltk1.3-dev     \
+	libgig-dev
+
+
+if [ $QT5 ]
+then
+	PACKAGES_QT=libqt5-dev
+else
+	PACKAGES_QT=libqt4-dev
+fi
+
+sudo apt-get install -y $PACKAGES $PACKAGES_QT
