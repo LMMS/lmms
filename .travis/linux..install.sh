1dfc300a50d5c60d1618c845b68b426d21cb6006
Lukas W <lukaswhl@gmail.com>
2015-06-16 20:23:59 +0200

    Travis: Fix linux package list syntax

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -1,14 +1,13 @@
-PACKAGES=cmake libsndfile-dev fftw3-dev libvorbis-dev  libogg-dev              \
-	libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev libstk0-dev       \
-	libfluidsynth-dev portaudio19-dev wine-dev g++-multilib libfltk1.3-dev     \
-	libgig-dev
-
+PACKAGES="cmake libsndfile-dev fftw3-dev libvorbis-dev  libogg-dev
+	libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev libstk0-dev
+	libfluidsynth-dev portaudio19-dev wine-dev g++-multilib libfltk1.3-dev
+	libgig-dev"
 
 if [ $QT5 ]
 then
-	PACKAGES_QT=qtbase5-dev
+	PACKAGES="$PACKAGES qtbase5-dev"
 else
-	PACKAGES_QT=libqt4-dev
+	PACKAGES="$PACKAGES libqt4-dev"
 fi
 
-sudo apt-get install -y $PACKAGES $PACKAGES_QT
+sudo apt-get install -y $PACKAGES
