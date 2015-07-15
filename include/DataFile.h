2175a47849bfb435d8dd58f7825f44c35d20807f
Colin Wallace <wallacoloo@gmail.com>
2015-07-15 04:11:01 +0000

    Separate DataFile::Upgrade into smaller functions

--- include/DataFile.h
+++ include/DataFile.h
@@ -109,6 +109,20 @@ private:
 
 	void cleanMetaNodes( QDomElement de );
 
+	// helper upgrade routines
+	void upgrade_0_2_1_20070501();
+	void upgrade_0_2_1_20070508();
+	void upgrade_0_3_0_rc2();
+	void upgrade_0_3_0();
+	void upgrade_0_4_0_20080104();
+	void upgrade_0_4_0_20080118();
+	void upgrade_0_4_0_20080129();
+	void upgrade_0_4_0_20080409();
+	void upgrade_0_4_0_20080607();
+	void upgrade_0_4_0_20080622();
+	void upgrade_0_4_0_beta1();
+	void upgrade_0_4_0_rc2();
+
 	void upgrade();
 
 	void loadData( const QByteArray & _data, const QString & _sourceFile );
