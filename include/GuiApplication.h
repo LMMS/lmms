edcaf164ae8971e9ce9d28bc4547d9c586fd19a5
Colin Wallace <wallacoloo@gmail.com>
2015-08-01 02:25:45 +0000

    Staged gui initialization for improved responsiveness

--- include/GuiApplication.h
+++ include/GuiApplication.h
@@ -25,12 +25,14 @@
 #ifndef GUIAPPLICATION_H
 #define GUIAPPLICATION_H
 
-#include <QtCore/QObject>
+#include <QObject>
+#include <QString>
 
 #include "export.h"
 #include "OscMsgListener.h"
 
 class QLabel;
+class QSplashScreen;
 
 class AutomationEditorWindow;
 class BBEditor;
@@ -45,7 +47,8 @@ class EXPORT GuiApplication : public QObject, public OscMsgListener
 {
 	Q_OBJECT;
 public:
-	explicit GuiApplication();
+	explicit GuiApplication(const QString &fileToLoad, const QString &fileToImport,
+		bool fullscreen, bool exitAfterImport);
 	~GuiApplication();
 
 	static GuiApplication* instance();
@@ -60,13 +63,39 @@ public:
 	ControllerRackView* getControllerRackView() { return m_controllerRackView; }
 	
 	// override of callback defined by OscMsgListener
-	void processMessage(const char *message);
+	void processMessage(const QByteArray &msg);
 
 public slots:
 	void displayInitProgress(const QString &msg);
 
 private slots:
 	void childDestroyed(QObject *obj);
+	// this slot should always be connected via Qt::QueuedConnection so that it operates in the gui thread
+	void processOscMsgInGuiThread(QByteArray msg);
+	// staged initialization (in order to avoid blocking the gui thread)
+	void initEngine();
+	void initMainWindow();
+	void initSongEditorWindow();
+	void initFxMixerView();
+	void initControllerRackView();
+	void initProjectNotes();
+	void initBbEditor();
+	void initPianoRoll();
+	void initAutomationEditor();
+	void handleCtorOptions();
+signals:
+	void postInitEngine();
+	void postInitMainWindow();
+	void postInitSongEditorWindow();
+	void postInitFxMixerView();
+	void postInitControllerRackView();
+	void postInitProjectNotes();
+	void postInitBbEditor();
+	void postInitPianoRoll();
+	void postInitAutomationEditor();
+	// sent whenever we receive an Open Sound Control message 
+	//   (presumably from the core or another section of the UI)
+	void receivedOscMessage(QByteArray msg);
 
 private:
 	static GuiApplication* s_instance;
@@ -80,6 +109,13 @@ private:
 	ProjectNotes* m_projectNotes;
 	ControllerRackView* m_controllerRackView;
 	QLabel* m_loadingProgressLabel;
+	QSplashScreen* m_splashScreen;
+
+	// initialization arguments:
+	QString m_fileToLoad;
+	QString m_fileToImport;
+	bool m_fullscreen;
+	bool m_exitAfterImport;
 };
 
 #define gui GuiApplication::instance()
