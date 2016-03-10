LMMS/lmms

commit fcec8ddd0283152935bcd87d8f1b5862d07f1a62

Fix BBtrack updating; Fix the Pattern tooltip

src/tracks/Pattern.cpp
@@ -701,9 +701,9 @@ PatternView::PatternView( Pattern* pattern, TrackView* parent ) :
 		s_stepBtnOffLight = new QPixmap( embed::getIconPixmap(
 						"step_btn_off_light" ) );
 	}
+	
+	update();	
 
-	ToolTip::add( this,
-		tr( "use mouse wheel to set velocity of a step" ) );
 	setStyle( QApplication::style() );
 }
 
@@ -722,7 +722,22 @@ PatternView::~PatternView()
 
 void PatternView::update()
 {
-	m_pat->changeLength( m_pat->length() );
+	if( fixedTCOs() )
+	{
+		m_pat->changeLength( m_pat->length() );
+	}
+	
+	if ( m_pat->m_patternType == Pattern::BeatPattern )
+	{
+		ToolTip::add( this,
+			tr( "use mouse wheel to set velocity of a step" ) );	
+	}
+	else 
+	{
+		ToolTip::add( this,
+			tr( "double-click to open in Piano Roll" ) );		
+	}
+	
 	TrackContentObjectView::update();
 }
 

src/gui/AutomationPatternView.cpp
@@ -60,6 +60,8 @@ AutomationPatternView::AutomationPatternView( AutomationPattern * _pattern,
 	
 	if( s_pat_rec == NULL ) { s_pat_rec = new QPixmap( embed::getIconPixmap(
 							"pat_rec" ) ); }
+							
+	update();
 }
 
 

commit 6a10cb184d3469f94f562a7708c8baf4788e02dd

Fix regression caused by fcec8dd

src/tracks/Pattern.cpp
@@ -722,10 +722,7 @@ PatternView::~PatternView()
 
 void PatternView::update()
 {
-	if( fixedTCOs() )
-	{
-		m_pat->changeLength( m_pat->length() );
-	}
+	m_pat->changeLength( m_pat->length() );
 	
 	if ( m_pat->m_patternType == Pattern::BeatPattern )
 	{