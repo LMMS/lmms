ee6f441b4c731f116d821c0d15ff157f51ba11d8
Umcaruje <uros.maravic@gmail.com>
2016-08-20 00:36:39 +0200

    Amplifier redesign

--- plugins/Amplifier/AmplifierControlDialog.cpp
+++ plugins/Amplifier/AmplifierControlDialog.cpp
@@ -38,33 +38,32 @@ AmplifierControlDialog::AmplifierControlDialog( AmplifierControls* controls ) :
 	QPalette pal;
 	pal.setBrush( backgroundRole(), PLUGIN_NAME::getIconPixmap( "artwork" ) );
 	setPalette( pal );
-	setFixedSize( 100, 125 );
+	setFixedSize( 100, 110 );
 
 	Knob * volumeKnob = new Knob( knobBright_26, this);
-	volumeKnob -> move( 20, 30 );
+	volumeKnob -> move( 20, 10 );
 	volumeKnob -> setVolumeKnob( true );
 	volumeKnob->setModel( &controls->m_volumeModel );
 	volumeKnob->setLabel( tr( "VOL" ) );
 	volumeKnob->setHintText( tr( "Volume:" ) , "%" );
 
 	Knob * panKnob = new Knob( knobBright_26, this);
-	panKnob -> move( 60, 30 );
+	panKnob -> move( 60, 10 );
 	panKnob->setModel( &controls->m_panModel );
 	panKnob->setLabel( tr( "PAN" ) );
 	panKnob->setHintText( tr( "Panning:" ) , "" );
 
 	Knob * leftKnob = new Knob( knobBright_26, this);
-	leftKnob -> move( 20, 80 );
+	leftKnob -> move( 20, 65 );
 	leftKnob -> setVolumeKnob( true );
 	leftKnob->setModel( &controls->m_leftModel );
 	leftKnob->setLabel( tr( "LEFT" ) );
 	leftKnob->setHintText( tr( "Left gain:" ) , "%" );
 
 	Knob * rightKnob = new Knob( knobBright_26, this);
-	rightKnob -> move( 60, 80 );
+	rightKnob -> move( 60, 65 );
 	rightKnob -> setVolumeKnob( true );
 	rightKnob->setModel( &controls->m_rightModel );
 	rightKnob->setLabel( tr( "RIGHT" ) );
 	rightKnob->setHintText( tr( "Right gain:" ) , "%" );
 }
-
