dde141911495a4018a2e83b7dae9e580d704a973
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2016-09-29 07:24:46 -0400

    Speed up apple builds (#3058)
    - fltk appears to have been fixed in Homebrew
     - removed "reinstall" to speed up builds

--- .travis/osx..install.sh
+++ .travis/osx..install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PACKAGES="cmake pkgconfig fftw libogg libvorbis libsndfile libsamplerate jack sdl stk fluid-synth portaudio node"
+PACKAGES="cmake pkgconfig fftw libogg libvorbis libsndfile libsamplerate jack sdl stk fluid-synth portaudio node fltk"
 
 if [ $QT5 ]; then
 	PACKAGES="$PACKAGES qt55"
@@ -8,13 +8,6 @@ else
 	PACKAGES="$PACKAGES qt"
 fi
 
-brew reinstall $PACKAGES
+brew install $PACKAGES
 
 sudo npm install -g appdmg
-
-# Workaround per Homebrew bug #44806
-brew reinstall fltk
-if [ $? -ne 0 ]; then
-  echo "Warning: fltk installation failed, trying workaround..."
-  brew reinstall --devel https://raw.githubusercontent.com/dpo/homebrew/ec46018128dde5bf466b013a6c7086d0880930a3/Library/Formula/fltk.rb
-fi
