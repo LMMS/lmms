5a7acd322095b5a124fe349b008031e90004dceb
Umcaruje <uros.maravic@gmail.com>
2016-10-31 23:43:05 +0100

    Write a ConfigManager upgrade

--- src/core/ConfigManager.cpp
+++ src/core/ConfigManager.cpp
@@ -136,6 +136,16 @@ void ConfigManager::upgrade_1_1_90()
 	}
 }
 
+	
+void ConfigManager::upgrade_1_1_91()
+{		
+	// rename displaydbv to displaydbfs
+	if ( !value( "app", "displaydbv" ).isNull() ) {
+		setValue( "app", "displaydbfs", value( "app", "displaydbv" ) );
+		deleteValue( "app", "displaydbv" );
+	}
+}
+
 
 void ConfigManager::upgrade()
 {
@@ -144,12 +154,17 @@ void ConfigManager::upgrade()
 	{
 		return;
 	}
-
+	
 	ProjectVersion createdWith = m_version;
 	
 	if ( createdWith.setCompareType(ProjectVersion::Build) < "1.1.90" )
 	{
 		upgrade_1_1_90();
+	}
+	
+	if ( createdWith.setCompareType(ProjectVersion::Build) < "1.1.91" )
+	{
+		upgrade_1_1_91();
 	}
 	
 	// Don't use old themes as they break the UI (i.e. 0.4 != 1.0, etc)
@@ -546,5 +561,3 @@ void ConfigManager::saveConfigFile()
 	outfile.write( xml.toUtf8() );
 	outfile.close();
 }
-
-
