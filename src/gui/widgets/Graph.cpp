4603f2bf0a02e7a8adad72ca9aa5f026854fbda0
DeRobyJ <derobyj@gmail.com>
2016-11-27 13:39:11 +0100

    Changes noncyclic smoothing behaviour

--- src/gui/widgets/Graph.cpp
+++ src/gui/widgets/Graph.cpp
@@ -624,12 +624,12 @@ void graphModel::smoothNonCyclic()
 	QVector<float> temp = m_samples;
 
 	// Smoothing
-	m_samples[0] = ( ( temp[0] * 3 ) + temp[1] ) * 0.25f;
+	//m_samples[0] = ( ( temp[0] * 3 ) + temp[1] ) * 0.25f;
 	for ( int i=1; i < ( length()-1 ); i++ )
 	{
 		m_samples[i] = ( temp[i-1] + ( temp[i] * 2 ) + temp[i+1] ) * 0.25f;
 	}
-	m_samples[length()-1] = ( temp[length()-2] + ( temp[length()-1] * 3 ) ) * 0.25f;
+	//m_samples[length()-1] = ( temp[length()-2] + ( temp[length()-1] * 3 ) ) * 0.25f;
 
 	emit samplesChanged(0, length()-1);
 }
