07ae0b757cb450a48cdf744db8716a896e1bda30
Orr Dvori <dvoreader@gmail.com>
2017-01-08 14:31:01 +0200

    First Preview of the X-Pressive Plugin (exprtk.hpp is not included, get it from my exprtk fork in the branch internal_data_functions) available keys: f- note's frequency. available only in the output expressions t- time in seconds. in the Waves (W1,W2,W3) it's in the range [0,1) key- the note's keyboard key. available only in the output expressions. v- the note's velocity (divided by 255.0 so it is in the range [0,1]). available only in the output expressions. rel- gives 0 while the key is holded, and 1 after the key release. available only in the output expressions. A1,A2,A3- general purpose knobs (you can control them with the automations). available only in the output expressions. W1,W2,W3- precalculated wave forms. can be also load from file. you can use them only in the output expressions available functions: cent(x)- gives pow(2,x/1200) rand()- random number generator. in range [-1,1). each call gives other value. randv(i)- random vector (with pseudo infinite integer cells). the values are in range [-1,1). it's stays consistent only across the note playback. so each note playback will get other vector (even on the same key). sinew- sine wave with period of 1. saww- saw wave with period of 1. squarew- square wave with period of 1. trianglew- triangle wave with period of 1. expw- exponent wave with period of 1. expnw- another exponent wave with period of 1. moogw- moog wave with period of 1. moogsaww- moog-saw wave with period of 1. you can use * % ^ / + - pow sin log pi etc.

---
+++ plugins/xpressive/exprfront.h
@@ -0,0 +1,56 @@
+/*
+ * exprfront.h - header file to a Frontend to ExprTk
+ *
+ * Copyright (c) 2016-2017 Orr Dvori
+ *
+ * This file is part of LMMS - http://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef EXPRFRONT_H_
+#define EXPRFRONT_H_
+
+class ExprFrontData;
+
+inline float positiveFraction(float x)
+{
+	if (x<0)
+	{
+		x+=static_cast<int>(1-x);
+	}
+	return x-static_cast<int>(x);
+}
+class ExprFront
+{
+public:
+	typedef float (*ff1data_functor)(void*, float);
+	ExprFront(const char * expr);
+	~ExprFront();
+	bool compile();
+	inline bool isValid(){return m_valid;}
+	float evaluate();
+	bool add_variable(const char * name, float & ref);
+	bool add_constant(const char * name, float  ref);
+	bool add_cyclic_vector(const char *name, const float *data, size_t length);
+private:
+	ExprFrontData *m_data;
+	bool m_valid;
+};
+
+
+#endif /* EXPRFRONT_H_ */
