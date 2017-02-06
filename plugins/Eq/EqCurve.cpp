d71a55e99f290b6d5c609eb59a2248adc88db7ce
grejppi <grejppi@gmail.com>
2017-02-06 02:41:15 +0200

    update all copyright headers to the proper url (#3326)

--- plugins/Eq/EqCurve.cpp
+++ plugins/Eq/EqCurve.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2015 Steffen Baranowsky <BaraMGB/at/freenet/dot/de>
  *
- * This file is part of LMMS - http://lmms.io
+ * This file is part of LMMS - https://lmms.io
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
