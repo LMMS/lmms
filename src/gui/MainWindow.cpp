15265866441215f490c896999d041aaac2f15d68
follower <follower@rancidbacon.com>
2017-02-17 07:19:23 +1300

    According to Qt, separator issue was fixed in 5.6+, so exclude them.
    @tresf confirmed fixed in 5.7.
    
    See: https://bugreports.qt.io/browse/QTBUG-40071

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -316,7 +316,7 @@ void MainWindow::finalize()
 					Qt::CTRL + Qt::Key_M );*/
 
 // Prevent dangling separator at end of menu per https://bugreports.qt.io/browse/QTBUG-40071
-#if !(defined(LMMS_BUILD_APPLE) && (QT_VERSION >= 0x050000))
+#if !(defined(LMMS_BUILD_APPLE) && (QT_VERSION >= 0x050000) && (QT_VERSION < 0x050600))
 	project_menu->addSeparator();
 #endif
 	project_menu->addAction( embed::getIconPixmap( "exit" ), tr( "&Quit" ),
@@ -394,7 +394,7 @@ void MainWindow::finalize()
 					this, SLOT( enterWhatsThisMode() ) );
 
 // Prevent dangling separator at end of menu per https://bugreports.qt.io/browse/QTBUG-40071
-#if !(defined(LMMS_BUILD_APPLE) && (QT_VERSION >= 0x050000))
+#if !(defined(LMMS_BUILD_APPLE) && (QT_VERSION >= 0x050000) && (QT_VERSION < 0x050600))
 	help_menu->addSeparator();
 #endif
 	help_menu->addAction( embed::getIconPixmap( "icon" ), tr( "About" ),
