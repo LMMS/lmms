66267c11ae162aff6c3902fd7e439193e43aab5a
Orr Dvori <dvoreader@gmail.com>
2017-02-28 12:48:27 +0200

    X-Pressive plug-in - until somebody will find a better solution, exprtk.hpp is patched under the name exprtk.patched.hpp ...

--- plugins/xpressive/exprfront.cpp
+++ plugins/xpressive/exprfront.cpp
@@ -37,7 +37,7 @@
 #include "lmms_math.h"
 #include "NotePlayHandle.h"
 
-#include "exprtk.hpp"
+#include "exprtk.patched.hpp"
 
 typedef exprtk::symbol_table<float> symbol_table_t;
 typedef exprtk::expression<float> expression_t;
