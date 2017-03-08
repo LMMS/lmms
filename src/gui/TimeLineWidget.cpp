9545f968b3a1639e0ff6974ac567382da49d1a8d
Oskar Wallgren <oskar.wallgren13@gmail.com>
2017-03-08 10:58:56 +0100

    Fix TimeLineWidget overflow (#3410)

--- src/gui/TimeLineWidget.cpp
+++ src/gui/TimeLineWidget.cpp
@@ -371,7 +371,9 @@ void TimeLineWidget::mouseMoveEvent( QMouseEvent* event )
 	{
 		case MovePositionMarker:
 			m_pos.setTicks( t.getTicks() );
-			Engine::getSong()->setMilliSeconds(((((t.getTicks()))*60*1000/48)/Engine::getSong()->getTempo()));
+			Engine::getSong()->setMilliSeconds( ( t.getTicks() *
+					( 60 * 1000 / 48 ) ) /
+						Engine::getSong()->getTempo() );
 			m_pos.setCurrentFrame( 0 );
 			updatePosition();
 			positionMarkerMoved();
