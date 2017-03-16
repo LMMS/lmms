825c4c1b120e861019ac214dec856f37ec9a488f
Lukas W <lukaswhl@gmail.com>
2017-03-16 12:50:00 +0100

    Travis: Enable ccache on OSX

--- .travis/osx..script.sh
+++ .travis/osx..script.sh
@@ -5,4 +5,4 @@ if [ $QT5 ]; then
         export CMAKE_PREFIX_PATH="$(brew --prefix qt55)"
 fi
 
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWANT_QT5=$QT5 -DUSE_WERROR=OFF ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWANT_QT5=$QT5 -DUSE_WERROR=OFF -DUSE_CCACHE=ON ..
