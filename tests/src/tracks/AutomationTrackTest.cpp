LMMS/lmms

commit 42fcaedbfd745d353a785d02cb77f2d5bd29b6ef

Automation refactorings & record fix

src/core/Song.cpp
@@ -31,6 +31,7 @@
 #include <QMessageBox>
 #include <QApplication>
 
+#include <functional>
 #include <math.h>
 
 #include "AutomationTrack.h"
@@ -424,50 +425,63 @@ void Song::processAutomations(const TrackList &tracklist, MidiTime timeStart, fp
 			tracks << dynamic_cast<AutomationTrack*>(track);
 		}
 	}
+	std::remove_if(tracks.begin(), tracks.end(), std::mem_fn(&Track::isMuted));
 
-	MidiTime time_end = timeStart + static_cast<int>(frames / Engine::framesPerTick());
+	Track::tcoVector tcos;
+	AutomatedValueMap values;
 
-	// Collect all relevant patterns, sorted by start position
-	QVector<TrackContentObject*> tcos;
-	for (Track* track: tracks)
+	if (tcoNum < 0)
 	{
-		if(track->isMuted())
+		// Collect all relevant patterns, sorted by start position
+		MidiTime timeEnd = timeStart + static_cast<int>(frames / Engine::framesPerTick());
+		for (AutomationTrack* track: tracks)
 		{
-			continue;
+			track->getTCOsInRange(tcos, 0, timeEnd);
+		}
+		std::remove_if(tcos.begin(), tcos.end(), std::mem_fn(&TrackContentObject::isMuted));
+
+		values = automatedValuesAt(tcos, timeStart);
+	}
+	else
+	{
+		if (tracklist.size() != 1)
+		{
+			qCritical() << "processAutomations called with specified tcoNum but more than one track";
+			return;
 		}
 
-		int tco_begin = 0, tco_end = track->numOfTCOs();
-		if( tcoNum >= 0 )
+		Track* track = tracklist.at(1);
+		TrackContentObject* tco = track->getTCO(tcoNum);
+		auto p = dynamic_cast<AutomationPattern *>(tco);
+
+		for (AutomatableModel* object : p->objects())
 		{
-			tco_begin = tcoNum;
-			tco_end = tcoNum + 1;
+			values[object] = p->valueAt(timeStart);
 		}
+		tcos << tco;
+	}
 
-		for (int i = tco_begin; i < tco_end; i++)
+	QSet<const AutomatableModel*> recordedModels;
+	// Process recording
+	for (TrackContentObject* tco : tcos)
+	{
+		auto p = dynamic_cast<AutomationPattern *>(tco);
+		MidiTime relTime = timeStart - p->startPosition();
+		if (p->isRecording() && relTime >= 0 && relTime < p->length())
 		{
-			TrackContentObject* tco = track->getTCO(i);
+			const AutomatableModel* recordedModel = p->firstObject();
+			p->recordValue(relTime, recordedModel->value<float>());
 
-			// Skip this TCO if it's muted or not contained in the time period we're processing
-			if( tco->isMuted() || tco->startPosition() > time_end)
-			{
-				continue;
-			}
-
-			tcos.insert(std::upper_bound(tcos.begin(), tcos.end(), tco, TrackContentObject::comparePosition), tco);
+			recordedModels << recordedModel;
 		}
 	}
 
-	for(TrackContentObject* tco : tcos)
+	// Apply values
+	for (auto it = values.cbegin(); it != values.cend(); it++)
 	{
-		auto p = dynamic_cast<AutomationPattern *>(tco);
-
-		if (tcoNum < 0)
+		if (! recordedModels.contains(it.key()))
 		{
-			p->processMidiTime(timeStart - p->startPosition());
-		}
-		else
-		{
-			p->processMidiTime(timeStart);
+			it.key()->setAutomatedValue(it.value());
 		}
 	}
 }
@@ -836,6 +850,30 @@ bpm_t Song::getTempo()
 AutomationPattern * Song::tempoAutomationPattern()
 {
 	return AutomationPattern::globalAutomationPattern( &m_tempoModel );
+}
+
+AutomatedValueMap Song::automatedValuesAt(const Track::tcoVector &tcos, MidiTime time)
+{
+	AutomatedValueMap valueMap;
+
+	for(TrackContentObject* tco : tcos)
+	{
+		AutomationPattern* p = dynamic_cast<AutomationPattern *>(tco);
+		if (!p) {
+			qCritical() << "automatedValuesAt: tco passed is not an automation pattern";
+			continue;
+		}
+
+		MidiTime relTime = time - p->startPosition();
+		float value = p->valueAt(relTime);
+
+		for (AutomatableModel* model : p->objects())
+		{
+			valueMap[model] = value;
+		}
+	}
+
+	return valueMap;
 }
 
 

include/AutomatableModel.h
@@ -444,6 +444,7 @@ public:
 
 } ;
 
+using AutomatedValueMap = QMap<AutomatableModel*, float>;
 
 #endif
 

include/AutomationPattern.h
@@ -59,6 +59,7 @@ public:
 	bool addObject( AutomatableModel * _obj, bool _search_dup = true );
 
 	const AutomatableModel * firstObject() const;
+	const objectVector& objects() const;
 
 	// progression-type stuff
 	inline ProgressionTypes progressionType() const
@@ -81,6 +82,8 @@ public:
 
 	void removeValue( const MidiTime & _time,
 					  const bool _quant_pos = true );
+
+	void recordValue(MidiTime time, float value);
 
 	MidiTime setDragValue( const MidiTime & _time, const float _value,
 						   const bool _quant_pos = true );
@@ -139,8 +142,6 @@ public:
 
 	static const QString classNodeName() { return "automationpattern"; }
 	QString nodeName() const { return classNodeName(); }
-
-	void processMidiTime( const MidiTime & _time );
 
 	virtual TrackContentObjectView * createView( TrackView * _tv );
 

include/Song.h
@@ -31,7 +31,6 @@
 #include <QtCore/QVector>
 
 #include "TrackContainer.h"
-#include "AutomatableModel.h"
 #include "Controller.h"
 #include "MeterModel.h"
 #include "VstSyncController.h"
@@ -97,7 +96,6 @@ public:
 
 
 	void processNextBuffer();
-	void processAutomations(const TrackList& tracks, MidiTime timeStart, fpp_t frames, int tcoNum);
 
 	inline int getMilliseconds() const
 	{
@@ -205,6 +203,8 @@ public:
 	{
 		return m_globalAutomationTrack;
 	}
+
+	static AutomatedValueMap automatedValuesAt(const Track::tcoVector& tcos, MidiTime time);
 
 	// file management
 	void createNewProject();
@@ -326,6 +326,7 @@ private:
 
 	void removeAllControllers();
 
+	void processAutomations(const TrackList& tracks, MidiTime timeStart, fpp_t frames, int tcoNum);
 
 	AutomationTrack * m_globalAutomationTrack;
 

src/core/AutomationPattern.cpp
@@ -109,16 +109,9 @@ AutomationPattern::~AutomationPattern()
 
 bool AutomationPattern::addObject( AutomatableModel * _obj, bool _search_dup )
 {
-	if( _search_dup )
+	if( _search_dup && m_objects.contains(_obj) )
 	{
-		for( objectVector::iterator it = m_objects.begin();
-					it != m_objects.end(); ++it )
-		{
-			if( *it == _obj )
-			{				
-				return false;
-			}
-		}
+		return false;
 	}
 
 	// the automation track is unconnected and there is nothing in the track
@@ -181,6 +174,11 @@ const AutomatableModel * AutomationPattern::firstObject() const
 
 	static FloatModel _fm( 0, DEFAULT_MIN_VALUE, DEFAULT_MAX_VALUE, 0.001 );
 	return &_fm;
+}
+
+const AutomationPattern::objectVector& AutomationPattern::objects() const
+{
+	return m_objects;
 }
 
 
@@ -261,6 +259,21 @@ void AutomationPattern::removeValue( const MidiTime & _time,
 	}
 
 	emit dataChanged();
+}
+
+
+
+void AutomationPattern::recordValue(MidiTime time, float value)
+{
+	if( value != m_lastRecordedValue )
+	{
+		putValue( time, value, true );
+		m_lastRecordedValue = value;
+	}
+	else if( valueAt( time ) != value )
+	{
+		removeValue( time, false );
+	}
 }
 
 
@@ -615,44 +628,6 @@ const QString AutomationPattern::name() const
 	#endif
 }
 
-
-
-
-void AutomationPattern::processMidiTime( const MidiTime & time )
-{
-	if( ! isRecording() )
-	{
-		if( time >= 0 && hasAutomation() )
-		{
-			const float val = valueAt( time );
-			for( objectVector::iterator it = m_objects.begin();
-							it != m_objects.end(); ++it )
-			{
-				if( *it )
-				{
-					( *it )->setAutomatedValue( val );
-				}
-
-			}
-		}
-	}
-	else
-	{
-		if( time >= 0 && time < length() && ! m_objects.isEmpty() )
-		{
-			const float value = static_cast<float>( firstObject()->value<float>() );
-			if( value != m_lastRecordedValue )
-			{
-				putValue( time, value, true );
-				m_lastRecordedValue = value;
-			}
-			else if( valueAt( time ) != value )
-			{
-				removeValue( time, false );
-			}
-		}
-	}
-}
 
 
 

commit e2808837d2010091317351c5c9c54de73975f852

Automation: Fix empty patterns (regression from 42fcaedbfd745d353a7)

src/core/Song.cpp
@@ -863,6 +863,10 @@ AutomatedValueMap Song::automatedValuesAt(const Track::tcoVector &tcos, MidiTime
 			continue;
 		}
 
+		if (! p->hasAutomation()) {
+			continue;
+		}
+
 		MidiTime relTime = time - p->startPosition();
 		float value = p->valueAt(relTime);
 

tests/src/tracks/AutomationTrackTest.cpp
@@ -87,12 +87,17 @@ private slots:
 		p2.movePosition(100);
 		p2.addObject(&model);
 
-		QCOMPARE(Song::automatedValuesAt({&p1, &p2},   0)[&model], 0.0f);
-		QCOMPARE(Song::automatedValuesAt({&p1, &p2},   5)[&model], 0.5f);
-		QCOMPARE(Song::automatedValuesAt({&p1, &p2},  10)[&model], 1.0f);
-		QCOMPARE(Song::automatedValuesAt({&p1, &p2},  50)[&model], 1.0f);
-		QCOMPARE(Song::automatedValuesAt({&p1, &p2}, 100)[&model], 0.0f);
-		QCOMPARE(Song::automatedValuesAt({&p1, &p2}, 150)[&model], 0.5f);
+		AutomationPattern p3(nullptr);
+		p3.addObject(&model);
+		//XXX: Why is this even necessary?
+		p3.clear();
+
+		QCOMPARE(Song::automatedValuesAt({&p1, &p2, &p3},   0)[&model], 0.0f);
+		QCOMPARE(Song::automatedValuesAt({&p1, &p2, &p3},   5)[&model], 0.5f);
+		QCOMPARE(Song::automatedValuesAt({&p1, &p2, &p3},  10)[&model], 1.0f);
+		QCOMPARE(Song::automatedValuesAt({&p1, &p2, &p3},  50)[&model], 1.0f);
+		QCOMPARE(Song::automatedValuesAt({&p1, &p2, &p3}, 100)[&model], 0.0f);
+		QCOMPARE(Song::automatedValuesAt({&p1, &p2, &p3}, 150)[&model], 0.5f);
 	}
 } AutomationTrackTest;
 