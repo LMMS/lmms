4ec90b7a85c0a840a73cc1c53dac9b8f2eea5a1a
Javier Serrano Polo <javier@jasp.net>
2017-04-06 11:02:28 +0200

    fluidsynth is currently undistributable

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -2,8 +2,9 @@
 
 PACKAGES="cmake libsndfile-dev fftw3-dev libvorbis-dev  libogg-dev
 	libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev libstk0-dev
-	libfluidsynth-dev portaudio19-dev wine-dev g++-multilib libfltk1.3-dev
+	portaudio19-dev wine-dev g++-multilib libfltk1.3-dev
 	libgig-dev libsoundio-dev"
+# TODO: Add libfluidsynth-dev when ready
 
 # Help with unmet dependencies
 PACKAGES="$PACKAGES libjack0"
