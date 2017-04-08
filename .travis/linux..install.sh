7cdf6393415cf254a220dc63dc8d66c5c7f8f481
Lukas W <lukaswhl@gmail.com>
2017-04-08 23:56:07 +0200

    Travis: Add missing packages

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -2,11 +2,13 @@
 
 PACKAGES="cmake libsndfile-dev fftw3-dev libvorbis-dev  libogg-dev
 	libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev libstk0-dev
-	libfluidsynth-dev portaudio19-dev wine-dev g++-multilib libfltk1.3-dev
+	libfluidsynth-dev portaudio19-dev g++-multilib libfltk1.3-dev
 	libgig-dev libsoundio-dev"
 
+VST_PACKAGES="wine-dev libqt5x11extras5-dev qtbase5-private-dev libxcb-util-dev libxcb-keysyms1-dev"
+
 # Help with unmet dependencies
-PACKAGES="$PACKAGES libjack0"
+PACKAGES="$PACKAGES $VST_PACKAGES libjack0"
 
 if [ "$QT5" ]; then
 	PACKAGES="$PACKAGES qtbase5-dev qttools5-dev-tools qttools5-dev"
