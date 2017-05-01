0d09aa1f49e27807f1eaaa8b62f281c55f8244f0
curlymorphic <dave.french3@googlemail.com>
2017-05-01 14:04:59 +0100

    Flanger LFO rate syncronised
    The LFO rate was not correctly syncronising to tempo
    
    This has been rectifited, to utalise the TempoSyncKnob as intended, returning a period,
    instead of a frequency. The knob now reports the correct values in the GUI.
    
    Flanger LFO maximum period incressed to 60 seconds

--- plugins/Flanger/FlangerEffect.cpp
+++ plugins/Flanger/FlangerEffect.cpp
@@ -94,7 +94,7 @@ bool FlangerEffect::processAudioBuffer( sampleFrame *buf, const fpp_t frames )
 	const float noise = m_flangerControls.m_whiteNoiseAmountModel.value();
 	float amplitude = m_flangerControls.m_lfoAmountModel.value() * Engine::mixer()->processingSampleRate();
 	bool invertFeedback = m_flangerControls.m_invertFeedbackModel.value();
-	m_lfo->setFrequency(  m_flangerControls.m_lfoFrequencyModel.value() );
+	m_lfo->setFrequency(  1.0/m_flangerControls.m_lfoFrequencyModel.value() );
 	m_lDelay->setFeedback( m_flangerControls.m_feedbackModel.value() );
 	m_rDelay->setFeedback( m_flangerControls.m_feedbackModel.value() );
 	sample_t dryS[2];
