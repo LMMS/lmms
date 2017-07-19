dc829f48af29a7c5e428e2f9727385dd9fc95c08
Karmo Rosental <karmo.rosental@gmail.com>
2017-07-19 22:46:27 +0300

    Added support for wine-stable from WineHQ repositories. (#3708)
    Added support for wine-stable from WineHQ repositories.

--- cmake/modules/FindWine.cmake
+++ cmake/modules/FindWine.cmake
@@ -7,7 +7,7 @@
 #  WINE_DEFINITIONS - Compiler switches required for using wine
 #
 
-LIST(APPEND CMAKE_PREFIX_PATH /opt/wine-devel /opt/wine-staging)
+LIST(APPEND CMAKE_PREFIX_PATH /opt/wine-stable /opt/wine-devel /opt/wine-staging)
 
 FIND_PATH(WINE_INCLUDE_DIR windows/windows.h PATH_SUFFIXES wine)
 FIND_LIBRARY(WINE_LIBRARY NAMES wine PATH_SUFFIXES wine i386-linux-gnu/wine)
