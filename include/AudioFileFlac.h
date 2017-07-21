b41bbab592d1988cc2afee12c5b1eebf44a6ca39
Levin <irrenhaus3@gmail.com>
2017-07-21 21:49:14 +0200

    Indent with TAB character

--- include/AudioFileFlac.h
+++ include/AudioFileFlac.h
@@ -13,35 +13,35 @@
 class AudioFileFlac: public AudioFileDevice
 {
 public:
-    AudioFileFlac( OutputSettings const& outputSettings,
-            ch_cnt_t const channels,
-            bool& successful,
-            QString const& file,
-            Mixer* mixer
-    );
+	AudioFileFlac( OutputSettings const& outputSettings,
+			ch_cnt_t const channels,
+			bool& successful,
+			QString const& file,
+			Mixer* mixer
+	);
 
-    virtual ~AudioFileFlac();
+	virtual ~AudioFileFlac();
 
-    static AudioFileDevice* getInst( QString const& outputFilename,
-            OutputSettings const& outputSettings,
-            ch_cnt_t const channels,
-            Mixer* mixer,
-            bool& successful)
-    {
-        return new AudioFileFlac(outputSettings,channels,successful,outputFilename,mixer);
-    }
+	static AudioFileDevice* getInst( QString const& outputFilename,
+			OutputSettings const& outputSettings,
+			ch_cnt_t const channels,
+			Mixer* mixer,
+			bool& successful)
+	{
+		return new AudioFileFlac(outputSettings,channels,successful,outputFilename,mixer);
+	}
 
 private:
 
-    SF_INFO  m_sfinfo;
-    SNDFILE* m_sf;
+	SF_INFO  m_sfinfo;
+	SNDFILE* m_sf;
 
-    virtual void writeBuffer(surroundSampleFrame const* _ab,
-                        fpp_t const frames,
-                        float master_gain) override;
+	virtual void writeBuffer(surroundSampleFrame const* _ab,
+						fpp_t const frames,
+						float master_gain) override;
 
-    bool startEncoding();
-    void finishEncoding();
+	bool startEncoding();
+	void finishEncoding();
 
 };
 
