7f5a9f63099d40834501f967b3a4d51910cf739f
tresf <tres.finocchiaro@gmail.com>
2017-07-25 01:28:49 -0400

    Clean up wine dependencies

--- cmake/modules/FindWine.cmake
+++ cmake/modules/FindWine.cmake
@@ -14,6 +14,28 @@ FIND_PROGRAM(WINE_CXX NAMES wineg++ winegcc winegcc64 winegcc32)
 set(WINE_INCLUDE_DIRS ${WINE_INCLUDE_DIR} )
 set(WINE_LIBRARIES ${WINE_LIBRARY} )
 
+# Handle wine linking problems
+EXEC_PROGRAM(${WINE_CXX} ARGS "-v -m32 /dev/zero" OUTPUT_VARIABLE WINEBUILD_OUTPUT)
+
+# Debian systems 
+IF("${WINEBUILD_OUTPUT}" MATCHES ".*x86_64-linux-gnu/wine/libwinecrt0.a.*")
+	SET(WINE_LIBRARY_FIX "/usr/lib/i386-linux-gnu/wine/" )
+# Fedora systems 
+ELSEIF("${WINEBUILD_OUTPUT}" MATCHES ".*lib64/wine/libwinecrt0.a.*")
+	SET(WINE_LIBRARY_FIX "/usr/lib/i386/wine/")
+# Wine stable
+ELSEIF("${WINEBUILD_OUTPUT}" MATCHES "/opt/wine-stable/lib64/wine/libwinecrt0.a.*")
+	SET(WINE_LIBRARY_FIX "/opt/wine-stable/lib/wine/")
+# Wine development
+ELSEIF("${WINEBUILD_OUTPUT}" MATCHES "/opt/wine-devel/lib64/wine/libwinecrt0.a.*")
+	SET(WINE_LIBRARY_FIX "/opt/wine-devel/lib/wine/")
+# Wine staging
+ELSEIF("${WINEBUILD_OUTPUT}" MATCHES "/opt/wine-staging/lib64/wine/libwinecrt0.a.*")
+	SET(WINE_LIBRARY_FIX "/opt/wine-staging/lib/wine/")
+ENDIF()
+
+MESSAGE("${WINE_LIBRARY_FIX}")
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Wine DEFAULT_MSG WINE_LIBRARIES WINE_INCLUDE_DIRS)
 
