9c91079337993b88efbcc7c1bf9577f3188a5932
Lukas W <lukaswhl@gmail.com>
2017-08-30 13:05:49 +0200

    Revert "Find Boost in CMake, fix Travis"
    Boost not used anymore.
    
    This reverts commit 6b557d2b14e10d159f2cd04fd68643fb2608fff9.

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -3,7 +3,7 @@
 PACKAGES="cmake libsndfile-dev fftw3-dev libvorbis-dev libogg-dev libmp3lame-dev
 	libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev libstk0-dev
 	libfluidsynth-dev portaudio19-dev wine-dev g++-multilib libfltk1.3-dev
-	libgig-dev libsoundio-dev libboost-dev"
+	libgig-dev libsoundio-dev"
 
 # Help with unmet dependencies
 PACKAGES="$PACKAGES libjack0"
