LMMS/lmms

commit badeaa4682d14b32d8f9d686a1c375263c009667

Msys2 fixes

cmake/msys/fetch_ppa.sh
@@ -12,7 +12,7 @@ PPA_USER=tobydox
 PPA_PROJECT=mingw-x-trusty
 PPA_ROOT=$PPA_HOST/$PPA_USER/$PPA_PROJECT/ubuntu
 
-PPA_URL=$PPA_ROOT/dists/$PPA_DISTRO/main/binary-$PPA_ARCH/Packages
+PPA_URL=$PPA_ROOT/dists/$PPA_DISTRO/main/binary-$PPA_ARCH/Packages.gz
 
 ppa_dir=./ppa/
 
@@ -23,8 +23,11 @@ skip_files="binutils openssl flac libgig libogg libvorbis x-bootstrap zlib"
 skip_files="$skip_files x-runtime gcc qt_4 qt5 x-stk pkgconfig"
 skip_files="$skip_files glib2 libpng"
 
-echo "Connecting to $PPA_HOST to get list of packages..."
-wget -qO- $PPA_URL |grep "Filename:" > $temp_file
+echo "Connecting to $PPA_URL to get list of packages..."
+wget $PPA_URL -qO ${temp_file}.gz
+gzip -d ${temp_file}.gz
+cat $temp_file |grep "Filename:" > ${temp_file}_stripped
+mv ${temp_file}_stripped $temp_file
 
 for j in $skip_files ; do
 	grep -v "$j" $temp_file > $temp_temp_file
@@ -43,7 +46,7 @@ while read -r j
 do
 	echo "Downloading $j..."
 	echo "$PPA_ROOT/$j"
-	wget -O "$ppa_dir$(basename "$j")" "$(echo "$PPA_ROOT/$j" | sed 's/\/Filename: /\//gi')"
+	wget -qO "$ppa_dir$(basename "$j")" "$(echo "$PPA_ROOT/$j" | sed 's/\/Filename: /\//gi')"
 done < $temp_file
 
 

cmake/msys/extract_debs.sh
@@ -8,7 +8,7 @@ for f in *.deb; do
 	ar xv "$f"
 	rm debian-binary
 	rm control.tar.*
-	tar xf data.tar.*
+	tar xf data.tar.* --exclude=*mingw*/bin/fluid
 	rm data.tar.*
 done
 

cmake/msys/msys_helper.sh
@@ -22,7 +22,8 @@ if ! env | grep MINGW; then
 fi
 
 info "Preparing the git directory..."
-mkdir "$HOME/.git"; touch "$HOME/.git/config" > /dev/null &2>1
+mkdir "$HOME/.git" || true
+touch "$HOME/.git/config" > /dev/null 2>&1
 git config --global http.sslverify false
 
 info "Cloning the repository..."

commit ab7c190d21f41eac8b718cb3d2d3bd5bb0fc0148

Don't use temporary gz file for ppa listing
Fix shellcheck error, introduced by commit badeaa4.

cmake/msys/fetch_ppa.sh
@@ -24,10 +24,7 @@ skip_files="$skip_files x-runtime gcc qt_4 qt5 x-stk pkgconfig"
 skip_files="$skip_files glib2 libpng"
 
 echo "Connecting to $PPA_URL to get list of packages..."
-wget $PPA_URL -qO ${temp_file}.gz
-gzip -d ${temp_file}.gz
-cat $temp_file |grep "Filename:" > ${temp_file}_stripped
-mv ${temp_file}_stripped $temp_file
+wget -qO- $PPA_URL | gzip -d -c | grep "Filename:" > $temp_file
 
 for j in $skip_files ; do
 	grep -v "$j" $temp_file > $temp_temp_file