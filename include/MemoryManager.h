LMMS/lmms

commit 73728d79da858b2ee07db5df4e1bd032bdd89911

Fix C++03 compatibility

include/MemoryManager.h
@@ -118,9 +118,10 @@ struct MmAllocator
 	{
 		MemoryManager::free( p );
 	}
+
+	typedef std::vector<T, MmAllocator<T> > vector;
 };
 
-template <typename T> using MmVector = std::vector<T, MmAllocator<T>>;
 
 #define MM_OPERATORS								\
 public: 											\

commit d1aa39bfd194b0645aeadb647808c7ed15ce8f7e

Fix C++03 compatibility again
Same as 73728d79da858b2ee07db5df4e1bd032bdd89911, accidentally removed
space in aebbe2287e0e581a15173a0496a6e3bc8fcc8000

include/MemoryManager.h
@@ -119,7 +119,7 @@ struct MmAllocator
 		MemoryManager::free( p );
 	}
 
-	typedef std::vector<T, MmAllocator<T>> vector;
+	typedef std::vector< T, MmAllocator<T> > vector;
 };
 
 