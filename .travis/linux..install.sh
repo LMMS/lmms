3e90e3735213f2bc0e151c7886f3519ab7e54804
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2017-10-16 15:09:30 -0400

    Add AppImage support to Linux builds (#3688)
    Add AppImage support

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PACKAGES="cmake libsndfile-dev fftw3-dev libvorbis-dev libogg-dev libmp3lame-dev
-	libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev libstk0-dev
+	libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev libstk0-dev stk
 	libfluidsynth-dev portaudio19-dev wine-dev g++-multilib libfltk1.3-dev
 	libgig-dev libsoundio-dev"
 
