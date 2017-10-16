76ce6dddfabe38c842f11a3a495680afecb767f9
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2017-10-16 13:48:38 -0400

    Switch Travis-CI to Qt5.8 (#3885)

--- .travis/linux..before_install.sh
+++ .travis/linux..before_install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo add-apt-repository ppa:beineri/opt-qt58-trusty -y
 sudo add-apt-repository ppa:andrewrk/libgroove -y
 sudo sed -e "s/trusty/precise/" -i \
 	/etc/apt/sources.list.d/andrewrk-libgroove-trusty.list
