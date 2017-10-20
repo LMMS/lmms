b6f8f129fe6ab3bc4b36347a5d3af6f74230ac3c
Lukas W <lukaswhl@gmail.com>
2017-10-20 10:54:24 +0200

    Revert Travis docker changes

--- .travis/linux.win64.install.sh
+++ .travis/linux.win64.install.sh
@@ -2,3 +2,24 @@
 
 set -e
 
+# First, install 32-bit deps
+"$TRAVIS_BUILD_DIR/.travis/linux.win32.install.sh"
+
+if [ "$QT5" ]; then
+	MINGW_PACKAGES="mingw64-x-qt5base"
+else
+	MINGW_PACKAGES="mingw64-x-qt"
+fi
+
+MINGW_PACKAGES="mingw64-x-sdl mingw64-x-libvorbis mingw64-x-fluidsynth mingw64-x-stk
+	mingw64-x-glib2 mingw64-x-portaudio mingw64-x-libsndfile
+	mingw64-x-fftw mingw64-x-flac mingw64-x-fltk mingw64-x-libsamplerate
+	mingw64-x-pkgconfig mingw64-x-binutils mingw64-x-gcc mingw64-x-runtime
+	mingw64-x-libgig mingw64-x-libsoundio mingw64-x-lame $MINGW_PACKAGES"
+
+export MINGW_PACKAGES
+
+"$TRAVIS_BUILD_DIR/.travis/linux.win.download.sh" win64
+
+# shellcheck disable=SC2086
+sudo apt-get install -y $MINGW_PACKAGES
