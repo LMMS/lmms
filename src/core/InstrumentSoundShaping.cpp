6ad5b0bd2b3d4bf5024acde5f450b6ba0c3e6191
Hyunjin Song <tteu.ingog@gmail.com>
2017-10-23 07:55:40 +0900

    Fix wrong condition

--- src/core/InstrumentSoundShaping.cpp
+++ src/core/InstrumentSoundShaping.cpp
@@ -306,7 +306,7 @@ f_cnt_t InstrumentSoundShaping::releaseFrames() const
 
 	f_cnt_t ret_val = m_instrumentTrack->instrument()->desiredReleaseFrames();
 
-	if( !m_instrumentTrack->instrument()->flags().testFlag( Instrument::IsSingleStreamed ) )
+	if( m_instrumentTrack->instrument()->flags().testFlag( Instrument::IsSingleStreamed ) )
 	{
 		return ret_val;
 	}
