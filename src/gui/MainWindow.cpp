f1f3ea1a33e4610cf17309a53d7d4ffb812691d2
Hussam Eddin Alhomsi <hussameddin.alhomsi@gmail.com>
2017-11-04 18:27:18 +0300

    Reorder the main windows' buttons & shortcuts.

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -520,12 +520,20 @@ void MainWindow::finalize()
 				"in an easy way."
 				) );
 
+	ToolButton * controllers_window = new ToolButton(
+					embed::getIconPixmap( "controller" ),
+					tr( "Show/hide controller rack" ) +
+								" (F9)",
+					this, SLOT( toggleControllerRack() ),
+								m_toolBar );
+	controllers_window->setShortcut( Qt::Key_F9 );
+
 	ToolButton * fx_mixer_window = new ToolButton(
 					embed::getIconPixmap( "fx_mixer" ),
-					tr( "Show/hide FX Mixer" ) + " (F9)",
+					tr( "Show/hide FX Mixer" ) + " (F10)",
 					this, SLOT( toggleFxMixerWin() ),
 					m_toolBar );
-	fx_mixer_window->setShortcut( Qt::Key_F9 );
+	fx_mixer_window->setShortcut( Qt::Key_F10 );
 	fx_mixer_window->setWhatsThis(
 		tr( "Click here to show or hide the "
 			"FX Mixer. The FX Mixer is a very powerful tool "
@@ -535,30 +543,22 @@ void MainWindow::finalize()
 	ToolButton * project_notes_window = new ToolButton(
 					embed::getIconPixmap( "project_notes" ),
 					tr( "Show/hide project notes" ) +
-								" (F10)",
+								" (F11)",
 					this, SLOT( toggleProjectNotesWin() ),
 								m_toolBar );
-	project_notes_window->setShortcut( Qt::Key_F10 );
+	project_notes_window->setShortcut( Qt::Key_F11 );
 	project_notes_window->setWhatsThis(
 		tr( "Click here to show or hide the "
 			"project notes window. In this window you can put "
 			"down your project notes.") );
 
-	ToolButton * controllers_window = new ToolButton(
-					embed::getIconPixmap( "controller" ),
-					tr( "Show/hide controller rack" ) +
-								" (F11)",
-					this, SLOT( toggleControllerRack() ),
-								m_toolBar );
-	controllers_window->setShortcut( Qt::Key_F11 );
-
 	m_toolBarLayout->addWidget( song_editor_window, 1, 1 );
 	m_toolBarLayout->addWidget( bb_editor_window, 1, 2 );
 	m_toolBarLayout->addWidget( piano_roll_window, 1, 3 );
 	m_toolBarLayout->addWidget( automation_editor_window, 1, 4 );
-	m_toolBarLayout->addWidget( fx_mixer_window, 1, 5 );
-	m_toolBarLayout->addWidget( project_notes_window, 1, 6 );
-	m_toolBarLayout->addWidget( controllers_window, 1, 7 );
+	m_toolBarLayout->addWidget( controllers_window, 1, 5 );
+	m_toolBarLayout->addWidget( fx_mixer_window, 1, 6 );
+	m_toolBarLayout->addWidget( project_notes_window, 1, 7 );
 	m_toolBarLayout->setColumnStretch( 100, 1 );
 
 	// setup-dialog opened before?
@@ -1195,18 +1195,17 @@ void MainWindow::updateViewMenu()
 			      this,
 			      SLOT( toggleAutomationEditorWin())
 		);
+	m_viewMenu->addAction(embed::getIconPixmap( "controller" ),
+			      tr( "Controller Rack" ) + " (F9)",
+			      this, SLOT( toggleControllerRack() )
+		);
 	m_viewMenu->addAction(embed::getIconPixmap( "fx_mixer" ),
-			      tr( "FX Mixer" ) + " (F9)",
+			      tr( "FX Mixer" ) + " (F10)",
 			      this, SLOT( toggleFxMixerWin() )
 		);
 	m_viewMenu->addAction(embed::getIconPixmap( "project_notes" ),
-			      tr( "Project Notes" ) +	" (F10)",
+			      tr( "Project Notes" ) + " (F11)",
 			      this, SLOT( toggleProjectNotesWin() )
-		);
-	m_viewMenu->addAction(embed::getIconPixmap( "controller" ),
-			      tr( "Controller Rack" ) +
-			      " (F11)",
-			      this, SLOT( toggleControllerRack() )
 		);
 
 	m_viewMenu->addSeparator();
