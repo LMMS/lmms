d75b449e19f4d794b2421e7314cadcfaebc62f33
Lukas W <lukaswhl@gmail.com>
2017-11-27 21:26:03 +0100

    VST build fixes

--- cmake/modules/FindWine.cmake
+++ cmake/modules/FindWine.cmake
@@ -9,11 +9,12 @@
 
 LIST(APPEND CMAKE_PREFIX_PATH /opt/wine-stable /opt/wine-devel /opt/wine-staging /usr/lib/wine/)
 
-FIND_PATH(WINE_INCLUDE_DIR windows/windows.h PATH_SUFFIXES wine wine/wine)
+FIND_PATH(WINE_INCLUDE_DIR wine/exception.h PATH_SUFFIXES wine)
 FIND_LIBRARY(WINE_LIBRARY NAMES wine PATH_SUFFIXES wine i386-linux-gnu/wine)
 FIND_PROGRAM(WINE_CXX
 	NAMES wineg++ winegcc winegcc64 winegcc32 winegcc-stable
 	PATHS /usr/lib/wine)
+FIND_PROGRAM(WINE_BUILD NAMES winebuild)
 
 SET(WINE_INCLUDE_DIRS ${WINE_INCLUDE_DIR} )
 SET(WINE_LIBRARIES ${WINE_LIBRARY} )
@@ -41,4 +42,12 @@ ENDFOREACH()
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Wine DEFAULT_MSG WINE_CXX WINE_LIBRARIES WINE_INCLUDE_DIRS)
 
-mark_as_advanced(WINE_INCLUDE_DIR WINE_LIBRARY)
+mark_as_advanced(WINE_INCLUDE_DIR WINE_LIBRARY WINE_CXX WINE_BUILD)
+
+IF(WINE_LIBRARY_FIX)
+	SET(WINE_32_FLAGS "-L${WINE_LIBRARY_FIX}wine/ -L${WINE_LIBRARY_FIX}")
+ENDIF()
+
+# Create winegcc wrapper
+configure_file(${CMAKE_CURRENT_LIST_DIR}/../linux/winegcc_wrapper.in winegcc_wrapper @ONLY)
+SET(WINEGCC "${CMAKE_CURRENT_BINARY_DIR}/winegcc_wrapper")
