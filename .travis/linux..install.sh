80c443e07c1148f2c225c32d436435c84d64f96a
Hyunjin Song <tteu.ingog@gmail.com>
2017-12-20 12:54:31 +0900

    Travis: install jack2 in Linux build
    Per #4026: Installing Carla will remove jack1 if it's installed, since Carla requires jack2.

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 PACKAGES="cmake libsndfile-dev fftw3-dev libvorbis-dev libogg-dev libmp3lame-dev
-	libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev libstk0-dev stk
+	libasound2-dev libjack-jackd2-dev libsdl-dev libsamplerate0-dev libstk0-dev stk
 	libfluidsynth-dev portaudio19-dev g++-multilib libfltk1.3-dev
 	libgig-dev libsoundio-dev"
 
 VST_PACKAGES="wine-dev libqt5x11extras5-dev qtbase5-private-dev libxcb-util0-dev libxcb-keysyms1-dev"
 
 # Help with unmet dependencies
-PACKAGES="$PACKAGES $VST_PACKAGES libjack0"
+PACKAGES="$PACKAGES $VST_PACKAGES libjack-jackd2-0"
 
 if [ $QT5 ]; then
 	PACKAGES="$PACKAGES qt59base qt59translations qt59tools"
