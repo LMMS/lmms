LMMS/lmms

commit 76766c4c85e3f3793f604e1c2ffb986fea101ff6

Piano Roll - Don't quantize when pasting notes (#4058)
Some changes on pasting notes in the Piano Roll:
* Don't quantize notes when pasting
* Add journal checkpoint
* Set project changed

src/gui/editors/PianoRoll.cpp
@@ -3763,7 +3763,7 @@ void PianoRoll::pasteNotes()
 			cur_note.setSelected( true );
 
 			// add to pattern
-			m_pattern->addNote( cur_note );
+			m_pattern->addNote( cur_note, false );
 		}
 
 		// we only have to do the following lines if we pasted at
@@ -3926,6 +3926,8 @@ void PianoRoll::quantizeNotes()
 		return;
 	}
 
+	m_pattern->addJournalCheckPoint();
+
 	NoteVector notes = getSelectedNotes();
 
 	if( notes.empty() )
@@ -3951,6 +3953,7 @@ void PianoRoll::quantizeNotes()
 
 	update();
 	gui->songEditor()->update();
+	Engine::getSong()->setModified();
 }
 
 

commit 7de9649b2f2cb3c1bb80398a10fbb0363ae5ab82

Fix quantization of pasted notes Fixes a regression caused by #4058

src/gui/editors/PianoRoll.cpp
@@ -3757,7 +3757,7 @@ void PianoRoll::pasteNotes()
 			// create the note
 			Note cur_note;
 			cur_note.restoreState( list.item( i ).toElement() );
-			cur_note.setPos( cur_note.pos() + m_timeLine->pos() );
+			cur_note.setPos( cur_note.pos() + Note::quantized( m_timeLine->pos(), quantization() ) );
 
 			// select it
 			cur_note.setSelected( true );