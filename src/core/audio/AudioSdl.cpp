c965b1c03fa504bc8b2d4b3cd8ce83bbadb81fca
Shmuel H <shmuelhazan0@gmail.com>
2018-02-24 15:35:51 +0200

    AudioSdl -> Recording - Bugfix: Due to spec.sampleRate being wrong, recording would stop early

--- src/core/audio/AudioSdl.cpp
+++ src/core/audio/AudioSdl.cpp
@@ -100,6 +100,7 @@ AudioSdl::AudioSdl( bool & _success_ful, Mixer*  _mixer ) :
 
 #ifdef LMMS_HAVE_SDL2
 	m_inputAudioHandle = m_audioHandle;
+	m_inputAudioHandle.freq = mixer ()->inputSampleRate ();
 	m_inputAudioHandle.callback = sdlInputAudioCallback;
 
 	m_inputDevice = SDL_OpenAudioDevice (NULL,
@@ -107,7 +108,7 @@ AudioSdl::AudioSdl( bool & _success_ful, Mixer*  _mixer ) :
 										 &m_inputAudioHandle,
 										 &actual,
 										 0);
-	if (m_inputDevice != 0) {
+	if (m_inputDevice != 0 && actual.freq == mixer ()->inputSampleRate ()) {
 		m_supportsCapture = true;
 	} else {
 		m_supportsCapture = false;
