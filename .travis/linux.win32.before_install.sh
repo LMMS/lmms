80ef8e5609198ef73bc478ae9d4a3ac41a2cbc2a
Shmuel H <shmuelhazan0@gmail.com>
2018-02-24 17:46:28 +0200

    .

--- .travis/linux.win32.before_install.sh
+++ .travis/linux.win32.before_install.sh
@@ -3,7 +3,7 @@
 set +e
 
 sudo add-apt-repository ppa:tobydox/mingw-x-trusty -y
-sudo add-apt-repository ppa:tobydox/mingw-w64 -y
+sudo add-apt-repository "deb http://ppa.launchpad.net/tobydox/mingw-w64/ubuntu xenial main"  ppa:tobydox/mingw-w64 -y
 
 
 sudo apt-get update
