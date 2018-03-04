1d63bd3b4e1077200498533e2a4863d1fca3dd4b
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2018-03-03 22:46:07 -0500

    Use Canonical Paths for Relative Paths Calculations (#4211)
    Fix redundant path elements
    Closes #4173

--- src/core/SampleBuffer.cpp
+++ src/core/SampleBuffer.cpp
@@ -1415,7 +1415,8 @@ QString SampleBuffer::tryToMakeRelative( const QString & file )
 {
 	if( QFileInfo( file ).isRelative() == false )
 	{
-		QString f = QString( file ).replace( QDir::separator(), '/' );
+		// Normalize the path
+		QString f = QFileInfo( file ).canonicalFilePath().replace( QDir::separator(), '/' );
 
 		// First, look in factory samples
 		// Isolate "samples/" from "data:/samples/"
