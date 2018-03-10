d132df4cab9887019f6ab880242127853726d675
Colin Wallace <colin@mooooo.ooo>
2018-03-09 23:48:07 -0800

    Fix missing includes

--- include/stdshims.h
+++ include/stdshims.h
@@ -3,6 +3,9 @@
 
 #pragma once
 
+#include <memory>
+#include <utility>
+
 /// Shim for http://en.cppreference.com/w/cpp/memory/unique_ptr/make_unique
 template<typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&&... args)
