32a7773a7a2c5c883182f98548067a9ce0fe4ce8
tresf <tres.finocchiaro@gmail.com>
2018-03-12 17:29:35 -0400

    Fix toFloat Qt5 regression Closes #4241

--- plugins/zynaddsubfx/zynaddsubfx/src/Misc/QtXmlWrapper.cpp
+++ plugins/zynaddsubfx/zynaddsubfx/src/Misc/QtXmlWrapper.cpp
@@ -505,7 +505,7 @@ float QtXmlWrapper::getparreal(const char *name, float defaultpar) const
 		return defaultpar;
 	}
 
-    return tmp.attribute( "value" ).toFloat();
+	return QLocale().toFloat( tmp.attribute( "value" ) );
 }
 
 float QtXmlWrapper::getparreal(const char *name,
