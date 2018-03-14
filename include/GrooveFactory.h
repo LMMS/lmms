d6529ae1e8dc91f3a8b5e792f85fc3644ed6c1dd
Hyunin Song <tteu.ingog@gmail.com>
2018-03-15 00:01:58 +0900

    Update old homepage URL and program name

--- include/GrooveFactory.h
+++ include/GrooveFactory.h
@@ -6,7 +6,7 @@
  *
  * Copyright (c) 2005-2008 teknopaul <teknopaul/at/users.sourceforge.net>
  *
- * This file is part of Linux MultiMedia Studio - http://lmms.sourceforge.net
+ * This file is part of LMMS - https://lmms.io
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
--- src/core/GrooveExperiments.cpp
+++ src/core/GrooveExperiments.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2004-2014 teknopaul <teknopaul/at/users.sourceforge.net>
  *
- * This file is part of LMMS - http://lmms.io
+ * This file is part of LMMS - https://lmms.io
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
