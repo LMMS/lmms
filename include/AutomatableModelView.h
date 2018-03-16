85f58f58f6b86cc2544b885462100b2a7d477567
Colin Wallace <colin@mooooo.ooo>
2018-03-15 18:45:13 -0700

    Copy/paste model values to system clipboard
    Previously they were copy/pasted internally, and not visible across LMMS
    instances.

--- include/AutomatableModelView.h
+++ include/AutomatableModelView.h
@@ -75,7 +75,6 @@ protected:
 
 	QString m_description;
 	QString m_unit;
-
 } ;
 
 
@@ -94,6 +93,11 @@ public slots:
 	void unlinkAllModels();
 	void removeSongGlobalAutomation();
 
+private slots:
+	/// Copy the model's value to the clipboard.
+	void copyToClipboard();
+	/// Paste the model's value from the clipboard.
+	void pasteFromClipboard();
 
 protected:
 	AutomatableModelView* m_amv;
