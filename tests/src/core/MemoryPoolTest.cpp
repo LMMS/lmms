b5f343081ab58c4e9800d285170da6f73c72736b
Lukas W <lukaswhl@gmail.com>
2018-04-17 16:30:06 +0200

    Replace LocklessAllocator with new MemoryPool class
    MemoryPool maintains a free list of pre-allocated entries stored in a
    libcds MPMC Queue. In contrast to LocklessAllocator, it's a lot faster,
    less (and less complex) code to maintain for us and it doesn't fail when
    it's full.

---
+++ tests/src/core/MemoryPoolTest.cpp
@@ -0,0 +1,58 @@
+/*
+ * MemoryPoolTest.cpp
+ *
+ * Copyright (c) 2018 Lukas W <lukaswhl/at/gmail.com>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include "QTestSuite.h"
+
+#include "MemoryPool.h"
+
+#include <array>
+#include <stack>
+
+class MemoryPoolTest : QTestSuite
+{
+	Q_OBJECT
+private slots:
+	void MemoryPoolTests()
+	{
+		using T = std::array<char, 16>;
+		MemoryPool<T> pool(256);
+
+		std::stack<T*> ptrs;
+
+		for (int i=0; i < 256; i++) {
+			ptrs.push(pool.allocate_bounded());
+			QVERIFY(ptrs.top());
+		}
+		QCOMPARE(pool.allocate_bounded(), nullptr);
+		ptrs.push(pool.allocate());
+		QVERIFY(ptrs.top());
+
+		while (!ptrs.empty()) {
+			pool.deallocate(ptrs.top());
+			ptrs.pop();
+		}
+	}
+} MemoryPoolTests;
+
+#include "MemoryPoolTest.moc"
