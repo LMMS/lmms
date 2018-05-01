LMMS/lmms

commit 389b60e030f9c46ae08666b12b070cf9bdc7f222

Fix `changeLang` connection

src/gui/SetupDialog.cpp
@@ -314,7 +314,7 @@ SetupDialog::SetupDialog(ConfigTabs tab_to_open) :
 	}
 	connect(changeLang, SIGNAL(currentIndexChanged(int)),
 			this, SLOT(setLanguage(int)));
-	connect(changeLang, SIGNAL(toggled(bool)),
+	connect(changeLang, SIGNAL(currentIndexChanged(int)),
 			this, SLOT(toggleRestartWarning()));
 
 	// If language unset, fallback to system language when available.

commit ca7dbbb9a1827ae6b640e67c4f8372019f7658a7

Fix crash introduced by 389b60e

src/gui/SetupDialog.cpp
@@ -312,10 +312,6 @@ SetupDialog::SetupDialog(ConfigTabs tab_to_open) :
 		QString lang = QLocale(m_languages.last()).nativeLanguageName();
 		changeLang->addItem(lang);
 	}
-	connect(changeLang, SIGNAL(currentIndexChanged(int)),
-			this, SLOT(setLanguage(int)));
-	connect(changeLang, SIGNAL(currentIndexChanged(int)),
-			this, SLOT(toggleRestartWarning()));
 
 	// If language unset, fallback to system language when available.
 	if(m_lang == "")
@@ -339,6 +335,11 @@ SetupDialog::SetupDialog(ConfigTabs tab_to_open) :
 			break;
 		}
 	}
+
+	connect(changeLang, SIGNAL(currentIndexChanged(int)),
+			this, SLOT(setLanguage(int)));
+	connect(changeLang, SIGNAL(currentIndexChanged(int)),
+			this, SLOT(toggleRestartWarning()));
 
 
 	// General layout ordering.