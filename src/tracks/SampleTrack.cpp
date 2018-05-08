e79c3af2cf753981405eed560a1eefd30656740a
Shmuel H <shmuelhazan0@gmail.com>
2018-05-09 00:23:45 +0300

    SampleTCO -> recording: Export is_record attribute for future loading.

--- src/tracks/SampleTrack.cpp
+++ src/tracks/SampleTrack.cpp
@@ -257,6 +257,7 @@ void SampleTCO::saveSettings( QDomDocument & _doc, QDomElement & _this )
 	}
 
 	_this.setAttribute ("sample_rate", m_sampleBuffer->sampleRate ());
+	_this.setAttribute ("is_record", isRecord ());
 	// TODO: start- and end-frame
 }
 
@@ -280,6 +281,10 @@ void SampleTCO::loadSettings( const QDomElement & _this )
 
 	if (_this.hasAttribute ("sample_rate")) {
 		m_sampleBuffer->setSampleRate (_this.attribute ("sample_rate").toInt ());
+	}
+
+	if (_this.hasAttribute ("is_record")) {
+		setRecord (_this.attribute ("is_record").toInt ());
 	}
 }
 
