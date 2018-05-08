ee5981d293bc7705df8ab5a4a8140ceb445012c1
Shmuel H <shmuelhazan0@gmail.com>
2018-05-09 00:22:04 +0300

    Recording: Don't apply masterGain for input frames.

--- include/Mixer.h
+++ include/Mixer.h
@@ -290,7 +290,7 @@ public:
 		return m_fifoWriter != NULL;
 	}
 
-	void pushInputFrames(const sampleFrame *_ab, const f_cnt_t _frames , bool shouldApplyMasterGain=false);
+	void pushInputFrames(const sampleFrame *_ab, const f_cnt_t _frames);
 
 	inline const sampleFrame * inputBuffer()
 	{
@@ -311,9 +311,6 @@ public:
 
 	inline bool isMetronomeActive() const { return m_metronomeActive; }
 	inline void setMetronomeActive(bool value = true) { m_metronomeActive = value; }
-
-	void applyMasterGainToInputBuffer (sampleFrame	*frames_data,
-								  const size_t frames_count, uint channels_count, float gain);
 
 	void requestChangeInModel();
 	void doneChangeInModel();
