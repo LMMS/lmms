LMMS/lmms

commit fd59a0e20058e3e6ae5bd644a4d250a158d32638

SampleBuffer: Remove the god function "update" and split it into a number of functions: - beginBufferChange, doneBufferChange: Guards for direct access to m_data. They will also resample the data if it is needed. Functions like `addData` will check additional parameter to know if we should lock the mixer in this context. - Add reverse and remove the attribute m_reversed: The only user was AudioFileProcessor and it added complexity for the file loading process. Right now we'll just `reverse` the buffer manually. - changeAudioFile: Will reset the current buffer and load a file into it. - Remove the raw constructor `SampleBuffer(sampleFrame*, f_cnt_t)`: It was used only once and could easily be converted to another constructor.

include/SampleBuffer.h
@@ -111,9 +111,7 @@ public:
 	// constructor which either loads sample _audio_file or decodes
 	// base64-data out of string
 	SampleBuffer( const QString & _audio_file, bool _is_base64_data = false );
-	SampleBuffer( const sampleFrame * _data, const f_cnt_t _frames );
-	explicit SampleBuffer( const f_cnt_t _frames );
-	SampleBuffer(SampleBuffer::DataVector &&movedData);
+	SampleBuffer(DataVector &&movedData, sample_rate_t sampleRate);
 
 	bool play( sampleFrame * _ab, handleState * _state,
 				const fpp_t _frames,
@@ -179,11 +177,6 @@ public:
 		return m_amplification;
 	}
 
-	inline bool reversed() const
-	{
-		return m_reversed;
-	}
-
 	inline float frequency() const
 	{
 		return m_frequency;
@@ -219,12 +212,6 @@ public:
 	QString openAndSetWaveformFile();
 
 	QString & toBase64( QString & _dst ) const;
-
-
-	// protect calls from the GUI to this function with dataReadLock() and
-	// dataUnlock()
-	SampleBuffer resample( const sample_rate_t _src_sr,
-						const sample_rate_t _dst_sr );
 
 	void normalizeSampleRate( const sample_rate_t _src_sr,
 						bool _keep_settings = false );
@@ -265,28 +252,42 @@ public:
 
 	/**
 	 * @brief Add data to the buffer,
-	 * @param begin	Begining of an InputIterator.
+	 * @param begin	Beginning of an InputIterator.
 	 * @param end	End of an InputIterator.
+	 * @param shouldLockMixer	Should we call requestChangeInModel?
 	 *
 	 * @warning That locks m_varLock for write.
 	 */
-	void addData(const DataVector::iterator begin, const DataVector::iterator end);
+	void addData(const DataVector &vector, sample_rate_t sampleRate, bool shouldLockMixer=true);
 
-	void resetData(DataVector &&newData);
+	/**
+	 * @brief Reset the class and initialize it with @a newData.
+	 * @param newData	mm, that's the new data.
+	 * @param dataSampleRate	Sample rate for @a newData.
+	 * @param shouldLockMixer	Should we call requestChangeInModel?
+	 */
+	void resetData(DataVector &&newData, sample_rate_t dataSampleRate, bool shouldLockMixer=true);
+
+	/**
+	 * @brief Just reverse the current buffer.
+	 * @param shouldLockMixer	Should we call requestChangeInModel?
+	 *
+	 * This function simply calls `std::reverse` on m_data.
+	 */
+	void reverse(bool shouldLockMixer=true);
 
 public slots:
 	void setAudioFile( const QString & _audio_file );
-	void loadFromBase64( const QString & _data );
+	void loadFromBase64(const QString & _data , bool shouldLock);
 	void setStartFrame( const f_cnt_t _s );
 	void setEndFrame( const f_cnt_t _e );
 	void setAmplification( float _a );
-	void setReversed( bool _on );
 	void sampleRateChanged();
 
-private:
+protected:
 	// HACK: libsamplerate < 0.1.8 doesn't get read-only variables
 	//	     as const. It has been fixed in 0.1.9 but has not been
-	//		 shipped for some destributions.
+	//		 shipped for some distributions.
 	//		 This function just returns a variable that should have
 	//		 been `const` as non-const to bypass using 0.1.9.
 	inline static sampleFrame * libSampleRateSrc (const sampleFrame *ptr)
@@ -294,22 +295,17 @@ private:
 		return const_cast<sampleFrame*>(ptr);
 	}
 
-	void update( bool _keep_settings = false );
+	void changeAudioFile (QString audioFile, bool shouldLock, bool shouldKeepSettings);
 
-	void convertIntToFloat ( int_sample_t * & _ibuf, f_cnt_t _frames, int _channels);
-	void directFloatWrite ( sample_t * & _fbuf, f_cnt_t _frames, int _channels);
+	static DataVector convertIntToFloat(int_sample_t * & _ibuf, f_cnt_t _frames, int _channels);
 
-	f_cnt_t decodeSampleSF( const char * _f, sample_t * & _buf,
-						ch_cnt_t & _channels,
-						sample_rate_t & _sample_rate );
+	static DataVector decodeSampleSF( const char * _f, ch_cnt_t & _channels, sample_rate_t & _sample_rate);
 #ifdef LMMS_HAVE_OGGVORBIS
-	f_cnt_t decodeSampleOGGVorbis( const char * _f, int_sample_t * & _buf,
+	static DataVector decodeSampleOGGVorbis( const char * _f,
 						ch_cnt_t & _channels,
-						sample_rate_t & _sample_rate );
+						sample_rate_t & _sample_rate);
 #endif
-	f_cnt_t decodeSampleDS( const char * _f, int_sample_t * & _buf,
-						ch_cnt_t & _channels,
-						sample_rate_t & _sample_rate );
+	static DataVector decodeSampleDS( const char * _f, ch_cnt_t & _channels, sample_rate_t & _sample_rate);
 
 	inline sampleFrame * data()
 	{
@@ -317,7 +313,6 @@ private:
 	}
 
 	QString m_audioFile;
-	DataVector m_origData;
 	DataVector m_data;
 	QReadWriteLock m_varLock;
 	f_cnt_t m_startFrame;
@@ -325,7 +320,6 @@ private:
 	f_cnt_t m_loopStartFrame;
 	f_cnt_t m_loopEndFrame;
 	float m_amplification;
-	bool m_reversed;
 	float m_frequency;
 	sample_rate_t m_sampleRate;
 
@@ -339,6 +333,25 @@ private:
 	f_cnt_t getPingPongIndex( f_cnt_t _index, f_cnt_t _startf, f_cnt_t _endf  ) const;
 
 
+	static DataVector resampleData(const DataVector &inputData, sample_rate_t inputSampleRate, sample_rate_t requiredSampleRate);
+
+	/**
+	 * @brief Do the actions necessary before changing m_data.
+	 * @param shouldLock	Is anyone else might be using m_data?
+	 */
+	void beginBufferChange (bool shouldLock, bool shouldLockMixer=true);
+
+	/**
+	 * @brief Do some actions necessary after changing m_data.
+	 * @param shoguldLock	The same value you've used on @a beginBufferChange.
+	 * @param shouldKeepSettings	Should we keep playback settings?
+	 * @param bufferSampleRate		The new m_data's sample rate.
+	 * @param shouldNotNotify		Should we not notify anyone about this change?
+	 */
+	void doneBufferChange (bool shouldLock,
+						   bool shouldKeepSettings,
+						   sample_rate_t bufferSampleRate,
+						   bool shouldLockMixer=true);
 signals:
 	void sampleUpdated();
 

plugins/audio_file_processor/audio_file_processor.cpp
@@ -247,7 +247,7 @@ void audioFileProcessor::loadSettings( const QDomElement & _this )
 	}
 	else if( _this.attribute( "sampledata" ) != "" )
 	{
-		m_sampleBuffer.loadFromBase64( _this.attribute( "srcdata" ) );
+		m_sampleBuffer.loadFromBase64( _this.attribute( "srcdata" ) , true);
 	}
 
 	m_loopModel.loadSettings( _this, "looped" );
@@ -267,6 +267,8 @@ void audioFileProcessor::loadSettings( const QDomElement & _this )
 	}
 
 	m_reverseModel.loadSettings( _this, "reversed" );
+	// The current state of m_sampleBuffer.
+	m_isCurrentlyReversed = m_reverseModel.value ();
 
 	m_stutterModel.loadSettings( _this, "stutter" );
 	if( _this.hasAttribute( "interp" ) )
@@ -343,7 +345,12 @@ void audioFileProcessor::setAudioFile( const QString & _audio_file,
 
 void audioFileProcessor::reverseModelChanged( void )
 {
-	m_sampleBuffer.setReversed( m_reverseModel.value() );
+	if (m_reverseModel.value () != m_isCurrentlyReversed) {
+		m_isCurrentlyReversed = m_reverseModel.value ();
+
+		m_sampleBuffer.reverse (true);
+	}
+
 	m_nextPlayStartPoint = m_sampleBuffer.startFrame();
 	m_nextPlayBackwards = false;
 }
@@ -784,7 +791,6 @@ AudioFileProcessorWaveView::AudioFileProcessorWaveView( QWidget * _parent, int _
 	m_endKnob( 0 ),
 	m_loopKnob( 0 ),
 	m_isDragging( false ),
-	m_reversed( false ),
 	m_framesPlayed( 0 ),
 	m_animation(ConfigManager::inst()->value("ui", "animateafp").toInt())
 {
@@ -1063,11 +1069,7 @@ void AudioFileProcessorWaveView::updateGraph()
 		m_to = m_sampleBuffer.endFrame();
 	}
 
-	if( m_sampleBuffer.reversed() != m_reversed )
-	{
-		reverse();
-	}
-	else if( m_last_from == m_from && m_last_to == m_to && m_sampleBuffer.amplification() == m_last_amp )
+	if( m_last_from == m_from && m_last_to == m_to && m_sampleBuffer.amplification() == m_last_amp )
 	{
 		return;
 	}
@@ -1239,24 +1241,6 @@ void AudioFileProcessorWaveView::slideSampleByFrames( f_cnt_t _frames )
 	if( m_loopKnob ) {
 		m_loopKnob->slideBy( v, false );
 	}
-}
-
-
-
-
-void AudioFileProcessorWaveView::reverse()
-{
-	slideSampleByFrames(
-		m_sampleBuffer.frames()
-			- m_sampleBuffer.endFrame()
-			- m_sampleBuffer.startFrame()
-	);
-
-	const f_cnt_t from = m_from;
-	m_from = m_sampleBuffer.frames() - m_to;
-	m_to = m_sampleBuffer.frames() - from;
-
-	m_reversed = ! m_reversed;
 }
 
 

plugins/audio_file_processor/audio_file_processor.h
@@ -102,6 +102,15 @@ private:
 	f_cnt_t m_nextPlayStartPoint;
 	bool m_nextPlayBackwards;
 
+	/**
+	 * @brief Is currently m_sampleBuffer reversed?
+	 *
+	 * Since m_sampleBuffer has no longer the `reversed`
+	 * attribute, we track it here; m_reverseModel is
+	 * only what the user want it to be.
+	 */
+	bool m_isCurrentlyReversed = false;
+
 	friend class AudioFileProcessorView;
 
 } ;

plugins/patman/patman.cpp
@@ -346,7 +346,7 @@ patmanInstrument::LoadErrors patmanInstrument::loadPatch(
 			}
 		}
 
-		sampleFrame * data = new sampleFrame[frames];
+		SampleBuffer::DataVector data(frames);
 
 		for( f_cnt_t frame = 0; frame < frames; ++frame )
 		{
@@ -357,9 +357,8 @@ patmanInstrument::LoadErrors patmanInstrument::loadPatch(
 			}
 		}
 
-		SampleBuffer* psample = new SampleBuffer( data, frames );
+		SampleBuffer* psample = new SampleBuffer( std::move(data), sample_rate );
 		psample->setFrequency( root_freq / 1000.0f );
-		psample->setSampleRate( sample_rate );
 
 		if( modes & MODES_LOOPING )
 		{
@@ -370,7 +369,6 @@ patmanInstrument::LoadErrors patmanInstrument::loadPatch(
 		m_patchSamples.push_back( psample );
 
 		delete[] wave_samples;
-		delete[] data;
 	}
 	fclose( fd );
 	return( LoadOK );

src/core/SampleRecordHandle.cpp
@@ -51,7 +51,7 @@ SampleRecordHandle::~SampleRecordHandle()
 {
 	m_tco->updateLength ();
 
-	// If this is an automaticlly created tco,
+	// If this is an automatically created tco,
 	// enable resizing.
 	m_tco->setAutoResize (false);
 	m_tco->setRecord( false );
@@ -70,13 +70,15 @@ void SampleRecordHandle::play( sampleFrame * /*_working_buffer*/ )
 	// It is the first buffer.
 	if (m_framesRecorded == 0) {
 		// Make sure we don't have the previous data.
-		m_tco->sampleBuffer ()->resetData(std::move (m_currentBuffer));
+		m_tco->sampleBuffer ()->resetData (std::move (m_currentBuffer),
+										   Engine::mixer ()->inputSampleRate (),
+										   false);
 		m_tco->setStartTimeOffset (m_startRecordTimeOffset);
-
-		m_tco->sampleBuffer ()->setSampleRate( Engine::mixer()->inputSampleRate() );
 	} else {
 		if (! m_currentBuffer.empty ()) {
-			m_tco->sampleBuffer ()->addData (m_currentBuffer.begin (), m_currentBuffer.end ());
+			m_tco->sampleBuffer ()->addData (m_currentBuffer,
+											 Engine::mixer ()->inputSampleRate (),
+											 false);
 		}
 	}
 

src/tracks/SampleTrack.cpp
@@ -273,7 +273,7 @@ void SampleTCO::loadSettings( const QDomElement & _this )
 	setSampleFile( _this.attribute( "src" ) );
 	if( sampleFile().isEmpty() && _this.hasAttribute( "data" ) )
 	{
-		m_sampleBuffer->loadFromBase64( _this.attribute( "data" ) );
+		m_sampleBuffer->loadFromBase64( _this.attribute( "data" ), true);
 	}
 	changeLength( _this.attribute( "len" ).toInt() );
 	setMuted( _this.attribute( "muted" ).toInt() );
@@ -403,7 +403,7 @@ void SampleTCOView::dropEvent( QDropEvent * _de )
 	else if( StringPairDrag::decodeKey( _de ) == "sampledata" )
 	{
 		m_tco->m_sampleBuffer->loadFromBase64(
-					StringPairDrag::decodeValue( _de ) );
+					StringPairDrag::decodeValue( _de ) , true);
 		m_tco->updateLength();
 		update();
 		_de->accept();

commit 7ad8c62888c4bc03756e9bef0a821a8060c5b66e

SampleBuffer: Fix a few issues caused by fd59a0e: 1. Minor naming and comment issues. 2. minor inconsistency in addData. 3. calling resampleData with the wrong arguments.

include/SampleBuffer.h
@@ -343,15 +343,14 @@ protected:
 
 	/**
 	 * @brief Do some actions necessary after changing m_data.
-	 * @param shoguldLock	The same value you've used on @a beginBufferChange.
+	 * @param shouldUnlock	The same value you've used on @a beginBufferChange.
 	 * @param shouldKeepSettings	Should we keep playback settings?
 	 * @param bufferSampleRate		The new m_data's sample rate.
-	 * @param shouldNotNotify		Should we not notify anyone about this change?
 	 */
-	void doneBufferChange (bool shouldLock,
+	void doneBufferChange (bool shouldUnlock,
 						   bool shouldKeepSettings,
 						   sample_rate_t bufferSampleRate,
-						   bool shouldLockMixer=true);
+						   bool shouldUnlockMixer=true);
 signals:
 	void sampleUpdated();
 

src/core/SampleBuffer.cpp
@@ -246,7 +246,7 @@ void SampleBuffer::normalizeSampleRate( const sample_rate_t _src_sr,
 	// do samplerate-conversion to our default-samplerate
 	if( _src_sr != sampleRate ())
 	{
-		m_data = resampleData (m_data, sampleRate (), sampleRate ());
+		m_data = resampleData (m_data, _src_sr, sampleRate ());
 	}
 
 	if( _keep_settings == false )
@@ -1118,10 +1118,10 @@ void SampleBuffer::beginBufferChange(bool shouldLock, bool shouldLockMixer)
 	}
 }
 
-void SampleBuffer::doneBufferChange(bool shouldLock,
+void SampleBuffer::doneBufferChange(bool shouldUnlock,
 									bool shouldKeepSettings,
 									sample_rate_t bufferSampleRate,
-									bool shouldLockMixer) {
+									bool shouldUnlockMixer) {
 
 	normalizeSampleRate (bufferSampleRate, shouldKeepSettings);
 
@@ -1135,11 +1135,11 @@ void SampleBuffer::doneBufferChange(bool shouldLock,
 		m_data.resize (previousFrames);
 	}
 
-	if (shouldLock) {
+	if (shouldUnlock) {
 		m_varLock.unlock ();
 	}
 
-	if (shouldLockMixer) {
+	if (shouldUnlockMixer) {
 		Engine::mixer ()->doneChangeInModel ();
 	}
 
@@ -1164,8 +1164,8 @@ void SampleBuffer::addData(const SampleBuffer::DataVector &vector, sample_rate_t
 			m_data.insert (m_data.end (), newVector.cbegin (), newVector.cend ());
 		} else {
 			// Insert to the end of the vector.
-			m_data.reserve (m_data.size () + (newVector.size ()));
-			m_data.insert (m_data.end (), vector.begin (), vector.end ());
+			m_data.reserve (m_data.size () + (vector.size ()));
+			m_data.insert (m_data.end (), vector.cbegin (), vector.cend ());
 		}
 	}
 	doneBufferChange (true, /* lock */