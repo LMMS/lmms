fe1645172ea670620474dbfcf69d4e44f68b7214
justnope <dont@email.invalid>
2018-05-20 23:31:35 +0200

    Added support portaudio
    todo: volume too loud? clipping

--- cmake/modules/FindPortaudio.cmake
+++ cmake/modules/FindPortaudio.cmake
@@ -13,24 +13,31 @@
 #
 
 
-if (PORTAUDIO_LIBRARIES AND PORTAUDIO_INCLUDE_DIRS)
-  # in cache already
-  set(PORTAUDIO_FOUND TRUE)
-else (PORTAUDIO_LIBRARIES AND PORTAUDIO_INCLUDE_DIRS)
-   include(FindPkgConfig)
-   pkg_check_modules(PORTAUDIO portaudio-2.0)
-  if (PORTAUDIO_FOUND)
-    if (NOT Portaudio_FIND_QUIETLY)
-      message(STATUS "Found Portaudio: ${PORTAUDIO_LIBRARIES}")
-    endif (NOT Portaudio_FIND_QUIETLY)
-  else (PORTAUDIO_FOUND)
-    if (Portaudio_FIND_REQUIRED)
-      message(FATAL_ERROR "Could not find Portaudio")
-    endif (Portaudio_FIND_REQUIRED)
-  endif (PORTAUDIO_FOUND)
+IF(PORTAUDIO_LIBRARIES AND PORTAUDIO_INCLUDE_DIRS)
+	# in cache already
+	set(PORTAUDIO_FOUND TRUE)
+ELSE(PORTAUDIO_LIBRARIES AND PORTAUDIO_INCLUDE_DIRS)
+	IF(USING_VCPKG)
+		FIND_FILE(PORTAUDIO_INCLUDE_DIRS "portaudio.h")
+		FIND_LIBRARY(PORTAUDIO_LIBRARIES "portaudio.lib")
+		IF(PORTAUDIO_INCLUDE_DIRS)
+			SET(PORTAUDIO_FOUND TRUE)
+		ENDIF(PORTAUDIO_INCLUDE_DIRS)
+	ELSE(USING_VCPKG)
+		include(FindPkgConfig)
+		pkg_check_modules(PORTAUDIO portaudio-2.0)
+		if (PORTAUDIO_FOUND)
+		if (NOT Portaudio_FIND_QUIETLY)
+			message(STATUS "Found Portaudio: ${PORTAUDIO_LIBRARIES}")
+		endif (NOT Portaudio_FIND_QUIETLY)
+		else (PORTAUDIO_FOUND)
+		if (Portaudio_FIND_REQUIRED)
+			message(FATAL_ERROR "Could not find Portaudio")
+		endif (Portaudio_FIND_REQUIRED)
+		endif (PORTAUDIO_FOUND)
 
-  # show the PORTAUDIO_INCLUDE_DIRS and PORTAUDIO_LIBRARIES variables only in the advanced view
-  mark_as_advanced(PORTAUDIO_INCLUDE_DIRS PORTAUDIO_LIBRARIES)
-
-endif (PORTAUDIO_LIBRARIES AND PORTAUDIO_INCLUDE_DIRS)
+		# show the PORTAUDIO_INCLUDE_DIRS and PORTAUDIO_LIBRARIES variables only in the advanced view
+		mark_as_advanced(PORTAUDIO_INCLUDE_DIRS PORTAUDIO_LIBRARIES)
+	ENDIF(USING_VCPKG)
+ENDIF(PORTAUDIO_LIBRARIES AND PORTAUDIO_INCLUDE_DIRS)
 
