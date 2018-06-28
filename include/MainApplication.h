e9f2b57107a5e4fed9e61d80dac87747b2adfbdd
Hyunin Song <tteu.ingog@gmail.com>
2018-06-28 20:23:53 +0900

    Fix project URL for some files
    They were created before https://github.com/LMMS/lmms/pull/3326
    and added after it.

--- include/MainApplication.h
+++ include/MainApplication.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2017-2017 Tres Finocchiaro <tres.finocchiaro/at/gmail.com>
  *
- * This file is part of LMMS - http://lmms.io
+ * This file is part of LMMS - https://lmms.io
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
