e79aeb711d1756690ff7ceba4d1ff29e0578c227
Hyunin Song <tteu.ingog@gmail.com>
2018-07-05 00:26:39 +0900

    Fixup wrong changes

--- plugins/VstEffect/VstEffectControls.cpp
+++ plugins/VstEffect/VstEffectControls.cpp
@@ -62,7 +62,7 @@ void VstEffectControls::loadSettings( const QDomElement & _this )
 {
 	//m_effect->closePlugin();
 	//m_effect->openPlugin( _this.attribute( "plugin" ) );
-	if (!m_effect->m_pluginMutex.tryLock()) {return;}
+	m_effect->m_pluginMutex.lock();
 	if( m_effect->m_plugin != NULL )
 	{
 		m_vstGuiVisible = _this.attribute( "guivisible" ).toInt();
