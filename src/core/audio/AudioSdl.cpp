dac89ed759a9bc4c4846a44d0a4652c6c32b672e
Hyunjin Song <tteu.ingog@gmail.com>
2018-07-10 11:13:56 +0900

    Fix occasional audio interface deadlock (#4450)

--- src/core/audio/AudioSdl.cpp
+++ src/core/audio/AudioSdl.cpp
@@ -168,6 +168,7 @@ void AudioSdl::sdlAudioCallback( Uint8 * _buf, int _len )
 			const fpp_t frames = getNextBuffer( m_outBuf );
 			if( !frames )
 			{
+				m_stopped = true;
 				memset( _buf, 0, _len );
 				return;
 			}
