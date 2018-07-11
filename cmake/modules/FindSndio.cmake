LMMS/lmms

commit c45a21aac3beb0bba4c1796cf591ea44d2f2bc46

CMake: Fix FindSndio module
Fixes a bug where the module would set SNDIO_LIBRARY to NOT-FOUND
resulting in a CMake error when trying to link to ${SNDIO_LIBRARY}.

cmake/modules/FindSndio.cmake
@@ -19,14 +19,16 @@ if(SNDIO_LIBRARY)
    get_filename_component(SNDIO_LIBRARY_DIR ${SNDIO_LIBRARY} PATH)
 endif(SNDIO_LIBRARY)
 
-check_library_exists(sndio sio_open "${SNDIO_LIBRARY_DIR}" HAVE_SNDIO)
-if(HAVE_SNDIO)
-    message(STATUS "Found sndio: ${SNDIO_LIBRARY}")
-else(HAVE_SNDIO)
-    message(STATUS "sndio not found")
+check_library_exists(sndio sio_open "${SNDIO_LIBRARY_DIR}" HAVE_SIO_OPEN)
+
+find_path(SNDIO_INCLUDE_DIR sndio.h)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SNDIO DEFAULT_MSG SNDIO_LIBRARY SNDIO_INCLUDE_DIR HAVE_SIO_OPEN)
+
+if(SNDIO_FOUND)
+    set(SNDIO_INCLUDE_DIRS "${SNDIO_INCLUDE_DIR}")
+    set(SNDIO_LIBRARIES "${SNDIO_LIBRARY}")
 endif(HAVE_SNDIO)
-set(SNDIO_FOUND ${HAVE_SNDIO})
 
-find_path(SNDIO_INCLUDES sndio.h)
-
-mark_as_advanced(SNDIO_INCLUDES SNDIO_LIBRARY)
+mark_as_advanced(SNDIO_INCLUDE_DIR SNDIO_LIBRARY SNDIO_INCLUDE_DIRS SNDIO_LIBRARIES)

src/CMakeLists.txt
@@ -138,7 +138,7 @@ SET(LMMS_REQUIRED_LIBS ${LMMS_REQUIRED_LIBS}
 	${SDL_LIBRARY}
 	${PORTAUDIO_LIBRARIES}
 	${SOUNDIO_LIBRARY}
-	${SNDIO_LIBRARY}
+	${SNDIO_LIBRARIES}
 	${PULSEAUDIO_LIBRARIES}
 	${JACK_LIBRARIES}
 	${OGGVORBIS_LIBRARIES}

commit 3a7b5207b458f2ba4e146a23e1eef00d15e576ff

CMake: Fix mismatching if condition error
Regression from c45a21aac3beb0bba4c1796cf591ea44d2f2bc46

cmake/modules/FindSndio.cmake
@@ -29,6 +29,6 @@ find_package_handle_standard_args(SNDIO DEFAULT_MSG SNDIO_LIBRARY SNDIO_INCLUDE_
 if(SNDIO_FOUND)
     set(SNDIO_INCLUDE_DIRS "${SNDIO_INCLUDE_DIR}")
     set(SNDIO_LIBRARIES "${SNDIO_LIBRARY}")
-endif(HAVE_SNDIO)
+endif()
 
 mark_as_advanced(SNDIO_INCLUDE_DIR SNDIO_LIBRARY SNDIO_INCLUDE_DIRS SNDIO_LIBRARIES)