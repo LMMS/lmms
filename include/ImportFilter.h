LMMS/lmms

commit 2dadb15cf31c4ccefa2e06f698e74e672621c4d8

Fix MIDI import encoding issue on Windows

include/ImportFilter.h
@@ -78,6 +78,11 @@ protected:
 		return m_file.read( _data, _len );
 	}
 
+	inline QByteArray readAllData()
+	{
+		return m_file.readAll();
+	}
+
 	inline void ungetChar( char _ch )
 	{
 		m_file.ungetChar( _ch );

plugins/MidiImport/MidiImport.cpp
@@ -26,8 +26,11 @@
 #include <QDomDocument>
 #include <QDir>
 #include <QApplication>
+#include <QFile>
 #include <QMessageBox>
 #include <QProgressDialog>
+
+#include <sstream>
 
 #include "MidiImport.h"
 #include "TrackContainer.h"
@@ -279,8 +282,6 @@ public:
 
 bool MidiImport::readSMF( TrackContainer* tc )
 {
-	QString filename = file().fileName();
-	closeFile();
 
 	const int preTrackSteps = 2;
 	QProgressDialog pd( TrackContainer::tr( "Importing MIDI-file..." ),
@@ -291,7 +292,11 @@ bool MidiImport::readSMF( TrackContainer* tc )
 
 	pd.setValue( 0 );
 
-	Alg_seq_ptr seq = new Alg_seq(filename.toLocal8Bit(), true);
+	std::stringstream stream;
+	QByteArray arr = readAllData();
+	stream.str(std::string(arr.constData(), arr.size()));
+
+	Alg_seq_ptr seq = new Alg_seq(stream, true);
 	seq->convert_to_beats();
 
 	pd.setMaximum( seq->tracks()  + preTrackSteps );

commit bc2530ccfccc725a75738f9877f6ec861ad1607a

Fix broken MIDI import
Fix a regression in 2dadb15cf31c4ccefa2e06f698e74e672621c4d8 (#4401) that
readAllData doesn't read the file from the beginning

include/ImportFilter.h
@@ -80,6 +80,7 @@ protected:
 
 	inline QByteArray readAllData()
 	{
+		m_file.seek(0);
 		return m_file.readAll();
 	}
 