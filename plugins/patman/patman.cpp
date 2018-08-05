LMMS/lmms

commit 84a1f359d7dfa58bd3432f6c36237fce93484008

SampleBuffer: A few changes * Exporting and restoring is universally done now by saveSettings and loadSettings. That's allows us to make sure we don't left any important attributes like "sampleRate" behind. * Don't use sharedObject. It does not make any sense and could lead to bugs when allocated locally (see audio_file_processor). When needed, this commit used std::shared_ptr which was made for that.

plugins/patman/patman.cpp
@@ -170,7 +170,7 @@ void patmanInstrument::playNote( NotePlayHandle * _n,
 void patmanInstrument::deleteNotePluginData( NotePlayHandle * _n )
 {
 	handle_data * hdata = (handle_data *)_n->m_pluginData;
-	sharedObject::unref( hdata->sample );
+
 	delete hdata->state;
 	delete hdata;
 }
@@ -357,7 +357,7 @@ patmanInstrument::LoadErrors patmanInstrument::loadPatch(
 			}
 		}
 
-		SampleBuffer* psample = new SampleBuffer( std::move(data), sample_rate );
+		auto psample = std::make_shared<SampleBuffer>( std::move(data), sample_rate );
 		psample->setFrequency( root_freq / 1000.0f );
 
 		if( modes & MODES_LOOPING )
@@ -366,7 +366,7 @@ patmanInstrument::LoadErrors patmanInstrument::loadPatch(
 			psample->setLoopEndFrame( loop_end );
 		}
 
-		m_patchSamples.push_back( psample );
+		m_patchSamples.push_back( std::move(psample) );
 
 		delete[] wave_samples;
 	}
@@ -379,11 +379,7 @@ patmanInstrument::LoadErrors patmanInstrument::loadPatch(
 
 void patmanInstrument::unloadCurrentPatch( void )
 {
-	while( !m_patchSamples.empty() )
-	{
-		sharedObject::unref( m_patchSamples.back() );
-		m_patchSamples.pop_back();
-	}
+	m_patchFile.clear ();
 }
 
 
@@ -394,18 +390,18 @@ void patmanInstrument::selectSample( NotePlayHandle * _n )
 	const float freq = _n->frequency();
 
 	float min_dist = HUGE_VALF;
-	SampleBuffer* sample = NULL;
+	std::shared_ptr<SampleBuffer> sample;
 
-	for( QVector<SampleBuffer *>::iterator it = m_patchSamples.begin(); it != m_patchSamples.end(); ++it )
+	for( auto &element : m_patchSamples )
 	{
-		float patch_freq = ( *it )->frequency();
+		float patch_freq = element->frequency();
 		float dist = freq >= patch_freq ? freq / patch_freq :
 							patch_freq / freq;
 
 		if( dist < min_dist )
 		{
 			min_dist = dist;
-			sample = *it;
+			sample = element;
 		}
 	}
 
@@ -413,11 +409,11 @@ void patmanInstrument::selectSample( NotePlayHandle * _n )
 	hdata->tuned = m_tunedModel.value();
 	if( sample )
 	{
-		hdata->sample = sharedObject::ref( sample );
+		hdata->sample = sample;
 	}
 	else
 	{
-		hdata->sample = new SampleBuffer( NULL, 0 );
+		hdata->sample = std::make_shared<SampleBuffer>();
 	}
 	hdata->state = new SampleBuffer::handleState( _n->hasDetuningInfo() );
 

include/SampleBuffer.h
@@ -39,6 +39,7 @@
 #include "lmms_math.h"
 #include "shared_object.h"
 #include "MemoryManager.h"
+#include "JournallingObject.h"
 
 
 class QPainter;
@@ -50,7 +51,8 @@ class QRect;
 // may need to be higher - conversely, to optimize, some may work with lower values
 const f_cnt_t MARGIN[] = { 64, 64, 64, 4, 4 };
 
-class EXPORT SampleBuffer : public QObject, public sharedObject
+class EXPORT SampleBuffer : public QObject,
+							public JournallingObject
 {
 	Q_OBJECT
 	MM_OPERATORS
@@ -106,12 +108,18 @@ public:
 
 	} ;
 
-
 	SampleBuffer();
 	// constructor which either loads sample _audio_file or decodes
 	// base64-data out of string
-	SampleBuffer( const QString & _audio_file, bool _is_base64_data = false );
+	SampleBuffer(const QString & _audio_file, bool _is_base64_data, sample_rate_t sampleRate=0);
 	SampleBuffer(DataVector &&movedData, sample_rate_t sampleRate);
+
+	inline virtual QString nodeName() const override
+	{
+		return "samplebuffer";
+	}
+	virtual void saveSettings(QDomDocument& doc, QDomElement& _this ) override;
+	virtual void loadSettings(const QDomElement& _this ) override;
 
 	bool play( sampleFrame * _ab, handleState * _state,
 				const fpp_t _frames,
@@ -197,11 +205,6 @@ public:
 		m_frequency = _freq;
 	}
 
-	inline void setSampleRate( sample_rate_t _rate )
-	{
-		m_sampleRate = _rate;
-	}
-
 	inline const sampleFrame * data() const
 	{
 		return m_data.data ();
@@ -210,8 +213,6 @@ public:
 	QString openAudioFile() const;
 	QString openAndSetAudioFile();
 	QString openAndSetWaveformFile();
-
-	QString & toBase64( QString & _dst ) const;
 
 	void normalizeSampleRate( const sample_rate_t _src_sr,
 						bool _keep_settings = false );
@@ -276,15 +277,22 @@ public:
 	 */
 	void reverse(bool shouldLockMixer=true);
 
+	void loadFromBase64(const QString & _data , sample_rate_t sampleRate, bool shouldLock);
+
 public slots:
 	void setAudioFile( const QString & _audio_file );
-	void loadFromBase64(const QString & _data , bool shouldLock);
 	void setStartFrame( const f_cnt_t _s );
 	void setEndFrame( const f_cnt_t _e );
 	void setAmplification( float _a );
 	void sampleRateChanged();
 
 protected:
+	QString & toBase64( QString & _dst ) const;
+	inline void setSampleRate( sample_rate_t _rate )
+	{
+		m_sampleRate = _rate;
+	}
+
 	static sample_rate_t mixerSampleRate();
 
 	// HACK: libsamplerate < 0.1.8 doesn't get read-only variables

include/SamplePlayHandle.h
@@ -29,6 +29,8 @@
 #include "AutomatableModel.h"
 #include "PlayHandle.h"
 
+#include <memory>
+
 class BBTrack;
 class SampleTCO;
 class Track;
@@ -38,7 +40,7 @@ class AudioPort;
 class SamplePlayHandle : public PlayHandle
 {
 public:
-	SamplePlayHandle(SampleBuffer* sampleBuffer , bool shouldCreateAudioPort=true);
+	SamplePlayHandle(const std::shared_ptr<SampleBuffer> &sampleBuffer, bool shouldCreateAudioPort=true);
 	SamplePlayHandle( const QString& sampleFile );
 	SamplePlayHandle( SampleTCO* tco );
 	virtual ~SamplePlayHandle();
@@ -76,7 +78,7 @@ public:
 
 
 private:
-	SampleBuffer * m_sampleBuffer;
+	std::shared_ptr<SampleBuffer> m_sampleBuffer;
 	bool m_doneMayReturnTrue;
 
 	f_cnt_t m_frame;

include/SampleTrack.h
@@ -53,7 +53,7 @@ public:
 		return "sampletco";
 	}
 
-	SampleBuffer* sampleBuffer()
+	const std::shared_ptr<SampleBuffer> sampleBuffer()
 	{
 		return m_sampleBuffer;
 	}
@@ -82,7 +82,7 @@ private slots:
 	void onSampleBufferChanged ();
 
 private:
-	SampleBuffer* m_sampleBuffer;
+	std::shared_ptr<SampleBuffer> m_sampleBuffer;
 	BoolModel m_recordModel;
 	bool m_isPlaying;
 

plugins/audio_file_processor/audio_file_processor.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * audio_file_processor.cpp - instrument for using audio-files
  *
  * Copyright (c) 2004-2014 Tobias Doerffel <tobydox/at/users.sourceforge.net>
@@ -210,13 +210,7 @@ void audioFileProcessor::deleteNotePluginData( NotePlayHandle * _n )
 void audioFileProcessor::saveSettings( QDomDocument & _doc,
 							QDomElement & _this )
 {
-	_this.setAttribute( "src", m_sampleBuffer.audioFile() );
-	if( m_sampleBuffer.audioFile() == "" )
-	{
-		QString s;
-		_this.setAttribute( "sampledata",
-						m_sampleBuffer.toBase64( s ) );
-	}
+	m_sampleBuffer.saveState (_doc, _this);
 	m_reverseModel.saveSettings( _doc, _this, "reversed" );
 	m_loopModel.saveSettings( _doc, _this, "looped" );
 	m_ampModel.saveSettings( _doc, _this, "amp" );
@@ -247,7 +241,12 @@ void audioFileProcessor::loadSettings( const QDomElement & _this )
 	}
 	else if( _this.attribute( "sampledata" ) != "" )
 	{
-		m_sampleBuffer.loadFromBase64( _this.attribute( "srcdata" ) , true);
+		m_sampleBuffer.loadFromBase64( _this.attribute( "srcdata" ) ,
+									   true,
+									   Engine::mixer ()->baseSampleRate ());
+		qWarning("Using default sampleRate. That could lead to invalid values");
+	} else {
+		m_sampleBuffer.restoreState (_this.firstChildElement (m_sampleBuffer.nodeName ()));
 	}
 
 	m_loopModel.loadSettings( _this, "looped" );

plugins/patman/patman.h
@@ -31,6 +31,7 @@
 #include "SampleBuffer.h"
 #include "AutomatableModel.h"
 #include "MemoryManager.h"
+#include <memory>
 
 class PixmapButton;
 
@@ -82,11 +83,11 @@ private:
 		MM_OPERATORS
 		SampleBuffer::handleState* state;
 		bool tuned;
-		SampleBuffer* sample;
+		std::shared_ptr<SampleBuffer> sample;
 	} handle_data;
 
 	QString m_patchFile;
-	QVector<SampleBuffer *> m_patchSamples;
+	QVector<std::shared_ptr<SampleBuffer>> m_patchSamples;
 	BoolModel m_loopedModel;
 	BoolModel m_tunedModel;
 

plugins/triple_oscillator/TripleOscillator.cpp
@@ -130,7 +130,7 @@ OscillatorObject::OscillatorObject( Model * _parent, int _idx ) :
 
 OscillatorObject::~OscillatorObject()
 {
-	sharedObject::unref( m_sampleBuffer );
+	delete m_sampleBuffer;
 }
 
 

src/core/LfoController.cpp
@@ -71,7 +71,7 @@ LfoController::LfoController( Model * _parent ) :
 
 LfoController::~LfoController()
 {
-	sharedObject::unref( m_userDefSampleBuffer );
+	delete m_userDefSampleBuffer;
 	m_baseModel.disconnect( this );
 	m_speedModel.disconnect( this );
 	m_amountModel.disconnect( this );

commit 4fbc4e099595cd8651f98d8a46d2a75f7aabeb4d

Fix PatMan patch loading
Fixes a regression in 84a1f359d7dfa58bd3432f6c36237fce93484008.

plugins/patman/patman.cpp
@@ -378,7 +378,7 @@ patmanInstrument::LoadErrors patmanInstrument::loadPatch(
 
 void patmanInstrument::unloadCurrentPatch( void )
 {
-	m_patchFile.clear ();
+	m_patchSamples.clear();
 }
 
 