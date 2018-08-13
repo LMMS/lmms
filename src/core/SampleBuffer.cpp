3b7f4fa4e2752a42d5ba4935080e01a405006fa3
Shmuel H <shmuelhazan0@gmail.com>
2018-08-13 13:22:47 -0700

    Bugfix -> SampleBuffer: Don't unlock the mixer when it has not been locked.

--- src/core/SampleBuffer.cpp
+++ src/core/SampleBuffer.cpp
@@ -1185,7 +1185,7 @@ bool SampleBuffer::tryBeginBufferChange(bool shouldLock, bool shouldLockMixer) {
 	if (shouldLock) {
 		result = m_varLock.tryLockForWrite();
 
-		if (! result)
+		if (!result && shouldLockMixer)
 			Engine::mixer ()->doneChangeInModel();
 	}
 
