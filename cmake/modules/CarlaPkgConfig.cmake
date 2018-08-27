0c4ea23f42e721504d5f666711599383401cf637
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2018-08-27 01:29:20 -0400

    Update CarlaPkgConfig.cmake

--- cmake/modules/CarlaPkgConfig.cmake
+++ cmake/modules/CarlaPkgConfig.cmake
@@ -6,7 +6,7 @@ SET(CARLA_PKGCONFIG_FILE ${CARLA_SUBMODULE_SOURCE}/data/carla-standalone.pc)
 IF(EXISTS ${CARLA_PKGCONFIG_FILE})
 	SET(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH};${CMAKE_BINARY_DIR}")
 	FILE(READ ${CARLA_PKGCONFIG_FILE} CARLA_PKGCONFIG_TEXT)
-	# add source/includes and source/backend to include location
+	# add source/includes to include location
 	STRING(REPLACE "X-INCLUDEDIR-X/carla" "${CARLA_SUBMODULE_SOURCE}/source/includes" CARLA_PKGCONFIG_TEXT "${CARLA_PKGCONFIG_TEXT}")
 	IF(LMMS_BUILD_APPLE)
 		# Remove linker flags
@@ -18,5 +18,4 @@ IF(EXISTS ${CARLA_PKGCONFIG_FILE})
 	FILE(WRITE "${CMAKE_BINARY_DIR}/carla-standalone.pc" "${CARLA_PKGCONFIG_TEXT}")
 	SET(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:${CMAKE_BINARY_DIR}")
 	MESSAGE("-- Using carla headers from ${CARLA_SUBMODULE_SOURCE}/source/includes")
-	MESSAGE("--  DEBUG:\n${CARLA_PKGCONFIG_TEXT}")
 ENDIF()
