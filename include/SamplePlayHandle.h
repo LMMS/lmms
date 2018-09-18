LMMS/lmms

commit 3b7ea8e955c22e4031dc0c96667460f42b25db82

Refactoring: Remove duplicate code (#4310)

include/SamplePlayHandle.h
@@ -38,8 +38,8 @@ class AudioPort;
 class SamplePlayHandle : public PlayHandle
 {
 public:
-	SamplePlayHandle( const QString& sampleFile );
 	SamplePlayHandle( SampleBuffer* sampleBuffer );
+	SamplePlayHandle( const QString& sampleFile );
 	SamplePlayHandle( SampleTCO* tco );
 	virtual ~SamplePlayHandle();
 

src/core/SamplePlayHandle.cpp
@@ -32,23 +32,6 @@
 
 
 
-SamplePlayHandle::SamplePlayHandle( const QString& sampleFile ) :
-	PlayHandle( TypeSamplePlayHandle ),
-	m_sampleBuffer( new SampleBuffer( sampleFile ) ),
-	m_doneMayReturnTrue( true ),
-	m_frame( 0 ),
-	m_ownAudioPort( true ),
-	m_defaultVolumeModel( DefaultVolume, MinVolume, MaxVolume, 1 ),
-	m_volumeModel( &m_defaultVolumeModel ),
-	m_track( NULL ),
-	m_bbTrack( NULL )
-{
-	setAudioPort( new AudioPort( "SamplePlayHandle", false ) );
-}
-
-
-
-
 SamplePlayHandle::SamplePlayHandle( SampleBuffer* sampleBuffer ) :
 	PlayHandle( TypeSamplePlayHandle ),
 	m_sampleBuffer( sharedObject::ref( sampleBuffer ) ),
@@ -66,17 +49,20 @@ SamplePlayHandle::SamplePlayHandle( SampleBuffer* sampleBuffer ) :
 
 
 
-SamplePlayHandle::SamplePlayHandle( SampleTCO* tco ) :
-	PlayHandle( TypeSamplePlayHandle ),
-	m_sampleBuffer( sharedObject::ref( tco->sampleBuffer() ) ),
-	m_doneMayReturnTrue( true ),
-	m_frame( 0 ),
-	m_ownAudioPort( false ),
-	m_defaultVolumeModel( DefaultVolume, MinVolume, MaxVolume, 1 ),
-	m_volumeModel( &m_defaultVolumeModel ),
-	m_track( tco->getTrack() ),
-	m_bbTrack( NULL )
+SamplePlayHandle::SamplePlayHandle( const QString& sampleFile ) :
+	SamplePlayHandle( new SampleBuffer( sampleFile ) )
 {
+	sharedObject::unref( m_sampleBuffer );
+	setAudioPort( new AudioPort( "SamplePlayHandle", false ) );
+}
+
+
+
+
+SamplePlayHandle::SamplePlayHandle( SampleTCO* tco ) :
+	SamplePlayHandle( tco->sampleBuffer() )
+{
+	m_track = tco->getTrack();
 	setAudioPort( ( (SampleTrack *)tco->getTrack() )->audioPort() );
 }
 

include/BandLimitedWave.h
@@ -125,48 +125,9 @@ public:
 	 */
 	static inline sample_t oscillate( float _ph, float _wavelen, Waveforms _wave )
 	{
-		// high wavelen/ low freq
-		if( _wavelen > TLENS[ MAXTBL ] )
-		{
-			const int t = MAXTBL;
-			const int tlen = TLENS[t];
-			const float ph = fraction( _ph );
-			const float lookupf = ph * static_cast<float>( tlen );
-			const int lookup = static_cast<int>( lookupf );
-			const float ip = fraction( lookupf );
-
-			const sample_t s1 = s_waveforms[ _wave ].sampleAt( t, lookup );
-			const sample_t s2 = s_waveforms[ _wave ].sampleAt( t, ( lookup + 1 ) % tlen );
-			const int lm = lookup == 0 ? tlen - 1 : lookup - 1;
-			const sample_t s0 = s_waveforms[ _wave ].sampleAt( t, lm );
-			const sample_t s3 = s_waveforms[ _wave ].sampleAt( t, ( lookup + 2 ) % tlen );
-			const sample_t sr = optimal4pInterpolate( s0, s1, s2, s3, ip );
-
-			return sr;
-		}
-		// low wavelen/ high freq
-		if( _wavelen < 3.0f )
-		{
-			const int t = 0;
-			const int tlen = TLENS[t];
-			const float ph = fraction( _ph );
-			const float lookupf = ph * static_cast<float>( tlen );
-			const int lookup = static_cast<int>( lookupf );
-			const float ip = fraction( lookupf );
-
-			const sample_t s1 = s_waveforms[ _wave ].sampleAt( t, lookup );
-			const sample_t s2 = s_waveforms[ _wave ].sampleAt( t, ( lookup + 1 ) % tlen );
-			const int lm = lookup == 0 ? tlen - 1 : lookup - 1;
-			const sample_t s0 = s_waveforms[ _wave ].sampleAt( t, lm );
-			const sample_t s3 = s_waveforms[ _wave ].sampleAt( t, ( lookup + 2 ) % tlen );
-			const sample_t sr = optimal4pInterpolate( s0, s1, s2, s3, ip );
-
-			return sr;
-		}
-
 		// get the next higher tlen
-		int t = MAXTBL - 1;
-		while( _wavelen < TLENS[t] ) { t--; }
+		int t = 0;
+		while( t < MAXTBL && _wavelen >= TLENS[t+1] ) { t++; }
 
 		int tlen = TLENS[t];
 		const float ph = fraction( _ph );

include/DspEffectLibrary.h
@@ -245,14 +245,36 @@ namespace DspEffectLibrary
 	} ;
 
 
-	class FoldbackDistortion : public MonoBase<FoldbackDistortion>
+	template<class T>
+	class DistortionBase : public MonoBase<T>
 	{
 	public:
-		FoldbackDistortion( float threshold, float gain ) :
+		DistortionBase( float threshold, float gain ) :
 			m_threshold( threshold ),
 			m_gain( gain )
 		{
 		}
+
+		void setThreshold( float threshold )
+		{
+			m_threshold = threshold;
+		}
+
+		void setGain( float gain )
+		{
+			m_gain = gain;
+		}
+
+	protected:
+		float m_threshold;
+		float m_gain;
+	};
+
+
+	class FoldbackDistortion : public DistortionBase<FoldbackDistortion>
+	{
+	public:
+		using DistortionBase<FoldbackDistortion>::DistortionBase;
 
 		sample_t nextSample( sample_t in )
 		{
@@ -262,54 +284,18 @@ namespace DspEffectLibrary
 			}
 			return in * m_gain;
 		}
-
-		void setThreshold( float threshold )
-		{
-			m_threshold = threshold;
-		}
-
-		void setGain( float gain )
-		{
-			m_gain = gain;
-		}
-
-
-	private:
-		float m_threshold;
-		float m_gain;
-
 	} ;
 
 
-	class Distortion : public MonoBase<Distortion>
+	class Distortion : public DistortionBase<Distortion>
 	{
 	public:
-		Distortion( float threshold, float gain ) :
-			m_threshold( threshold ),
-			m_gain( gain )
-		{
-		}
+		using DistortionBase<Distortion>::DistortionBase;
 
 		sample_t nextSample( sample_t in )
 		{
 			return m_gain * ( in * ( fabsf( in )+m_threshold ) / ( in*in +( m_threshold-1 )* fabsf( in ) + 1 ) );
 		}
-
-		void setThreshold( float threshold )
-		{
-			m_threshold = threshold;
-		}
-
-		void setGain( float gain )
-		{
-			m_gain = gain;
-		}
-
-
-	private:
-		float m_threshold;
-		float m_gain;
-
 	} ;
 
 

include/LadspaManager.h
@@ -334,6 +334,11 @@ private:
 	uint16_t  getPluginInputs( const LADSPA_Descriptor * _descriptor );
 	uint16_t  getPluginOutputs( const LADSPA_Descriptor * _descriptor );
 
+	const LADSPA_PortDescriptor* getPortDescriptor( const ladspa_key_t& _plugin,
+													uint32_t _port );
+	const LADSPA_PortRangeHint* getPortRangeHint( const ladspa_key_t& _plugin,
+													uint32_t _port );
+
 	typedef QMap<ladspa_key_t, ladspaManagerDescription *>
 						ladspaManagerMapType;
 	ladspaManagerMapType m_ladspaManagerMap;

include/RenderManager.h
@@ -64,6 +64,8 @@ private:
 	QString pathForTrack( const Track *track, int num );
 	void restoreMutedState();
 
+	void render( QString outputPath );
+
 	const Mixer::qualitySettings m_qualitySettings;
 	const Mixer::qualitySettings m_oldQualitySettings;
 	const OutputSettings m_outputSettings;

include/SampleBuffer.h
@@ -103,12 +103,12 @@ public:
 	} ;
 
 
+	SampleBuffer();
 	// constructor which either loads sample _audio_file or decodes
 	// base64-data out of string
-	SampleBuffer( const QString & _audio_file = QString(),
-						bool _is_base64_data = false );
+	SampleBuffer( const QString & _audio_file, bool _is_base64_data = false );
 	SampleBuffer( const sampleFrame * _data, const f_cnt_t _frames );
-	SampleBuffer( const f_cnt_t _frames );
+	explicit SampleBuffer( const f_cnt_t _frames );
 
 	virtual ~SampleBuffer();
 

include/Track.h
@@ -438,6 +438,7 @@ private slots:
 	void cloneTrack();
 	void removeTrack();
 	void updateMenu();
+	void toggleRecording(bool on);
 	void recordingOn();
 	void recordingOff();
 	void clearTrack();

commit 00cc6dc22ba857243de64340ac77969716244534

Fix sample track playback crash (#4586)
Fixes double-freeing `AudioPort` which is a regression in #4310.

Co-authored-by: Shmuel H <shmuelhazan0@gmail.com>
Co-authored-by: Hyunjin Song <tteu.ingog@gmail.com>

include/SamplePlayHandle.h
@@ -1,8 +1,9 @@
+
 /*
  * SamplePlayHandle.h - play-handle for playing a sample
  *
  * Copyright (c) 2005-2014 Tobias Doerffel <tobydox/at/users.sourceforge.net>
- * 
+ *
  * This file is part of LMMS - https://lmms.io
  *
  * This program is free software; you can redistribute it and/or
@@ -38,7 +39,7 @@ class AudioPort;
 class SamplePlayHandle : public PlayHandle
 {
 public:
-	SamplePlayHandle( SampleBuffer* sampleBuffer );
+	SamplePlayHandle( SampleBuffer* sampleBuffer , bool ownAudioPort = true );
 	SamplePlayHandle( const QString& sampleFile );
 	SamplePlayHandle( SampleTCO* tco );
 	virtual ~SamplePlayHandle();

src/core/SamplePlayHandle.cpp
@@ -32,35 +32,37 @@
 
 
 
-SamplePlayHandle::SamplePlayHandle( SampleBuffer* sampleBuffer ) :
+SamplePlayHandle::SamplePlayHandle( SampleBuffer* sampleBuffer , bool ownAudioPort ) :
 	PlayHandle( TypeSamplePlayHandle ),
 	m_sampleBuffer( sharedObject::ref( sampleBuffer ) ),
 	m_doneMayReturnTrue( true ),
 	m_frame( 0 ),
-	m_ownAudioPort( true ),
+	m_ownAudioPort( ownAudioPort ),
 	m_defaultVolumeModel( DefaultVolume, MinVolume, MaxVolume, 1 ),
 	m_volumeModel( &m_defaultVolumeModel ),
 	m_track( NULL ),
 	m_bbTrack( NULL )
 {
-	setAudioPort( new AudioPort( "SamplePlayHandle", false ) );
+	if (ownAudioPort)
+	{
+		setAudioPort( new AudioPort( "SamplePlayHandle", false ) );
+	}
 }
 
 
 
 
 SamplePlayHandle::SamplePlayHandle( const QString& sampleFile ) :
-	SamplePlayHandle( new SampleBuffer( sampleFile ) )
+	SamplePlayHandle( new SampleBuffer( sampleFile ) , true)
 {
 	sharedObject::unref( m_sampleBuffer );
-	setAudioPort( new AudioPort( "SamplePlayHandle", false ) );
 }
 
 
 
 
 SamplePlayHandle::SamplePlayHandle( SampleTCO* tco ) :
-	SamplePlayHandle( tco->sampleBuffer() )
+	SamplePlayHandle( tco->sampleBuffer() , false)
 {
 	m_track = tco->getTrack();
 	setAudioPort( ( (SampleTrack *)tco->getTrack() )->audioPort() );