5c69315be90153637a698949691611a93d21e689
Hyunjin Song <tteu.ingog@gmail.com>
2018-09-23 10:58:41 +0900

    Fix the bash completion install script
    * Remove irrelevant CMP0053 settings
    * Include FindUnixCommands to fix the shebang
    * Fix the install script call
    * Fix whitespaces
    * Fix Windows/macOS build
    * Minor: replace ugo+r with a+r

--- cmake/modules/BashCompletion.cmake
+++ cmake/modules/BashCompletion.cmake
@@ -21,10 +21,6 @@
 #    2. Final fallback to ${CMAKE_INSTALL_PREFIX}/share/bash-completion/completions if
 #       detected path is unwritable.
 
-IF(COMMAND CMAKE_POLICY AND NOT CMAKE_VERSION VERSION_LESS 3.1.0)
-	CMAKE_POLICY(SET CMP0053 OLD)
-ENDIF()
-
 # - Windows does not support bash completion
 # - macOS support should eventually be added for Homebrew (TODO)
 IF(WIN32)
@@ -32,14 +28,15 @@ IF(WIN32)
 ELSEIF(APPLE)
 	MESSAGE(STATUS "Bash completion is not yet implemented for this platform.")
 ELSE()
+	INCLUDE(FindUnixCommands)
 	# Honor manual override if provided
 	IF(NOT BASHCOMP_PKG_PATH)
 		# First, use pkg-config, which is the most reliable
 		FIND_PACKAGE(PkgConfig QUIET)
 		IF(PKGCONFIG_FOUND)
-			 PKG_CHECK_MODULES(BASH_COMPLETION bash-completion)
-			 PKG_GET_VARIABLE(BASHCOMP_PKG_PATH bash-completion completionsdir)
-		ELSE()	
+			PKG_CHECK_MODULES(BASH_COMPLETION bash-completion)
+			PKG_GET_VARIABLE(BASHCOMP_PKG_PATH bash-completion completionsdir)
+		ELSE()
 			# Second, use cmake (preferred but less common)
 			FIND_PACKAGE(bash-completion QUIET)
 			IF(BASH_COMPLETION_FOUND)
@@ -52,10 +49,10 @@ ELSE()
 			SET(BASHCOMP_PKG_PATH "/usr/share/bash-completion/completions")
 		ENDIF()
 	ENDIF()
-	
+
 	# Always provide a fallback for non-root INSTALL()
 	SET(BASHCOMP_USER_PATH "${CMAKE_INSTALL_PREFIX}/share/bash-completion/completions")
-	
+
 	# Cmake doesn't allow easy use of conditional logic at INSTALL() time
 	# this is a problem because ${BASHCOMP_PKG_PATH} may not be writable and we
 	# need sane fallback behavior for bundled INSTALL() (e.g. .AppImage, etc).
@@ -83,11 +80,11 @@ fi\n\
 echo -e \"\\nInstalling bash completion...\\n\"\n\
 mkdir -p \"\$BASHCOMP_PKG_PATH\"\n\
 cp \"${CMAKE_CURRENT_SOURCE_DIR}/${SCRIPT_NAME}\" \"\$BASHCOMP_PKG_PATH\"\n\
-chmod ugo+r \"\$BASHCOMP_PKG_PATH/${SCRIPT_NAME}\"\n\
+chmod a+r \"\$BASHCOMP_PKG_PATH/${SCRIPT_NAME}\"\n\
 echo -e \"Bash completion for ${SCRIPT_NAME} has been installed to \$BASHCOMP_PKG_PATH/${SCRIPT_NAME}\"\n\
 ")
 		INSTALL(CODE "EXECUTE_PROCESS(COMMAND chmod u+x \"install_${SCRIPT_NAME}_completion.sh\" WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} )")
-		INSTALL(CODE "EXECUTE_PROCESS(COMMAND \"install_${SCRIPT_NAME}_completion.sh\" WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} )")
+		INSTALL(CODE "EXECUTE_PROCESS(COMMAND \"./install_${SCRIPT_NAME}_completion.sh\" WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} )")
 
 		MESSAGE(STATUS "Bash completion script for ${SCRIPT_NAME} will be installed to ${BASHCOMP_PKG_PATH} or fallback to ${BASHCOMP_USER_PATH} if unwritable.")
 	ENDMACRO()
