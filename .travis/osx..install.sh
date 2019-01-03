c90904eadaad8f6c22bc4cf1f7d393552bcd9664
Javier Serrano Polo <javier@jasp.net>
2019-01-04 00:06:10 +0100

    Install debianutils

--- .travis/osx..install.sh
+++ .travis/osx..install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PACKAGES="cmake pkgconfig fftw libogg libvorbis lame libsndfile libsamplerate jack sdl libgig libsoundio stk fluid-synth portaudio node fltk carla"
+PACKAGES="cmake debianutils pkgconfig fftw libogg libvorbis lame libsndfile libsamplerate jack sdl libgig libsoundio stk fluid-synth portaudio node fltk carla"
 
 if [ $QT5 ]; then
 	PACKAGES="$PACKAGES qt5"
