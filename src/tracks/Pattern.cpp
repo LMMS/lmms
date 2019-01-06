4e5493f23d738848a61622273d0ab53f8017fda6
CYBERDEViLNL <CYBERDEViLNL@github.com>
2019-01-07 00:52:26 +0100

    * Don’t show `Set as ghost in piano-roll` on empty pattern. * Small change to comply more with the rest of the coding style.

--- src/tracks/Pattern.cpp
+++ src/tracks/Pattern.cpp
@@ -677,14 +677,19 @@ void PatternView::constructContextMenu( QMenu * _cm )
 	connect( a, SIGNAL( triggered( bool ) ),
 					this, SLOT( openInPianoRoll() ) );
 
-	if( gui->pianoRoll()->currentPattern() && gui->pianoRoll()->currentPattern() != m_pat ){
+	if( gui->pianoRoll()->currentPattern() &&
+			gui->pianoRoll()->currentPattern() != m_pat &&
+			not m_pat->empty() )
+	{
 		QAction * b = new QAction( embed::getIconPixmap( "ghost_note" ),
 						tr( "Set as ghost in piano-roll" ), _cm );
 		_cm->insertAction( _cm->actions()[1], b );
 		connect( b, SIGNAL( triggered( bool ) ),
 						this, SLOT( setGhostInPianoRoll() ) );
 		_cm->insertSeparator( _cm->actions()[2] );
-	} else {
+	}
+	else
+	{
 		_cm->insertSeparator( _cm->actions()[1] );
 	}
 
