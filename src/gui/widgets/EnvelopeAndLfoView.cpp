55055023c7b313b196a416ac386c66532d5c6d04
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2019-01-07 13:09:11 -0500

    Fix left 1px lfo graph
    Closes #4597, supersedes #4613

--- src/gui/widgets/EnvelopeAndLfoView.cpp
+++ src/gui/widgets/EnvelopeAndLfoView.cpp
@@ -491,9 +491,9 @@ void EnvelopeAndLfoView::paintEvent( QPaintEvent * )
 	p.fillRect( x5, y_base - 1, 2, 2, end_points_color );
 
 
-	int LFO_GRAPH_W = s_lfoGraph->width() - 6;	// substract border
+	int LFO_GRAPH_W = s_lfoGraph->width() - 3;      // substract border
 	int LFO_GRAPH_H = s_lfoGraph->height() - 6;	// substract border
-	int graph_x_base = LFO_GRAPH_X + 3;
+	int graph_x_base = LFO_GRAPH_X + 2;
 	int graph_y_base = LFO_GRAPH_Y + 3 + LFO_GRAPH_H / 2;
 
 	const float frames_for_graph = SECS_PER_LFO_OSCILLATION *
