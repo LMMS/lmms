3dd7468d8267d2b7a141fdb851c0c715861d9f55
Javier Serrano Polo <javier@jasp.net>
2019-02-06 02:46:32 +0100

    Disable repositories without Xenial support

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -11,7 +11,7 @@ VST_PACKAGES="wine-dev libqt5x11extras5-dev qtbase5-private-dev libxcb-util0-dev
 PACKAGES="$PACKAGES $VST_PACKAGES libjack-jackd2-0"
 
 if [ $QT5 ]; then
-	PACKAGES="$PACKAGES qt59base qt59translations qt59tools"
+	: PACKAGES="$PACKAGES qt59base qt59translations qt59tools"
 else
 	PACKAGES="$PACKAGES libqt4-dev"
 fi
@@ -19,7 +19,7 @@ fi
 sudo apt-get install -y $PACKAGES
 
 # kxstudio repo offers Carla; avoid package conflicts (wine, etc) by running last
-sudo add-apt-repository -y ppa:kxstudio-debian/libs
-sudo add-apt-repository -y ppa:kxstudio-debian/apps
-sudo apt-get update
-sudo apt-get install -y carla-git
+#sudo add-apt-repository -y ppa:kxstudio-debian/libs
+#sudo add-apt-repository -y ppa:kxstudio-debian/apps
+#sudo apt-get update
+#sudo apt-get install -y carla-git
