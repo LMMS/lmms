e730e13c8a29a4461b5a1b8087c1b4c30581ad9d
Javier Serrano Polo <javier@jasp.net>
2019-02-06 03:00:57 +0100

    Install missing packages

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -3,6 +3,7 @@
 PACKAGES="cmake libsndfile-dev fftw3-dev libvorbis-dev libogg-dev libmp3lame-dev
 	libasound2-dev libjack-jackd2-dev libsdl-dev libsamplerate0-dev libstk0-dev stk
 	libfluidsynth-dev portaudio19-dev g++-multilib libfltk1.3-dev
+	fluid
 	libgig-dev libsoundio-dev"
 
 VST_PACKAGES="wine-dev libqt5x11extras5-dev qtbase5-private-dev libxcb-util0-dev libxcb-keysyms1-dev"
@@ -11,6 +12,7 @@ VST_PACKAGES="wine-dev libqt5x11extras5-dev qtbase5-private-dev libxcb-util0-dev
 PACKAGES="$PACKAGES $VST_PACKAGES libjack-jackd2-0"
 
 if [ $QT5 ]; then
+	PACKAGES="$PACKAGES qttools5-dev-tools"
 	: PACKAGES="$PACKAGES qt59base qt59translations qt59tools"
 else
 	PACKAGES="$PACKAGES libqt4-dev"
