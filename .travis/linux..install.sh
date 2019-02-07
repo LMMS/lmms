ed2c0136f969a1ca4b538cdcf549c17c53c320e6
Javier Serrano Polo <javier@jasp.net>
2019-02-07 21:59:19 +0100

    Remove unnecessary comments
    These repositories and packages do not seem necessary for Xenial. Also, sort
    PACKAGES.

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-PACKAGES="cmake libsndfile-dev fftw3-dev libvorbis-dev libogg-dev libmp3lame-dev
-	libasound2-dev libjack-jackd2-dev libsdl-dev libsamplerate0-dev libstk0-dev stk
-	libfluidsynth-dev portaudio19-dev g++-multilib libfltk1.3-dev
-	fluid
-	libgig-dev libsoundio-dev"
+PACKAGES="cmake fftw3-dev fluid g++-multilib libasound2-dev libfltk1.3-dev
+	libfluidsynth-dev libgig-dev libjack-jackd2-dev libmp3lame-dev
+	libogg-dev libsamplerate0-dev libsdl-dev libsoundio-dev libsndfile-dev
+	libstk0-dev libvorbis-dev portaudio19-dev stk"
 
 VST_PACKAGES="wine-dev libqt5x11extras5-dev qtbase5-private-dev libxcb-util0-dev libxcb-keysyms1-dev"
 
@@ -13,7 +12,6 @@ PACKAGES="$PACKAGES $VST_PACKAGES libjack-jackd2-0"
 
 if [ $QT5 ]; then
 	PACKAGES="$PACKAGES qttools5-dev-tools"
-	: PACKAGES="$PACKAGES qt59base qt59translations qt59tools"
 else
 	PACKAGES="$PACKAGES libqt4-dev"
 fi
