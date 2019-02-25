e34f75a8c7605cbc9f31fbb43c91255679ede6fb
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2019-02-24 21:57:53 -0500

    Add needed macOS shortcuts (#4851)
    Fix insert bars, delete bars, delete notes on Apple keyboard

--- src/gui/editors/SongEditor.cpp
+++ src/gui/editors/SongEditor.cpp
@@ -315,13 +315,13 @@ void SongEditor::keyPressEvent( QKeyEvent * ke )
 {
 	if( /*_ke->modifiers() & Qt::ShiftModifier*/
 		gui->mainWindow()->isShiftPressed() == true &&
-						ke->key() == Qt::Key_Insert )
+						( ke->key() == Qt::Key_Insert || ke->key() == Qt::Key_Enter || ke->key() == Qt::Key_Return ) )
 	{
 		m_song->insertBar();
 	}
 	else if(/* _ke->modifiers() & Qt::ShiftModifier &&*/
 			gui->mainWindow()->isShiftPressed() == true &&
-						ke->key() == Qt::Key_Delete )
+						( ke->key() == Qt::Key_Delete || ke->key() == Qt::Key_Backspace ) )
 	{
 		m_song->removeBar();
 	}
