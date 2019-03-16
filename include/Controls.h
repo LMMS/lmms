e43d68e1cec886c876c87813c81a6d1a5c1f3f06
Johannes Lorenz <j.git@lorenz-ho.me>
2019-03-16 20:29:48 +0100

    Add Control classes
    Add labeled controls for different types with a common base class

---
+++ include/Controls.h
@@ -0,0 +1,129 @@
+/*
+ * Controls.h - labeled control widgets
+ *
+ * Copyright (c) 2019-2019 Johannes Lorenz <j.git$$$lorenz-ho.me, $$$=@>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef CONTROLS_H
+#define CONTROLS_H
+
+
+#include "Model.h"
+
+// headers only required for covariance
+#include "AutomatableModel.h"
+#include "ComboBoxModel.h"
+
+
+class QString;
+class QWidget;
+class AutomatableModel;
+
+
+/**
+	These classes provide
+	- a control with a text label
+	- a type safe way to set a model
+		(justification: setting the wrong typed model to a widget will cause
+		hard-to-find runtime errors)
+*/
+class ControlBase
+{
+public:
+	virtual QWidget* topWidget() = 0;
+	virtual void setText(const QString& text) = 0;
+
+	virtual void setModel(AutomatableModel* model) = 0;
+	virtual AutomatableModel* model() = 0;
+
+	virtual ~ControlBase();
+};
+
+
+class KnobControl : public ControlBase
+{
+	class Knob* m_knob;
+
+public:
+	void setText(const QString& text) override;
+	QWidget* topWidget() override;
+
+	void setModel(AutomatableModel* model) override;
+	FloatModel* model() override;
+
+	KnobControl(QWidget* parent = nullptr);
+	~KnobControl() override;
+};
+
+
+class ComboControl : public ControlBase
+{
+	QWidget* m_widget;
+	class ComboBox* m_combo;
+	class QLabel* m_label;
+
+public:
+	void setText(const QString& text) override;
+	QWidget* topWidget() override { return m_widget; }
+
+	void setModel(AutomatableModel* model) override;
+	ComboBoxModel* model() override;
+
+	ComboControl(QWidget* parent = nullptr);
+	~ComboControl() override;
+};
+
+
+class LcdControl : public ControlBase
+{
+	class LcdSpinBox* m_lcd;
+
+public:
+	void setText(const QString& text) override;
+	QWidget* topWidget() override;
+
+	void setModel(AutomatableModel* model) override;
+	IntModel* model() override;
+
+	LcdControl(int numDigits, QWidget* parent = nullptr);
+	~LcdControl() override;
+};
+
+
+class CheckControl : public ControlBase
+{
+	QWidget* m_widget;
+	class LedCheckBox* m_checkBox;
+	QLabel* m_label;
+
+public:
+	void setText(const QString& text) override;
+	QWidget* topWidget() override;
+
+	void setModel(AutomatableModel* model) override;
+	BoolModel *model() override;
+
+	CheckControl(QWidget* parent = nullptr);
+	~CheckControl() override;
+};
+
+
+#endif // CONTROLS_H
