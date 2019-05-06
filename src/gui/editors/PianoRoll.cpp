LMMS/lmms

commit a4301de1f1c784bd0ae8d80e111b6c0849f9e175

Don't draw note detuning info over the volume/panning area (#4965)

src/gui/editors/PianoRoll.cpp
@@ -883,6 +883,9 @@ void PianoRoll::drawDetuningInfo( QPainter & _p, const Note * _n, int _x,
 {
 	int middle_y = _y + KEY_LINE_HEIGHT / 2;
 	_p.setPen( noteColor() );
+	_p.setClipRect(WHITE_KEY_WIDTH, PR_TOP_MARGIN,
+		width() - WHITE_KEY_WIDTH,
+		keyAreaBottom() - PR_TOP_MARGIN);
 
 	int old_x = 0;
 	int old_y = 0;
@@ -3076,6 +3079,10 @@ void PianoRoll::paintEvent(QPaintEvent * pe )
 					y_base - key * KEY_LINE_HEIGHT );
 			}
 		}
+
+		p.setClipRect(WHITE_KEY_WIDTH, PR_TOP_MARGIN,
+			width() - WHITE_KEY_WIDTH,
+			height() - PR_TOP_MARGIN - PR_TOP_MARGIN);
 
 		p.setPen( QPen( noteColor(), NOTE_EDIT_LINE_WIDTH + 2 ) );
 		p.drawPoints( editHandles );

commit bba84ad39eae976fcccea8f66bb9d153369fa37b

Fix invisible note editing handles when a note has detuning info
Fixes a regression in a4301de1f1c784bd0ae8d80e111b6c0849f9e175,
the clipping area was restored in a wrong place.
Also, a wrong value was used while restoring.

src/gui/editors/PianoRoll.cpp
@@ -3077,12 +3077,12 @@ void PianoRoll::paintEvent(QPaintEvent * pe )
 				drawDetuningInfo( p, note,
 					x + WHITE_KEY_WIDTH,
 					y_base - key * KEY_LINE_HEIGHT );
+				p.setClipRect(WHITE_KEY_WIDTH, PR_TOP_MARGIN,
+					width() - WHITE_KEY_WIDTH,
+					height() - PR_TOP_MARGIN);
 			}
 		}
 
-		p.setClipRect(WHITE_KEY_WIDTH, PR_TOP_MARGIN,
-			width() - WHITE_KEY_WIDTH,
-			height() - PR_TOP_MARGIN - PR_TOP_MARGIN);
 
 		p.setPen( QPen( noteColor(), NOTE_EDIT_LINE_WIDTH + 2 ) );
 		p.drawPoints( editHandles );