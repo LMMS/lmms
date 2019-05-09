LMMS/lmms

commit 3fc1cfd1cf37318e852d310ec8985f992b462a85

Don't try to connect to nonexistent controllers (#4939)
Fixes crash on loading presets with controllers.

src/core/ControllerConnection.cpp
@@ -205,7 +205,7 @@ void ControllerConnection::loadSettings( const QDomElement & _this )
 	else
 	{
 		m_controllerId = _this.attribute( "id", "-1" ).toInt();
-		if( m_controllerId < 0 )
+		if( m_controllerId < 0 || m_controllerId >= Engine::getSong()->controllers().size() )
 		{
 			qWarning( "controller index invalid\n" );
 			m_controllerId = -1;

commit 81a9fce0d8e0a1dc953de042df8b17d9b98655ef

Fix controller loading error on loading projects
Fixes a regression in 3fc1cfd1cf37318e852d310ec8985f992b462a85,
which added the range check into the wrong if statement.

src/core/ControllerConnection.cpp
@@ -205,13 +205,15 @@ void ControllerConnection::loadSettings( const QDomElement & _this )
 	else
 	{
 		m_controllerId = _this.attribute( "id", "-1" ).toInt();
-		if( m_controllerId < 0 || m_controllerId >= Engine::getSong()->controllers().size() )
+		if( m_controllerId < 0 )
 		{
 			qWarning( "controller index invalid\n" );
 			m_controllerId = -1;
 		}
 
-		if (!Engine::getSong()->isLoadingProject() && m_controllerId != -1)
+		if (!Engine::getSong()->isLoadingProject()
+			&& m_controllerId != -1
+			&& m_controllerId < Engine::getSong()->controllers().size())
 		{
 			setController( Engine::getSong()->
 					controllers().at( m_controllerId ) );