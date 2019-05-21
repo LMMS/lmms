4fa3d485d0961fab66ebea89b4759b81bc61b539
Spekular <Spekularr@gmail.com>
2019-05-21 19:29:56 +0200

    Use modifier keys instead of toggle for snap behavior, allow different snapping for selections, allow alt for fine movement

--- include/Track.h
+++ include/Track.h
@@ -298,6 +298,7 @@ private:
 	QPoint m_initialMousePos;
 	QPoint m_initialMouseGlobalPos;
 	MidiTime m_initialTCOPos;
+	QVector<MidiTime> m_initialOffsets;
 
 	TextFloat * m_hint;
 
@@ -318,6 +319,7 @@ private:
 		m_initialMouseGlobalPos = mapToGlobal( pos );
 		m_initialTCOPos = m_tco->startPosition();
 	}
+	void setInitialOffsets();
 
 	bool mouseMovedDistance( QMouseEvent * me, int distance );
 
