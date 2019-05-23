fd83f87df527af8d21a4208d9d357a7f338eff45
Shmuel H <shmuelhazan0@gmail.com>
2019-05-23 10:28:52 +0300

    Threading: Add runAccordingToLaunchType helper that will run a callable async / sync in the same thread according to a variable.

--- include/Threading.h
+++ include/Threading.h
@@ -8,4 +8,24 @@ inline auto runAsync(Callable &&callable) -> QFuture<decltype(callable())> {
 	return QtConcurrent::run(std::forward<Callable>(callable));
 }
 
+template<class Callable>
+inline auto runSync(Callable &&callable) -> decltype(callable()) {
+	return callable();
+}
+
+namespace LaunchType {
+	struct Async {};
+	struct Sync{};
+}
+
+template<class Callable>
+inline auto runAccordingToLaunchType(Callable &&callable, LaunchType::Async) -> QFuture<decltype(callable())> {
+	return runAsync(std::forward<Callable>(callable));
+}
+
+template<class Callable>
+inline auto runAccordingToLaunchType(Callable &&callable, LaunchType::Sync) -> decltype(callable()) {
+	return callable();
+}
+
 #endif // THREADING_H
