c37228c7e3809ae550be2fc89148ba97efb9bfee
Hyunjin Song <tteu.ingog@gmail.com>
2019-06-06 19:33:10 +0900

    Travis: fix macOS build due to the old default version of Node.js
    appdmg recently dropped support for Node.js < 8.5

--- .travis/osx..before_install.sh
+++ .travis/osx..before_install.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 brew update
+
+# appdmg doesn't work with old versions of Node.js
+nvm install --lts
+
+# Avoid permission issues with npm and node-gyp
+# https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
