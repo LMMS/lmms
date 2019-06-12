4cb243b0ef03dd74df36739fa2206c16e4ed42d7
Hyunjin Song <tteu.ingog@gmail.com>
2019-06-12 17:38:53 +0900

    Travis: drop unneeded prefix setting for npm

--- .travis/osx..before_install.sh
+++ .travis/osx..before_install.sh
@@ -4,9 +4,3 @@ brew update
 
 # appdmg doesn't work with old versions of Node.js
 nvm install --lts
-
-# Avoid permission issues with npm and node-gyp
-# https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-export PATH=~/.npm-global/bin:$PATH
