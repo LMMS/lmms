67d3f75723793654ac268d534da44eae20846157
Ian Sannar <8443754+iansannar@users.noreply.github.com>
2019-08-27 15:43:17 -0600

    Revert formatting in Graph.cpp

--- src/gui/widgets/Graph.cpp
+++ src/gui/widgets/Graph.cpp
@@ -37,7 +37,7 @@ namespace gui
 {
 
 Graph::Graph( QWidget * _parent, graphStyle _style, int _width,
-		int _height) :
+		int _height ) :
 	QWidget( _parent ),
 	/* TODO: size, background? */
 	ModelView( new graphModel( -1.0, 1.0, 128, nullptr, true ), this ),
@@ -47,9 +47,8 @@ Graph::Graph( QWidget * _parent, graphStyle _style, int _width,
 	m_graphColor = QColor( 0xFF, 0xAA, 0x00 );
 
 	resize( _width, _height );
-
-	setCursor(Qt::CrossCursor);
-	setAcceptDrops(true);
+	setAcceptDrops( true );
+	setCursor( Qt::CrossCursor );
 
 	auto gModel = castModel<graphModel>();
 
@@ -200,11 +199,11 @@ void Graph::drawLineAt( int _x, int _y, int _lastx )
 
 	float range = minVal - maxVal;
 	float val = ( _y*range/( height()-5 ) ) + maxVal;
-
+	
 	int sample_begin, sample_end;
 	float lastval;
 	float val_begin, val_end;
-
+	
 	if (_lastx > _x)
 	{
 		sample_begin = (int)((_x) * xscale);
@@ -221,9 +220,9 @@ void Graph::drawLineAt( int _x, int _y, int _lastx )
 		lastval = model() -> m_samples[ (int)( sample_begin ) ];
 		val_begin = lastval;
 		val_end = val;
-
+		
 	}
-
+	
 	// calculate line drawing variables
 	int linelen = sample_end - sample_begin;
 	if (linelen == 1)
@@ -269,6 +268,8 @@ void Graph::changeSampleAt( int _x, int _y )
 	model()->setSampleAt( (int)( _x*xscale ), val );
 }
 
+
+
 void Graph::mouseReleaseEvent( QMouseEvent * _me )
 {
 	if( _me->button() == Qt::LeftButton )
@@ -280,6 +281,8 @@ void Graph::mouseReleaseEvent( QMouseEvent * _me )
 		emit drawn();
 	}
 }
+
+
 
 void Graph::paintEvent( QPaintEvent * )
 {
@@ -298,6 +301,7 @@ void Graph::paintEvent( QPaintEvent * )
 
 	// Max index, more useful below
 	length--;
+
 
 	switch( m_graphStyle )
 	{
@@ -699,10 +703,10 @@ void graphModel::shiftPhase( int _deg )
 {
 	// calculate offset in samples
 	const int offset = ( _deg * length() ) / 360; //multiply first because integers
-
+	
 	// store values in temporary array
 	QVector<float> temp = m_samples;
-
+	
 	// shift phase
 	for( int i = 0; i < length(); i++ )
 	{
@@ -710,7 +714,7 @@ void graphModel::shiftPhase( int _deg )
 		while( o < 0 ) o += length();
 		m_samples[i] = temp[o];
 	}
-
+	
 	emit samplesChanged( 0, length()-1 );
 }
 
