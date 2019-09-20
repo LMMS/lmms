eebbdefcad23732fbae4759103244e03b1cb55b3
Veratil <veratil@gmail.com>
2019-09-20 13:23:31 -0500

    Handle Shift+Wheel in TrackContainerView::wheelEvent; pass scroll up from AutomationEditor if at scroll limits

--- src/gui/editors/AutomationEditor.cpp
+++ src/gui/editors/AutomationEditor.cpp
@@ -1738,13 +1738,35 @@ void AutomationEditor::wheelEvent(QWheelEvent * we )
 	}
 	else if(we->modifiers() & Qt::ShiftModifier)
 	{
-		m_leftRightScroll->setValue(m_leftRightScroll->value() -
-							we->angleDelta().y() * 2 / 15);
+		auto cur_pos = m_leftRightScroll->value();
+		auto new_pos = cur_pos - we->angleDelta().y() * 2 / 15;
+		// If we are already at the limits of scrolling value, check if the new
+		// position is less/greater than min/max in order to allow the parent
+		// to process the wheelEvent
+		if (
+			(cur_pos == m_leftRightScroll->minimum() && new_pos < m_leftRightScroll->minimum())
+			|| (cur_pos == m_leftRightScroll->maximum() && new_pos > m_leftRightScroll->maximum())
+		)
+		{
+			we->ignore();
+			return;
+		}
+		m_leftRightScroll->setValue(new_pos);
 	}
 	else
 	{
-		m_topBottomScroll->setValue(m_topBottomScroll->value() -
-							(we->angleDelta().x() + we->angleDelta().y()) / 30);
+		auto cur_pos = m_topBottomScroll->value();
+		auto new_pos = cur_pos - ((we->angleDelta().x() + we->angleDelta().y()) / 30);
+		// same as above comment for m_leftRightScroll
+		if (
+			(cur_pos == m_topBottomScroll->minimum() && new_pos < m_topBottomScroll->minimum())
+			|| (cur_pos == m_topBottomScroll->maximum() && new_pos > m_topBottomScroll->maximum())
+		)
+		{
+			we->ignore();
+			return;
+		}
+		m_topBottomScroll->setValue(new_pos);
 	}
 }
 
