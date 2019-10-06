LMMS/lmms

commit 3cb95611b7ee35e343bfddf39c6ccb83f55a616f

Give focus to AutomationEditor when the window gets the foucs (#5170)
This will make shortcuts work on opening the editor.

src/gui/editors/AutomationEditor.cpp
@@ -198,6 +198,8 @@ AutomationEditor::AutomationEditor() :
 	setCurrentPattern( NULL );
 
 	setMouseTracking( true );
+	setFocusPolicy( Qt::StrongFocus );
+	setFocus();
 }
 
 
@@ -2514,6 +2516,11 @@ void AutomationEditorWindow::clearCurrentPattern()
 {
 	m_editor->m_pattern = nullptr;
 	setCurrentPattern(nullptr);
+}
+
+void AutomationEditorWindow::focusInEvent(QFocusEvent * event)
+{
+	m_editor->setFocus( event->reason() );
 }
 
 void AutomationEditorWindow::play()

include/AutomationEditor.h
@@ -297,6 +297,9 @@ public slots:
 signals:
 	void currentPatternChanged();
 
+protected:
+	virtual void focusInEvent(QFocusEvent * event);
+
 protected slots:
 	void play();
 	void stop();

commit a8aebf862a18eb796fb3d9868406b4fb9e8a7111

Fix regression introduced in #5170

src/gui/editors/AutomationEditor.cpp
@@ -1484,7 +1484,7 @@ void AutomationEditor::paintEvent(QPaintEvent * pe )
 		m_leftRightScroll->setPageStep( l );
 	}
 
-	if( validPattern() && GuiApplication::instance()->automationEditor()->hasFocus() )
+	if(validPattern() && GuiApplication::instance()->automationEditor()->m_editor->hasFocus())
 	{
 
 		drawCross( p );