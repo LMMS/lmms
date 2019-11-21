202b7e114e1c31b9e5a08ea03111fb1a759bac90
Martin Pavelek <he29.HS@gmail.com>
2019-11-21 14:44:18 +0100

    Spectrum analyzer update (#5160)
    * advanced config: expose hidden constants to user screen
    * advanced config: add support for FFT window overlapping
    * waterfall: display at native resolution on high-DPI screens
    * waterfall: add cursor and improve label density
    * FFT: fix normalization so that 0 dBFS matches full-scale sinewave
    * FFT: decouple data acquisition from processing and display
    * FFT: separate lock for reallocation (to avoid some needless waiting)
    * moved ranges and other constants to a separate file
    * debug: better performance measurements
    * minor fixes
    * build the ringbuffer library as part of LMMS core

--- include/lmms_constants.h
+++ include/lmms_constants.h
@@ -49,4 +49,47 @@ const float F_PI_SQR = (float) LD_PI_SQR;
 const float F_E = (float) LD_E;
 const float F_E_R = (float) LD_E_R;
 
+// Frequency ranges (in Hz).
+// Arbitrary low limit for logarithmic frequency scale; >1 Hz.
+const int LOWEST_LOG_FREQ = 10;
+
+// Full range is defined by LOWEST_LOG_FREQ and current sample rate.
+enum FREQUENCY_RANGES
+{
+	FRANGE_FULL = 0,
+	FRANGE_AUDIBLE,
+	FRANGE_BASS,
+	FRANGE_MIDS,
+	FRANGE_HIGH
+};
+
+const int FRANGE_AUDIBLE_START = 20;
+const int FRANGE_AUDIBLE_END = 20000;
+const int FRANGE_BASS_START = 20;
+const int FRANGE_BASS_END = 300;
+const int FRANGE_MIDS_START = 200;
+const int FRANGE_MIDS_END = 5000;
+const int FRANGE_HIGH_START = 4000;
+const int FRANGE_HIGH_END = 20000;
+
+// Amplitude ranges (in dBFS).
+// Reference: full scale sine wave (-1.0 to 1.0) is 0 dB.
+// Doubling or halving the amplitude produces 3 dB difference.
+enum AMPLITUDE_RANGES
+{
+	ARANGE_EXTENDED = 0,
+	ARANGE_AUDIBLE,
+	ARANGE_LOUD,
+	ARANGE_SILENT
+};
+
+const int ARANGE_EXTENDED_START = -80;
+const int ARANGE_EXTENDED_END = 20;
+const int ARANGE_AUDIBLE_START = -50;
+const int ARANGE_AUDIBLE_END = 0;
+const int ARANGE_LOUD_START = -30;
+const int ARANGE_LOUD_END = 0;
+const int ARANGE_SILENT_START = -60;
+const int ARANGE_SILENT_END = -10;
+
 #endif
