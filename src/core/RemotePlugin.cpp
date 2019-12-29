464137a8640a096b4e85bb188d4133dd430e868c
Johannes Lorenz <j.git@lorenz-ho.me>
2019-12-29 10:08:29 +0100

    RemotePlugin: Take fix from stable-1.2

--- src/core/RemotePlugin.cpp
+++ src/core/RemotePlugin.cpp
@@ -99,14 +99,14 @@ RemotePlugin::RemotePlugin() :
 
 	m_socketFile = QDir::tempPath() + QDir::separator() +
 						QUuid::createUuid().toString();
-	const char * path = m_socketFile.toUtf8().constData();
-	size_t length = strlen( path );
+	const QByteArray path = m_socketFile.toUtf8();
+	size_t length = strlen( path.constData() );
 	if ( length >= sizeof sa.sun_path )
 	{
 		length = sizeof sa.sun_path - 1;
 		qWarning( "Socket path too long." );
 	}
-	memcpy( sa.sun_path, path, length );
+	memcpy( sa.sun_path, path.constData(), length );
 	sa.sun_path[length] = '\0';
 
 	m_server = socket( PF_LOCAL, SOCK_STREAM, 0 );
@@ -114,7 +114,7 @@ RemotePlugin::RemotePlugin() :
 	{
 		qWarning( "Unable to start the server." );
 	}
-	remove( path );
+	remove( path.constData() );
 	int ret = bind( m_server, (struct sockaddr *) &sa, sizeof sa );
 	if ( ret == -1 || listen( m_server, 1 ) == -1 )
 	{
