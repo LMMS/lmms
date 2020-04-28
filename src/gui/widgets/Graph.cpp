LMMS/lmms

commit 9e033994a5d09291dbc3770decb3e0e33380b8e5

Xpressive plugin (#3259)
* First Preview of the X-Pressive Plugin
(exprtk.hpp is not included, get it from my exprtk fork in the branch
internal_data_functions)
available keys:
f- note's frequency. available only in the output expressions
t- time in seconds. in the Waves (W1,W2,W3) it's in the range [0,1)
key- the note's keyboard key. available only in the output expressions.
v- the note's velocity (divided by 255.0 so it is in the range [0,1]).
available only in the output expressions.
rel- gives 0 while the key is holded, and 1 after the key release.
available only in the output expressions.
A1,A2,A3- general purpose knobs (you can control them with the
automations). available only in the output expressions.
W1,W2,W3- precalculated wave forms. can be also load from file. you can
use them only in the output expressions
available functions:
cent(x)- gives pow(2,x/1200)
rand()- random number generator. in range [-1,1). each call gives other
value.
randv(i)- random vector (with pseudo infinite integer cells). the values
are in range [-1,1). it's stays consistent only across the note
playback. so each note playback will get other vector (even on the same
key).
sinew- sine wave with period of 1.
saww- saw wave with period of 1.
squarew- square wave with period of 1.
trianglew- triangle wave with period of 1.
expw- exponent wave with period of 1.
expnw- another exponent wave with period of 1.
moogw- moog wave with period of 1.
moogsaww- moog-saw wave with period of 1.
you can use * % ^ / + - pow sin log pi etc.

* Xpressive Plug-In:
Added Release transition knob that control the "rel" variable. (the
duration of transit from 0 to 1)
Fixed some problems in the displays. (update display when changing
A1,A2,A3, clear display with invalid expression.

* X-Pressive Plug-In: Few more fixes
Changed the callbacks in exprfront.cpp to be templated.
Added help.
Added ExprTk.hpp.
some bug fixes (inf issues).
Added integrate function.

src/gui/widgets/Graph.cpp
@@ -130,14 +130,8 @@ void Graph::mouseMoveEvent ( QMouseEvent * _me )
 	x = qMax( 2, qMin( x, width()-3 ) );
 	y = qMax( 2, qMin( y, height()-3 ) );
 
-	if( qAbs( diff ) > 1 )
-	{
-		drawLineAt( x, y, m_lastCursorX );
-	}
-	else
-	{
-		changeSampleAt( x, y );
-	}
+
+	drawLineAt( x, y, m_lastCursorX );
 
 	// update mouse
 	if( diff != 0 )
@@ -209,25 +203,47 @@ void Graph::drawLineAt( int _x, int _y, int _lastx )
 
 	float range = minVal - maxVal;
 	float val = ( _y*range/( height()-5 ) ) + maxVal;
-	float lastval = model() -> m_samples[ (int)( _lastx * xscale ) ];
-
-	// calculate line drawing variables
-	int linelen = qAbs( _x - _lastx ) + 1;
-	int xstep = _x > _lastx ? -1 : 1;
-	float ystep = ( lastval - val ) / linelen;
-
-	int start = INT_MAX;
-	int end = 0;
-	// draw a line
-	for ( int i = 0; i < linelen; i++ )
+	
+	int sample_begin, sample_end;
+	float lastval;
+	float val_begin, val_end;
+	
+	if (_lastx > _x)
 	{
-		int x = (_x + (i * xstep)) * xscale;		// get x value
-		model()->drawSampleAt( x, val + (i * ystep));
-		start = qMin( start, x );
-		end = qMax( end, x );
+		sample_begin = (int)((_x) * xscale);
+		sample_end = (int)ceil((_lastx+1) * xscale);
+		lastval = model() -> m_samples[ (int)( sample_end - 1 ) ];
+		val_begin = val;
+		val_end = lastval;
+
+	}
+	else
+	{
+		sample_begin = (int)(_lastx * xscale);
+		sample_end =  (int)ceil((_x+1) * xscale);
+		lastval = model() -> m_samples[ (int)( sample_begin ) ];
+		val_begin = lastval;
+		val_end = val;
+		
 	}
 	
-	model()->samplesChanged( start, end );
+	// calculate line drawing variables
+	int linelen = sample_end - sample_begin;
+	if (linelen == 1)
+	{
+		val_begin = val;
+	}
+	//int xstep = _x > _lastx ? -1 : 1;
+	float ystep = ( val_end - val_begin ) / linelen;
+
+	// draw a line
+	for ( int i = 0 ; i < linelen; i++ )
+	{
+		model()->drawSampleAt( sample_begin + i , val_begin + ((i ) * ystep));
+	}
+
+	
+	model()->samplesChanged( sample_begin, sample_end );
 }
 
 void Graph::changeSampleAt( int _x, int _y )
@@ -265,6 +281,7 @@ void Graph::mouseReleaseEvent( QMouseEvent * _me )
 		m_mouseDown = false;
 		setCursor( Qt::CrossCursor );
 		update();
+		emit drawn();
 	}
 }
 
@@ -633,7 +650,24 @@ void graphModel::smoothNonCyclic()
 	emit samplesChanged(0, length()-1);
 }
 
-
+//makes a cyclic convolution.
+void graphModel::convolve(const float *convolution, const int convolutionLength, const int centerOffset)
+{
+	// store values in temporary array
+	QVector<float> temp = m_samples;
+	const int graphLength = length();
+	float sum;
+	for ( int i = 0; i <  graphLength; i++ )
+	{
+		sum = 0;
+		for ( int j = 0; j < convolutionLength; j++ )
+		{
+			sum += convolution[j] * temp[( i + j ) % graphLength];
+		}
+		m_samples[( i + centerOffset ) % graphLength] = sum;
+	}
+	emit samplesChanged(0, graphLength - 1);
+}
 
 void graphModel::normalize()
 {
@@ -676,7 +710,7 @@ void graphModel::invert()
 void graphModel::shiftPhase( int _deg )
 {
 	// calculate offset in samples
-	int offset = ( _deg * length() ) / 360; //multiply first because integers 
+	const int offset = ( _deg * length() ) / 360; //multiply first because integers
 	
 	// store values in temporary array
 	QVector<float> temp = m_samples;
@@ -690,6 +724,14 @@ void graphModel::shiftPhase( int _deg )
 	}
 	
 	emit samplesChanged( 0, length()-1 );
+}
+
+void graphModel::clear()
+{
+	const int graph_length = length();
+	for( int i = 0; i < graph_length; i++ )
+		m_samples[i] = 0;
+	emit samplesChanged( 0, graph_length - 1 );
 }
 
 

cmake/modules/BuildPlugin.cmake
@@ -9,7 +9,7 @@ MACRO(BUILD_PLUGIN PLUGIN_NAME)
 	CMAKE_PARSE_ARGUMENTS(PLUGIN "" "" "MOCFILES;EMBEDDED_RESOURCES;UICFILES;LINK" ${ARGN})
 	SET(PLUGIN_SOURCES ${PLUGIN_UNPARSED_ARGUMENTS})
 
-	INCLUDE_DIRECTORIES(${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/include ${CMAKE_SOURCE_DIR}/src/gui)
+	INCLUDE_DIRECTORIES(${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/include)
 
 	ADD_DEFINITIONS(-DPLUGIN_NAME=${PLUGIN_NAME})
 

commit 84b9b904aa5118229ad0b98463aab5c64ffd3a68

Fix crash on drawing line on the end of a graph (#5471)
This bug was introduced in 9e033994a5d09291dbc3770decb3e0e33380b8e5.
The function changes [sample_end, sample_begin), but emits the signal
as if [sample_end, sample_begin] has been changed. That bug made
Multitap Echo crash when tweaking the cutoff of the 32nd stage.

This commit fixes the issue by emitting sampleChanged with sample_end - 1.

src/gui/widgets/Graph.cpp
@@ -235,8 +235,9 @@ void Graph::drawLineAt( int _x, int _y, int _lastx )
 		model()->drawSampleAt( sample_begin + i , val_begin + ((i ) * ystep));
 	}
 
-	
-	model()->samplesChanged( sample_begin, sample_end );
+	// We've changed [sample_end, sample_begin)
+	// However, samplesChanged expects two end points
+	model()->samplesChanged(sample_begin, sample_end - 1);
 }
 
 void Graph::changeSampleAt( int _x, int _y )