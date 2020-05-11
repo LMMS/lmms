1cd8b7a895d77af313d8deec8765f234df8e6aed
Lukas W <lukaswhl@gmail.com>
2020-05-11 14:55:29 +0200

    Fix libcds nifty counter typo
    Thanks @PhysSong
    
    Co-authored-by: Hyunjin Song <tteu.ingog@gmail.com>

--- include/libcds.h
+++ include/libcds.h
@@ -17,6 +17,5 @@ namespace _cdslib
 	void thread_deinit();
 
 	static NiftyCounter<init, deinit> _counter;
-	static NiftyCounterTL<thread_init, thread_init> _thread_counter;
+	static NiftyCounterTL<thread_init, thread_deinit> _thread_counter;
 }
-
