0df9ca7dcb19cb8c805af0354692049a06149340
Hussam al-Homsi <sawuare@gmail.com>
2020-06-09 19:56:10 -0400

    Remove only use of macro Q_UNLIKELY()

--- include/AutomatableModel.h
+++ include/AutomatableModel.h
@@ -148,7 +148,7 @@ public:
 	template<class T>
 	inline T value( int frameOffset = 0 ) const
 	{
-		if( Q_UNLIKELY( hasLinkedModels() || m_controllerConnection != NULL ) )
+		if( hasLinkedModels() || m_controllerConnection != NULL )
 		{
 			return castValue<T>( controllerValue( frameOffset ) );
 		}
