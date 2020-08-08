95d54fae037d1fd4c12c7671cc5511523361c6c1
Johannes Lorenz <j.git@lorenz-ho.me>
2020-08-08 12:19:35 +0200

    UridMap::map fixes
    * Move string creation into try-block
    * Extend map mutex around unordered_map::find call

--- src/core/lv2/Lv2UridMap.cpp
+++ src/core/lv2/Lv2UridMap.cpp
@@ -50,33 +50,39 @@ UridMap::UridMap()
 LV2_URID UridMap::map(const char *uri)
 {
 	LV2_URID result = 0u;
-	auto itr = m_map.find(uri);
 
-	if (itr == m_map.end())
+	// the Lv2 docs say that 0 should be returned in any case
+	// where creating an ID for the given URI fails
+	try
 	{
-		try
-		{
-			std::string uriStr = uri;
+		// TODO:
+		// when using C++14, we can get around any string allocation
+		// in the case the URI is already inside the map:
+		// * use `m_map.find(uri)` instead of `m_map.find(uriStr)`
+		// * to avoid temporary string construction in the `find` call, create
+		//   m_map like this:
+		//   std::unordered_map<std::string, LV2_URID,
+		//     std::hash<std::string>, std::equal<>> m_map;
+		// * move the try block inside the case where the URI is not in the map
+		const std::string uriStr = uri;
 
-			// the URID map is global, so mutex it
-			std::lock_guard<std::mutex> guard (m_MapMutex);
+		std::lock_guard<std::mutex> guard (m_MapMutex);
+
+		auto itr = m_map.find(uriStr);
+		if (itr == m_map.end())
+		{
+			// 1 is the first free URID
+			std::size_t index = 1u + m_unMap.size();
+			auto pr = m_map.emplace(std::move(uriStr), index);
+			if (pr.second)
 			{
-				// 1 is the first free URID
-				std::size_t index = 1u + m_unMap.size();
-				auto pr = m_map.emplace(std::move(uriStr), index);
-				if (pr.second)
-				{
-					m_unMap.emplace_back(pr.first->first.c_str());
-					result = static_cast<LV2_URID>(index);
-				}
+				m_unMap.emplace_back(pr.first->first.c_str());
+				result = static_cast<LV2_URID>(index);
 			}
 		}
-		catch(...) { /* result variable is already 0 */ }
+		else { result = itr->second; }
 	}
-	else
-	{
-		result = itr->second;
-	}
+	catch(...) { /* result variable is already 0 */ }
 
 	return result;
 }
