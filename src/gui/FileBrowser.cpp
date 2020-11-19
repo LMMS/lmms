2544917f65ad8d7a7510529a44c1496032b3c332
Thomas Mueller <64359888+thmueller64@users.noreply.github.com>
2020-11-19 18:28:48 +0100

    Remove object name and replace by descendant selector

--- src/gui/FileBrowser.cpp
+++ src/gui/FileBrowser.cpp
@@ -76,7 +76,6 @@ void FileBrowser::addContentCheckBox()
 
 	auto configCheckBox = [this, &filterWidgetLayout](QCheckBox* box)
 	{
-		box->setObjectName("FileBrowserFilterCheckBox");
 		box->setCheckState(Qt::Checked);
 		connect(box, SIGNAL(stateChanged(int)), this, SLOT(reloadTree()));
 		filterWidgetLayout->addWidget(box);
