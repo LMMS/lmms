985041b115ec4917e29066f5227800960510c083
Dan Giddins <daniel.giddins112@gmail.com>
2021-04-02 18:02:10 +0100

    various changes

--- src/sys/Time.cpp
+++ src/sys/Time.cpp
@@ -33,22 +33,22 @@
 #ifndef TIME
 #define TIME
 
-int gettimeofday(struct timeval* t, void* timezone)
-{
-	struct _timeb timebuffer;
-	_ftime(&timebuffer);
-	t->tv_sec = timebuffer.time;
-	t->tv_usec = 1000 * timebuffer.millitm;
-	return 0;
-}
-
-clock_t times(struct tms* __buffer) {
-
-	__buffer->tms_utime = clock();
-	__buffer->tms_stime = 0;
-	__buffer->tms_cstime = 0;
-	__buffer->tms_cutime = 0;
-	return __buffer->tms_utime;
-}
+//int gettimeofday(struct timeval* t, void* timezone)
+//{
+//	struct _timeb timebuffer;
+//	_ftime(&timebuffer);
+//	t->tv_sec = timebuffer.time;
+//	t->tv_usec = 1000 * timebuffer.millitm;
+//	return 0;
+//}
+//
+//clock_t times(struct tms* __buffer) {
+//
+//	__buffer->tms_utime = clock();
+//	__buffer->tms_stime = 0;
+//	__buffer->tms_cstime = 0;
+//	__buffer->tms_cutime = 0;
+//	return __buffer->tms_utime;
+//}
 
 #endif
