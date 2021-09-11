6facbeeb96eddcbd9a9bb3af732dd31c7f601b9c
Levin Oehlmann <irrenhaus3@gmail.com>
2021-09-11 14:07:42 +0200

    Convert LMMS_BUILDCONF macros in versioninfo.h to string literals.

--- include/versioninfo.h
+++ include/versioninfo.h
@@ -1,61 +1,49 @@
 #include "lmms_basics.h"
 
 #ifdef __GNUC__
-#define LMMS_BUILDCONF_COMPILER_VERSION "GCC " __VERSION__
+constexpr const char* LMMS_BUILDCONF_COMPILER_VERSION = "GCC " __VERSION__;
 #elif defined(_MSC_VER)
-#define LMMS_BUILDCONF_COMPILER_VERSION "MSVC " STRINGIFY(_MSC_FULL_VER)
+constexpr const char* LMMS_BUILDCONF_COMPILER_VERSION = "MSVC " STRINGIFY(_MSC_FULL_VER);
 #else
-#define LMMS_BUILDCONF_COMPILER_VERSION "unknown compiler"
+constexpr const char* LMMS_BUILDCONF_COMPILER_VERSION = "unknown compiler";
 #endif
 
 #ifdef LMMS_HOST_X86
-#define LMMS_BUILDCONF_MACHINE "i386"
-#endif
-
-#ifdef LMMS_HOST_X86_64
-#define LMMS_BUILDCONF_MACHINE "x86_64"
-#endif
-
-#ifdef LMMS_HOST_ARM32
-#define LMMS_BUILDCONF_MACHINE "arm32"
-#endif
-
-#ifdef LMMS_HOST_ARM64
-#define LMMS_BUILDCONF_MACHINE "arm64"
-#endif
-
-#ifdef LMMS_HOST_RISCV32
-#define LMMS_BUILDCONF_MACHINE "riscv32"
-#endif
-
-#ifdef LMMS_HOST_RISCV64
-#define LMMS_BUILDCONF_MACHINE "riscv64"
-#endif
-
-#ifndef LMMS_BUILDCONF_MACHINE
-#define LMMS_BUILDCONF_MACHINE "unknown processor"
+constexpr const char* LMMS_BUILDCONF_MACHINE = "i386";
+#elif defined(LMMS_HOST_X86_64)
+constexpr const char* LMMS_BUILDCONF_MACHINE = "x86_64";
+#elif defined(LMMS_HOST_ARM32)
+constexpr const char* LMMS_BUILDCONF_MACHINE = "arm32";
+#elif defined(LMMS_HOST_ARM64)
+constexpr const char* LMMS_BUILDCONF_MACHINE = "arm64";
+#elif defined(LMMS_HOST_RISCV32)
+constexpr const char* LMMS_BUILDCONF_MACHINE = "riscv32";
+#elif defined(LMMS_HOST_RISCV64)
+constexpr const char* LMMS_BUILDCONF_MACHINE = "riscv64";
+#else
+constexpr const char* LMMS_BUILDCONF_MACHINE = "unknown processor";
 #endif
 
 #ifdef LMMS_BUILD_LINUX
-#define LMMS_BUILDCONF_PLATFORM "Linux"
+constexpr const char* LMMS_BUILDCONF_PLATFORM = "Linux";
 #endif
 
 #ifdef LMMS_BUILD_APPLE
-#define LMMS_BUILDCONF_PLATFORM "OS X"
+constexpr const char* LMMS_BUILDCONF_PLATFORM = "OS X";
 #endif
 
 #ifdef LMMS_BUILD_OPENBSD
-#define LMMS_BUILDCONF_PLATFORM "OpenBSD"
+constexpr const char* LMMS_BUILDCONF_PLATFORM = "OpenBSD";
 #endif
 
 #ifdef LMMS_BUILD_FREEBSD
-#define LMMS_BUILDCONF_PLATFORM "FreeBSD"
+constexpr const char* LMMS_BUILDCONF_PLATFORM = "FreeBSD";
 #endif
 
 #ifdef LMMS_BUILD_WIN32
-#define LMMS_BUILDCONF_PLATFORM "win32"
+constexpr const char* LMMS_BUILDCONF_PLATFORM = "win32";
 #endif
 
 #ifdef LMMS_BUILD_HAIKU
-#define LMMS_BUILDCONF_PLATFORM "Haiku"
+constexpr const char* LMMS_BUILDCONF_PLATFORM = "Haiku";
 #endif
