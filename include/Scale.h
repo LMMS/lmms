671878de84895c394365e0d25b552ad066d98176
Martin Pavelek <he29.hs@gmail.com>
2021-10-28 22:58:07 +0200

    Replace deprecated sprintf() function

--- include/Scale.h
+++ include/Scale.h
@@ -45,7 +45,7 @@ public:
 	QString getString() const
 	{
 		if (m_denominator) {return QString::number(m_numerator) + "/" + QString::number(m_denominator);}
-		else {return QString().sprintf("%.4f", m_cents);}
+		else {return QString("%1").arg(m_cents, 0, 'f', 4);}
 	}
 
 	void saveSettings(QDomDocument &doc, QDomElement &element) override;
