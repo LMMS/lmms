LMMS/lmms

commit 7db3fa94a103e2561de397c796b3b28ebd1112b5

Improve includes (#6320)
* Update ringbuffer submodule to fix includes

* Remove cyclic includes

* Remove Qt include prefixes

* Include C++ versions of C headers

E.g.: assert.h -> cassert

* Move CLIP_BORDER_WIDTH into ClipView

This allows to remove includes to TrackView.h in ClipView cpp files.

* Elliminate useless includes

This improves the include structure by elliminating includes that are
not used. Most of this was done by using `include-what-you-use` with
`CMAKE_C_INCLUDE_WHAT_YOU_USE` and `CMAKE_CXX_INCLUDE_WHAT_YOU_USE`
set to (broken down here):

```
include-what-you-use;
    -Xiwyu;--mapping_file=/usr/share/include-what-you-use/qt5_11.imp;
    -Xiwyu;--keep=*/xmmintrin.h;
    -Xiwyu;--keep=*/lmmsconfig.h;
    -Xiwyu;--keep=*/weak_libjack.h;
    -Xiwyu;--keep=*/sys/*;
    -Xiwyu;--keep=*/debug.h;
    -Xiwyu;--keep=*/SDL/*;
    -Xiwyu;--keep=*/alsa/*;
    -Xiwyu;--keep=*/FL/x.h;
    -Xiwyu;--keep=*/MidiApple.h;
    -Xiwyu;--keep=*/MidiWinMM.h;
    -Xiwyu;--keep=*/AudioSoundIo.h
```

* Fixup: Remove empty #if-#ifdef pairs

* Remove LMMS_HAVE_STD(LIB|INT)_H

include/AudioSoundIo.h
@@ -25,10 +25,8 @@
 #ifndef AUDIO_SOUNDIO_H
 #define AUDIO_SOUNDIO_H
 
-#include <QtCore/QObject>
 
 #include "lmmsconfig.h"
-#include "ComboBoxModel.h"
 
 #ifdef LMMS_HAVE_SOUNDIO
 

src/core/MixHelpers.cpp
@@ -24,12 +24,12 @@
 
 #include "MixHelpers.h"
 
-#include <cstdio>
 
-#include "lmms_math.h"
+#include <cmath>
+#include <QtGlobal>
+
 #include "ValueBuffer.h"
 
-#include <cstdio>
 
 
 static bool s_NaNHandler;

commit 80a6672da46a58dd6865a3dda8a7a4f05776393e

Fix build due to missing includes
Fixes build errors introduced in
7db3fa94a103e2561de397c796b3b28ebd1112b5

include/AudioSoundIo.h
@@ -25,8 +25,10 @@
 #ifndef AUDIO_SOUNDIO_H
 #define AUDIO_SOUNDIO_H
 
+#include <QObject>
 
 #include "lmmsconfig.h"
+#include "ComboBoxModel.h"
 
 #ifdef LMMS_HAVE_SOUNDIO
 

src/core/MixHelpers.cpp
@@ -24,6 +24,9 @@
 
 #include "MixHelpers.h"
 
+#ifdef LMMS_DEBUG
+#include <cstdio>
+#endif
 
 #include <cmath>
 #include <QtGlobal>
@@ -102,6 +105,7 @@ bool sanitize( sampleFrame * src, int frames )
 			if( std::isinf( src[f][c] ) || std::isnan( src[f][c] ) )
 			{
 				#ifdef LMMS_DEBUG
+					// TODO don't use printf here
 					printf("Bad data, clearing buffer. frame: ");
 					printf("%d: value %f\n", f, src[f][c]);
 				#endif