bac3732481c211acb2643fb342f06c66673580bd
sakertooth <sakertooth@gmail.com>
2022-08-01 17:48:41 -0400

    Replace QVector with std::vector in AudioEngine

--- include/AudioEngine.h
+++ include/AudioEngine.h
@@ -32,10 +32,10 @@
 #endif
 
 #include <QThread>
-#include <QVector>
 #include <QWaitCondition>
 #include <samplerate.h>
 
+#include <vector>
 
 #include "lmms_basics.h"
 #include "LocklessList.h"
@@ -416,7 +416,7 @@ private:
 
 	bool m_renderOnly;
 
-	QVector<AudioPort *> m_audioPorts;
+	std::vector<AudioPort *> m_audioPorts;
 
 	fpp_t m_framesPerPeriod;
 
@@ -430,7 +430,7 @@ private:
 	surroundSampleFrame * m_outputBufferWrite;
 
 	// worker thread stuff
-	QVector<AudioEngineWorkerThread *> m_workers;
+	std::vector<AudioEngineWorkerThread *> m_workers;
 	int m_numWorkers;
 
 	// playhandle stuff
