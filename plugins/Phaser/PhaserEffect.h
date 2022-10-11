6211fcda13be6f94328f64c2b4f749f32b182261
Robert Daniel Black <r94231@gmail.com>
2022-10-11 12:26:16 -0700

    Replace oversampling vectors with arrays

--- plugins/Phaser/PhaserEffect.h
+++ plugins/Phaser/PhaserEffect.h
@@ -100,11 +100,11 @@ private:
 
 	float m_aliasFlip = 1;
 	
-	LinkwitzRiley<2>* m_oversampleAnalogIn1[2];
-	LinkwitzRiley<2>* m_oversampleAnalogIn2[2];
-	LinkwitzRiley<2>* m_oversampleAnalogOut[2];
-	LinkwitzRiley<2>* m_oversampleFeedbackIn[2];
-	LinkwitzRiley<2>* m_oversampleFeedbackOut[2];
+	LinkwitzRiley<2> * m_oversampleAnalogIn1[2];
+	LinkwitzRiley<2> * m_oversampleAnalogIn2[2];
+	LinkwitzRiley<2> * m_oversampleAnalogOut[2];
+	LinkwitzRiley<2> * m_oversampleFeedbackIn[2];
+	LinkwitzRiley<2> * m_oversampleFeedbackOut[2];
 
 	friend class PhaserControls;
 
