fb948051d71a730349b937b1876ab67238ec12a8
sakertooth <sakertooth@gmail.com>
2022-12-17 22:31:00 -0500

    Begin reimplementing MixerChannelView
    MixerChannelView is now a combination of the
    MixerLine with the previous MixerChannelView

---
+++ include/MixerChannelView.h
@@ -0,0 +1,94 @@
+/*
+ * MixerChannelView.h - the mixer channel view
+ *
+ * Copyright (c) 2022 saker <sakertooth@gmail.com>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef MIXER_CHANNEL_VIEW_H
+#define MIXER_CHANNEL_VIEW_H
+
+#include "EffectRackView.h"
+#include "Fader.h"
+#include "Knob.h"
+#include "LcdWidget.h"
+#include "PixmapButton.h"
+#include "SendButtonIndicator.h"
+
+#include <QWidget>
+#include <QPixmap>
+#include <QLabel>
+#include <QLineEdit>
+
+namespace lmms::gui 
+{
+    constexpr int MIXER_CHANNEL_WIDTH = 53;
+    constexpr int MIXER_CHANNEL_INNER_BORDER_SIZE = 3;
+    constexpr int MIXER_CHANNEL_OUTER_BORDER_SIZE = 1;
+    
+    class MixerChannelView : public QWidget
+    {
+        Q_OBJECT
+    public:
+        enum class SendReceiveState 
+        {
+            None, Send, Receive
+        };
+
+        MixerChannelView(int channelIndex, MixerView* mixerView, QWidget* parent = nullptr);
+        void contextMenuEvent(QContextMenuEvent*) override;
+        void mousePressEvent(QMouseEvent*) override;
+	    void mouseDoubleClickEvent(QMouseEvent*) override;
+
+        int channelIndex() const;
+        void setChannelIndex(int index);
+
+        SendReceiveState sendReceiveState() const;
+        void setSendReceiveState(const SendReceiveState& state);
+        
+        QSize sizeHint() const override;
+        bool eventFilter (QObject *dist, QEvent *event) override;
+    
+    public slots:
+        void renameChannel();
+        void resetColor();
+        void selectColor();
+        void randomizeColor();
+
+    private slots:
+        void renameFinished();
+        void removeChannel();
+        void removeUnusedChannels();
+        void moveChannelLeft();
+        void moveChannelRight();
+
+    private:
+        QString elideName(const QString& name);
+
+    private:
+        MixerView* m_mixerView;
+        SendReceiveState m_sendReceiveState = SendReceiveState::None;
+        int m_channelIndex = 0;
+        bool m_inRename = false;
+        friend class MixerView;
+    };
+} // namespace lmms::gui
+
+#endif
---
+++ src/gui/widgets/MixerChannelView.cpp
@@ -0,0 +1,226 @@
+/*
+ * MixerChannelView.h - the mixer channel view
+ *
+ * Copyright (c) 2022 saker <sakertooth@gmail.com>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include "CaptionMenu.h"
+#include "Mixer.h"
+#include "MixerChannelView.h"
+#include "MixerView.h"
+#include "gui_templates.h"
+#include "lmms_math.h"
+
+#include <QGraphicsProxyWidget>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QMenu>
+#include <QPainter>
+
+namespace lmms::gui
+{
+    MixerChannelView::MixerChannelView(int channelIndex, MixerView* mixerView, QWidget* parent) : 
+        QWidget(parent),
+        m_mixerView(mixerView),
+        m_channelIndex(channelIndex)
+    {
+        // assert(dynamic_cast<MixerView*>(parent) != nullptr);
+
+        auto sendButton = new SendButtonIndicator{nullptr, this, mixerView}; 
+        auto sendKnob = new Knob{knobBright_26, nullptr, tr("Channel send amount")};
+
+        auto channelNumberLcd = new LcdWidget{2, nullptr};
+        channelNumberLcd->setValue(channelIndex);
+
+        auto mixerChannel = Engine::mixer()->mixerChannel(channelIndex);
+        auto mixerName = mixerChannel->m_name;
+    	setToolTip(mixerName);
+
+        auto renameLineEdit = new QLineEdit{};
+        renameLineEdit->setText(mixerName);
+        renameLineEdit->setFont(pointSizeF(font(), 7.5f));
+    	renameLineEdit->installEventFilter(this);
+
+        auto renameLineEditScene = new QGraphicsScene{};        
+        auto renameLineEditView = new QGraphicsView{};
+        renameLineEditView->setStyleSheet("border-style: none; background: transparent;");
+        renameLineEditView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        renameLineEditView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        renameLineEditView->setAttribute(Qt::WA_TransparentForMouseEvents, true);
+        renameLineEditView->setScene(renameLineEditScene);
+        
+        auto renameLineEditProxy = renameLineEditScene->addWidget(renameLineEdit);
+        renameLineEditProxy->setRotation(-90);
+
+        auto sendArrow = new QLabel{};
+        sendArrow->setPixmap(embed::getIconPixmap("send_bg_arrow"));
+        sendArrow->setVisible(m_sendReceiveState == SendReceiveState::Send);
+
+        auto receiveArrow = new QLabel{};
+        receiveArrow->setPixmap(embed::getIconPixmap("receive_bg_arrow"));
+        receiveArrow->setVisible(m_sendReceiveState == SendReceiveState::Receive);
+
+        auto muteBtn = new PixmapButton(nullptr, tr("Mute"));
+        muteBtn->setModel(&mixerChannel->m_muteModel);
+        muteBtn->setActiveGraphic(embed::getIconPixmap("led_off"));
+        muteBtn->setInactiveGraphic(embed::getIconPixmap("led_green"));
+        muteBtn->setCheckable(true);
+        muteBtn->setToolTip(tr("Mute this channel"));
+
+        auto soloBtn = new PixmapButton(nullptr, tr("Solo"));
+        soloBtn->setModel(&mixerChannel->m_soloModel);
+        soloBtn->setActiveGraphic(embed::getIconPixmap("led_red"));
+        soloBtn->setInactiveGraphic(embed::getIconPixmap("led_off"));
+        soloBtn->setCheckable(true);
+        connect(&mixerChannel->m_soloModel, SIGNAL(dataChanged()), mixerView, SLOT(toggledSolo()), Qt::DirectConnection);
+        soloBtn->setToolTip(tr("Solo this channel"));
+
+        auto fader = new Fader{&mixerChannel->m_volumeModel, tr("Fader %1").arg(channelIndex), nullptr};
+        fader->setLevelsDisplayedInDBFS();
+        fader->setMinPeak(dbfsToAmp(-42));
+        fader->setMaxPeak(dbfsToAmp(9));
+
+        auto mainLayout = new QVBoxLayout{this};
+        mainLayout->addWidget(receiveArrow, 0, Qt::AlignHCenter);
+        mainLayout->addWidget(sendButton, 0, Qt::AlignHCenter);
+        mainLayout->addWidget(sendKnob, 0, Qt::AlignHCenter);
+        mainLayout->addWidget(sendArrow, 0, Qt::AlignHCenter);
+        mainLayout->addWidget(channelNumberLcd, 0, Qt::AlignHCenter);
+        mainLayout->addStretch();
+        mainLayout->addWidget(renameLineEditView, 0, Qt::AlignHCenter);
+        mainLayout->addStretch();
+        mainLayout->addWidget(soloBtn, 0, Qt::AlignHCenter);
+        mainLayout->addWidget(muteBtn, 0, Qt::AlignHCenter);
+        mainLayout->addWidget(fader, 0, Qt::AlignHCenter);
+
+        connect(renameLineEdit, &QLineEdit::editingFinished, this, &MixerChannelView::renameFinished);
+        connect(&Engine::mixer()->mixerChannel(m_channelIndex)->m_muteModel, SIGNAL(dataChanged()), this, SLOT(update()));
+    }
+
+    void MixerChannelView::contextMenuEvent(QContextMenuEvent*)
+    {
+        QPointer<CaptionMenu> contextMenu = new CaptionMenu( Engine::mixer()->mixerChannel( m_channelIndex )->m_name, this );
+        if( m_channelIndex != 0 ) // no move-options in master
+        {
+            contextMenu->addAction( tr( "Move &left" ),	this, SLOT(moveChannelLeft()));
+            contextMenu->addAction( tr( "Move &right" ), this, SLOT(moveChannelRight()));
+        }
+        contextMenu->addAction( tr( "Rename &channel" ), this, SLOT(renameChannel()));
+        contextMenu->addSeparator();
+
+        if( m_channelIndex != 0 ) // no remove-option in master
+        {
+            contextMenu->addAction( embed::getIconPixmap( "cancel" ), tr( "R&emove channel" ), this, SLOT(removeChannel()));
+            contextMenu->addSeparator();
+        }
+        contextMenu->addAction( embed::getIconPixmap( "cancel" ), tr( "Remove &unused channels" ), this, SLOT(removeUnusedChannels()));
+        contextMenu->addSeparator();
+
+        QMenu colorMenu(tr("Color"), this);
+        colorMenu.setIcon(embed::getIconPixmap("colorize"));
+        colorMenu.addAction(tr("Change"), this, SLOT(selectColor()));
+        colorMenu.addAction(tr("Reset"), this, SLOT(resetColor()));
+        colorMenu.addAction(tr("Pick random"), this, SLOT(randomizeColor()));
+        contextMenu->addMenu(&colorMenu);
+
+        contextMenu->exec( QCursor::pos() );
+        delete contextMenu;
+    }
+
+    int MixerChannelView::channelIndex() const 
+    {
+        return m_channelIndex;
+    }
+    
+    void MixerChannelView::setChannelIndex(int index) 
+    {
+        m_channelIndex = index;
+    }
+
+    MixerChannelView::SendReceiveState MixerChannelView::sendReceiveState() const 
+    {
+        return m_sendReceiveState;
+    }
+
+    void MixerChannelView::setSendReceiveState(const SendReceiveState& state) 
+    {
+        m_sendReceiveState = state;        
+        switch (state) 
+        {
+            case SendReceiveState::Send:
+                break;
+            case SendReceiveState::Receive:
+                break;
+        }
+    }
+    
+    QSize MixerChannelView::sizeHint() const
+    {
+        const auto parentHeight = static_cast<QWidget*>(parent())->height();
+        return QSize{MIXER_CHANNEL_WIDTH, parentHeight};
+    }
+
+    void MixerChannelView::renameChannel() 
+    {
+
+    }
+
+    void MixerChannelView::renameFinished() 
+    {
+
+    }
+
+    void MixerChannelView::resetColor() 
+    {
+
+    }
+
+    void MixerChannelView::selectColor() 
+    {
+
+    }
+
+    void MixerChannelView::randomizeColor() 
+    {
+
+    }
+
+    void MixerChannelView::removeChannel() 
+    {
+
+    }
+
+    void MixerChannelView::removeUnusedChannels() 
+    {
+
+    }
+
+    void MixerChannelView::moveChannelLeft() 
+    {
+
+    }
+
+    void MixerChannelView::moveChannelRight() 
+    {
+
+    }
+    
+} // namespace lmms::gui
