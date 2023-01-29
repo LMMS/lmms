4992b2a201bd44f5627dba7b621baafd3926dec8
Dalton Messmer <messmerd97@gmail.com>
2023-01-28 22:54:43 -0500

    Refactor ClapManager

--- include/ClapManager.h
+++ include/ClapManager.h
@@ -1,7 +1,7 @@
 /*
  * ClapManager.h - Implementation of ClapManager class
  *
- * Copyright (c) 2022 Dalton Messmer <messmer/dot/dalton/at/gmail/dot/com>
+ * Copyright (c) 2023 Dalton Messmer <messmer.dalton/at/gmail.com>
  *
  * This file is part of LMMS - https://lmms.io
  *
@@ -30,6 +30,7 @@
 #ifdef LMMS_HAVE_CLAP
 
 #include <filesystem>
+#include <vector>
 #include <map>
 #include <set>
 #include <clap/clap.h>
@@ -41,57 +42,138 @@
 namespace lmms
 {
 
-//! Class to keep track of all CLAP plugins
+//! Manages CLAP plugins and implements CLAP host
 class ClapManager
 {
 public:
-	void initPlugins();
-
 	ClapManager();
 	~ClapManager();
 
-	//! Class representing info for one plugin
-	class ClapInfo
+	struct ClapPluginDeleter
+	{
+		void operator()(const clap_plugin_t* p) const noexcept { p->destroy(p); }
+	};
+	using ClapPluginPtr = std::unique_ptr<const clap_plugin_t, ClapPluginDeleter>;
+
+	//! Class representing info for one .clap file, which contains 1 or more CLAP plugins
+	class ClapFile
 	{
 	public:
-		//! use only for std::map internals
-		ClapInfo() : m_library(nullptr) {}
-		//! ctor used inside ClapManager
-		ClapInfo(const std::shared_ptr<QLibrary>& lib, const clap_plugin_descriptor_t* desc, Plugin::PluginTypes type, bool valid)
-			: m_library(lib), m_descriptor(desc), m_type(type), m_valid(valid) {}
-		ClapInfo(ClapInfo&& other) = default;
-		ClapInfo& operator=(ClapInfo&& other) = default;
+		//! Loads .clap file and plugin info
+		ClapFile(const ClapManager& manager, std::filesystem::path&& filename);
+		~ClapFile() = default;
 
-		std::shared_ptr<QLibrary> library() const { return m_library; }
-		const clap_plugin_descriptor_t* descriptor() const { return m_descriptor; }
-		Plugin::PluginTypes type() const { return m_type; }
-		bool isValid() const { return m_valid; }
+		ClapFile(const ClapFile&) = delete;
+		ClapFile(ClapFile&& other) noexcept;
+		ClapFile& operator=(const ClapFile&) = delete;
+		ClapFile& operator=(ClapFile&&) noexcept = delete;
+
+		//! Represents a CLAP plugin within a .clap file
+		class ClapPlugin
+		{
+		public:
+			//! Loads plugin info but does not activate
+			ClapPlugin(const ClapFile& parent, uint32_t index, const clap_plugin_descriptor_t* desc);
+
+			~ClapPlugin() = default;
+			ClapPlugin(const ClapPlugin&) = delete;
+			ClapPlugin(ClapPlugin&& other) noexcept;
+			ClapPlugin& operator=(const ClapPlugin&) = delete;
+			ClapPlugin& operator=(ClapPlugin&&) noexcept = delete;
+
+			auto activate() -> bool;
+			void deactivate();
+			auto isValid() const -> bool { return m_valid; }
+			auto isActivated() const -> bool { return m_plugin != nullptr; }
+
+			auto getParent() const -> const ClapFile& { return m_parent; }
+			auto getIndex() const -> uint32_t { return m_index; }
+			auto getType() const -> Plugin::PluginTypes { return m_type; }
+			auto getDescriptor() const -> const clap_plugin_descriptor_t* { return m_descriptor; }
+			auto getPlugin() const -> const clap_plugin_t* { return m_plugin.get(); }
+
+		private:
+			// Are set when the .clap file is loaded:
+			const ClapFile& m_parent;
+			const clap_host_t* m_host{nullptr};
+			uint32_t m_index{0}; //!< Plugin index within the .clap file
+			const clap_plugin_descriptor_t* m_descriptor{nullptr};
+			Plugin::PluginTypes m_type{Plugin::PluginTypes::Undefined};
+			bool m_valid{false};
+
+			//! Is set after plugin activization
+			ClapPluginPtr m_plugin{nullptr};
+		};
+
+		auto load() -> bool;
+		void unload();
+		auto getParent() const -> const ClapManager& { return m_parent; }
+		auto getFilename() const -> const std::filesystem::path& { return m_filename; }
+		auto getFactory() const -> const clap_plugin_factory_t* { return m_factory; }
+
+		//! Only includes plugins that successfully loaded
+		auto getPlugins() const -> const std::vector<ClapPlugin>& { return m_plugins; }
+
+		//! Includes plugins that failed to load
+		auto getPluginCount() const -> uint32_t { return m_pluginCount; }
+		auto isValid() const -> bool { return m_valid; }
 
 	private:
+		// Are set when the .clap file is loaded:
+		const ClapManager& m_parent;
+		std::filesystem::path m_filename;
 		std::shared_ptr<QLibrary> m_library;
-		const clap_plugin_descriptor_t* m_descriptor;
-		Plugin::PluginTypes m_type;
+		const clap_plugin_factory_t* m_factory{nullptr};
+		std::vector<ClapPlugin> m_plugins; //!< Only includes plugins that successfully loaded
+		uint32_t m_pluginCount{0}; //!< Includes plugins that failed to load
 		bool m_valid{false};
 	};
 
-	// CLAP API plugin -> host
+	using ClapPlugin = ClapFile::ClapPlugin;
+
+	auto getHost() const -> const clap_host* { return &m_host; }
+
+	// CLAP API implementation: plugin -> host
 	static const void* hostGetExtension(const clap_host* host, const char* extension);
 	static void hostRequestCallback(const clap_host* host);
 	static void hostRequestProcess(const clap_host* host);
 	static void hostRequestRestart(const clap_host* host);
 
+	// For iterating over m_clapInfoMap
+	//auto begin() { return m_clapInfoMap.begin(); }
+	//auto end() { return m_clapInfoMap.end(); }
+
+	//! Return descriptor with URI @p uri or nullptr if none exists
+	const clap_plugin_t* getPlugin(const std::string& uri);
+	//! Return descriptor with URI @p uri or nullptr if none exists
+	const clap_plugin_t* getPlugin(const QString& uri);
+
+	void initPlugins(); //!< Called by Engine at LMMS startup
+
 private:
-	// data
-	bool m_debug; //!< if set, debug output will be printed
-	std::set<std::filesystem::path> m_pluginPaths;
-	clap_host m_host;
-	int m_pluginIndex;
-	std::vector<ClapInfo> m_loadedPlugins;
 
-	// functions
+	clap_host_t m_host; //!< CLAP host info and functions for plugins to use
+	std::vector<std::filesystem::path> m_searchPaths; //!< Owns all CLAP search paths; Populated by findSearchPaths()
+	std::vector<ClapFile> m_clapFiles; //!< Owns all loaded .clap files; Populated by findClapFiles()
 
-	//! Finds all ".clap" files in CLAP search paths
-	void findPlugins();
+	// Non-owning plugin caches (for fast iteration)
+	std::vector<const clap_plugin_t*> m_plugins; //!< Non-owning vector of all loaded CLAP plugins
+	//std::vector<const clap_plugin_t*> m_instrumentPlugins; //!< Non-owning vector of all loaded CLAP instrument plugins
+	//std::vector<const clap_plugin_t*> m_effectPlugins; //!< Non-owning vector of all loaded CLAP effect plugins
+
+	static bool m_debug; //!< If set, debug output will be printed
+
+	//! Finds all CLAP search paths and populates m_searchPaths
+	void findSearchPaths();
+
+	//! Returns search paths found by prior call to findSearchPaths()
+	auto getSearchPaths() const -> const std::vector<std::filesystem::path>& { return m_searchPaths; }
+
+	//! Finds and loads all .clap files in the provided search paths @p searchPaths
+	void findClapFiles(const std::vector<std::filesystem::path>& searchPaths);
+
+	//! Returns ClapFile vector found by prior call to findClapFiles()
+	auto getClapFiles() const -> const std::vector<ClapFile>& { return m_clapFiles; }
 };
 
 
--- src/core/clap/ClapManager.cpp
+++ src/core/clap/ClapManager.cpp
@@ -1,7 +1,7 @@
 /*
  * ClapManager.cpp - Implementation of ClapManager class
  *
- * Copyright (c) 2022 Dalton Messmer <messmer/dot/dalton/at/gmail/dot/com>
+ * Copyright (c) 2023 Dalton Messmer <messmer.dalton/at/gmail.com>
  *
  * This file is part of LMMS - https://lmms.io
  *
@@ -64,6 +64,11 @@ namespace
 	}
 }
 
+////////////////////////////////
+// ClapManager
+////////////////////////////////
+
+bool ClapManager::m_debug = false;
 
 ClapManager::ClapManager()
 {
@@ -81,16 +86,22 @@ ClapManager::ClapManager()
 	m_host.request_process = ClapManager::hostRequestProcess;
 	m_host.request_restart = ClapManager::hostRequestRestart;
 
-	m_pluginIndex = 0;
+	// TODO
 }
-
 
 ClapManager::~ClapManager()
 {
 }
 
+void ClapManager::initPlugins()
+{
+	findSearchPaths();
+	if (m_debug)
+		qDebug() << "Found .clap files:";
+	findClapFiles(getSearchPaths());
+}
 
-void ClapManager::findPlugins()
+void ClapManager::findSearchPaths()
 {
 	// FROM CLAP DOCUMENTATION:
 	// CLAP plugins standard search path:
@@ -115,7 +126,7 @@ void ClapManager::findPlugins()
 	// Each directory should be recursively searched for files and/or bundles as appropriate in your OS
 	// ending with the extension `.clap`.
 
-	std::set<std::filesystem::path> searchPaths;
+	m_searchPaths.clear();
 
 	// Get CLAP_PATH paths
 	if (const char* clapPathTemp = getenv("CLAP_PATH"))
@@ -127,7 +138,7 @@ void ClapManager::findPlugins()
 		{
 			std::filesystem::path path{expandHomeDir(clapPath.substr(0, pos))};
 			if (std::filesystem::is_directory(path, ec))
-				searchPaths.insert(path.make_preferred());
+				m_searchPaths.emplace_back(std::move(path.make_preferred()));
 			clapPath = clapPath.substr(pos + 1);
 			pos = clapPath.find(LADSPA_PATH_SEPERATOR);
 		}
@@ -135,7 +146,7 @@ void ClapManager::findPlugins()
 		{
 			std::filesystem::path path{expandHomeDir(clapPath)};
 			if (std::filesystem::is_directory(path, ec))
-				searchPaths.insert(path.make_preferred());
+				m_searchPaths.emplace_back(std::move(path.make_preferred()));
 		}
 	}
 
@@ -144,10 +155,10 @@ void ClapManager::findPlugins()
 	std::filesystem::path path{expandHomeDir("~/.clap")};
 	std::error_code ec;
 	if (std::filesystem::is_directory(path, ec))
-		searchPaths.insert(path.make_preferred());
+		m_searchPaths.emplace_back(std::move(path.make_preferred()));
 	path = "/usr/lib/clap";
 	if (std::filesystem::is_directory(path, ec))
-		searchPaths.insert(path.make_preferred());
+		m_searchPaths.emplace_back(std::move(path.make_preferred()));
 #elif defined(LMMS_BUILD_WIN32) || defined(LMMS_BUILD_WIN64)
 	std::error_code ec;
 	if (auto commonProgFiles = getenv("COMMONPROGRAMFILES"))
@@ -155,152 +166,83 @@ void ClapManager::findPlugins()
 		std::filesystem::path path{commonProgFiles};
 		path /= "CLAP";
 		if (std::filesystem::is_directory(path, ec))
-			searchPaths.insert(path.make_preferred());
+			m_searchPaths.emplace_back(std::move(path.make_preferred()));
 	}
 	if (auto localAppData = getenv("LOCALAPPDATA"))
 	{
 		std::filesystem::path path{localAppData};
 		path /= std::filesystem::path{"Programs/Common/CLAP"};
 		if (std::filesystem::is_directory(path, ec))
-			searchPaths.insert(path.make_preferred());
+			m_searchPaths.emplace_back(std::move(path.make_preferred()));
 	}
 #elif defined(LMMS_BUILD_APPLE)
 	std::error_code ec;
 	std::filesystem::path path{"/Library/Audio/Plug-Ins/CLAP"};
 	if (std::filesystem::is_directory(path, ec))
-		searchPaths.insert(path.make_preferred());
+		m_searchPaths.emplace_back(std::move(path.make_preferred()));
 	path = expandHomeDir("~/Library/Audio/Plug-Ins/CLAP");
 	if (std::filesystem::is_directory(path, ec))
-		searchPaths.insert(path.make_preferred());
+		m_searchPaths.emplace_back(std::move(path.make_preferred()));
 #endif
 
 	if (m_debug)
 	{
 		qDebug() << "CLAP search paths:";
-		for (const auto& path : searchPaths)
+		for (const auto& path : m_searchPaths)
 		{
 			qDebug() << "-" << path.c_str();
-		}
-		qDebug() << "Found CLAP plugins:";
-	}
-
-	// Search searchPaths for files with ".clap" extension
-	for (const auto& path : searchPaths)
-	{
-		for (const auto& entry : std::filesystem::recursive_directory_iterator(path))
-		{
-			if (entry.path().extension() != ".clap")
-				continue;
-
-			m_pluginPaths.insert(entry);
-			if (m_debug)
-				qDebug() << "-" << entry.path().c_str();
 		}
 	}
 }
 
-
-void ClapManager::initPlugins()
+void ClapManager::findClapFiles(const std::vector<std::filesystem::path>& searchPaths)
 {
-	qDebug() << "ClapManager::initPlugins";
+	if (!m_clapFiles.empty())
+		return; // Cannot unload CLAP plugins yet
 
-	findPlugins();
+	m_clapFiles.clear();
+	m_plugins.clear();
 
-	std::size_t pluginCount = m_pluginPaths.size();
-	std::size_t pluginsLoaded = 0;
 	QElapsedTimer timer;
 	timer.start();
 
-	for (auto& path : m_pluginPaths)
+	// Search searchPaths for files with ".clap" extension
+	int totalClapFiles = 0, totalClapPlugins = 0;
+	for (const auto& path : searchPaths)
 	{
-		auto pluginLib = std::make_shared<QLibrary>();
-		pluginLib->setFileName(QString::fromUtf8(path.c_str()));
-		pluginLib->setLoadHints(QLibrary::ResolveAllSymbolsHint | QLibrary::DeepBindHint);
-		if (!pluginLib->load())
+		for (const auto& entry : std::filesystem::recursive_directory_iterator(path))
 		{
-			qWarning() << pluginLib->errorString();
-			continue;
-		}
+			auto entryPath = entry.path();
+			if (entryPath.extension() != ".clap")
+				continue;
 
-		auto pluginEntry = reinterpret_cast<const clap_plugin_entry_t*>(pluginLib->resolve("clap_entry"));
-		if (!pluginEntry)
-		{
-			qWarning() << "Unable to resolve entry point 'clap_entry' in '" << path.c_str() << "'";
-			pluginLib->unload();
-			continue;
-		}
+			++totalClapFiles;
 
-		pluginEntry->init(path.c_str());
-		auto pluginFactory = static_cast<const clap_plugin_factory_t*>(pluginEntry->get_factory(CLAP_PLUGIN_FACTORY_ID));
-
-		auto count = pluginFactory->get_plugin_count(pluginFactory);
-		if (m_debug)
-			qDebug() << "plugin count:" << std::to_string(count).c_str();
-
-		if (m_pluginIndex > count)
-		{
-			qWarning() << "plugin index greater than count:" << count;
-			continue;
-		}
-
-		auto desc = pluginFactory->get_plugin_descriptor(pluginFactory, m_pluginIndex);
-		if (!desc)
-		{
-			qWarning() << "no plugin descriptor";
-			continue;
-		}
-
-		if (!clap_version_is_compatible(desc->clap_version))
-		{
-			qWarning() << "Incompatible CLAP version: Plugin is: " << desc->clap_version.major << "."
-						<< desc->clap_version.minor << "." << desc->clap_version.revision << " Host is "
-						<< CLAP_VERSION.major << "." << CLAP_VERSION.minor << "." << CLAP_VERSION.revision;
-			continue;
-		}
-
-		if (m_debug)
-		{
-			qDebug() << "name:" << desc->name;
-			qDebug() << "description:" << desc->description;
-		}
-
-		auto plugin = pluginFactory->create_plugin(pluginFactory, &m_host, desc->id);
-		if (!plugin)
-		{
-			qWarning() << "could not create the plugin with id: " << desc->id;
-			continue;
-		}
-
-		if (!plugin->init(plugin))
-		{
-			qWarning() << "could not init the plugin with id: " << desc->id;
-			continue;
-		}
-
-		Plugin::PluginTypes type = Plugin::PluginTypes::Other;
-		auto features = desc->features;
-		while (features && *features)
-		{
-			std::string_view feature = *features;
 			if (m_debug)
-				qDebug() << "feature:" << feature.data();
-			if (feature == CLAP_PLUGIN_FEATURE_INSTRUMENT)
-				type = Plugin::PluginTypes::Instrument;
-			else if (feature == CLAP_PLUGIN_FEATURE_AUDIO_EFFECT || feature == CLAP_PLUGIN_FEATURE_NOTE_EFFECT)
-				type = Plugin::PluginTypes::Effect;
-			else if (feature == CLAP_PLUGIN_FEATURE_ANALYZER)
-				type = Plugin::PluginTypes::Tool;
-			++features;
-		}
+				qDebug() << "-" << entryPath.c_str();
 
-		m_loadedPlugins.emplace_back(pluginLib, desc, type, true);
-		++pluginsLoaded;
+			auto clapFile = ClapFile{*this, std::move(entryPath)};
+			if (!clapFile.isValid())
+			{
+				qWarning() << "Failed to load .clap file";
+				continue;
+			}
+
+			totalClapPlugins += clapFile.getPluginCount();
+			for (const auto& plugin : clapFile.getPlugins())
+			{
+				m_plugins.push_back(plugin.getPlugin());
+			}
+
+			m_clapFiles.emplace_back(std::move(clapFile));
+		}
 	}
 
 	qDebug() << "CLAP plugin SUMMARY:"
-		<< pluginsLoaded << "of" << pluginCount << " loaded in"
-		<< timer.elapsed() << "msecs.";
-	if (pluginsLoaded != pluginCount)
+		<< m_clapFiles.size() << "of" << totalClapFiles << " .clap files"
+		<< "containing" << m_plugins.size() << "of" << totalClapPlugins
+		<< "CLAP plugins loaded in" << timer.elapsed() << "msecs.";
+	if (m_clapFiles.size() != totalClapFiles || m_plugins.size() != totalClapPlugins)
 	{
 		if (m_debug)
 		{
@@ -318,8 +260,9 @@ void ClapManager::initPlugins()
 	}
 }
 
+// clap_host functions
 
-const void *ClapManager::hostGetExtension(const clap_host *host, const char *extension_id)
+const void* ClapManager::hostGetExtension(const clap_host* host, const char* extension_id)
 {
 	//qDebug() << "hostGetExtension called; extension='" << extension_id << "'";
 
@@ -350,24 +293,214 @@ const void *ClapManager::hostGetExtension(const clap_host *host, const char *ext
 	return nullptr;
 }
 
-void ClapManager::hostRequestCallback(const clap_host *host) {
+void ClapManager::hostRequestCallback(const clap_host* host)
+{
    //auto h = fromHost(host);
    //h->_scheduleMainThreadCallback = true;
    //qDebug() << "hostRequestCallback called";
 }
 
-void ClapManager::hostRequestProcess(const clap_host *host) {
+void ClapManager::hostRequestProcess(const clap_host* host)
+{
    //auto h = fromHost(host);
    //h->_scheduleProcess = true;
    //qDebug() << "hostRequestProcess called";
 }
 
-void ClapManager::hostRequestRestart(const clap_host *host) {
+void ClapManager::hostRequestRestart(const clap_host* host)
+{
    //auto h = fromHost(host);
    //h->_scheduleRestart = true;
    //qDebug() << "hostRequestRestart called";
 }
 
+/*
+const clap_plugin_t* ClapManager::getPlugin(const std::string& uri)
+{
+	auto iter = m_clapInfoMap.find(uri);
+	return iter != m_clapInfoMap.end() ? iter->second.plugin() : nullptr;
+}
+
+
+const clap_plugin_t* ClapManager::getPlugin(const QString& uri)
+{
+	return getPlugin(uri.toStdString());
+}
+*/
+
+////////////////////////////////
+// ClapFile
+////////////////////////////////
+
+ClapManager::ClapFile::ClapFile(const ClapManager& manager, std::filesystem::path&& filename)
+	: m_parent(manager), m_filename(std::move(filename))
+{
+	m_filename.make_preferred();
+	m_library = std::make_shared<QLibrary>();
+	m_library->setFileName(QString::fromUtf8(m_filename.c_str()));
+	m_library->setLoadHints(QLibrary::ResolveAllSymbolsHint | QLibrary::DeepBindHint);
+	load();
+}
+
+ClapManager::ClapFile::ClapFile(ClapFile&& other) noexcept
+	: m_parent(other.m_parent)
+{
+	m_filename = std::move(other.m_filename);
+	m_library = std::exchange(other.m_library, nullptr);
+	m_factory = std::exchange(other.m_factory, nullptr);
+	m_pluginCount = other.m_pluginCount;
+	m_valid = std::exchange(other.m_valid, false);
+	m_plugins = std::move(other.m_plugins);
+}
+
+auto ClapManager::ClapFile::load() -> bool
+{
+	m_valid = false;
+	if (!m_library)
+		return false;
+
+	// Do not allow reloading yet
+	if (m_library->isLoaded())
+		return false;
+
+	if (!m_library->load())
+	{
+		qWarning() << m_library->errorString();
+		return false;
+	}
+
+	const auto pluginEntry = reinterpret_cast<const clap_plugin_entry_t*>(m_library->resolve("clap_entry"));
+	if (!pluginEntry)
+	{
+		qWarning() << "Unable to resolve entry point 'clap_entry' in '" << getFilename().c_str() << "'";
+		m_library->unload();
+		return false;
+	}
+
+	pluginEntry->init(getFilename().c_str());
+	m_factory = static_cast<const clap_plugin_factory_t*>(pluginEntry->get_factory(CLAP_PLUGIN_FACTORY_ID));
+
+	m_pluginCount = m_factory->get_plugin_count(m_factory);
+	if (m_debug)
+		qDebug() << "m_pluginCount:" << m_pluginCount;
+	if (m_pluginCount <= 0)
+		return false;
+
+	m_plugins.clear();
+	for (uint32_t i = 0; i < m_pluginCount; ++i)
+	{
+		const auto desc = m_factory->get_plugin_descriptor(m_factory, i);
+		if (!desc)
+		{
+			qWarning() << "no plugin descriptor";
+			continue;
+		}
+
+		if (!clap_version_is_compatible(desc->clap_version))
+		{
+			qWarning() << "Incompatible CLAP version: Plugin is: " << desc->clap_version.major << "."
+						<< desc->clap_version.minor << "." << desc->clap_version.revision << " Host is "
+						<< CLAP_VERSION.major << "." << CLAP_VERSION.minor << "." << CLAP_VERSION.revision;
+			continue;
+		}
+
+		if (m_debug)
+		{
+			qDebug() << "name:" << desc->name;
+			qDebug() << "description:" << desc->description;
+		}
+
+		auto plugin = ClapPlugin{*this, i, desc}; // Prints warning if anything goes wrong
+		if (!plugin.isValid())
+			continue;
+
+		m_plugins.emplace_back(std::move(plugin));
+	}
+
+	m_valid = true;
+	return true;
+}
+
+void ClapManager::ClapFile::unload()
+{
+	// TODO: Need to implement
+	return;
+}
+
+////////////////////////////////
+// ClapPlugin
+////////////////////////////////
+
+ClapManager::ClapFile::ClapPlugin::ClapPlugin(const ClapFile& parent, uint32_t index, const clap_plugin_descriptor_t* desc)
+	: m_parent(parent), m_host(parent.getParent().getHost())
+{
+	m_valid = false;
+	m_index = index;
+	m_descriptor = desc;
+
+	m_type = Plugin::PluginTypes::Undefined;
+	auto features = desc->features;
+	while (features && *features)
+	{
+		std::string_view feature = *features;
+		if (m_debug)
+			qDebug() << "feature:" << feature.data();
+		if (feature == CLAP_PLUGIN_FEATURE_INSTRUMENT)
+			m_type = Plugin::PluginTypes::Instrument;
+		else if (feature == CLAP_PLUGIN_FEATURE_AUDIO_EFFECT
+				|| feature == CLAP_PLUGIN_FEATURE_NOTE_EFFECT
+				|| feature == "effect" /* non-standard, but used by Surge XT Effects */)
+			m_type = Plugin::PluginTypes::Effect;
+		else if (feature == CLAP_PLUGIN_FEATURE_ANALYZER)
+			m_type = Plugin::PluginTypes::Tool;
+		++features;
+	}
+
+	if (m_type != Plugin::PluginTypes::Undefined)
+		m_valid = true;
+	else
+	{
+		qWarning() << "CLAP plugin is not recognized as an instrument, effect, or tool";
+	}
+}
+
+ClapManager::ClapFile::ClapPlugin::ClapPlugin(ClapPlugin&& other) noexcept
+	: m_parent(other.m_parent)
+{
+	m_host = std::exchange(other.m_host, nullptr);
+	m_index = other.m_index;
+	m_descriptor = std::exchange(other.m_descriptor, nullptr);
+	m_type = std::exchange(other.m_type, Plugin::PluginTypes::Undefined);
+	m_valid = std::exchange(other.m_valid, false);
+	m_plugin = std::exchange(other.m_plugin, nullptr);
+}
+
+auto ClapManager::ClapFile::ClapPlugin::activate() -> bool
+{
+	if (!isValid() || isActivated())
+		return false;
+
+	const auto factory = getParent().getFactory();
+	m_plugin = ClapPluginPtr{ factory->create_plugin(factory, m_host, getDescriptor()->id) };
+	if (!m_plugin)
+	{
+		qWarning() << "could not create the plugin with id: " << getDescriptor()->id;
+		return false;
+	}
+
+	if (!m_plugin->init(m_plugin.get()))
+	{
+		qWarning() << "could not init the plugin with id: " << getDescriptor()->id;
+		return false;
+	}
+
+	return true;
+}
+
+void ClapManager::ClapFile::ClapPlugin::deactivate()
+{
+	// TODO
+}
 
 } // namespace lmms
 
