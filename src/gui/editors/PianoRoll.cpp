52f530dc4c602f40e5ae24685b4ffd62bb043404
Levin Oehlmann <lv.oehlmann@gmail.com>
2023-06-03 20:47:33 +0200

    Quantize notes during recording if midi:autoquantize is enabled.

--- src/gui/editors/PianoRoll.cpp
+++ src/gui/editors/PianoRoll.cpp
@@ -4137,7 +4137,15 @@ void PianoRoll::finishRecordNote(const Note & n )
 					Note n1( n.length(), it->pos(),
 							it->key(), it->getVolume(),
 							it->getPanning() );
-					m_midiClip->addNote( n1, false );
+
+					// TODO this value is probably better off cached somewhere;
+					//  currently doing a string parse everytime a note is released.
+					bool doQuantize = (ConfigManager::inst()->value("midi", "autoquantize").toInt() != 0);
+					if (doQuantize) {
+						n1.quantizeLength(quantization());
+						n1.quantizePos(quantization());
+					}
+					m_midiClip->addNote(n1,false);
 					update();
 					m_recordingNotes.erase( it );
 					break;
