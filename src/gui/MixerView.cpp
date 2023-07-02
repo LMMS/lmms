a5f1fc91cf7cd0b611c3fb5f79e5dca5692e3776
Dominic Clark <mrdomclark@gmail.com>
2023-07-02 13:01:24 +0100

    Add even more Qt bug workarounds

--- src/gui/MixerView.cpp
+++ src/gui/MixerView.cpp
@@ -57,6 +57,16 @@ MixerView::MixerView() :
 	ModelView( nullptr, this ),
 	SerializingObjectHook()
 {
+#if QT_VERSION < 0x50C00
+	// Workaround for a bug in Qt versions below 5.12,
+	// where argument-dependent-lookup fails for QFlags operators
+	// declared inside a namepsace.
+	// This affects the Q_DECLARE_OPERATORS_FOR_FLAGS macro in Instrument.h
+	// See also: https://codereview.qt-project.org/c/qt/qtbase/+/225348
+
+	using ::operator|;
+#endif
+
 	Mixer * m = Engine::mixer();
 	m->setHook( this );
 
