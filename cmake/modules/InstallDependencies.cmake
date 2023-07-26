dd00fb0dc1335748ca56efe380cc1390c8f28e9d
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2023-07-26 13:50:55 -0400

    Try again to fix mingw builds

--- cmake/modules/InstallDependencies.cmake
+++ cmake/modules/InstallDependencies.cmake
@@ -1,6 +1,8 @@
 include(GetPrerequisites)
 include(CMakeParseArguments)
 
+CMAKE_POLICY(SET CMP0057 NEW) # Support new if() IN_LIST operator.
+
 function(make_absolute var)
 	get_filename_component(abs "${${var}}" ABSOLUTE BASE_DIR "${CMAKE_INSTALL_PREFIX}")
 	set(${var} ${abs} PARENT_SCOPE)
