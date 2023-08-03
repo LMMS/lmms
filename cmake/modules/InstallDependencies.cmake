bc99728534c90fba25c5deedef1568dc5d9d6e62
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2023-08-02 23:06:18 -0400

    Consolidate and simplify CMAKE_POLICY entries (#6780)
    Simplify CMake Policies
    Per https://github.com/LMMS/lmms/pull/6758#discussion_r1271346366

--- cmake/modules/InstallDependencies.cmake
+++ cmake/modules/InstallDependencies.cmake
@@ -1,8 +1,8 @@
 include(GetPrerequisites)
 include(CMakeParseArguments)
 
-CMAKE_POLICY(SET CMP0011 NEW)
-CMAKE_POLICY(SET CMP0057 NEW)
+# Project's cmake_minimum_required doesn't always propagate
+cmake_policy(SET CMP0057 NEW) # Support new if() IN_LIST operator.
 
 function(make_absolute var)
 	get_filename_component(abs "${${var}}" ABSOLUTE BASE_DIR "${CMAKE_INSTALL_PREFIX}")
