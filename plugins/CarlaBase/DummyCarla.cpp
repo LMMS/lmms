bc1de376ef1395139055ce562f207e012c98a192
Bimal Poudel <anytizer@users.noreply.github.com>
2023-08-17 16:07:00 -0600

    Update DummyCarla.cpp

--- plugins/CarlaBase/DummyCarla.cpp
+++ plugins/CarlaBase/DummyCarla.cpp
@@ -2,6 +2,7 @@
 #define BUILDING_CARLA
 #include "CarlaNativePlugin.h"
 
+#ifdef CARLA_PLUGIN_EXPORT
 CARLA_PLUGIN_EXPORT const char* carla_get_library_filename() { return nullptr; }
 CARLA_PLUGIN_EXPORT const char* carla_get_library_folder() { return nullptr; }
 CARLA_PLUGIN_EXPORT const NativePluginDescriptor* carla_get_native_rack_plugin() { return nullptr; }
@@ -11,3 +12,14 @@ CARLA_PLUGIN_EXPORT const NativePluginDescriptor* carla_get_native_patchbay32_pl
 CARLA_PLUGIN_EXPORT const NativePluginDescriptor* carla_get_native_patchbay64_plugin() { return nullptr; }
 CARLA_PLUGIN_EXPORT const NativePluginDescriptor* carla_get_native_patchbay_cv_plugin() { return nullptr; }
 CARLA_PLUGIN_EXPORT CarlaBackend::CarlaEngine* carla_get_native_plugin_engine(const NativePluginDescriptor* desc, NativePluginHandle handle) { return nullptr; }
+#else
+CARLA_EXPORT const char* carla_get_library_filename() { return nullptr; }
+CARLA_EXPORT const char* carla_get_library_folder() { return nullptr; }
+CARLA_EXPORT const NativePluginDescriptor* carla_get_native_rack_plugin() { return nullptr; }
+CARLA_EXPORT const NativePluginDescriptor* carla_get_native_patchbay_plugin() { return nullptr; }
+CARLA_EXPORT const NativePluginDescriptor* carla_get_native_patchbay16_plugin() { return nullptr; }
+CARLA_EXPORT const NativePluginDescriptor* carla_get_native_patchbay32_plugin() { return nullptr; }
+CARLA_EXPORT const NativePluginDescriptor* carla_get_native_patchbay64_plugin() { return nullptr; }
+CARLA_EXPORT const NativePluginDescriptor* carla_get_native_patchbay_cv_plugin() { return nullptr; }
+CARLA_EXPORT CarlaBackend::CarlaEngine* carla_get_native_plugin_engine(const NativePluginDescriptor* desc, NativePluginHandle handle) { return nullptr; }
+#endif
