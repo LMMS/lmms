793f096c4f6c01c9f48328530fe05320892835d1
Bimal Poudel <anytizer@users.noreply.github.com>
2023-08-20 00:52:23 -0600

    Update DummyCarla.cpp to use CARLA_PLUGIN_EXPORT (#6814)

--- plugins/CarlaBase/DummyCarla.cpp
+++ plugins/CarlaBase/DummyCarla.cpp
@@ -2,12 +2,16 @@
 #define BUILDING_CARLA
 #include "CarlaNativePlugin.h"
 
-CARLA_EXPORT const char* carla_get_library_filename() { return nullptr; }
-CARLA_EXPORT const char* carla_get_library_folder() { return nullptr; }
-CARLA_EXPORT const NativePluginDescriptor* carla_get_native_rack_plugin() { return nullptr; }
-CARLA_EXPORT const NativePluginDescriptor* carla_get_native_patchbay_plugin() { return nullptr; }
-CARLA_EXPORT const NativePluginDescriptor* carla_get_native_patchbay16_plugin() { return nullptr; }
-CARLA_EXPORT const NativePluginDescriptor* carla_get_native_patchbay32_plugin() { return nullptr; }
-CARLA_EXPORT const NativePluginDescriptor* carla_get_native_patchbay64_plugin() { return nullptr; }
-CARLA_EXPORT const NativePluginDescriptor* carla_get_native_patchbay_cv_plugin() { return nullptr; }
-CARLA_EXPORT CarlaBackend::CarlaEngine* carla_get_native_plugin_engine(const NativePluginDescriptor* desc, NativePluginHandle handle) { return nullptr; }
+#ifndef CARLA_PLUGIN_EXPORT
+#define CARLA_PLUGIN_EXPORT CARLA_EXPORT
+#endif
+
+CARLA_PLUGIN_EXPORT const char* carla_get_library_filename() { return nullptr; }
+CARLA_PLUGIN_EXPORT const char* carla_get_library_folder() { return nullptr; }
+CARLA_PLUGIN_EXPORT const NativePluginDescriptor* carla_get_native_rack_plugin() { return nullptr; }
+CARLA_PLUGIN_EXPORT const NativePluginDescriptor* carla_get_native_patchbay_plugin() { return nullptr; }
+CARLA_PLUGIN_EXPORT const NativePluginDescriptor* carla_get_native_patchbay16_plugin() { return nullptr; }
+CARLA_PLUGIN_EXPORT const NativePluginDescriptor* carla_get_native_patchbay32_plugin() { return nullptr; }
+CARLA_PLUGIN_EXPORT const NativePluginDescriptor* carla_get_native_patchbay64_plugin() { return nullptr; }
+CARLA_PLUGIN_EXPORT const NativePluginDescriptor* carla_get_native_patchbay_cv_plugin() { return nullptr; }
+CARLA_PLUGIN_EXPORT CarlaBackend::CarlaEngine* carla_get_native_plugin_engine(const NativePluginDescriptor* desc, NativePluginHandle handle) { return nullptr; }
