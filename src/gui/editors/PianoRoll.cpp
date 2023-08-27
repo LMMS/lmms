3c80d3d4f3715d48e7a5cbd0709a306e4aebfbf2
Levin Oehlmann <lv.oehlmann@gmail.com>
2023-08-27 21:20:08 +0200

    Use '!' instead of 'not' to please MSVC.

--- src/gui/editors/PianoRoll.cpp
+++ src/gui/editors/PianoRoll.cpp
@@ -251,7 +251,7 @@ PianoRoll::PianoRoll() :
 	connect(ConfigManager::inst(), &ConfigManager::valueChanged,
 		[this](QString const& cls, QString const& attribute, QString const& value)
 		{
-			if (not (cls == "midi" && attribute == "autoquantize"))
+			if (!(cls == "midi" && attribute == "autoquantize"))
 			{
 				return;
 			}
