b92e175f3353c4c63ed321651b207272c4b53cde
mvoorhes <mvoorhes@ucsc.edu>
2023-09-13 15:03:06 -0700

    Added flats to array of keys

--- src/gui/editors/PianoRoll.cpp
+++ src/gui/editors/PianoRoll.cpp
@@ -130,7 +130,7 @@ QPixmap* PianoRoll::s_toolKnife = nullptr;
 
 SimpleTextFloat * PianoRoll::s_textFloat = nullptr;
 
-static std::array<QString, 12> s_noteStrings {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+static std::array<QString, 12> s_noteStrings {"C", "C#/D\u266D", "D", "D#/E\u266D", "E", "F", "F#/G\u266D", "G", "G#/A\u266D", "A", "A#/B\u266D", "B"};
 
 static QString getNoteString(int key)
 {
@@ -1687,7 +1687,7 @@ void PianoRoll::mousePressEvent(QMouseEvent * me )
 		m_moveStartY = me->y();
 	}
 
-	if(me->button() == Qt::LeftButton &&
+	if( me->button() == Qt::LeftButton &&
 		me->y() > keyAreaBottom() && me->y() < noteEditTop())
 	{
 		// resizing the note edit area
