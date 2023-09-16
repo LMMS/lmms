e87f592d33fe4a41d794c73063c093b0d149007a
Ross maxx <mrroshan127@gmail.com>
2023-09-16 12:26:04 +0530

    attempt to fix last pending crash

--- src/gui/editors/PianoRoll.cpp
+++ src/gui/editors/PianoRoll.cpp
@@ -1816,8 +1816,8 @@ void PianoRoll::mousePressEvent(QMouseEvent * me )
 					// reset it so that it can be used for
 					// ops (move, resize) after this
 					// code-block
-					it = notes.rend();
-					while (it != notes.rbegin()+1 && *it != created_new_note)
+					it = notes.rbegin();
+					while (it != notes.rend() && *it != created_new_note)
 					{
 						++it;
 					}
