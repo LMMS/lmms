33d1baddc0284bd68df752a08ff9c673a706c4d7
Johannes Lorenz <j.git@lorenz-ho.me>
2023-09-22 23:27:02 +0200

    Implement Lv2 Worker (#6484)

--- src/core/lv2/Lv2Manager.cpp
+++ src/core/lv2/Lv2Manager.cpp
@@ -31,6 +31,7 @@
 #include <lilv/lilv.h>
 #include <lv2/lv2plug.in/ns/ext/buf-size/buf-size.h>
 #include <lv2/lv2plug.in/ns/ext/options/options.h>
+#include <lv2/lv2plug.in/ns/ext/worker/worker.h>
 #include <QDebug>
 #include <QElapsedTimer>
 
@@ -172,6 +173,7 @@ Lv2Manager::Lv2Manager() :
 	m_supportedFeatureURIs.insert(LV2_URID__map);
 	m_supportedFeatureURIs.insert(LV2_URID__unmap);
 	m_supportedFeatureURIs.insert(LV2_OPTIONS__options);
+	m_supportedFeatureURIs.insert(LV2_WORKER__schedule);
 	// min/max is always passed in the options
 	m_supportedFeatureURIs.insert(LV2_BUF_SIZE__boundedBlockLength);
 	// block length is only changed initially in AudioEngine CTOR
