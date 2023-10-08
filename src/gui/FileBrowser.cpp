33783bf8a01e5ab11574111c4c6873d357452c48
saker <sakertooth@gmail.com>
2023-10-07 20:16:00 -0400

    Simplify if condition in src/gui/FileBrowser.cpp
    Co-authored-by: Dalton Messmer <33463986+messmerd@users.noreply.github.com>

--- src/gui/FileBrowser.cpp
+++ src/gui/FileBrowser.cpp
@@ -667,7 +667,7 @@ void FileBrowserTreeWidget::previewFileItem(FileItem* file)
 			embed::getIconPixmap("sample_file", 24, 24), 0);
 		// TODO: this can be removed once we do this outside the event thread
 		qApp->processEvents(QEventLoop::ExcludeUserInputEvents);
-		if (auto buffer = SampleLoader::createBufferFromFile(fileName); buffer)
+		if (auto buffer = SampleLoader::createBufferFromFile(fileName))
 		{
 			auto s = new SamplePlayHandle(new lmms::Sample{std::move(buffer)});
 			s->setDoneMayReturnTrue(false);
