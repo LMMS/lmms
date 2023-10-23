57d55d3896fbf2230d5a41e6025b3a69de8a8751
Johannes Lorenz <j.git@lorenz-ho.me>
2023-10-23 23:08:50 +0200

    `if(` -> `if (`

--- src/core/lv2/Lv2Proc.cpp
+++ src/core/lv2/Lv2Proc.cpp
@@ -444,7 +444,7 @@ void Lv2Proc::initPlugin()
 	{
 		const auto iface = static_cast<const LV2_Worker_Interface*>(
 			lilv_instance_get_extension_data(m_instance, LV2_WORKER__interface));
-		if(iface) {
+		if (iface) {
 			m_worker->setHandle(lilv_instance_get_handle(m_instance));
 			m_worker->setInterface(iface);
 		}
