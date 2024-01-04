64a2d2e65dc7bdf50816cc0d8af9dbe7a87b0e7b
Lost Robot <34612565+LostRobotMusic@users.noreply.github.com>
2024-01-04 13:13:45 -0800

    setSampleRate function in BasicFilters

--- include/BasicFilters.h
+++ include/BasicFilters.h
@@ -328,6 +328,12 @@ public:
 		}
 	}
 
+	inline void setSampleRate(float sampleRate)
+	{
+		m_sampleRate = sampleRate;
+		m_sampleRatio = 1.f / m_sampleRate;
+	}
+
 	inline sample_t update( sample_t _in0, ch_cnt_t _chnl )
 	{
 		sample_t out;
