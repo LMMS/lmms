e67f5f8cb5884cbddca53aa1b6879a060a3bd517
Michael Gregorius <michael.gregorius.git@arcor.de>
2024-01-07 10:20:29 +0100

    Remove TODO

--- src/gui/widgets/Fader.cpp
+++ src/gui/widgets/Fader.cpp
@@ -422,7 +422,8 @@ void Fader::paintLevels(QPaintEvent * ev, QPainter & painter, bool linear)
 	auto const peakRectR = ph.getPersistentPeakRect(rightMeterMargins, mappedPersistentPeakR);
 	painter.fillRect(peakRectR, linearGrad);
 
-	// TODO
+	// Draw a transparent white line around the levels outlines to make it look similar to the pixmaps
+	// that were used before the change to painting the levels in code.
 	QPen pen(QColor(255, 255, 255, 18));
 	pen.setWidth(2);
 	painter.setPen(pen);
