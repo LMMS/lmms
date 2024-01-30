731dc8d8cc1c6aef853ccc737d3c85173adee471
Rossmaxx <74815851+Rossmaxx@users.noreply.github.com>
2024-01-30 13:28:17 +0530

    Removed the winEventFilter function and fixed styling.

--- src/gui/MainApplication.cpp
+++ src/gui/MainApplication.cpp
@@ -86,37 +86,33 @@ bool MainApplication::event(QEvent* event)
 }
 
 #ifdef LMMS_BUILD_WIN32
-// This can be moved into nativeEventFilter once Qt4 support has been dropped
-bool MainApplication::winEventFilter(MSG* msg, long* result)
-{
-	switch(msg->message)
-	{
-		case WM_STYLECHANGING:
-			if(msg->wParam == GWL_EXSTYLE)
-			{
-				// Prevent plugins making the main window transparent
-				STYLESTRUCT * style = reinterpret_cast<STYLESTRUCT *>(msg->lParam);
-				if(!(style->styleOld & WS_EX_LAYERED))
-				{
-					style->styleNew &= ~WS_EX_LAYERED;
-				}
-				*result = 0;
-				return true;
-			}
-			return false;
-		default:
-			return false;
-	}
-}
-
 bool MainApplication::nativeEventFilter(const QByteArray& eventType,
 					void* message, long* result)
 {
-	if(eventType == "windows_generic_MSG")
+	if(eventType != "windows_generic_MSG")
 	{
-		return winEventFilter(static_cast<MSG *>(message), result);
+		return false;
 	}
-	return false;
+
+	MSG *msg = static_cast<MSG *>(message);
+	if((msg->message) != WM_STYLECHANGING)
+	{
+		return false;
+	}
+	
+	if(msg->wParam != GWL_EXSTYLE)
+	{
+		return false
+	}
+	
+	// Prevent plugins making the main window transparent
+	STYLESTRUCT * style = reinterpret_cast<STYLESTRUCT *>(msg->lParam);
+	if(!(style->styleOld & WS_EX_LAYERED))
+	{
+		style->styleNew &= ~WS_EX_LAYERED;
+	}
+	*result = 0;
+	return true;
 }
 #endif // LMMS_BUILD_WIN32
 
