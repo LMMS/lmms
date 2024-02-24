63588cbf80d1374d5aaf9cf65d5985d09219bdbf
szeli1 <143485814+szeli1@users.noreply.github.com>
2024-02-24 12:35:47 +0100

    added_temp_plugin_for_testing

--- cmake/modules/PluginList.cmake
+++ cmake/modules/PluginList.cmake
@@ -38,6 +38,7 @@ SET(LMMS_PLUGIN_LIST
 	DynamicsProcessor
 	Eq
 	Flanger
+	FFTFilter
 	GranularPitchShifter
 	HydrogenImport
 	LadspaBrowser
---
+++ plugins/FFTFilter/FFTFilter.h
@@ -0,0 +1,93 @@
+/*
+ * Amplifier.h - amplifier-effect-plugin
+ *
+ * Copyright (c) 2014 Vesa Kivimäki <contact/dot/diizy/at/nbl/dot/fi>
+ * Copyright (c) 2006-2014 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef LMMS_FFTFILTER_H
+#define LMMS_FFTFILTER_H
+
+#include "vector"
+
+#include "Effect.h"
+#include "FFTFilterControls.h"
+
+namespace lmms
+{
+
+class FFTFilterEffect : public Effect
+{
+public:
+	FFTFilterEffect(Model* parent, const Descriptor::SubPluginFeatures::Key* key);
+	~FFTFilterEffect() override = default;
+	bool processAudioBuffer(sampleFrame* buf, const fpp_t frames) override;
+
+	EffectControls* controls() override
+	{
+		return &m_filterControls;
+	}
+
+private:
+	void runFFTOnBuffer(std::vector<sample_t>* inputReIn, std::vector<sample_t>* inputImIn, std::vector<sample_t> bufferIn);
+	// implementation of fft
+	void FFT(std::vector<sample_t>* inputReIn, std::vector<sample_t>* inputImIn);
+	// inverse fft
+	void IFFT(std::vector<sample_t>* inputReIn, std::vector<sample_t>* inputImIn);
+
+	inline sample_t getCurSample(unsigned int posIn)
+	{
+		if (m_useSecondBufferOut == true)
+		{
+			return m_bufferB[posIn];
+		}
+		else
+		{
+			return m_bufferA[posIn];
+		}
+	}
+	inline void setCurSample(unsigned int posIn, sample_t sampleIn)
+	{
+		if (m_useSecondBufferOut == true)
+		{
+			m_bufferA[posIn] = sampleIn;
+		}
+		else
+		{
+			m_bufferB[posIn] = sampleIn;
+		}
+	}
+
+	FFTFilterControls m_filterControls;
+
+	std::vector<sample_t> m_bufferA;
+	std::vector<sample_t> m_bufferB;
+	bool m_useSecondBufferOut;
+
+	std::vector<float> m_FFTFilterData;
+
+
+	friend class FFTFilterControls;
+};
+
+} // namespace lmms
+
+#endif // LMMS_FFTFILTER_H
