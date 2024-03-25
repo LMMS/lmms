c7b779b5845f2609907bdc1af82f857b77808673
saker <sakertooth@gmail.com>
2024-03-24 20:54:31 -0400

    Move controller type into separate variable

--- src/core/Controller.cpp
+++ src/core/Controller.cpp
@@ -220,9 +220,9 @@ Controller * Controller::create( ControllerType _ct, Model * _parent )
 
 Controller * Controller::create( const QDomElement & _this, Model * _parent )
 {
-	auto controller = static_cast<ControllerType>(_this.attribute("type").toInt()) == ControllerType::Peak
-		? PeakController::getControllerBySetting(_this)
-		: create(static_cast<ControllerType>(_this.attribute("type").toInt()), _parent);
+	const auto controllerType = static_cast<ControllerType>(_this.attribute("type").toInt());
+	auto controller = controllerType == ControllerType::Peak ? PeakController::getControllerBySetting(_this)
+															 : create(controllerType, _parent);
 	if (controller) { controller->restoreState(_this); }
 	return controller;
 }
