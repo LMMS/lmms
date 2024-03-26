9e8dff6bcc01d43e9532b94babd2b2da948d5d05
saker <sakertooth@gmail.com>
2024-03-25 22:30:37 -0400

    Update src/gui/widgets/Fader.cpp
    Co-authored-by: Kevin Zander <veratil@gmail.com>

--- src/gui/widgets/Fader.cpp
+++ src/gui/widgets/Fader.cpp
@@ -200,12 +200,12 @@ void Fader::mouseDoubleClickEvent( QMouseEvent* mouseEvent )
 {
 	bool ok = false;
 	// TODO: dbV handling
+	auto minv = model()->minValue() * m_conversionFactor;
+	auto maxv = model()->maxValue() * m_conversionFactor;
 	float newValue = QInputDialog::getDouble(this, tr("Set value"),
-						 tr("Please enter a new value between %1 and %2:")
-							 .arg(model()->minValue() * m_conversionFactor)
-							 .arg(model()->maxValue() * m_conversionFactor),
-						 model()->getRoundedValue() * m_conversionFactor, model()->minValue() * m_conversionFactor,
-						 model()->maxValue() * m_conversionFactor, model()->getDigitCount(), &ok)
+			tr("Please enter a new value between %1 and %2:").arg(minv).arg(maxv),
+			model()->getRoundedValue() * m_conversionFactor,
+			minv, maxv, model()->getDigitCount(), &ok)
 		/ m_conversionFactor;
 
 	if( ok )
