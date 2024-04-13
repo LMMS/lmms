24b576f34456aa70e35873602b8d4244b7864759
szeli1 <143485814+szeli1@users.noreply.github.com>
2024-04-13 11:57:28 +0200

    FFTFilter_filter_implemented_partially

--- plugins/FFTFilter/FFTProcessor.cpp
+++ plugins/FFTFilter/FFTProcessor.cpp
@@ -32,20 +32,23 @@ FFTProcessor::FFTProcessor(unsigned int bufferSizeIn, bool isThreadedIn, FFTWind
 	m_normSpectrum.resize(binCount(m_blockSize));
 	//m_complexMultiplier;
 
+	/*
 	m_out = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * binCount(m_blockSize));
 	// fftwf_plan fftwf_plan_dft_r2c_1d(int n, float *in, fftwf_complex *out, unsigned flags);
 	// fftwf_plan fftwf_plan_dft_c2r_1d(int n, fftwf_complex *in, float *out, unsigned flags);
 	m_plan = fftwf_plan_dft_r2c_1d(m_blockSize, m_samplesIn.data(), m_out, FFTW_ESTIMATE);
 	m_iplan = fftwf_plan_dft_c2r_1d(m_blockSize, m_out, m_samplesOut.data(), FFTW_ESTIMATE);
+	*/
 
-	m_fftWindow.resize(m_blockSize, 1.0);
-	precomputeWindow(m_fftWindow.data(), m_blockSize, m_FFTWindowType);
+	//m_fftWindow.resize(m_blockSize, 1.0);
+	//precomputeWindow(m_fftWindow.data(), m_blockSize, m_FFTWindowType);
 }
 FFTProcessor::~FFTProcessor()
 {
 	qDebug("FFTProcessor destrc -1");
 
 	threadedTerminate();
+	/*
 	if (m_plan != nullptr)
 	{
 		fftwf_destroy_plan(m_plan);
@@ -60,9 +63,10 @@ FFTProcessor::~FFTProcessor()
 	{
 		fftwf_free(m_out);
 	}
+	*/
 	
 	qDebug("FFTProcessor destrc 2");
-	m_out = nullptr;
+	//m_out = nullptr;
 	qDebug("FFTProcessor destrc 3");
 }
 
@@ -92,7 +96,7 @@ void FFTProcessor::threadedStartThread(LocklessRingBuffer<sampleFrame>* ringBuff
 		qDebug("analyze threaded  thread init");
 		m_terminate = false;
 		unsigned int blockSize = m_blockSize;
-		m_FFTThread = std::thread(FFTProcessor::threadedAnalyze, &m_terminate, &m_plan, &m_iplan, m_out, &m_samplesIn, &m_samplesOut, &m_outputSpectrumChanged, &m_outputSamplesChanged,
+		m_FFTThread = std::thread(FFTProcessor::threadedAnalyze, &m_terminate, &m_samplesOut, &m_samplesOut, &m_outputSpectrumChanged, &m_outputSamplesChanged,
 			ringBufferIn, &m_complexMultiplier, sampLocIn, blockSize, &m_normSpectrum, &m_outputAccess);
 		qDebug("analyze threaded  thread running");
 	}
@@ -129,22 +133,33 @@ void FFTProcessor::setComplexMultiplier(std::vector<float>* complexMultiplierIn)
 	m_complexMultiplier = *complexMultiplierIn;
 }
 
-void FFTProcessor::threadedAnalyze(std::atomic<bool>* terminateIn, fftwf_plan* planIn, fftwf_plan* inversePlanIn, fftwf_complex* complexIn, std::vector<float>* samplesIn, std::vector<float>* samplesOut,
+void FFTProcessor::threadedAnalyze(std::atomic<bool>* terminateIn, std::vector<float>* samplesInB, std::vector<float>* samplesOutB,
 	std::atomic<bool>* spectrumChangedOut, std::atomic<bool>* samplesChangedOut,
 	LocklessRingBuffer<sampleFrame>* ringBufferIn, std::vector<float>* filterSpectrumIn, unsigned int sampLocIn, unsigned int blockSizeIn,
 	std::vector<float>* spectrumOut, std::mutex* outputAccessIn)
 {
 	LocklessRingBufferReader<sampleFrame> reader(*ringBufferIn);
 	
-	//std::vector<float> samples(blockSizeIn);
+	std::vector<float> samplesIn(blockSizeIn);
+	//std::vector<float> samplesInBuffer(blockSizeIn);
+	std::vector<float> samplesOut(blockSizeIn);
+	std::vector<float> samplesOutBufferA(blockSizeIn);
+	std::vector<float> samplesOutBufferB(blockSizeIn);
 	std::vector<float> absSpectrum(blockSizeIn);
 	std::vector<float> outputSpectrum(blockSizeIn);
+
+	fftwf_complex* fftComplex = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * binCount(blockSizeIn));
+	// fftwf_plan fftwf_plan_dft_r2c_1d(int n, float *in, fftwf_complex *out, unsigned flags);
+	// fftwf_plan fftwf_plan_dft_c2r_1d(int n, fftwf_complex *in, float *out, unsigned flags);
+	fftwf_plan plan = fftwf_plan_dft_r2c_1d(blockSizeIn, samplesIn.data(), fftComplex, FFTW_ESTIMATE);
+	fftwf_plan iplan = fftwf_plan_dft_c2r_1d(blockSizeIn, fftComplex, samplesOut.data(), FFTW_ESTIMATE);
 
 	// TODO might not be the correct size
 	float pi = 3.141592654f;
 	std::vector<float> hanningWindow(blockSizeIn);
 	precomputeWindow(hanningWindow.data(), blockSizeIn, FFTWindow::Hanning);
-	float sumWindow = 0.0f;
+	float maxWindow = 0.0f;
+	/*
 	for (int i = 0; i < blockSizeIn; i++)
 	{
 		// applying sinc function
@@ -156,21 +171,26 @@ void FFTProcessor::threadedAnalyze(std::atomic<bool>* terminateIn, fftwf_plan* p
 		sumWindow = sumWindow + hanningWindow[i];
 		qDebug("FFTProcessor final window [%d]: %f, %f", i, hanningWindow[i], sumWindow);
 	}
+	*/
+	for (int i = 0; i < blockSizeIn; i++)
+	{
+		maxWindow = maxWindow < hanningWindow[i] ? hanningWindow[i] : maxWindow;
+	}
 	for (int i = 0; i < blockSizeIn; i++)
 	{
 		// normalise
-		hanningWindow[i] = hanningWindow[i] / sumWindow;
-		//qDebug("FFTProcessor sumWindow [%d]: %f,  %f", i, hanningWindow[i], sumWindow);
+		hanningWindow[i] = hanningWindow[i] / maxWindow;
+		qDebug("FFTProcessor sumWindow [%d]: %f,  %f", i, hanningWindow[i], maxWindow);
 	}
 
-	std::vector<float> inverseOldReal(binCount(blockSizeIn));
-	std::vector<float> inverseOldImg(binCount(blockSizeIn));
 
+	fpp_t frameFillLoc = blockSizeIn / 2;
+	//fpp_t frameFillLoc = 0;
+	int outFillLoc = 0;
 
-	std::vector<float> storedSamples(binCount(blockSizeIn));
-
-	fpp_t frameFillLoc = 0;
-
+	bool writeSamplesOut = true;
+	bool writeToBufferA = true;
+	int runFFTCount = 0;
 	qDebug("analyzeB 0");
 	while (*terminateIn == false)
 	{
@@ -182,79 +202,79 @@ void FFTProcessor::threadedAnalyze(std::atomic<bool>* terminateIn, fftwf_plan* p
 		auto bufferIn = reader.read_max(ringBufferIn->capacity());
 		size_t frameCount = bufferIn.size();
 
+		if (frameCount > 0)
+		{
+			qDebug("analyzeB 0.5 DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG count: %ld", frameCount);
+		}
+
 		fpp_t frameIn = 0;
 
 		while (frameIn < frameCount)
 		{
 			qDebug("analyzeB 1");
-			outputAccessIn->lock();
 			while (frameIn < frameCount && frameFillLoc < blockSizeIn)
 			//while (frameIn < frameCount && frameFillLoc < blockSizeIn / 2)
 			{
-			//qDebug("analyzeB 2");
-				samplesIn->operator[](frameFillLoc + blockSizeIn / 2) = bufferIn[frameIn][sampLocIn];
-				samplesIn->operator[](frameFillLoc) = storedSamples[frameFillLoc];
-				storedSamples[frameFillLoc] = bufferIn[frameIn][sampLocIn];
+				//qDebug("analyzeB 2");
+				//samplesInBuffer[frameFillLoc] = bufferIn[frameIn][sampLocIn];
+				samplesIn[frameFillLoc - blockSizeIn / 2] = samplesIn[frameFillLoc];
+				samplesIn[frameFillLoc] = bufferIn[frameIn][sampLocIn];
+
+				//samplesIn[frameFillLoc + blockSizeIn / 2] = bufferIn[frameIn][sampLocIn];
+				//samplesIn[frameFillLoc] = storedSamples[frameFillLoc];
+				//storedSamples[frameFillLoc] = bufferIn[frameIn][sampLocIn];
 				frameIn++;
 				frameFillLoc++;
 			}
-			outputAccessIn->unlock();
-			
 
 			if (frameFillLoc < blockSizeIn)
 			//if (frameFillLoc < blockSizeIn / 2)
 			{
 				break;
 			}
-			frameFillLoc = 0;
+			frameFillLoc = blockSizeIn / 2;
+			//frameFillLoc = 0;
+			/*
+			runFFTCount = 2;
+		}
+		while (runFFTCount > 0)
+		{
+			runFFTCount--;
+			int readStartLocation = runFFTCount == 1 ? 0 : blockSizeIn / 2;
+			for (unsigned int i = 0; i < blockSizeIn / 2; i++)
+			{
+				samplesIn[i] = samplesIn[i + blockSizeIn / 2];
+				samplesIn[i + blockSizeIn / 2] = samplesInBuffer[i + readStartLocation];
+			}
+			*/
+
 
 			qDebug("analyzeB run");
 
 			for (unsigned int i = 0; i < blockSizeIn; i++)
 			{
-				//qDebug("FFTProcessor analyze samplesIn [%d]: %f", i, samplesIn->operator[](i));
+				qDebug("FFTProcessor analyze samplesIn [%d]: %f", i, samplesIn[i]);
 			}
+			qDebug("analyzeB run2 size: %ld", samplesIn.size());
 			
 			// applying window
 			// this is needed to attenuate the reverse complex correctly
 			for (unsigned int i = 0; i < blockSizeIn; i++)
 			{
 				//qDebug("FFTProcessor analyze samplesIn before window [%d]: %f", i, samplesIn->operator[](i));
-				//samplesIn->operator[](i) = samplesIn->operator[](i) * hanningWindow[i];
+				//samplesIn[i] = samplesIn[i] * hanningWindow[i];
 				//qDebug("FFTProcessor analyze samplesOut after window [%d]: %f", i, samplesIn->operator[](i));
 			}
 
-			// TODO 1.:
-			// TODO x = samples (x is the sound input with a big length)		Done
-			// TODO create hanning widnow		Done
-			// TODO window = element wise multiplication of hanning and sinc(?) TODO function		Done
-			// TODO normalise window		Done
-			// TODO window_complex = fft(window, N) (it can be applyed before too?)		Done
-			// TODO complex * window_complex		Done
-			// TODO ifft()
-			//
-			// TODO 2.:
-			// TODO size = 512
-			// TODO buffer(size * 2, 0) // create a (size * 2) buffer filled with 0-s
-			// TODO index = iterator througth 1 - 512
-			// TODO output_buffer(size * 2, 0)
-			// TODO for (unsingned int j = 0; j < size; j++)  // process 512 points of x at a time
-			// TODO		shift buffer's second half to the first half
-			// TODO		fill buffer's second half with new data x[index] (=samples)
-			// TODO		fftbuffer = ifft(fft(buffer) * fft(window, size * 2)) // multiply complexes, window_complex is sized (size * 2) and contains the fft of window
-			// TODO		output_buffer[index] = real(fftbuffer(size + 1:end)) // getting the real part (when complex?) of fftbuffer (from size + 1 to end) (setting output_buffers first 512 (sie) values)
-			// TODO		index = index + size; // iterate througth the next 512 x (=sample) values
-			//
-			// TODO 3.:
-			// TODO 
 
 			//unsigned int reverseSize = blockSizeIn / 2;
 			
 
-			//fftwf_execute(*planIn);
+			fftwf_execute(plan);
 
+			qDebug("analyzeB run3");
 
-			absspec(complexIn, absSpectrum.data(), binCount(blockSizeIn));
+			absspec(fftComplex, absSpectrum.data(), binCount(blockSizeIn));
 			normalize(absSpectrum, outputSpectrum, blockSizeIn);
 
 			/*
@@ -264,6 +284,7 @@ void FFTProcessor::threadedAnalyze(std::atomic<bool>* terminateIn, fftwf_plan* p
 			}
 			*/
 
+			qDebug("analyzeB run4");
 			outputAccessIn->lock();
 			for (unsigned int i = 0; i < binCount(blockSizeIn); i++)
 			{
@@ -273,8 +294,9 @@ void FFTProcessor::threadedAnalyze(std::atomic<bool>* terminateIn, fftwf_plan* p
 			*spectrumChangedOut = true;
 
 			qDebug("analyzeB try inverse");
-			if (filterSpectrumIn->size() == blockSizeIn && false)
+			if (filterSpectrumIn->size() == blockSizeIn)
 			{
+				/*
 				for (unsigned int i = 0; i < binCount(blockSizeIn); i++)
 				{
 					inverseOldReal[i] = complexIn[i][0];
@@ -288,10 +310,10 @@ void FFTProcessor::threadedAnalyze(std::atomic<bool>* terminateIn, fftwf_plan* p
 				// and then it gets run througth an fft
 				for (unsigned int i = 0; i < blockSizeIn / 2; i++)
 				{
-					/*
+					
 					complexIn[i][0] = complexIn[i][0] * complexMultiplierIn->operator[](i);
 					complexIn[i][1] = complexIn[i][1] * complexMultiplierIn->operator[](i);
-					*/
+					
 					samplesIn->operator[](i) = filterSpectrumIn->operator[](i + blockSizeIn / 2);
 					//changedAbsSpectum[i] = absSpectrum[i];
 					//qDebug("FFTProcessor analyze complexMultiplier [%d]: %f", i, complexMultiplierIn->operator[](i));
@@ -316,17 +338,159 @@ void FFTProcessor::threadedAnalyze(std::atomic<bool>* terminateIn, fftwf_plan* p
 					//complexIn[i][0] = inverseOldReal[i];// * complexIn[i][0];
 					//complexIn[i][1] = inverseOldImg[i];// * complexIn[i][1];
 				}
-				fftwf_execute(*inversePlanIn);
+				*/
+				/*
+				for (unsigned int i = 0; i < binCount(blockSizeIn); i++)
+				{
+					qDebug("analyzeB complex start %d, %f, %f", i, fftComplex[i][0], fftComplex[i][1]);
+				}
+				*/
+				std::vector<float> absSpectrumB(blockSizeIn);
 				for (unsigned int i = 0; i < blockSizeIn; i++)
 				{
-					samplesOut->operator[](i) = samplesOut->operator[](i) / blockSizeIn;
-					//samplesOut->operator[](i) = samplesOut->operator[](i) / hanningWindow[i] / blockSizeIn;
-					//qDebug("FFTProcessor analyze samplesOut [%d]: %f", i, samplesOut->operator[](i));
+					absSpectrumB[i] = absSpectrum[i] * filterSpectrumIn->operator[](i);
 				}
-				outputAccessIn->unlock();
-				*samplesChangedOut = true;
+				inverseAbsspec(fftComplex, absSpectrumB.data(), 44100, binCount(blockSizeIn));
+				/*
+				for (unsigned int i = 0; i < binCount(blockSizeIn); i++)
+				{
+					qDebug("analyzeB complex end %d, %f, %f", i, fftComplex[i][0], fftComplex[i][1]);
+				}
+				*/
+				fftwf_execute(iplan);
+
+				for (unsigned int i = 0; i < blockSizeIn; i++)
+				{
+					samplesOut[i] = samplesOut[i] / blockSizeIn * hanningWindow[i];
+					//samplesOut[i] = samplesOut[i] / blockSizeIn;
+					//qDebug("FFTProcessor analyze 0. samplesOut convert_back: [%d]:%f", i, samplesOut[i]);
+				}
+				/*
+				if (writeSamplesOut == false)
+				{
+					if (writeToBufferA == true)
+					{
+						samplesOutBufferA = samplesOut;
+						qDebug("FFTProcessor analyze A reset reset reset reset reset reset reset reset reset reset reset reset reset");
+					}
+					else
+					{
+						samplesOutBufferB = samplesOut;
+						qDebug("FFTProcessor analyze B reset reset reset reset reset reset reset reset reset reset reset reset reset");
+					}
+					writeToBufferA = !writeToBufferA;
+					writeSamplesOut = true;
+				}
+				*/
+				//else
+				//{
+					outputAccessIn->lock();
+					float lastDebug = samplesOutB->operator[](samplesOutB->size() - 1);
+					/*
+					if (writeToBufferA == true)
+					{
+						for (unsigned int i = 0; i < blockSizeIn / 2; i++)
+						{
+							samplesOutB->operator[](i) = samplesOutBufferA[i + blockSizeIn / 2];
+							qDebug("FFTProcessor analyze 1.A samplesOut [%d]: %f, %f, %d", i, samplesOutB->operator[](i), samplesOutBufferA[i + blockSizeIn / 2], i + blockSizeIn / 2);
+						}
+						for (unsigned int i = blockSizeIn / 2; i < blockSizeIn; i++)
+						{
+							samplesOutB->operator[](i) = samplesOutBufferB[i - blockSizeIn / 2];
+							qDebug("FFTProcessor analyze 3.B samplesOut [%d]: %f, %f, %d", i, samplesOutB->operator[](i), samplesOutBufferB[i - blockSizeIn / 2], i - blockSizeIn / 2);
+						}
+					}
+					else
+					{
+						for (unsigned int i = 0; i < blockSizeIn / 2; i++)
+						{
+							samplesOutB->operator[](i) = samplesOutBufferB[i + blockSizeIn / 2];
+							qDebug("FFTProcessor analyze 1.B samplesOut [%d]: %f, %f, %d", i, samplesOutB->operator[](i), samplesOutBufferB[i + blockSizeIn / 2], i + blockSizeIn / 2);
+						}
+						for (unsigned int i = blockSizeIn / 2; i < blockSizeIn; i++)
+						{
+							samplesOutB->operator[](i) = samplesOutBufferA[i - blockSizeIn / 2];
+							qDebug("FFTProcessor analyze 3.A samplesOut [%d]: %f, %f, %d", i, samplesOutB->operator[](i), samplesOutBufferA[i - blockSizeIn / 2], i - blockSizeIn / 2);
+						}
+					}
+
+					for (unsigned int i = 0; i < blockSizeIn; i++)
+					{
+						//qDebug("FFTProcessor analyze 2. samplesOut [%d]: %f, %f, %d", i, samplesOutB->operator[](i), samplesOut[i], i);
+						samplesOutB->operator[](i) += samplesOut[i];
+						qDebug("FFTProcessor analyze 2. samplesOut [%d]: %f, %f, %d", i, samplesOutB->operator[](i), samplesOut[i], i);
+					}
+					*/
+
+					if (outFillLoc == 0)
+					{
+						for (unsigned int i = 0; i < blockSizeIn; i++)
+						{
+							samplesOutB->operator[](i) = 0.0f;
+						}
+						for (unsigned int i = 0; i < blockSizeIn / 2; i++)
+						{
+							samplesOutB->operator[](i) = samplesOutBufferA[i + blockSizeIn / 2];
+							//qDebug("FFTProcessor analyze 1. samplesOut [%d]: %f, %f, %d", i, samplesOutB->operator[](i), samplesOutBufferA[i + blockSizeIn / 2], i + blockSizeIn / 2);
+						}
+					}
+					for (unsigned int i = outFillLoc; i < blockSizeIn; i++)
+					{
+						samplesOutB->operator[](i) += samplesOut[i - outFillLoc];
+						//samplesOutB->operator[](i) = samplesOut[i - outFillLoc];
+						//qDebug("FFTProcessor analyze 2. samplesOut [%d]: %f, %f, %d", i, samplesOutB->operator[](i), samplesOut[i - outFillLoc], i - outFillLoc);
+					}
+					outputAccessIn->unlock();
+
+					outFillLoc += blockSizeIn / 2;
+					if (outFillLoc + 1 >= blockSizeIn)
+					{
+						qDebug("FFTProcessor analyze write out, samplesOut copyed");
+						samplesOutBufferA = samplesOut;
+						*samplesChangedOut = true;
+						outFillLoc = 0;
+					}
+
+					/*
+					if (std::abs(samplesOutB->operator[](0) - lastDebug) > 0.08f && lastDebug != 0.0f)
+					{
+						qDebug("FFTProcessor analyze break 2");
+						int* debugval = nullptr;
+						// *debugval = 1;
+					}
+
+					for (unsigned int i = 0; i < blockSizeIn; i++)
+					{
+						qDebug("FFTProcessor analyze end samplesOut [%d]: %f", i, samplesOutB->operator[](i));
+						if (i > 0 && std::abs(samplesOutB->operator[](i - 1) - samplesOutB->operator[](i)) > 0.15f && std::abs(samplesOutB->operator[](i)) >= 0.01f && std::abs(samplesOutB->operator[](i - 1)) >= 0.01f)
+						{
+
+						qDebug("FFTProcessor analyze break 1");
+						int* debugval = nullptr;
+						// *debugval = 1;
+
+						}
+					}
+					*/
+					writeSamplesOut = false;
+				//}
 			}
 		}
+	}
+
+	if (plan != nullptr)
+	{
+		fftwf_destroy_plan(plan);
+	}
+	if (iplan != nullptr)
+	{
+		fftwf_destroy_plan(iplan);
+	}
+	qDebug("FFTProcessor destrc 0");
+	qDebug("FFTProcessor destrc 1");
+	if (fftComplex != nullptr)
+	{
+		fftwf_free(fftComplex);
 	}
 
 	qDebug("analyzeB end");
@@ -350,8 +514,8 @@ void FFTProcessor::threadedTerminate()
 // TODO multithread
 void FFTProcessor::rebuildWindow(FFTWindow FFTWindowIn)
 {
-	m_FFTWindowType = FFTWindowIn;
-	precomputeWindow(m_fftWindow.data(), m_blockSize, FFTWindowIn);
+	//m_FFTWindowType = FFTWindowIn;
+	//precomputeWindow(m_fftWindow.data(), m_blockSize, FFTWindowIn);
 }
 
 // TODO multithread
