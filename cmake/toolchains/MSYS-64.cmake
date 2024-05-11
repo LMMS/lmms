79a2df075e6526add755a5a19bacccabe8d1c3a3
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2024-05-11 16:23:46 -0400

    Remove support for msys2

--- cmake/toolchains/MSYS-64.cmake
+++
@@ -1,5 +0,0 @@
-INCLUDE(${CMAKE_CURRENT_LIST_DIR}/common/MSYS.cmake)
-INCLUDE(${CMAKE_CURRENT_LIST_DIR}/common/Win64.cmake)
-
-SET(MINGW_PREFIX                /mingw64)
-SET(MINGW_PREFIX32              /mingw32)
