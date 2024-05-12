615927ca43f2ec950a02e85b91b4d99a4ba49bab
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2024-05-12 14:12:08 -0400

    Initial refactoring

--- cmake/apple/CPackApple.cmake
+++
@@ -1,7 +0,0 @@
-# Create LMMS.app bundle using package_apple.sh
-# TODO: Port this script to CMake
-execute_process(COMMAND chmod u+x ${CPACK_BINARY_DIR}/package_apple.sh)
-execute_process(COMMAND ${CPACK_BINARY_DIR}/install_apple.sh ${CPACK_TEMPORARY_INSTALL_DIRECTORY} RESULT_VARIABLE EXIT_CODE)
-if(NOT EXIT_CODE EQUAL 0)
-	message(FATAL_ERROR "Execution of package_apple.sh failed")
-endif()
