349a13f0ab4fdc091f121da42964bb3ff7fbc794
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2024-05-12 01:08:34 -0400

    Remove appdmg dependency

--- cmake/apple/CPackApple.cmake
+++ cmake/apple/CPackApple.cmake
@@ -1,15 +1,10 @@
-# Create LMMS.app bundle using install_apple.sh
+# Create LMMS.app bundle using package_apple.sh
 # TODO: Port this script to CMake
-execute_process(COMMAND chmod u+x ${CPACK_BINARY_DIR}/install_apple.sh)
+execute_process(COMMAND chmod u+x ${CPACK_BINARY_DIR}/package_apple.sh)
 execute_process(COMMAND ${CPACK_BINARY_DIR}/install_apple.sh ${CPACK_TEMPORARY_INSTALL_DIRECTORY} RESULT_VARIABLE EXIT_CODE)
 if(NOT EXIT_CODE EQUAL 0)
-	message(FATAL_ERROR "Execution of install_apple.sh failed")
+	message(FATAL_ERROR "Execution of package_apple.sh failed")
 endif()
 
-# Create DMG
-message("Generating DMG")
-# REMOVE_RECURSE supresses missing file warnings
-file(REMOVE_RECURSE "${CPACK_DMG_FILE}")
-execute_process(
-	COMMAND appdmg "${CPACK_BINARY_DIR}/package_apple.json" "${CPACK_DMG_FILE}"
-)
+# Create tiff from two pngs
+execute_process(COMMAND convert "${CPACK_DMG_BACKGROUND_PNGS}" "${CPACK_DMG_BACKGROUND_IMAGE}")
