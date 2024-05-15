cb3c19dd49db5ebabf372c22a2150e53b621507d
Rossmaxx <mrroshan127@gmail.com>
2024-05-15 15:59:29 +0530

    enable vst on linux qt 6 and disable x11 embedding

--- plugins/VstBase/VstPlugin.cpp
+++ plugins/VstBase/VstPlugin.cpp
@@ -34,7 +34,8 @@
 #include <QLocale>
 #include <QTemporaryFile>
 
-#ifdef LMMS_BUILD_LINUX
+#if defined(LMMS_BUILD_LINUX && (QT_VERSION < QT_VERSION_CHECK(6,0,0)))
+// x11 embedding is currently unsupported on qt6
 #	include <QX11Info>
 #	include "X11EmbedContainer.h"
 #endif
@@ -416,7 +417,7 @@ bool VstPlugin::processMessage( const message & _m )
 					(LONG_PTR) gui::getGUI()->mainWindow()->winId() );
 #endif
 
-#ifdef LMMS_BUILD_LINUX
+#if defined(LMMS_BUILD_LINUX && (QT_VERSION < QT_VERSION_CHECK(6,0,0)))
 			XSetTransientForHint( QX11Info::display(),
 					m_pluginWindowID,
 					gui::getGUI()->mainWindow()->winId() );
