12825152dd049db786db78f849f98924b83a3f38
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2024-05-16 22:19:00 -0400

    Fix relative symlinks

--- cmake/modules/CreateSymlink.cmake
+++ cmake/modules/CreateSymlink.cmake
@@ -1,10 +1,35 @@
-# Offer symlink support via "cmake -E create_symlink"
+# Offer relative symlink support via "cmake -E create_symlink"
 # For verbose, set COMMAND_ECHO to STDOUT in calling script
 macro(create_symlink filepath sympath)
-	if(NOT DEFINED COMMAND_ECHO)
-		set(_COMMAND_ECHO STDOUT)
+	if(CMAKE_COMMAND)
+		set(_cmake_command "${CMAKE_COMMAND}")
+	elseif(CPACK_CMAKE_COMMAND)
+		set(_cmake_command "${CPACK_CMAKE_COMMAND}")
 	else()
-		set(_COMMAND_ECHO "${COMMAND_ECHO}")
+		message(FATAL_ERROR "Sorry, can't resolve variable CMAKE_COMMAND")
 	endif()
-	execute_process(COMMAND ${CPACK_CMAKE_COMMAND} -E create_symlink "${filepath}" "${sympath}" COMMAND_ECHO ${_COMMAND_ECHO})
+
+	if(NOT IS_ABSOLUTE "${sympath}")
+		message(FATAL_ERROR "Sorry, this command only works with absolute paths")
+	endif()
+
+	if(NOT DEFINED COMMAND_ECHO)
+		set(_command_echo NONE)
+	else()
+		set(_command_echo "${COMMAND_ECHO}")
+	endif()
+
+	# Calculate the relative path
+	file(RELATIVE_PATH reldir "${sympath}/../" "${filepath}")
+	get_filename_component(symname "${sympath}" NAME)
+
+	# Calculate the working directory
+	get_filename_component(sympath_parent "${sympath}" DIRECTORY)
+	message(STATUS "WORKING_DIRECTORY: ${sympath_parent}")
+
+	# Create the symbolic link
+	execute_process(COMMAND "${_cmake_command}" -E create_symlink "${reldir}" "${symname}"
+		WORKING_DIRECTORY "${sympath_parent}"
+		COMMAND_ECHO ${_command_echo}
+		COMMAND_ERROR_IS_FATAL ANY)
 endmacro()
