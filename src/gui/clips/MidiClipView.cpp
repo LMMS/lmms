8c62b2205f7a58695d65e9b26fee62865853c13e
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2024-05-21 11:32:28 -0400

    Add "natural" scrolling support for trackpads (#5510)
    Adds QWheelEvent::inverted() support to spinboxes, knobs, sliders

--- src/gui/clips/MidiClipView.cpp
+++ src/gui/clips/MidiClipView.cpp
@@ -334,7 +334,8 @@ void MidiClipView::wheelEvent(QWheelEvent * we)
 		}
 
 		Note * n = m_clip->noteAtStep( step );
-		if(!n && we->angleDelta().y() > 0)
+		const int direction = (we->angleDelta().y() > 0 ? 1 : -1) * (we->inverted() ? -1 : 1);
+		if(!n && direction > 0)
 		{
 			n = m_clip->addStepNote( step );
 			n->setVolume( 0 );
@@ -342,8 +343,7 @@ void MidiClipView::wheelEvent(QWheelEvent * we)
 		if( n != nullptr )
 		{
 			int vol = n->getVolume();
-
-			if(we->angleDelta().y() > 0)
+			if(direction > 0)
 			{
 				n->setVolume( qMin( 100, vol + 5 ) );
 			}
