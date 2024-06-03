df2fe2a6bcd841ec201499ad6768da0fcfceacab
Dalton Messmer <messmer.dalton@gmail.com>
2024-06-02 22:35:35 -0400

    Install gpg earlier

--- .github/workflows/setup-cmake.sh
+++ .github/workflows/setup-cmake.sh
@@ -4,7 +4,7 @@
 # As of 5/21/2024, only focal and jammy are supported
 . /etc/os-release
 if [ "$UBUNTU_CODENAME" = "focal" ] || [ "$UBUNTU_CODENAME" = "jammy" ]; then
-    sudo apt-get install --yes wget
+    sudo apt-get install --yes wget gpg
     test -f /usr/share/doc/kitware-archive-keyring/copyright || \
         wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null \
         | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
