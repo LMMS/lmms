340b010e5627055816c207f0b5ccbd1d8efc50ab
szeli1 <143485814+szeli1@users.noreply.github.com>
2024-06-27 12:35:17 +0200

    AutomatableModel_new_setValue_signal_added

--- include/AutomatableModel.h
+++ include/AutomatableModel.h
@@ -322,7 +322,9 @@ public slots:
 	void unlinkControllerConnection();
 	void setUseControllerValue(bool b = true);
 
-
+signals:
+	// runs when setValue was successful
+	void setValueEvent();
 protected:
 	AutomatableModel(
 						const float val = 0,
