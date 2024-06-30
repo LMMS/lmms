21911b34ada8fa5565ebcddf892bc248c24fccfb
Johannes Lorenz <j.git@lorenz-ho.me>
2024-06-30 02:06:12 +0200

    Avoid Effects being to small

--- plugins/Lv2Effect/Lv2FxControlDialog.cpp
+++ plugins/Lv2Effect/Lv2FxControlDialog.cpp
@@ -36,6 +36,7 @@ Lv2FxControlDialog::Lv2FxControlDialog(Lv2FxControls *controls) :
 	EffectControlDialog(controls),
 	Lv2ViewBase(this, controls)
 {
+	setMinimumSize(440, 60);
 	if (m_reloadPluginButton) {
 		connect(m_reloadPluginButton, &QPushButton::clicked,
 				this, [this](){ lv2Controls()->reload(); });
