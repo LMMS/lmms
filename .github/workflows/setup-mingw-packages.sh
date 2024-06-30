b8389d06bae7539f1af983a2b1033a8a23626fe7
Dalton Messmer <messmer.dalton@gmail.com>
2024-06-30 05:15:11 -0400

    Use PPA for everything but fluidsynth, sndfile, lilv, and lv2

--- .github/workflows/setup-mingw-packages.sh
+++ .github/workflows/setup-mingw-packages.sh
@@ -2,12 +2,18 @@
 
 # TODO: Stop using this PPA once all MinGW dependencies are available through vcpkg and work with CMake.
 
-# Currently using these PPA packages:
-#   fftw-mingw-w64        (CMake cannot find vcpkg's version)
-#   fltk-mingw-w64        (vcpkg fails to build it)
-#   libgig-mingw-w64      (vcpkg does not support MinGW yet)
-#   portaudio-mingw-w64   (CMake cannot find vcpkg's version)
-#   qt5base-mingw-w64     (vcpkg takes too much memory to build)
+# All MinGW dependencies are installed from tobydox's PPA *except* for these:
+#   fluidsynth
+#   libsndfile
+#   lilv
+#   lv2
+
+# Some issues with vcpkg dependencies include:
+#   fftw3                 (CMake cannot find vcpkg's version)
+#   fltk                  (vcpkg fails to build it)
+#   libgig                (vcpkg does not support MinGW yet)
+#   portaudio             (CMake cannot find vcpkg's version)
+#   qt5-base              (vcpkg takes too much memory to build)
 
 sudo echo "deb http://ppa.launchpad.net/tobydox/mingw-w64/ubuntu focal main" >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 72931B477E22FEFD47F8DECE02FE5F12ADDE29B2
