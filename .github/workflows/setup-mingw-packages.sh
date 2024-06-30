d9c6c52a183c354d4529f28a100083a318d31234
Dalton Messmer <messmer.dalton@gmail.com>
2024-06-29 22:30:08 -0400

    again

--- .github/workflows/setup-mingw-packages.sh
+++ .github/workflows/setup-mingw-packages.sh
@@ -5,6 +5,6 @@
 #   libgig-mingw-w64
 #   qt5base-mingw-w64
 
-echo "deb http://ppa.launchpad.net/tobydox/mingw-w64/ubuntu focal main" >> /etc/apt/sources.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 72931B477E22FEFD47F8DECE02FE5F12ADDE29B2
+sudo echo "deb http://ppa.launchpad.net/tobydox/mingw-w64/ubuntu focal main" >> /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 72931B477E22FEFD47F8DECE02FE5F12ADDE29B2
 sudo apt-get update
