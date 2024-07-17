20d8342a58397562918df848f1f9023a4b718d1c
Andrew Wiltshire <62200778+AW1534@users.noreply.github.com>
2024-07-17 18:02:10 +0100

    Update src/gui/FileBrowser.cpp
    Co-authored-by: saker <sakertooth@gmail.com>

--- src/gui/FileBrowser.cpp
+++ src/gui/FileBrowser.cpp
@@ -571,12 +571,11 @@ void FileBrowserTreeWidget::keyPressEvent(QKeyEvent * ke )
 
 	auto file = dynamic_cast<FileItem*>(item);
 
-	// If it's not a file, try to expand it.
-	if (file == nullptr) {
-		if (preview) {
+	// If it's not a file and the item is being previewed, assume it's a directory and try to expand or shrink it
+	if (!file && preview)
+	{
 			item->setExpanded(!item->isExpanded());
-		}
-		return;
+			return;
 	}
 	// When moving to a new sound, preview it. Skip presets, they can play forever
 	if (vertical && file->type() == FileItem::FileType::Sample)
