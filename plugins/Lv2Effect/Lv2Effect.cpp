d4e7e5c4b1b648f718d466648508fb81ce28c7d0
Johannes Lorenz <j.git@lorenz-ho.me>
2024-07-21 18:08:09 +0200

    Lv2: Do not duplicate mono effects

--- plugins/Lv2Effect/Lv2Effect.cpp
+++ plugins/Lv2Effect/Lv2Effect.cpp
@@ -1,7 +1,7 @@
 /*
  * Lv2Effect.cpp - implementation of LV2 effect
  *
- * Copyright (c) 2018-2023 Johannes Lorenz <jlsf2013$users.sourceforge.net, $=@>
+ * Copyright (c) 2018-2024 Johannes Lorenz <jlsf2013$users.sourceforge.net, $=@>
  *
  * This file is part of LMMS - https://lmms.io
  *
@@ -72,15 +72,15 @@ Effect::ProcessStatus Lv2Effect::processImpl(SampleFrame* buf, const fpp_t frame
 {
 	Q_ASSERT(frames <= static_cast<fpp_t>(m_tmpOutputSmps.size()));
 
-	m_controls.copyBuffersFromLmms(buf, frames);
-	m_controls.copyModelsFromLmms();
+	m_controls.copyBuffersFromCore(buf, frames);
+	m_controls.copyModelsFromCore();
 
 //	m_pluginMutex.lock();
 	m_controls.run(frames);
 //	m_pluginMutex.unlock();
 
-	m_controls.copyModelsToLmms();
-	m_controls.copyBuffersToLmms(m_tmpOutputSmps.data(), frames);
+	m_controls.copyModelsToCore();
+	m_controls.copyBuffersToCore(m_tmpOutputSmps.data(), frames);
 
 	bool corrupt = wetLevel() < 0; // #3261 - if w < 0, bash w := 0, d := 1
 	const float d = corrupt ? 1 : dryLevel();
---
+++ src/core/ModelGroup.cpp
@@ -0,0 +1,85 @@
+/*
+ * ModelGroup.cpp - base classes for groups of models
+ *
+ * Copyright (c) 2019-2024 Johannes Lorenz <j.git$$$lorenz-ho.me, $$$=@>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include "ModelGroup.h"
+
+#include <QDomDocument>
+#include <QDomElement>
+
+#include "AutomatableModel.h"
+
+namespace lmms {
+
+/*
+	ModelGroup
+*/
+
+void ModelGroup::saveSettings(QDomDocument& doc, QDomElement& that)
+{
+	QDomElement models = doc.createElement("models");
+	that.appendChild(models);
+	foreach_model([&doc, &models](const std::string&, ModelInfo& inf) {
+		inf.m_model->saveSettings(doc, models, inf.m_name); /* TODO: m_name useful */
+	});
+}
+
+void ModelGroup::loadSettings(const QDomElement& that)
+{
+	QDomElement models = that.firstChildElement("models");
+	foreach_model([&models](const std::string&, ModelInfo& inf) {
+		// try to load, if it fails, this will load a sane initial value
+		inf.m_model->loadSettings(models, inf.m_name); /* TODO: m_name useful */
+	});
+}
+
+void ModelGroup::addModel(AutomatableModel* model, const QString& name)
+{
+	model->setObjectName(name);
+	m_models.emplace(std::string(name.toUtf8().data()), ModelInfo(name, model));
+	/*
+	 * The following code is currently not used because Models can not be removed
+	 * in no kind of implementation that uses ModelGroup. The code is
+	 * deactivated because this is not a QObject anymore (an ugly connection
+	 * handler would be required for this)
+	 */
+#if 0
+	QObject::connect(model, &AutomatableModel::destroyed,
+				this, [this, model](jo_id_t){
+					if (const auto it = m_models.find(model->objectName().toStdString()); it != m_models.end())
+					{
+						m_models.erase(it);
+					}
+				},
+				Qt::DirectConnection);
+#endif
+	// View needs to create another child view, e.g. a new knob:
+	emit m_parent->dataChanged();
+}
+
+void ModelGroup::removeModel(AutomatableModel* mdl)
+{
+	if (const auto it = m_models.find(mdl->objectName().toStdString()); it != m_models.end()) { m_models.erase(it); }
+}
+
+} // namespace lmms
