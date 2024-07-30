92190aaf59af19346259eced2e9c38c4a982c931
szeli1 <143485814+szeli1@users.noreply.github.com>
2024-07-30 22:10:17 +0200

    MidiClipView_restore_to_master_2

--- src/gui/clips/MidiClipView.cpp
+++ src/gui/clips/MidiClipView.cpp
@@ -63,19 +63,10 @@ MidiClipView::MidiClipView( MidiClip* clip, TrackView* parent ) :
 			this, SLOT(update()));
 	update();
 
-	//setStyle( QApplication::style() );
-
-	setFixedWidth(300);
-
-	setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+	setStyle( QApplication::style() );
 }
 
 
-
-QSize MidiClipView::sizeHint() const
-{
-	return {200, 200};
-}
 
 
 MidiClip* MidiClipView::getMidiClip()
@@ -382,8 +373,6 @@ static int computeNoteRange(int minKey, int maxKey)
 
 void MidiClipView::paintEvent( QPaintEvent * )
 {
-	qDebug("minimum size w: %d", minimumSizeHint().width());
-	qDebug("minimum size ww: %d", minimumSize().width());
 	QPainter painter( this );
 
 	if( !needsUpdate() )
