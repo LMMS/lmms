c873d0142e0367529267d4adfcd563e1607fa0d5
szeli1 <143485814+szeli1@users.noreply.github.com>
2024-08-20 16:10:28 +0200

    LmmsExporter_adding_files

---
+++ src/core/LmmsExporter.cpp
@@ -0,0 +1,263 @@
+/*
+ * LmmsExporter.cpp - exporting files (currently only audio files), TODO rename class when things change
+ *
+ * Copyright (c) 2024 szeli1 <TODO/at/gmail/dot.com>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include <QFile>
+
+#include "LmmsExporter.h"
+
+#include "AudioFileWave.h"
+#include "AudioFileFlac.h"
+#include "AudioFileOgg.h"
+#include "AudioFileMP3.h"
+
+namespace lmms
+{
+
+const std::array<LmmsExporter::FileEncodeDevice, 5> LmmsExporter::s_fileEncodeDevices
+{
+	FileEncodeDevice{LmmsExporter::ExportAudioFileFormat::Wave,
+		QT_TRANSLATE_NOOP("LmmsExporter", "WAV (*.wav)"),
+					".wav", &AudioFileWave::getInst },
+	FileEncodeDevice{LmmsExporter::ExportAudioFileFormat::Flac,
+		QT_TRANSLATE_NOOP("LmmsExporter", "FLAC (*.flac)"),
+		".flac",
+		&AudioFileFlac::getInst
+	},
+	FileEncodeDevice{LmmsExporter::ExportAudioFileFormat::Ogg,
+		QT_TRANSLATE_NOOP("LmmsExporter", "OGG (*.ogg)"),
+					".ogg",
+#ifdef LMMS_HAVE_OGGVORBIS
+					&AudioFileOgg::getInst
+#else
+					nullptr
+#endif
+	},
+	FileEncodeDevice{LmmsExporter::ExportAudioFileFormat::MP3,
+		QT_TRANSLATE_NOOP("LmmsExporter", "MP3 (*.mp3)"),
+					".mp3",
+#ifdef LMMS_HAVE_MP3LAME
+					&AudioFileMP3::getInst
+#else
+					nullptr
+#endif
+	},
+	// Insert your own file-encoder infos here.
+	// Maybe one day the user can add own encoders inside the program.
+
+	FileEncodeDevice{LmmsExporter::ExportAudioFileFormat::Count, nullptr, nullptr, nullptr}
+};
+
+LmmsExporter::LmmsExporter(const ExportFileType fileType,
+			const QString& outputLocationAndName) :
+	m_exportFileType(fileType),
+	m_outputFile(outputLocationAndName),
+	m_fileDev(nullptr),
+	m_abort(false),
+	m_getBufferFunction(nullptr),
+	m_endFunction(nullptr),
+	m_getBufferData(nullptr),
+	m_buffer(0)
+{
+	m_thread = nullptr;
+}
+
+LmmsExporter::~LmmsExporter()
+{
+	// close thread
+	stopExporting();
+
+	if (m_fileDev != nullptr)
+	{
+		delete m_fileDev;
+	}
+}
+
+void LmmsExporter::setupAufioFile(
+		const AudioEngine::qualitySettings& qualitiSettings,
+		const OutputSettings& outputSettings,
+		ExportAudioFileFormat fileFormat,
+		const fpp_t defaultFrameCount,
+		SampleFrame* exportBuffer
+		const fpp_t exportBufferFrameCount);
+{
+	m_qualitySettings = qualitiSettings;
+	
+	setupAudioFileInternal(outputSettings, fileFormat, defaultFrameCount);
+	
+	processThisBuffer(exportFrameBuffer, exportFrameCount);
+}
+
+void LmmsExporter::setupAufioFile(
+		const AudioEngine::qualitySettings& qualitiSettings,
+		const OutputSettings& outputSettings,
+		ExportAudioFileFormat fileFormat,
+		const fpp_t defaultFrameCount,
+		BufferFn getBufferFunction,
+		EndFn endFunction,
+		void* getBufferData)
+{
+	m_qualitySettings = qualitiSettings;
+
+	m_getBufferFunction = getBufferFunction;
+	m_endFunction = endFunction;
+	m_getBufferData = getBufferData;
+
+	setupAudioFileInternal(outputSettings, fileFormat, defaultFrameCount);
+}
+
+
+
+static ExportAudioFileFormat LmmsExporter::getAudioFileFormatFromFileName(const QString& fileName)
+{
+
+}
+static ExportAudioFileFormat LmmsExporter::getAudioFileFormatFromExtension(const QString& extenisonString)
+{
+
+}
+static QString LmmsExporter::getAudioFileExtensionFromFormat(ExportAudioFileFormat fmt)
+{
+
+}
+
+
+
+void LmmsExporter::startExporting()
+{
+	if (m_thread.get() != nullptr) { return; }
+	m_abort = false;
+
+	if (canExportAutioFile())
+	{
+		m_thread = std::make_unique<std::thread>(processExportingAudioFile, this);
+	}
+}
+
+void LmmsExporter::stopExporting()
+{
+	// close thread
+	if (m_thread.get() != nullptr)
+	{
+		m_abort = true;
+		m_thread->join();
+		// deleting unique_ptr
+		m_thread = nullptr;
+	}
+}
+
+
+
+void LmmsExporter::processExportingAudioFile(LmmsExporter* thisExporter)
+{
+	while (!thisExporter->m_abort)
+	{
+		// if a function pointer was provided
+		// use that to fill m_buffer
+		if (thisExporter->m_getBufferFunction != nullptr)
+		{
+			processNextBuffer();
+		}
+		
+		// if thisExporter->m_buffer wasn't filled by
+		// processNextBuffer() or processThisBuffer() before
+		if (thisExporter->m_buffer.size() <= 0)
+		{
+			break;
+		}
+
+		thisExporter->m_fileDev->processThisBuffer(thisExporter->m_buffer.data(), thisExporter->m_buffer.size());
+		// Continually track and emit progress percentage to listeners.
+		progressChanged();
+		
+		// if no function pointer was provided
+		if (thisExporter->m_getBufferFunction == nullptr)
+		{
+			// clear buffer to end while loop
+			thisExporter->m_buffer.clear();
+			break;
+		}
+	}
+
+	if (thisExporter->m_endFunction != nullptr)
+	{
+		thisExporter->m_endFunction(thisExporter->m_getBufferData);
+	}
+
+	// If the user aborted export-process, the file has to be deleted.
+	if(thisExporter->m_abort)
+	{
+		const QString file = thisExporter->m_fileDev->outputFile();
+		QFile(file).remove();
+	}
+}
+
+bool LmmsExporter::canExportAutioFile() const
+{
+	return m_fileDev != nullptr && (m_buffer.size() > 0 || m_getBufferFunction != nullptr);
+}
+
+
+
+bool LmmsExporter::processNextBuffer()
+{
+	m_getBufferFunction(&m_buffer, m_getBufferData);
+	if (m_buffer.size() <= 0) { return true; }
+
+	return false;
+}
+
+bool LmmsExporter::processThisBuffer(SampleFrame* frameBuffer, const fpp_t frameCount)
+{
+	if (frameCount <= 0) { m_buffer.clear(); return true; }
+	if (m_buffer.size() != frameCount)
+	{
+		m_buffer.resize(frameCount);
+	}
+
+	memcpy(m_buffer.data(), frameBuffer, m_buffer.size() * sizeof(SampleFrame));
+	return false;
+}
+
+void setupAudioFileInternal(
+	OutputSettings& outputSettings
+	ExportAudioFileFormat fileFormat,
+	const fpp_t defaultFrameCount)
+{
+	AudioFileDeviceInstantiaton audioEncoderFactory = s_fileEncodeDevices[static_cast<std::size_t>(fileFormat)].m_getDevInst;
+
+	if (audioEncoderFactory)
+	{
+		bool successful = false;
+
+		m_fileDev = audioEncoderFactory(outputSettings, successful, m_outputFile, 2, defaultFrameCount);
+		if(!successful)
+		{
+			delete m_fileDev;
+			m_fileDev = nullptr;
+		}
+	}
+}
+
+} // namespace lmms
+
