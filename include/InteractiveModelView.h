9dce4da495dfabc10c45e4a7f0c6e0c26f647d79
szeli1 <143485814+szeli1@users.noreply.github.com>
2024-08-30 12:50:01 +0200

    InteractiveModelView_fixing_errors

--- include/InteractiveModelView.h
+++ include/InteractiveModelView.h
@@ -25,21 +25,26 @@
 #ifndef LMMS_GUI_INTERACTIVE_MODEL_VIEW_H
 #define LMMS_GUI_INTERACTIVE_MODEL_VIEW_H
 
-#include <array>
 #include <list>
 #include <memory>
-#include <utility>
 #include <vector>
 
-#include "Model.h"
+#include <QWidget>
+
+#include "Clipboard.h"
+#include "lmms_export.h"
+#include "ModelView.h"
 
 namespace lmms::gui
 {
 
-class LMMS_EXPORT InteractiveModelView : public ModelView
+
+
+class LMMS_EXPORT InteractiveModelView : public QWidget
 {
+Q_OBJECT
 public:
-	InteractiveModelView(Model* model, QWidget* widget);
+	InteractiveModelView(QWidget* widget);
 	~InteractiveModelView() override;
 
 	//virtual void setModel( Model* model, bool isOldModelValid = true );
@@ -56,8 +61,25 @@ public:
 	static void stopHighlighting();
 
 protected:
-	struct ModelShortcut
+	class ModelShortcut
 	{
+	public:
+	
+		inline bool operator==(ModelShortcut& rhs)
+		{
+			return m_key == rhs.m_key &&
+				m_modifier == rhs.m_modifier &&
+				m_times == rhs.m_times;
+		}
+		
+		/*
+		inline bool operator==(const ModelShortcut& firstShortcut, const ModelShortcut& secondShortcut)
+		{
+    		return firstShortcut.m_key == secondShortcut.m_key &&
+    			firstShortcut.m_modifier == secondShortcut.m_modifier &&
+    			firstShortcut.m_times == secondShortcut.m_times;
+		}
+		*/
 		Qt::Key m_key;
 		Qt::KeyboardModifier m_modifier;
 		unsigned int m_times;
@@ -65,29 +87,22 @@ protected:
 	};
 
 	void keyPressEvent(QKeyEvent* event) override;
-	void enterEvent(QEnterEvent* event) override;
-	void leaveEvent(QLeaveEvent* event) override;
+	void enterEvent(QEvent* event) override;
+	void leaveEvent(QEvent* event) override;
 	
-	virtual shortcutPressedEvent(size_t shortcutLocation, QKeyEvent* event) = 0;
+	virtual void shortcutPressedEvent(size_t shortcutLocation, QKeyEvent* event) = 0;
 	virtual bool canAcceptClipBoardData(Clipboard::StringPairDataType dataType);
-	virtual const std::vector<ModelShortcut> getShortcuts() = 0;
+	virtual std::vector<ModelShortcut> getShortcuts() = 0;
 
 	bool getIsHighlighted() const;
 private:
-	bool setIsHighlighted(bool isHighlighted);
-	bool doesShortcutMatch(ModelShortcut& shortcut, QKeyEvent* event, unsigned int times);
+	void setIsHighlighted(bool isHighlighted);
+	bool doesShortcutMatch(const ModelShortcut* shortcut, QKeyEvent* event, unsigned int times) const;
 
 	bool m_isHighlighted;
 	
 	ModelShortcut m_lastShortcut;
 	unsigned int m_lastShortcutCounter;
-
-	/*
-	static const std::array<std::pair<Qt::Key, QString>, 2> s_keyNames = {
-		std::make_pair<Qt::Key, QString>(Qt::Key_C, "c"),
-		std::make_pair<Qt::Key, QString>(Qt::Key_V, "v")
-	};
-	*/
 	
 	static std::list<InteractiveModelView*> s_interactiveWidgets;
 };
