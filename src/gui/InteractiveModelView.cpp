bab9b3775caf18e0dc1dc3b66dad50a466bc6580
szeli1 <143485814+szeli1@users.noreply.github.com>
2024-09-01 11:09:00 +0200

    IntractiveModelView_implementing_SimpleTextFloat

--- src/gui/InteractiveModelView.cpp
+++ src/gui/InteractiveModelView.cpp
@@ -27,23 +27,27 @@
 #include <algorithm>
 
 #include <QKeyEvent>
+#include <QKeySequence> // displaying qt key names
+
+#include "GuiApplication.h"
+#include "MainWindow.h"
+#include "SimpleTextFloat.h"
 
 namespace lmms::gui
 {
 
+SimpleTextFloat* InteractiveModelView::s_simpleTextFloat = nullptr;
 std::list<InteractiveModelView*> InteractiveModelView::s_interactiveWidgets;
 
 InteractiveModelView::InteractiveModelView(QWidget* widget) :
 	QWidget(widget),
 	m_isHighlighted(false),
-	m_lastShortcutCounter(0)
+	m_lastShortcutCounter(0),
+	m_focusedBeforeWidget(nullptr)
 {
 	s_interactiveWidgets.push_back(this);
 
-	m_lastShortcut.m_key = Qt::Key_F35;
-	m_lastShortcut.m_modifier = Qt::NoModifier;
-	m_lastShortcut.m_times = 0;
-	m_lastShortcut.m_shortcutDescription = "";
+	m_lastShortcut.reset();
 }
 
 InteractiveModelView::~InteractiveModelView()
@@ -74,50 +78,140 @@ void InteractiveModelView::stopHighlighting()
 	}
 }
 
+void InteractiveModelView::showMessage(QString& message)
+{
+	if (s_simpleTextFloat == nullptr)
+	{
+		// we don't own this object, so we do not need to delete it
+		s_simpleTextFloat = new SimpleTextFloat();
+	}
+	s_simpleTextFloat->setText(message);
+	s_simpleTextFloat->moveToGlobal(QPoint(2, getGUI()->mainWindow()->height()));
+	s_simpleTextFloat->showWithDelay(0, 60000);
+}
+
+void InteractiveModelView::hideMessage()
+{
+	if (s_simpleTextFloat == nullptr)
+	{
+		s_simpleTextFloat = new SimpleTextFloat();
+	}
+	s_simpleTextFloat->hide();
+}
+
 void InteractiveModelView::keyPressEvent(QKeyEvent* event)
 {
 	std::vector<ModelShortcut> shortcuts(getShortcuts());
-	for (size_t i = 0; i < shortcuts.size(); i++)
+
+	size_t foundIndex = 0;
+	unsigned int minMaxTimes = 0;
+	bool found = false;
+
+	// if the last shortcut's keys mach the current keys
+	if (doesShortcutMatch(&m_lastShortcut, event))
 	{
-		if (doesShortcutMatch(&(shortcuts[i]), event, m_lastShortcutCounter))
+		// find the highest m_times or
+		// the shortcut that's m_times == m_lastShortcutCounter
+		for (size_t i = 0; i < shortcuts.size(); i++)
 		{
-			ModelShortcut temp = shortcuts[i];
-			if (m_lastShortcut == temp)
+			if (doesShortcutMatch(&shortcuts[i], event))
 			{
-				m_lastShortcutCounter++;
+				// selecting the shortcut with the largest m_times
+				if (found == false || minMaxTimes < shortcuts[i].m_times)
+				{
+					foundIndex = i;
+					minMaxTimes = shortcuts[i].m_times;
+				}
+				found = true;
+			
+				if (m_lastShortcutCounter == shortcuts[i].m_times)
+				{
+					m_lastShortcutCounter = shortcuts[i].m_shouldLoop ? 0 : m_lastShortcutCounter + 1;
+					foundIndex = i;
+					break;
+				}
 			}
-			else
-			{
-				m_lastShortcut = shortcuts[i];
-				m_lastShortcutCounter = 0;
-			}
-			qDebug("found shortcut");
-			shortcutPressedEvent(i, event);
-			break;
 		}
+	}
+	else
+	{
+		// find the lowest m_times
+		for (size_t i = 0; i < shortcuts.size(); i++)
+		{
+			if (doesShortcutMatch(&shortcuts[i], event))
+			{
+				// selecting the shortcut with the largest m_times
+				if (found == false || minMaxTimes > shortcuts[i].m_times)
+				{
+					foundIndex = i;
+					minMaxTimes = shortcuts[i].m_times;
+				}
+				m_lastShortcut = shortcuts[i];
+				m_lastShortcutCounter = 1;
+				found = true;
+			}
+		}
+	}
+	if (found)
+	{
+		QString message = shortcuts[foundIndex].m_shortcutDescription;
+		showMessage(message);
+		shortcutPressedEvent(foundIndex, event);
 	}
 }
 
 void InteractiveModelView::enterEvent(QEvent* event)
 {
 	m_lastShortcutCounter = 0;
+	m_lastShortcut.reset();
 
-	m_lastShortcut.m_key = Qt::Key_F35;
-	m_lastShortcut.m_modifier = Qt::NoModifier;
-	m_lastShortcut.m_times = 0;
-	m_lastShortcut.m_shortcutDescription = "";
-	
-	qDebug("enter event");
+	showMessage(getShortcutMessage());
+
+	if (isVisible())
+	{
+		m_focusedBeforeWidget = QApplication::focusWidget();
+		// focus on this widget so keyPressEvent works
+		setFocus();
+	}
 }
 
 void InteractiveModelView::leaveEvent(QEvent* event)
 {
-	qDebug("leave event");
+	hideMessage();
+	// reset focus
+	if (m_focusedBeforeWidget)
+	{
+		m_focusedBeforeWidget->setFocus();
+	}
 }
 
-bool InteractiveModelView::canAcceptClipBoardData(Clipboard::StringPairDataType dataType)
+/*
+void InteractiveModelView::drawAutoHighlight(QPainter* painter)
 {
-	return false;
+	
+}
+*/
+
+QString InteractiveModelView::buildShortcutMessage()
+{
+	QString message = "";
+	std::vector<ModelShortcut> shortcuts(getShortcuts());
+	for (size_t i = 0; i < shortcuts.size(); i++)
+	{
+		message = message + QString("\"") + QKeySequence(shortcuts[i].m_modifier).toString()
+			+ QKeySequence(shortcuts[i].m_key).toString();
+		if (shortcuts[i].m_times > 0)
+		{
+			message = message + QString(" (x") + QString::number(shortcuts[i].m_times + 1) + QString(")");
+		}
+		message = message + QString("\": ")
+			+ shortcuts[i].m_shortcutDescription;
+		if (i + 1 < shortcuts.size())
+		{
+			message = message + QString(", ");
+		}
+	}
+	return message;
 }
 
 bool InteractiveModelView::getIsHighlighted() const
@@ -130,13 +224,21 @@ void InteractiveModelView::setIsHighlighted(bool isHighlighted)
 	if (m_isHighlighted != isHighlighted)
 	{
 		m_isHighlighted = isHighlighted;
-		update();
+		if (isVisible())
+		{
+			update();
+		}
 	}
 }
 
-bool InteractiveModelView::doesShortcutMatch(const ModelShortcut* shortcut, QKeyEvent* event, unsigned int times) const
+bool InteractiveModelView::doesShortcutMatch(const ModelShortcut* shortcut, QKeyEvent* event) const
 {
-	return shortcut->m_key == event->key() && (event->modifiers() & shortcut->m_modifier) && shortcut->m_times == times;
+	return shortcut->m_key == event->key() && (event->modifiers() & shortcut->m_modifier);
+}
+
+bool InteractiveModelView::doesShortcutMatch(const ModelShortcut* shortcutA, const ModelShortcut* shortcutB) const
+{
+	return shortcutA->m_key == shortcutB->m_key && (shortcutA->m_modifier & shortcutB->m_modifier);
 }
 
 } // namespace lmms::gui
