7aca60388166c3834f8cbd76400645b24363cf0c
szeli1 <143485814+szeli1@users.noreply.github.com>
2024-09-01 11:09:00 +0200

    IntractiveModelView_implementing_SimpleTextFloat

--- include/InteractiveModelView.h
+++ include/InteractiveModelView.h
@@ -26,9 +26,9 @@
 #define LMMS_GUI_INTERACTIVE_MODEL_VIEW_H
 
 #include <list>
-#include <memory>
 #include <vector>
 
+#include <QApplication>
 #include <QWidget>
 
 #include "Clipboard.h"
@@ -38,7 +38,8 @@
 namespace lmms::gui
 {
 
-
+//class QPainter;
+class SimpleTextFloat;
 
 class LMMS_EXPORT InteractiveModelView : public QWidget
 {
@@ -47,63 +48,88 @@ public:
 	InteractiveModelView(QWidget* widget);
 	~InteractiveModelView() override;
 
-	//virtual void setModel( Model* model, bool isOldModelValid = true );
-	//virtual void unsetModel();
-
-
-
-	// highlighting
-	// keyboard
-	// display shortcuts -> mouse enter event
-
-
 	static void startHighlighting(Clipboard::StringPairDataType dataType);
 	static void stopHighlighting();
+	static void showMessage(QString& message);
+	static void hideMessage();
 
 protected:
 	class ModelShortcut
 	{
 	public:
-	
+		inline ModelShortcut() {}
+		inline ModelShortcut(Qt::Key key, Qt::KeyboardModifier modifier, unsigned int times, QString description, bool shouldLoop) :
+			m_key(key),
+			m_modifier(modifier),
+			m_times(times),
+			m_shortcutDescription(description),
+			m_shouldLoop(shouldLoop)
+		{
+		}
+
 		inline bool operator==(ModelShortcut& rhs)
 		{
-			return m_key == rhs.m_key &&
-				m_modifier == rhs.m_modifier &&
-				m_times == rhs.m_times;
+			return m_key == rhs.m_key
+				&& m_modifier == rhs.m_modifier
+				&& m_times == rhs.m_times
+				&& m_shouldLoop == rhs.m_shouldLoop;
 		}
-		
-		/*
-		inline bool operator==(const ModelShortcut& firstShortcut, const ModelShortcut& secondShortcut)
+
+		inline void reset()
 		{
-    		return firstShortcut.m_key == secondShortcut.m_key &&
-    			firstShortcut.m_modifier == secondShortcut.m_modifier &&
-    			firstShortcut.m_times == secondShortcut.m_times;
+			m_key = Qt::Key_F35;
+			m_modifier = Qt::NoModifier;
+			m_times = 0;
+			m_shortcutDescription = "";
+			m_shouldLoop = false;
 		}
-		*/
-		Qt::Key m_key;
-		Qt::KeyboardModifier m_modifier;
-		unsigned int m_times;
-		QString m_shortcutDescription;
+
+		Qt::Key m_key = Qt::Key_F35;
+		Qt::KeyboardModifier m_modifier = Qt::NoModifier;
+		//! how many times do the keys need to be pressed to activate this shortcut
+		unsigned int m_times = 0;
+		//! what the shortcut does
+		QString m_shortcutDescription = "";
+		//! should it loop back if m_times is reached
+		bool m_shouldLoop = false;
 	};
 
 	void keyPressEvent(QKeyEvent* event) override;
 	void enterEvent(QEvent* event) override;
 	void leaveEvent(QEvent* event) override;
 	
-	virtual void shortcutPressedEvent(size_t shortcutLocation, QKeyEvent* event) = 0;
-	virtual bool canAcceptClipBoardData(Clipboard::StringPairDataType dataType);
+	//! return the avalible shortcuts for the widget
 	virtual std::vector<ModelShortcut> getShortcuts() = 0;
+	//! called when a `getShortcuts()` shortcut shortcut is pressed
+	virtual void shortcutPressedEvent(size_t shortcutLocation, QKeyEvent* event) = 0;
+	//! called when a shortcut message needs to be displayed
+	//! should return the message built with `buildShortcutMessage()`
+	virtual QString& getShortcutMessage() = 0;
+	//! return true if the widget supports pasting / dropping `dataType` (used for StringPairDrag and Copying)
+	virtual bool canAcceptClipBoardData(Clipboard::StringPairDataType dataType) = 0;
+	//! should implement dragging and dropping widgets or pasting from clipboard
+	//! should return if `QDropEvent` event can be accepted
+	//! force implement this method
+	virtual bool processPaste(Clipboard::StringPairDataType type, QString value) = 0;
+
+	//void drawAutoHighlight(QPainter* painter);
+	QString buildShortcutMessage();
 
 	bool getIsHighlighted() const;
 private:
 	void setIsHighlighted(bool isHighlighted);
-	bool doesShortcutMatch(const ModelShortcut* shortcut, QKeyEvent* event, unsigned int times) const;
+	//! draws
+	bool doesShortcutMatch(const ModelShortcut* shortcut, QKeyEvent* event) const;
+	bool doesShortcutMatch(const ModelShortcut* shortcutA, const ModelShortcut* shortcutB) const;
 
 	bool m_isHighlighted;
 	
 	ModelShortcut m_lastShortcut;
 	unsigned int m_lastShortcutCounter;
-	
+
+	QWidget* m_focusedBeforeWidget;
+
+	static SimpleTextFloat* s_simpleTextFloat;
 	static std::list<InteractiveModelView*> s_interactiveWidgets;
 };
 
