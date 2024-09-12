ad363cfa46906142026f7651e4ad954712229ff5
Dalton Messmer <messmer.dalton@gmail.com>
2024-09-11 20:29:02 -0400

    Use fast fma functions

--- include/interpolation.h
+++ include/interpolation.h
@@ -71,11 +71,11 @@ inline float cubicInterpolate( float v0, float v1, float v2, float v3, float x )
 {
 	float frsq = x*x;
 	float frcu = frsq*v0;
-	float t1 = std::fma(v1, 3, v3);
+	float t1 = fastFma(v1, 3, v3);
 
-	return (v1 + std::fma(0.5f, frcu, x) * (v2 - frcu * (1.0f / 6.0f) -
-		std::fma(t1, (1.0f / 6.0f), -v0) * (1.0f / 3.0f)) + frsq * x * (t1 *
-		(1.0f / 6.0f) - 0.5f * v2) + frsq * std::fma(0.5f, v2, -v1));
+	return (v1 + fastFma(0.5f, frcu, x) * (v2 - frcu * (1.0f / 6.0f) -
+		fastFma(t1, (1.0f / 6.0f), -v0) * (1.0f / 3.0f)) + frsq * x * (t1 *
+		(1.0f / 6.0f) - 0.5f * v2) + frsq * fastFma(0.5f, v2, -v1));
 }
 
 
@@ -83,13 +83,13 @@ inline float cubicInterpolate( float v0, float v1, float v2, float v3, float x )
 inline float cosinusInterpolate( float v0, float v1, float x )
 {
 	const float f = ( 1.0f - cosf( x * F_PI ) ) * 0.5f;
-	return std::fma(f, v1 - v0, v0);
+	return fastFma(f, v1 - v0, v0);
 }
 
 
 inline float linearInterpolate( float v0, float v1, float x )
 {
-	return std::fma(x, v1 - v0, v0);
+	return fastFma(x, v1 - v0, v0);
 }
 
 
@@ -104,7 +104,7 @@ inline float optimalInterpolate( float v0, float v1, float x )
 	const float c2 = even * -0.004541102062639801;
 	const float c3 = odd * -1.57015627178718420;
 	
-	return std::fma(std::fma(std::fma(c3, z, c2), z, c1), z, c0);
+	return fastFma(fastFma(fastFma(c3, z, c2), z, c1), z, c0);
 }
 
 
@@ -121,7 +121,7 @@ inline float optimal4pInterpolate( float v0, float v1, float v2, float v3, float
 	const float c2 = even1 * -0.246185007019907091 + even2 * 0.24614027139700284;
 	const float c3 = odd1 * -0.36030925263849456 + odd2 * 0.10174985775982505;
 
-	return std::fma(std::fma(std::fma(c3, z, c2), z, c1), z, c0);
+	return fastFma(fastFma(fastFma(c3, z, c2), z, c1), z, c0);
 }
 
 
@@ -132,7 +132,7 @@ inline float lagrangeInterpolate( float v0, float v1, float v2, float v3, float 
 	const float c1 = v2 - v0 * ( 1.0f / 3.0f ) - v1 * 0.5f - v3 * ( 1.0f / 6.0f );
 	const float c2 = 0.5f * (v0 + v2) - v1;
 	const float c3 = ( 1.0f/6.0f ) * ( v3 - v0 ) + 0.5f * ( v1 - v2 );
-	return std::fma(std::fma(std::fma(c3, x, c2), x, c1), x, c0);
+	return fastFma(fastFma(fastFma(c3, x, c2), x, c1), x, c0);
 }
 
 
