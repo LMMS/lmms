86a5ca075c2ae8c35efbfa8603867beed2af04c5
Michael Gregorius <michael.gregorius.git@arcor.de>
2024-09-28 23:34:04 +0200

    Use non-legacy knobs for instrument and sample track
    Use non-legacy knobs for the "VOL" and "PAN" knobs of the instrument and sample track. This gives a bit more separation between the knob and the label but to make this work the font size had to be decreased by one pixel.

--- src/gui/tracks/SampleTrackView.cpp
+++ src/gui/tracks/SampleTrackView.cpp
@@ -61,14 +61,20 @@ SampleTrackView::SampleTrackView( SampleTrack * _t, TrackContainerView* tcv ) :
 	m_mixerChannelNumber = new MixerChannelLcdSpinBox(2, getTrackSettingsWidget(), tr("Mixer channel"), this);
 	m_mixerChannelNumber->show();
 
-	m_volumeKnob = Knob::buildLegacyKnob(KnobType::Small17, tr("VOL"), getTrackSettingsWidget(), tr("Track volume"));
+	auto f = font();
+	f.setPixelSize(9);
+	m_volumeKnob = new Knob(KnobType::Small17, getTrackSettingsWidget(), tr("Track volume"));
+	m_volumeKnob->setFont(f);
+	m_volumeKnob->setLabel(tr("VOL"));
 	m_volumeKnob->setVolumeKnob( true );
 	m_volumeKnob->setModel( &_t->m_volumeModel );
 	m_volumeKnob->setHintText( tr( "Channel volume:" ), "%" );
 
 	m_volumeKnob->show();
 
-	m_panningKnob = Knob::buildLegacyKnob(KnobType::Small17, tr("PAN"), getTrackSettingsWidget(), tr("Panning"));
+	m_panningKnob = new Knob(KnobType::Small17, getTrackSettingsWidget(), tr("Panning"));
+	m_panningKnob->setFont(f);
+	m_panningKnob->setLabel(tr("PAN"));
 	m_panningKnob->setModel( &_t->m_panningModel );
 	m_panningKnob->setHintText( tr( "Panning:" ), "%" );
 	m_panningKnob->show();
