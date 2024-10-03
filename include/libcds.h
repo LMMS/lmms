f54ac456dd5db055dd0305ccf8c469af5a4d9ad9
Hyunjin Song <tteu.ingog@gmail.com>
2024-10-03 20:34:08 +0900

    Remove leftover of mingw-std-threads

--- include/libcds.h
+++ include/libcds.h
@@ -4,11 +4,6 @@
 
 #include "NiftyCounter.h"
 
-#if defined(__MINGW32__) && !defined(_GLIBCXX_HAS_GTHREADS)
-#	include "mingw-std-threads/thread"
-#	include "mingw-std-threads/mutex"
-#endif
-
 namespace lmms::_cdslib
 {
 	void init();
