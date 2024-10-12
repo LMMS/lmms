8f6e888bae9ab50f396bf2daa269c19847d03705
szeli1 <143485814+szeli1@users.noreply.github.com>
2024-10-12 12:57:40 +0200

    AudioFileFlacSample_adding_class_to_export_samples

---
+++ include/AudioFileFlacSample.h
@@ -0,0 +1,74 @@
+/*
+ * AudioFileFlac.h - Audio device which encodes a wave stream into a FLAC file.
+ *
+ * Copyright (c) 2017 to present Levin Oehlmann <irrenhaus3/at/gmail[dot]com> et al.
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef LMMS_AUDIO_FILE_FLAC_H
+#define LMMS_AUDIO_FILE_FLAC_H
+
+#include "lmmsconfig.h"
+
+#include "AudioFileDeviceSample.h"
+#include <sndfile.h>
+
+namespace lmms
+{
+
+class AudioFileFlacSample : public AudioFileDeviceSample
+{
+public:
+	AudioFileFlacSample(OutputSettings const& outputSettings,
+			bool& successful,
+			const QString& file,
+			const fpp_t defaultBufferSize);
+	~AudioFileFlacSample() override;
+
+	static AudioFileDeviceSample* getInst(
+			OutputSettings const &outputSettings,
+			bool &successful,
+			const QString & outputFilename,
+			const fpp_t defaultBufferSize)
+	{
+		return new AudioFileFlacSample(
+			outputSettings,
+			successful,
+			outputFilename,
+			defaultBufferSize
+		);
+	}
+
+private:
+
+	SF_INFO  m_sfinfo;
+	SNDFILE* m_sf;
+
+	void writeBuffer(const SampleFrame* _ab, fpp_t const frames) override;
+
+	bool startEncoding();
+	void finishEncoding();
+
+};
+
+
+} // namespace lmms
+
+#endif // LMMS_AUDIO_FILE_FLAC_H
