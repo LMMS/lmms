43e4df1c8dcddae3b589717c4651db520a5ce994
Dalton Messmer <messmer.dalton@gmail.com>
2025-01-17 02:03:36 -0500

    Try using vcpkg's zlib and mp3lame
    Also update install-powershell.sh script

--- .github/workflows/install-powershell.sh
+++ .github/workflows/install-powershell.sh
@@ -10,14 +10,14 @@ sudo apt-get update
 sudo apt-get install -y wget
 
 # Download the PowerShell package file
-wget https://github.com/PowerShell/PowerShell/releases/download/v7.4.3/powershell_7.4.3-1.deb_amd64.deb
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.4.6/powershell_7.4.6-1.deb_amd64.deb
 
 ###################################
 # Install the PowerShell package
-sudo dpkg -i powershell_7.4.3-1.deb_amd64.deb
+sudo dpkg -i powershell_7.4.6-1.deb_amd64.deb
 
 # Resolve missing dependencies and finish the install (if necessary)
 sudo apt-get install -f
 
 # Delete the downloaded package file
-rm powershell_7.4.3-1.deb_amd64.deb
+rm powershell_7.4.6-1.deb_amd64.deb
