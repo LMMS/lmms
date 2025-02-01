LMMS/lmms

commit ff435d551b5d96c7307c484799b9c38ec60f5542

Work around a Qt bug
Work around a Qt bug in conjunction with the scroll wheel and the Alt
key. Simply return 0 for the fader delta as soon as Alt is pressed.

src/gui/widgets/Fader.cpp
@@ -283,6 +283,11 @@ float Fader::determineAdjustmentDelta(const Qt::KeyboardModifiers & modifiers) c
 		// The control key gives more control, i.e. it enables more fine-grained adjustments
 		return 0.1f;
 	}
+	else if (modifiers | Qt::AltModifier)
+	{
+		// Work around a Qt bug in conjunction with the scroll wheel and the Alt key
+		return 0.f;
+	}
 
 	return 1.f;
 }

commit 5a6634848feb3af61b1757137e6f75454a63ca29

Fix wheel events without any modifier
Fix the handling of wheel events without any modifier key being pressed.

Commit ff435d551b5 accidentally tested against Alt using a logical OR
instead of an AND.

src/gui/widgets/Fader.cpp
@@ -283,7 +283,7 @@ float Fader::determineAdjustmentDelta(const Qt::KeyboardModifiers & modifiers) c
 		// The control key gives more control, i.e. it enables more fine-grained adjustments
 		return 0.1f;
 	}
-	else if (modifiers | Qt::AltModifier)
+	else if (modifiers & Qt::AltModifier)
 	{
 		// Work around a Qt bug in conjunction with the scroll wheel and the Alt key
 		return 0.f;