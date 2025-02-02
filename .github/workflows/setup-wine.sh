34eaf966386d1d3ff44227c7c149c2a2c487e421
Dalton Messmer <messmer.dalton@gmail.com>
2025-02-02 03:28:07 -0500

    Try building on Ubuntu 22.04 without container

--- .github/workflows/setup-wine.sh
+++ .github/workflows/setup-wine.sh
@@ -2,9 +2,11 @@
 
 # Add WineHQ APT repo
 sudo dpkg --add-architecture i386
-sudo apt-get install --yes wget
+sudo apt-get install --yes wget gpg
 sudo mkdir -pm755 /etc/apt/keyrings
-sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+wget -O - https://dl.winehq.org/wine-builds/winehq.key | \
+    sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
 . /etc/os-release
-sudo wget -NP /etc/apt/sources.list.d/ "https://dl.winehq.org/wine-builds/ubuntu/dists/${UBUNTU_CODENAME}/winehq-${UBUNTU_CODENAME}.sources"
-sudo apt-get update
+sudo wget -NP /etc/apt/sources.list.d/ \
+    "https://dl.winehq.org/wine-builds/ubuntu/dists/${UBUNTU_CODENAME}/winehq-${UBUNTU_CODENAME}.sources"
+sudo apt-get update -y
