8327aa88d26920f00d817b09c37c59455d01f883
tresf <tres.finocchiaro@gmail.com>
2025-02-05 02:34:16 -0500

    Patch for Arch linux, btw

--- cmake/linux/apprun-hooks/carla-hook.sh
+++ cmake/linux/apprun-hooks/carla-hook.sh
@@ -3,20 +3,20 @@
 # Workaround nuances with carla being an optional-yet-hard-linked plugin
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 ME="$( basename "${BASH_SOURCE[0]}")"
-CARLA_LIB="libcarla_native-plugin.so"
+CARLA_LIB_NAME="libcarla_native-plugin.so"
 KNOWN_LOCATIONS=("lib" "lib64")
+unset CARLA_LIB_FILE
 
 # Check for carla at "known" locations
-carla_lib_found=false
 if command -v carla > /dev/null 2>&1; then
 	CARLA_PATH="$(command -v carla)"
 	CARLA_PREFIX="${CARLA_PATH%/bin*}"
 
 	# Look for libcarla_native-plugin.so in adjacent lib directory
 	for lib in "${KNOWN_LOCATIONS[@]}"; do
-		if [ -e "$CARLA_PREFIX/$lib/carla/$CARLA_LIB" ]; then
+		if [ -e "$CARLA_PREFIX/$lib/carla/$CARLA_LIB_NAME" ]; then
 			# Add directory to LD_LIBRARY_PATH so libcarlabase.so can find it
-			carla_lib_found=true
+			CARLA_LIB_FILE="$CARLA_PREFIX/$lib/carla/$CARLA_LIB_NAME"
 			export LD_LIBRARY_PATH="$CARLA_PREFIX/$lib/carla/:$LD_LIBRARY_PATH"
 			echo "[$ME] Carla appears to be installed on this system at $CARLA_PREFIX/$lib/carla so we'll use it." >&2
 			break
@@ -29,28 +29,16 @@ fi
 # Additional workarounds for library conflicts
 # libgobject has been versioned "2.0" for over 20 years, but the ABI is constantly changing
 KNOWN_CONFLICTS=("libgobject-2.0.so.0")
-KNOWN_LDCONFIG=("ldconfig" "/sbin/ldconfig")
-if [ "$carla_lib_found" = true ]; then
-	# ldconfig may be in /sbin, see https://github.com/LMMS/lmms/issues/4846
-	for cmd in "${KNOWN_LDCONFIG[@]}"; do
-		ldconfig_path="$cmd"
-	done
-
-	if [ -n "$ldconfig_path" ]; then
-		for conflict in "${KNOWN_CONFLICTS[@]}"; do
-			if command -v "$cmd" > /dev/null 2>&1; then
-				# Only prepend LD_PRELOAD if we bundle the same version
-				if [ -e "$DIR/usr/lib/$conflict" ]; then
-					conflict_sys="$("$cmd" -p |grep "$(arch)" |grep "$conflict" |head -n 1 |awk '{print $4}')"
-					if [ -e "$conflict_sys" ]; then
-						# Add library to LD_PRELOAD so lmms can find it over its bundled version
-						echo "[$ME] Preferring the system's \"$conflict\" over the version bundled." >&2
-						export LD_PRELOAD="$conflict_sys:$LD_PRELOAD"
-					fi
-				fi
+if [ -n "$CARLA_LIB_FILE" ]; then
+	for conflict in "${KNOWN_CONFLICTS[@]}"; do
+		# Only prepend LD_PRELOAD if we bundle the same version
+		if [ -e "$DIR/usr/lib/$conflict" ]; then
+			conflict_sys="$(ldd "$CARLA_LIB_FILE" | grep "$conflict" | awk '{print $3}')"
+			if [ -e "$conflict_sys" ]; then
+				# Add library to LD_PRELOAD so lmms can find it over its bundled version
+				echo "[$ME] Preferring the system's \"$conflict\" over the version bundled." >&2
+				export LD_PRELOAD="$conflict_sys:$LD_PRELOAD"
 			fi
-		done
-	else
-		echo "[$ME] Unable to locate ldconfig, skipping workaround for" "${KNOWN_CONFLICTS[@]}" >&2
-	fi
+		fi
+	done
 fi
