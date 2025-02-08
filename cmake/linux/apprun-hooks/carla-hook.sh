07a1957e6107ee9d871e2b4e4f93b1e59690e136
tresf <tres.finocchiaro@gmail.com>
2025-02-08 03:59:16 -0500

    Fix regex

--- cmake/linux/apprun-hooks/carla-hook.sh
+++ cmake/linux/apprun-hooks/carla-hook.sh
@@ -24,7 +24,7 @@ if command -v carla > /dev/null 2>&1; then
 	done
 else
 	echo "[$ME] Carla does not appear to be installed, we'll remove it from the plugin listing." >&2
-	export "LMMS_EXCLUDE_PLUGINS=*carla*,${LMMS_EXCLUDE_PLUGINS}"
+	export "LMMS_EXCLUDE_PLUGINS=libcarla,${LMMS_EXCLUDE_PLUGINS}"
 fi
 
 # Additional workarounds for library conflicts
