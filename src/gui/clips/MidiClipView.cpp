212eacae3eec48c07b4953a28b354681359104e6
regulus79 <117475203+regulus79@users.noreply.github.com>
2025-02-09 18:22:05 -0500

    Fix hard splitting

--- src/gui/clips/MidiClipView.cpp
+++ src/gui/clips/MidiClipView.cpp
@@ -924,11 +924,11 @@ bool MidiClipView::destructiveSplitClip(const TimePos pos)
 	leftClip->movePosition(m_initialClipPos);
 	leftClip->setHasBeenResized(true);
 	leftClip->changeLength(splitPos - m_initialClipPos);
-	leftClip->setStartTimeOffset(m_clip->startTimeOffset());
 
 	rightClip->movePosition(splitPos);
 	rightClip->setHasBeenResized(true);
 	rightClip->changeLength(m_initialClipEnd - splitPos);
+	rightClip->setStartTimeOffset(0);
 
 	remove();
 	m_clip->getTrack()->restoreJournallingState();
