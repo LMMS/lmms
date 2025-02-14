10043f7e4adace297f16ce4af5518cc8454fdd68
Fawn Sannar <rubiefawn@gmail.com>
2025-02-14 10:54:05 -0700

    Revert changes to portsmf

--- plugins/MidiImport/portsmf/mfmidi.cpp
+++ plugins/MidiImport/portsmf/mfmidi.cpp
@@ -256,9 +256,11 @@ void Midifile_reader::readtrack()
 
 void Midifile_reader::badbyte(int c)
 {
-	char buff[32];
-	(void)snprintf(buff, sizeof(buff), "unexpected byte: 0x%02x", c);
-	mferror(buff);
+    char buff[32];
+//#pragma warning(disable: 4996) // safe in this case
+    (void) sprintf(buff,"unexpected byte: 0x%02x",c);
+//#pragma warning(default: 4996)
+    mferror(buff);
 }
 
 void Midifile_reader::metaevent(int type)
