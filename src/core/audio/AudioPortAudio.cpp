aee24f9bbe8a6aef450ec5466c014bddd7a6d50c
Sotonye Atemie <sakertooth@gmail.com>
2025-02-16 16:00:34 -0500

    More formatting

--- src/core/audio/AudioPortAudio.cpp
+++ src/core/audio/AudioPortAudio.cpp
@@ -111,7 +111,10 @@ int AudioPortAudio::processCallback(const float* inputBuffer, float* outputBuffe
 	{
 		if (m_outBufPos == 0 && getNextBuffer(m_outBuf.data()) == 0) { return paComplete; }
 
-		if (channels() == 1) { outputBuffer[frame] = m_outBuf[m_outBufPos].average(); }
+		if (channels() == 1)
+		{
+			outputBuffer[frame] = m_outBuf[m_outBufPos].average();
+		}
 		else
 		{
 			outputBuffer[frame * channels()] = m_outBuf[m_outBufPos][0];
