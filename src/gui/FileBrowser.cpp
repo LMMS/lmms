b9dbe3573369555d6f0ee95ebc7b772c4cd9425b
Andrew Wiltshire <62200778+AW1534@users.noreply.github.com>
2025-02-18 01:26:17 +0000

    Update src/gui/FileBrowser.cpp
    Co-authored-by: Tres Finocchiaro <tres.finocchiaro@gmail.com>

--- src/gui/FileBrowser.cpp
+++ src/gui/FileBrowser.cpp
@@ -656,8 +656,8 @@ void FileBrowserTreeWidget::contextMenuEvent(QContextMenuEvent* e)
 
 		auto songEditorHeader = new QAction(tr("Song Editor"), nullptr);
 		songEditorHeader->setDisabled(true);
-		contextMenu.addAction(songEditorHeader);
-		contextMenu.addActions(getContextActions(file, true));
+		contextMenu.addAction( songEditorHeader );
+		contextMenu.addActions( getContextActions(file, true) );
 
 		auto patternEditorHeader = new QAction(tr("Pattern Editor"), nullptr);
 		patternEditorHeader->setDisabled(true);
