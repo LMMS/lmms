92b59d941678af080d5c63138fbb45c8e02d77cf
Dalton Messmer <messmer.dalton@gmail.com>
2025-02-19 14:12:23 -0500

    Fix MSVC build
    Also fixes two memory leaks in MidiWinMM

--- src/core/midi/MidiWinMM.cpp
+++ src/core/midi/MidiWinMM.cpp
@@ -275,15 +275,15 @@ void MidiWinMM::openDevices()
 	m_inputDevices.clear();
 	for( unsigned int i = 0; i < midiInGetNumDevs(); ++i )
 	{
-		MIDIINCAPS c;
-		midiInGetDevCaps( i, &c, sizeof( c ) );
+		MIDIINCAPSW c;
+		midiInGetDevCapsW(i, &c, sizeof(c));
 		HMIDIIN hm = 0;
 		MMRESULT res = midiInOpen( &hm, i, (DWORD_PTR) &inputCallback,
 						(DWORD_PTR) this,
 							CALLBACK_FUNCTION );
 		if( res == MMSYSERR_NOERROR )
 		{
-			m_inputDevices[hm] = qstrdup( c.szPname );
+			m_inputDevices[hm] = QString::fromWCharArray(c.szPname);
 			midiInStart( hm );
 		}
 	}
@@ -291,13 +291,13 @@ void MidiWinMM::openDevices()
 	m_outputDevices.clear();
 	for( unsigned int i = 0; i < midiOutGetNumDevs(); ++i )
 	{
-		MIDIOUTCAPS c;
-		midiOutGetDevCaps( i, &c, sizeof( c ) );
+		MIDIOUTCAPSW c;
+		midiOutGetDevCapsW(i, &c, sizeof(c));
 		HMIDIOUT hm = 0;
 		MMRESULT res = midiOutOpen( &hm, i, 0, 0, CALLBACK_NULL );
 		if( res == MMSYSERR_NOERROR )
 		{
-			m_outputDevices[hm] = qstrdup( c.szPname );
+			m_outputDevices[hm] = QString::fromWCharArray(c.szPname);
 		}
 	}
 }
