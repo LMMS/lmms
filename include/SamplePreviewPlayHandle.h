7e4570e6ea9b2dbca46549cb6884d90126bcb9a7
Sotonye Atemie <sakertooth@gmail.com>
2025-03-09 06:40:10 -0400

    Add SamplePreviewPlayHandle

---
+++ include/SamplePreviewPlayHandle.h
@@ -0,0 +1,69 @@
+/*
+ * SampleStream.h
+ *
+ * Copyright (c) 2025 Sotonye Atemie <sakertooth@gmail.com>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef LMMS_SAMPLE_STREAM_H
+#define LMMS_SAMPLE_STREAM_H
+
+#include <filesystem>
+#include <future>
+#include <sndfile.h>
+#include <vector>
+
+#include "PlayHandle.h"
+
+namespace lmms {
+
+class SamplePreviewPlayHandle : public PlayHandle
+{
+public:
+	SamplePreviewPlayHandle(const std::filesystem::path& path, std::size_t size = DefaultStreamCapacity);
+
+	SamplePreviewPlayHandle(const SamplePreviewPlayHandle&) = delete;
+	SamplePreviewPlayHandle(SamplePreviewPlayHandle&&) = delete;
+
+	SamplePreviewPlayHandle& operator=(const SamplePreviewPlayHandle&) = delete;
+	SamplePreviewPlayHandle& operator=(SamplePreviewPlayHandle&&) = delete;
+
+	~SamplePreviewPlayHandle() noexcept;
+
+	void play(SampleFrame* buffer) override;
+	bool isFromTrack(const Track* _track) const override { return false; };
+	bool isFinished() const override { return !m_diskStream.valid(); }
+
+private:
+	void runDiskStream();
+	void mixCopy(SampleFrame* dst, const float* src, std::size_t frames, int channels);
+	std::vector<float> m_buffer;
+	std::future<void> m_diskStream;
+	std::atomic<bool> m_quit = false;
+	std::atomic<std::size_t> m_readIndex = 0;
+	std::atomic<std::size_t> m_writeIndex = 0;
+	SNDFILE* m_sndfile = nullptr;
+	SF_INFO m_sfinfo = SF_INFO{};
+	static constexpr auto DefaultStreamCapacity = 8192;
+};
+
+} // namespace lmms
+
+#endif // LMMS_SAMPLE_STREAM_H
---
+++ src/core/SamplePreviewPlayHandle.cpp
@@ -0,0 +1,113 @@
+/*
+ * SamplePreviewPlayHandle.cpp - an object meant to stream audio files
+ *
+ * Copyright (c) 2025 Sotonye Atemie <sakertooth@gmail.com>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include "SamplePreviewPlayHandle.h"
+
+#include "AudioEngine.h"
+#include "Engine.h"
+#include "SampleFrame.h"
+#include "ThreadPool.h"
+
+namespace lmms {
+SamplePreviewPlayHandle::SamplePreviewPlayHandle(const std::filesystem::path& path, std::size_t size)
+	: PlayHandle(Type::SamplePreviewPlayHandle)
+	, m_buffer(size * DEFAULT_CHANNELS)
+	, m_diskStream(ThreadPool::instance().enqueue(&SamplePreviewPlayHandle::runDiskStream, this))
+	, m_sndfile(
+#ifdef LMMS_BUILD_WIN32
+		  sf_wchar_open(path.c_str(), SFM_READ, &m_sfinfo)
+#else
+		  sf_open(path.c_str(), SFM_READ, &m_sfinfo)
+#endif
+	  )
+{
+	if (m_sndfile == nullptr)
+	{
+		m_quit = true;
+		m_diskStream.wait();
+		throw std::runtime_error{"Failed to create sample preview stream"};
+	}
+}
+
+SamplePreviewPlayHandle::~SamplePreviewPlayHandle() noexcept
+{
+    m_quit = true;
+    m_diskStream.wait();
+	sf_close(m_sndfile);
+}
+
+void SamplePreviewPlayHandle::play(SampleFrame* buffer)
+{
+	const auto size = Engine::audioEngine()->framesPerPeriod();
+    std::fill_n(buffer, size, SampleFrame{});
+
+	const auto framesAvailable = (m_writeIndex + m_buffer.size() - m_readIndex) % m_buffer.size();
+	const auto readSize = std::min(framesAvailable, size);
+    if (readSize == 0) { return; }
+
+	if (m_readIndex + readSize > m_buffer.size())
+	{
+		const auto readToEndSize = std::min(readSize, m_buffer.size() - m_readIndex);
+		const auto wrapAroundSize = readSize - readToEndSize;
+        mixCopy(buffer, m_buffer.data() + static_cast<int>(m_readIndex), readToEndSize, m_sfinfo.channels);
+        mixCopy(buffer, m_buffer.data(), wrapAroundSize, m_sfinfo.channels);
+	}
+	else { mixCopy(buffer, m_buffer.data() + static_cast<int>(m_readIndex), readSize, m_sfinfo.channels); }
+
+	m_readIndex = (m_readIndex + readSize) % m_buffer.size();
+}
+
+void SamplePreviewPlayHandle::runDiskStream()
+{
+	auto framesWritten = std::size_t{0};
+	while (framesWritten < m_sfinfo.frames && !m_quit)
+	{
+		const auto framesAvailable = (m_readIndex + m_buffer.size() - m_writeIndex - 1) % m_buffer.size();
+		const auto writeSize = std::min(framesAvailable, m_buffer.size() - framesWritten);
+		if (writeSize == 0) { continue; }
+
+		if (m_writeIndex + writeSize > m_buffer.size())
+		{
+			const auto writeToEndSize = std::min(writeSize, m_buffer.size() - m_writeIndex);
+			const auto wrapAroundSize = writeSize - writeToEndSize;            
+			sf_readf_float(m_sndfile, m_buffer.data() + m_writeIndex, static_cast<sf_count_t>(writeToEndSize));
+			sf_readf_float(m_sndfile, m_buffer.data(), static_cast<sf_count_t>(wrapAroundSize));
+		}
+		else { sf_readf_float(m_sndfile, m_buffer.data() + m_writeIndex, static_cast<sf_count_t>(writeSize)); }
+
+		m_writeIndex = (m_writeIndex + writeSize) % m_buffer.size();
+		framesWritten += writeSize;
+	}
+}
+
+void SamplePreviewPlayHandle::mixCopy(SampleFrame* dst, const float* src, std::size_t frames, int channels)
+{
+    for (auto i = 0; i < frames; ++i)
+    {
+        dst[i][0] = src[i];
+        dst[i][1] = m_sfinfo.channels == 1 ? src[i] : src[i + 1];
+    }
+}
+
+} // namespace lmms
