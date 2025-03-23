86bb30b4b494178d48bfa8071017794e19a8eb90
Sotonye Atemie <satemiej@gmail.com>
2025-03-22 22:38:19 -0400

    Update copyright

--- include/SamplePreviewPlayHandle.h
+++ include/SamplePreviewPlayHandle.h
@@ -1,7 +1,7 @@
 /*
  * SampleStream.h
  *
- * Copyright (c) 2025 Sotonye Atemie <satemiej@gmail.com>
+ * Copyright (c) 2025 Sotonye Atemie <sakertooth@gmail.com>
  *
  * This file is part of LMMS - https://lmms.io
  *
