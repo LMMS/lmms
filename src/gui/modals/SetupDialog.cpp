2ab0d8aa4bf95e410e84a3007b2c618a39a0ed67
saker <sakertooth@gmail.com>
2025-04-22 13:15:46 -0400

    Not sure I want do to that here, maybe in another PR

--- src/gui/modals/SetupDialog.cpp
+++ src/gui/modals/SetupDialog.cpp
@@ -634,11 +634,6 @@ SetupDialog::SetupDialog(ConfigTab tab_to_open) :
 
 	setBufferSize(m_bufferSizeSlider->value());
 
-	// Resampling quality group
-	auto resamplingQuality = new QGroupBox(tr("Resampling quality"), audio_w);
-	auto liveResamplingQualityBox = new QComboBox(resamplingQuality);
-	
-
 
 	// Audio layout ordering.
 	audio_layout->addWidget(audioInterfaceBox);
