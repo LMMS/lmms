b5160f9f4ef5cdd1c46572b22cf740ff8532dd39
saker <sakertooth@gmail.com>
2025-04-28 21:40:53 -0400

    Use AudioResampler in Watsyn

--- plugins/Watsyn/Watsyn.h
+++ plugins/Watsyn/Watsyn.h
@@ -26,6 +26,7 @@
 #ifndef WATSYN_H
 #define WATSYN_H
 
+#include "AudioResampler.h"
 #include "Instrument.h"
 #include "InstrumentView.h"
 #include "Graph.h"
@@ -187,28 +188,10 @@ private:
 	}
 
 	// memcpy utilizing libsamplerate (src) for sinc interpolation
-	inline void srccpy( float * _dst, float * _src )
+	inline void srccpy(float* _dst, float* _src)
 	{
-		int err;
-		const int margin = 64;
-		
-		// copy to temp array
-		float tmps [ GRAPHLEN + margin ]; // temp array in stack
-		float * tmp = &tmps[0];
-
-		memcpy( tmp, _src, sizeof( float ) * GRAPHLEN );
-		memcpy( tmp + GRAPHLEN, _src, sizeof( float ) * margin );
-		SRC_STATE * src_state = src_new( SRC_SINC_FASTEST, 1, &err );
-		SRC_DATA src_data;
-		src_data.data_in = tmp;
-		src_data.input_frames = GRAPHLEN + margin;
-		src_data.data_out = _dst;
-		src_data.output_frames = WAVELEN;
-		src_data.src_ratio = static_cast<double>( WAVERATIO );
-		src_data.end_of_input = 0;
-		err = src_process( src_state, &src_data ); 
-		if( err ) { qDebug( "Watsyn SRC error: %s", src_strerror( err ) ); }
-		src_delete( src_state );
+		const auto error = resampler.resample(_dst, WAVELEN, _src, GRAPHLEN, WAVERATIO);
+		if (error) { qDebug("Watsyn resample error: %s", AudioResampler::errorDescription(error)); }
 	}
 
 	// memcpy utilizing cubic interpolation
@@ -242,6 +225,7 @@ private:
 		}
 	}*/
 
+	AudioResampler resampler = AudioResampler{SRC_SINC_FASTEST, 1};
 
 	FloatModel a1_vol;
 	FloatModel a2_vol;
