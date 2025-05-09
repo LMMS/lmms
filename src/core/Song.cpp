368d8bfbbdc541444cce9fe311dda2524fe65a68
szeli1 <143485814+szeli1@users.noreply.github.com>
2025-05-09 17:49:38 +0200

    Song_removing_unused_code

--- src/core/Song.cpp
+++ src/core/Song.cpp
@@ -419,7 +419,6 @@ void Song::processAutomations(const TrackList &tracklist, TimePos timeStart, fpp
 	{
 		if (! recordedModels.contains(it.key()))
 		{
-			//it.key()->setAutomatedValue(it.value());
 			it.key()->setValue(it.value(), true);
 		}
 		else if (!it.key()->useControllerValue())
