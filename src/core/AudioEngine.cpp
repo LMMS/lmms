23615b730eb2c01dbfcacc4f7330b31156960b90
Steven-Jaro <jaromersen@gmail.com>
2025-05-31 19:15:03 -0600

    Update src/core/AudioEngine.cpp
    Co-authored-by: szeli1 <143485814+szeli1@users.noreply.github.com>

--- src/core/AudioEngine.cpp
+++ src/core/AudioEngine.cpp
@@ -207,7 +207,7 @@ AudioEngine::~AudioEngine()
 
 	if (m_fifo)
 	{
-		while( m_fifo->available() )
+		while (m_fifo->available())
 		{
 			SampleFrame* bufToDelete = m_fifo->read();
 			if (bufToDelete) delete[] bufToDelete;
