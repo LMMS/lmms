a95ed79a1416bccaf57cc120a6ce038702d30253
Fawn <rubiefawn@gmail.com>
2025-06-13 13:16:33 -0600

    Increase spacing between Dispersion gui components

--- plugins/Dispersion/DispersionControlDialog.cpp
+++ plugins/Dispersion/DispersionControlDialog.cpp
@@ -44,6 +44,7 @@ DispersionControlDialog::DispersionControlDialog(DispersionControls* controls) :
 {
 	setAutoFillBackground(true);
 	auto layout = new QHBoxLayout(this);
+	layout->setSpacing(5);
 
 	auto amountBox = new LcdSpinBox(3, this, "Amount");
 	amountBox->setModel(&controls->m_amountModel);
