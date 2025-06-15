382f50f4e94150159b0f9ba108215bd3f815d152
Andrew Wiltshire <62200778+AW1534@users.noreply.github.com>
2025-06-15 21:38:38 +0100

    Update src/gui/editors/TrackContainerView.cpp
    Co-authored-by: szeli1 <143485814+szeli1@users.noreply.github.com>

--- src/gui/editors/TrackContainerView.cpp
+++ src/gui/editors/TrackContainerView.cpp
@@ -398,8 +398,7 @@ void TrackContainerView::dropEvent(QDropEvent* _de)
 		// it->toggledInstrumentTrackButton(true);
 		_de->accept();
 	}
-	else if (
-		type == "samplefile" || type == "pluginpresetfile"
+	else if (type == "samplefile" || type == "pluginpresetfile"
 		|| type == "soundfontfile" || type == "vstpluginfile"
 		|| type == "patchfile" )
 	{
