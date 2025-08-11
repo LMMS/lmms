2805834b325eb2e631bd5104ee8ba8ac79eb7f52
Sotonye Atemie <sakertooth@gmail.com>
2025-08-11 06:57:52 -0400

    Remove my subspan function

--- include/AudioBufferView.h
+++ include/AudioBufferView.h
@@ -361,14 +361,6 @@ public:
 		}
 	}
 
-	//! @return a subview at the given frame offset `offset` with a frame count of `frames`
-	constexpr auto subspan(f_cnt_t offset, f_cnt_t frames) const -> InterleavedBufferView<SampleT, channelCount>
-	{
-		assert(offset <= this->m_frames);
-		assert(offset + frames <= this->m_frames);
-		return {this->m_data + offset * this->channels(), this->channels(), frames};
-	}
-
 	/**
 	 * @return pointer to the frame at the given index.
 	 * The size of the frame is `channels()`.
