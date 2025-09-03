7953c40c679c414af3d61dfc6c94379ad72b3f26
aisge <g.aistleitner@htl-leonding.ac.at>
2025-09-03 18:52:40 +0200

    unification: always use setToTime()-method

--- src/gui/editors/PianoRoll.cpp
+++ src/gui/editors/PianoRoll.cpp
@@ -1459,7 +1459,7 @@ void PianoRoll::keyPressEvent(QKeyEvent* ke)
 		case Qt::Key_Home:
 			m_timeLine->pos().setTicks( 0 );
 			m_timeLine->updatePosition();
-			Engine::getSong()->setToTimeByTicks(0);
+			Engine::getSong()->setToTime(0);
 			ke->accept();
 			break;
 
