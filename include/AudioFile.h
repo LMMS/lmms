a5729d7ffe5615cc5883fe5e0071ca0a16e5a361
Sotonye Atemie <sakertooth@gmail.com>
2025-09-22 20:48:40 -0400

    Add AudioFile

---
+++ include/AudioFile.h
@@ -0,0 +1,68 @@
+/*
+ * AudioFile.h
+ *
+ * Copyright (c) 2025 Sotonye Atemie <sakertooth@gmail.com>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef LMMS_AUDIO_FILE_H
+#define LMMS_AUDIO_FILE_H
+
+#include <filesystem>
+
+#include "OutputSettings.h"
+#include "ProjectRenderer.h"
+#include "AudioBufferView.h"
+
+namespace lmms {
+class AudioFile
+{
+public:
+	explicit AudioFile(std::filesystem::path path);
+
+	AudioFile(std::filesystem::path path, ProjectRenderer::ExportFileFormat format, OutputSettings settings);
+
+    ~AudioFile();
+
+	AudioFile(const AudioFile&) = delete;
+
+	AudioFile(AudioFile&&) = default;
+
+	AudioFile& operator=(const AudioFile&) = delete;
+
+	AudioFile& operator=(AudioFile&&) = default;
+
+    void read(InterleavedBufferView<float> dst);
+
+    void write(InterleavedBufferView<const float> src);
+
+    auto frames() const -> f_cnt_t;
+
+    auto channels() const -> ch_cnt_t;
+
+    auto sampleRate() const -> sample_rate_t;
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> m_impl;
+};
+} // namespace lmms
+
+#endif // LMMS_AUDIO_FILE_H
---
+++ src/core/audio/AudioFile.cpp
@@ -0,0 +1,165 @@
+/*
+ * AudioFile.cpp
+ *
+ * Copyright (c) 2025 Sotonye Atemie <sakertooth@gmail.com>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include "AudioFile.h"
+
+#include <sndfile.h>
+
+namespace {
+using namespace lmms;
+
+SNDFILE* openAudioFile(std::filesystem::path path, int mode, SF_INFO* sfinfo)
+{
+#ifdef LMMS_BUILD_WIN32
+	return sf_wchar_open(path.wstring().c_str(), mode, sfinfo);
+#else
+	return sf_open(path.string().c_str(), mode, sfinfo);
+#endif
+}
+
+SF_INFO sfInfoFromOutputSettings(ProjectRenderer::ExportFileFormat format, OutputSettings settings)
+{
+    auto sfFormat = 0;
+    auto sfBitDepth = 0;
+    auto sfChannels = 0;
+
+    switch (settings.getBitDepth())
+    {
+	case OutputSettings::BitDepth::Depth16Bit:
+        sfBitDepth = SF_FORMAT_PCM_16;
+        break;
+	case OutputSettings::BitDepth::Depth24Bit:
+        sfBitDepth = SF_FORMAT_PCM_24;
+        break;
+	case OutputSettings::BitDepth::Depth32Bit:
+        sfBitDepth = SF_FORMAT_FLOAT;
+		break;
+	}
+
+	switch (format)
+    {
+	case ProjectRenderer::ExportFileFormat::Wave:
+        sfFormat = SF_FORMAT_WAV | sfBitDepth;
+        break;
+	case ProjectRenderer::ExportFileFormat::Flac:
+        sfFormat = SF_FORMAT_FLAC | sfBitDepth;
+        break;
+	case ProjectRenderer::ExportFileFormat::Ogg:
+        sfFormat = SF_FORMAT_OGG | SF_FORMAT_VORBIS;
+        break;
+	case ProjectRenderer::ExportFileFormat::MP3:
+        sfFormat = SF_FORMAT_MPEG | SF_FORMAT_MPEG_LAYER_III;
+        break;
+    default:
+        return SF_INFO{};
+	}
+
+    switch (settings.getStereoMode())
+    {
+	case OutputSettings::StereoMode::Stereo:
+	case OutputSettings::StereoMode::JointStereo:
+        sfChannels = 2;
+        break;
+	case OutputSettings::StereoMode::Mono:
+        sfChannels = 1;
+		break;
+	}
+
+	return {.samplerate = static_cast<int>(settings.getSampleRate()), .channels = sfChannels, .format = sfFormat};
+}
+
+} // namespace
+
+namespace lmms {
+struct AudioFile::Impl
+{
+	Impl(std::filesystem::path path);
+	Impl(std::filesystem::path path, ProjectRenderer::ExportFileFormat format, OutputSettings settings);
+	SF_INFO m_info;
+	SNDFILE* m_sndfile;
+	std::filesystem::path m_path;
+};
+
+AudioFile::AudioFile(std::filesystem::path path)
+	: m_impl(std::make_unique<Impl>(path))
+{
+	if (!m_impl->m_sndfile)
+	{
+		throw std::runtime_error{"failed to construct audio file: " + std::string{sf_strerror(m_impl->m_sndfile)}};
+	}
+}
+
+AudioFile::AudioFile(std::filesystem::path path, ProjectRenderer::ExportFileFormat format, OutputSettings settings)
+	: m_impl(std::make_unique<Impl>(path, format, settings))
+{
+    if (!m_impl->m_sndfile)
+	{
+		throw std::runtime_error{"failed to construct audio file: " + std::string{sf_strerror(m_impl->m_sndfile)}};
+	}
+}
+
+AudioFile::~AudioFile() = default;
+
+AudioFile::Impl::Impl(std::filesystem::path path)
+	: m_info()
+	, m_sndfile(openAudioFile(path, SFM_READ, &m_info))
+	, m_path(path)
+{
+}
+
+AudioFile::Impl::Impl(std::filesystem::path path, ProjectRenderer::ExportFileFormat format, OutputSettings settings)
+	: m_info(sfInfoFromOutputSettings(format, settings))
+	, m_sndfile(openAudioFile(path, SFM_WRITE, &m_info))
+	, m_path(path)
+{
+}
+
+void AudioFile::read(InterleavedBufferView<float> dst)
+{
+    assert(dst.channels() == m_impl->m_info.channels && "invalid channel count");
+    sf_readf_float(m_impl->m_sndfile, dst.data(), dst.frames());
+}
+
+void AudioFile::write(InterleavedBufferView<const float> src)
+{
+	assert(src.channels() == m_impl->m_info.channels && "invalid channel count");
+	sf_writef_float(m_impl->m_sndfile, src.data(), src.frames());
+}
+
+auto AudioFile::frames() const -> f_cnt_t
+{
+	return m_impl->m_info.frames;
+}
+
+auto AudioFile::channels() const -> ch_cnt_t
+{
+	return m_impl->m_info.channels;
+}
+
+auto AudioFile::sampleRate() const -> sample_rate_t
+{
+	return m_impl->m_info.samplerate;
+}
+
+} // namespace lmms
