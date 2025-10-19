c2e7e684c23f45947b6dfdcf2526ab88f07ea905
Sotonye Atemie <sakertooth@gmail.com>
2025-10-19 10:40:24 -0400

    Update copyright

--- include/AudioFile.h
+++ include/AudioFile.h
@@ -1,7 +1,7 @@
 /*
  * AudioFile.h
  *
- * Copyright (c) 2025 Sotonye Atemie <sakertooth@gmail.com>
+ * Copyright (c) 2025 saker <sakertooth@gmail.com>
  *
  * This file is part of LMMS - https://lmms.io
  *
