44a68b8b01b272ac195d5a06b738fed925d49210
Sotonye Atemie <sakertooth@gmail.com>
2025-10-22 08:34:07 -0400

    Fix audio resampling functionality (#7858)
    Co-authored-by: Dalton Messmer <messmer.dalton@gmail.com>

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -190,12 +190,6 @@ void printHelp()
 		"          Default: 160.\n"
 		"  -f, --format <format>         Specify format of render-output where\n"
 		"          Format is either 'wav', 'flac', 'ogg' or 'mp3'.\n"
-		"  -i, --interpolation <method>   Specify interpolation method\n"
-		"          Possible values:\n"
-		"            - linear\n"
-		"            - sincfastest (default)\n"
-		"            - sincmedium\n"
-		"            - sincbest\n"
 		"  -l, --loop                     Render as a loop\n"
 		"  -m, --mode                     Stereo mode used for MP3 export\n"
 		"          Possible values: s, j, m\n"
@@ -375,7 +369,6 @@ int main( int argc, char * * argv )
 			new QCoreApplication( argc, argv ) :
 					new gui::MainApplication(argc, argv);
 
-	AudioEngine::qualitySettings qs(AudioEngine::qualitySettings::Interpolation::Linear);
 	OutputSettings os(44100, 160, OutputSettings::BitDepth::Depth16Bit, OutputSettings::StereoMode::JointStereo);
 	ProjectRenderer::ExportFileFormat eff = ProjectRenderer::ExportFileFormat::Wave;
 
@@ -615,39 +608,6 @@ int main( int argc, char * * argv )
 		{
 			os.setBitDepth(OutputSettings::BitDepth::Depth32Bit);
 		}
-		else if( arg == "--interpolation" || arg == "-i" )
-		{
-			++i;
-
-			if( i == argc )
-			{
-				return usageError( "No interpolation method specified" );
-			}
-
-
-			const QString ip = QString( argv[i] );
-
-			if( ip == "linear" )
-			{
-		qs.interpolation = AudioEngine::qualitySettings::Interpolation::Linear;
-			}
-			else if( ip == "sincfastest" )
-			{
-		qs.interpolation = AudioEngine::qualitySettings::Interpolation::SincFastest;
-			}
-			else if( ip == "sincmedium" )
-			{
-		qs.interpolation = AudioEngine::qualitySettings::Interpolation::SincMedium;
-			}
-			else if( ip == "sincbest" )
-			{
-		qs.interpolation = AudioEngine::qualitySettings::Interpolation::SincBest;
-			}
-			else
-			{
-				return usageError( QString( "Invalid interpolation method %1" ).arg( argv[i] ) );
-			}
-		}
 		else if( arg == "--import" )
 		{
 			++i;
@@ -776,7 +736,7 @@ int main( int argc, char * * argv )
 		}
 
 		// create renderer
-		auto r = new RenderManager(qs, os, eff, renderOut);
+		auto r = new RenderManager(os, eff, renderOut);
 		QCoreApplication::instance()->connect( r,
 				SIGNAL(finished()), SLOT(quit()));
 
