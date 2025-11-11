944328f42d0131f57bee4bd91c7406450fb015f6
SpomJ <mihail_a_m@mail.ru>
2025-11-11 11:53:43 +0300

    remove obsolete qt version workarounds

--- src/gui/SubWindow.cpp
+++ src/gui/SubWindow.cpp
@@ -288,16 +288,6 @@ void SubWindow::setBorderColor( const QColor &c )
 
 void SubWindow::detach()
 {
-#if QT_VERSION < 0x50C00
-	// Workaround for a bug in Qt versions below 5.12,
-	// where argument-dependent-lookup fails for QFlags operators
-	// declared inside a namepsace.
-	// This affects the Q_DECLARE_OPERATORS_FOR_FLAGS macro in Instrument.h
-	// See also: https://codereview.qt-project.org/c/qt/qtbase/+/225348
-
-	using ::operator|;
-#endif
-
 	if (isDetached()) { return; }
 
 	const auto pos = mapToGlobal(widget()->pos());
@@ -318,16 +308,6 @@ void SubWindow::detach()
 
 void SubWindow::attach()
 {
-#if QT_VERSION < 0x50C00
-	// Workaround for a bug in Qt versions below 5.12,
-	// where argument-dependent-lookup fails for QFlags operators
-	// declared inside a namepsace.
-	// This affects the Q_DECLARE_OPERATORS_FOR_FLAGS macro in Instrument.h
-	// See also: https://codereview.qt-project.org/c/qt/qtbase/+/225348
-
-	using ::operator|;
-#endif
-
 	if (!isDetached()) { return; }
 
 	const bool shown = widget()->isVisible();
