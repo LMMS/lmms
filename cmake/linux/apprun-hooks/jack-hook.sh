c2df5453ee4704eb007ad205b1a3838be1b249da
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2025-11-11 12:46:12 -0500

    Simplify env

--- cmake/linux/apprun-hooks/jack-hook.sh
+++ cmake/linux/apprun-hooks/jack-hook.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 # Workaround crash when jack is missing by providing a dummy version
-export LC_ALL=C # Set language to English
-if ldd "$APPDIR/usr/bin/lmms" |grep "libjack.so" |grep "not found" > /dev/null 2>&1; then
+if LC_ALL=C ldd "$APPDIR/usr/bin/lmms" |grep "libjack.so" |grep "not found" > /dev/null 2>&1; then
 	echo "[${0##*/}] Jack does not appear to be installed.  That's OK, we'll use a dummy version instead." >&2
 	export LD_LIBRARY_PATH="$APPDIR/usr/lib/jack:$LD_LIBRARY_PATH"
 else
 	echo "[${0##*/}] Jack appears to be installed on this system, so we'll use it." >&2
 fi
-unset LC_ALL # Restore language
