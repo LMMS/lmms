b69f5a895d8efbf3ec58ab7a3126699d81d9de24
Jan125 <Jan-Lotz@hotmail.de>
2025-11-16 08:45:26 +0100

    Add creatorplatformtype (productType())

--- src/core/DataFile.cpp
+++ src/core/DataFile.cpp
@@ -139,6 +139,7 @@ DataFile::DataFile( Type type ) :
 	root.setAttribute( "creator", "LMMS" );
 	root.setAttribute( "creatorversion", LMMS_VERSION );
 	root.setAttribute( "creatorplatform", QSysInfo::kernelType() );
+	root.setAttribute( "creatorplatformtype", QSysInfo::productType() );
 	appendChild( root );
 
 	m_head = createElement( "head" );
@@ -2103,6 +2104,7 @@ void DataFile::upgrade()
 	documentElement().setAttribute( "creator", "LMMS" );
 	documentElement().setAttribute( "creatorversion", LMMS_VERSION );
 	documentElement().setAttribute( "creatorplatform", QSysInfo::kernelType() );
+	documentElement().setAttribute( "creatorplatformtype", QSysInfo::productType() );
 
 	if( type() == Type::SongProject || type() == Type::SongProjectTemplate )
 	{
