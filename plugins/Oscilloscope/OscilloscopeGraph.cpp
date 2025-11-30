d3723656e6dc3f22e5191ddf0fc3bb377d228eda
regulus79 <117475203+regulus79@users.noreply.github.com>
2025-11-29 19:13:45 -0500

    Update connect signal/slot syntax

--- plugins/Oscilloscope/OscilloscopeGraph.cpp
+++ plugins/Oscilloscope/OscilloscopeGraph.cpp
@@ -47,8 +47,8 @@ OscilloscopeGraph::OscilloscopeGraph(QWidget* parent, OscilloscopeControls* cont
 	setMinimumSize(400, 200);
 	setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
-	connect(getGUI()->mainWindow(), SIGNAL(periodicUpdate()), this, SLOT(update()));
-	connect(Engine::audioEngine(), SIGNAL(sampleRateChanged()), this, SLOT(changeSampleRate()));
+	connect(getGUI()->mainWindow(), &MainWindow::periodicUpdate, this, static_cast<void (QWidget::*)()>(&QWidget::update));
+	connect(Engine::audioEngine(), &AudioEngine::sampleRateChanged, this, &OscilloscopeGraph::changeSampleRate);
 	changeSampleRate();
 }
 
