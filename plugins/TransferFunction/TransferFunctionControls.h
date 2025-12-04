494d5850b6f2749d3d533495c05d35c76491bdff
Ewan Pettigrew <ewanpettigrew@outlook.com>
2025-12-04 16:07:58 +1030

    Update TransferFunctionControls.h

--- plugins/TransferFunction/TransferFunctionControls.h
+++ plugins/TransferFunction/TransferFunctionControls.h
@@ -1,54 +1,123 @@
-/* TransferFunctionControls.h */
 #ifndef LMMS_TRANSFERFUNCTION_CONTROLS_H
+
 #define LMMS_TRANSFERFUNCTION_CONTROLS_H
 
+
+
 #include "EffectControls.h"
+
+#include "Graph.h"
+
 #include "TransferFunctionControlDialog.h"
-#include <QString> // Added
+
+#include <QString>
+
+
 
 namespace lmms
+
 {
+
+
 
 class TransferFunctionEffect;
 
+
+
 class TransferFunctionControls : public EffectControls
+
 {
-	Q_OBJECT
+
+    Q_OBJECT
+
+
+
 public:
-	TransferFunctionControls(TransferFunctionEffect* effect);
-	~TransferFunctionControls() override = default;
 
-	void saveSettings(QDomDocument& doc, QDomElement& parent) override;
-	void loadSettings(const QDomElement& parent) override;
-	
-	// Helper to access the string safely
-	QString getFormula() const { return m_formulaString; }
+    TransferFunctionControls(TransferFunctionEffect* effect);
 
-	inline QString nodeName() const override
-	{
-		return "TransferFunctionControls";
-	}
-	gui::EffectControlDialog* createView() override
-	{
-		return new gui::TransferFunctionControlDialog(this);
-	}
-	int controlCount() override { return 4; }
+    ~TransferFunctionControls() override = default;
+
+
+
+    void saveSettings(QDomDocument& doc, QDomElement& parent) override;
+
+    void loadSettings(const QDomElement& parent) override;
+
+
+
+    QString getFormula() const { return m_formulaString; }
+
+
+
+    inline QString nodeName() const override
+
+    {
+
+        return "TransferFunctionControls";
+
+    }
+
+
+
+    gui::EffectControlDialog* createView() override
+
+    {
+
+        return new gui::TransferFunctionControlDialog(this);
+
+    }
+
+
+
+    int controlCount() override { return 4; }
+
+
 
 public slots:
-	// Slot to receive text updates from GUI
-	void setFormula(const QString& text) { m_formulaString = text; }
+
+    void setFormula(const QString& text);
+
+    
+
+    // MOVED here so we can connect the Knob to it
+
+    void updateBodePlot();
+
+
 
 private:
-	TransferFunctionEffect* m_effect;
-	FloatModel m_volumeModel;
-	
-	// The String Variable
-	QString m_formulaString;
 
-	friend class gui::TransferFunctionControlDialog;
-	friend class TransferFunctionEffect;
+    TransferFunctionEffect* m_effect;
+
+
+
+    // Parameters
+
+    FloatModel m_volumeModel; // This is the PRESET knob
+
+    QString m_formulaString;
+
+
+
+    // Bode plot graph models
+
+    graphModel m_bodeMagModel;
+
+    graphModel m_bodePhaseModel;
+
+
+
+    friend class gui::TransferFunctionControlDialog;
+
+    friend class TransferFunctionEffect;
+
 };
 
+
+
 } // namespace lmms
+
+
 
 #endif // LMMS_TRANSFERFUNCTION_CONTROLS_H
