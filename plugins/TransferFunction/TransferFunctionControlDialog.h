2c8d15152e9c7d5c288675a48dd9b38b2bdcfd58
Ewan Pettigrew <ewanpettigrew@outlook.com>
2025-12-12 21:48:12 +1030

    Update TransferFunctionControlDialog.h

--- plugins/TransferFunction/TransferFunctionControlDialog.h
+++ plugins/TransferFunction/TransferFunctionControlDialog.h
@@ -1,83 +1,31 @@
 #ifndef LMMS_GUI_TRANSFERFUNCTION_CONTROL_DIALOG_H
-
 #define LMMS_GUI_TRANSFERFUNCTION_CONTROL_DIALOG_H
-
-
-
 #include "EffectControlDialog.h"
-
 #include <QEvent> 
-
 #include <QPushButton> 
-
 #include <QLabel> // Added this
-
-
-
 namespace lmms
-
 {
-
-
-
 class TransferFunctionControls;
-
-
-
 namespace gui
-
 {
-
-
-
 class TransferFunctionControlDialog : public EffectControlDialog
-
 {
-
     Q_OBJECT
-
 public:
-
     TransferFunctionControlDialog(TransferFunctionControls* controls);
-
     ~TransferFunctionControlDialog() override = default;
-
-
-
 protected:
-
     bool eventFilter(QObject* obj, QEvent* event) override;
-
-
-
 private slots:
-
     void updateUIState();
-
     void updateFormulaLabel(); // Fixed: Added declaration
-
-
-
 private:
-
     TransferFunctionControls* m_pluginControls;
-
     
-
     QPushButton* m_clearButton;
-
     QLabel* m_activeFormulaLabel; // Fixed: Added declaration
-
 };
-
-
-
 } // namespace gui
-
-
-
 } // namespace lmms
-
-
-
 #endif // LMMS_GUI_TRANSFERFUNCTION_CONTROL_DIALOG_H
