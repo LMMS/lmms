1620912128f60dfda8368e53125212de1e8fb45d
regulus79 <117475203+regulus79@users.noreply.github.com>
2025-12-15 11:57:08 -0500

    Kinda sorta working

--- plugins/SfzSampler/SfzSampler.cpp
+++ plugins/SfzSampler/SfzSampler.cpp
@@ -28,6 +28,7 @@
 #include <QDebug>
 
 #include "Engine.h"
+#include "InstrumentPlayHandle.h"
 #include "InstrumentTrack.h"
 #include "PathUtil.h"
 #include "ConfigManager.h"
@@ -59,10 +60,44 @@ PLUGIN_EXPORT Plugin* lmms_plugin_main(Model* m, void*)
 } // end extern
 
 
+
+
+int keyStringToInt(QString keyString)
+{
+	bool ok;
+	int asInt = keyString.toInt(&ok);
+	if (ok) { return asInt; }
+	qDebug() << "yyayyyy";
+	QString octaveString = keyString.right(1);
+	QString keyName = keyString.chopped(1).toLower();
+	qDebug() << octaveString << keyName;
+	bool ok2;
+	int octave = octaveString.toInt(&ok2);
+	if (!ok2) { qDebug() << "AAAA bad octave"; return 0; }
+	int keyOffset = 0;
+	if (keyName == "a") { keyOffset = 0; }
+	else if (keyName == "a#" || keyName == "bb") { keyOffset = 1; }
+	else if (keyName == "b") { keyOffset = 2; }
+	else if (keyName == "c") { keyOffset = 3; }
+	else if (keyName == "c#" || keyName == "db") { keyOffset = 4; }
+	else if (keyName == "d") { keyOffset = 5; }
+	else if (keyName == "d#" || keyName == "eb") { keyOffset = 6; }
+	else if (keyName == "e") { keyOffset = 7; }
+	else if (keyName == "f") { keyOffset = 8; }
+	else if (keyName == "f#" || keyName == "gb") { keyOffset = 9; }
+	else if (keyName == "g") { keyOffset = 10; }
+	else if (keyName == "g#" || keyName == "ab") { keyOffset = 11; }
+	else { qDebug() << "AAAA bad key";  return 0; }
+	qDebug() << "returning" << 21 + octave * 12 + keyOffset;
+	return 21 + octave * 12 + keyOffset;
+}
+
+
 SfzSampler::SfzSampler(InstrumentTrack* instrumentTrack)
-	: Instrument(instrumentTrack, &sfzsampler_plugin_descriptor)
+	: Instrument(instrumentTrack, &sfzsampler_plugin_descriptor, nullptr, Flag::IsSingleStreamed | Flag::IsMidiBased)
 	, m_originalSample1()
 	, m_originalSample2()
+	, m_tempBuffer(new SampleFrame[Engine::audioEngine()->framesPerPeriod()])
 	, m_parentTrack(instrumentTrack)
 {
 	QString path = ConfigManager::inst()->userSamplesDir() + "sfz/jlearman.jRhodes3c-master/jRhodes3c-looped-flac-sfz/";
@@ -75,10 +110,270 @@ SfzSampler::SfzSampler(InstrumentTrack* instrumentTrack)
 		m_originalSample2 = Sample(std::move(buffer));
 	}
 
+	QFile file(path + "_jRhodes-stereo-looped.sfz");
+
+	if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) { return; }
+
+	bool inGlobal = false;
+	bool inGroup = false;
+	bool inRegion = false;
+
+	while (!file.atEnd())
+	{
+		QString line = file.readLine();
+		// Trim comments off end
+		line = line.split("//")[0];
+		line = line.trimmed();
+		qDebug() << line;
+		// Split by "=" TODO make better
+		QStringList segments = line.split("=");
+		if (segments.length() == 0) { continue; }
+		if (segments.length() == 1)
+		{
+			if (line == "<global>")
+			{
+				qDebug() << "Global!";
+				inGlobal = true;
+				inGroup = false;
+				inRegion = false;
+			}
+			if (line == "<group>")
+			{
+				qDebug() << "New Group!";
+				inGlobal = false;
+				inGroup = true;
+				inRegion = false;
+				m_sfz.m_groups.push_back(SfzGroup());
+			}
+			if (line == "<region>")
+			{
+				qDebug() << "New Region!";
+				inGlobal = false;
+				inGroup = false;
+				inRegion = true;
+				m_sfz.m_groups.back().m_regions.push_back(SfzRegion());
+			}
+		}
+		if (segments.length() == 2)
+		{
+			QString opcode = segments[0];
+			QString value = segments[1];
+			SfzSettingState& currentSettingsState = inGlobal
+				? m_sfz.m_globalSettings
+				: inGroup
+					? m_sfz.m_groups.back().m_globalSettings
+					: m_sfz.m_groups.back().m_regions.back().m_settings;
+			if (opcode == "sample")
+			{
+				currentSettingsState.sampleFile = value;
+				qDebug() << "LOADING SAMPLE!!!" << value;
+				if (auto buffer = gui::SampleLoader::createBufferFromFile(path + value))
+				{
+					m_samples.push_back(Sample(std::move(buffer)));
+					currentSettingsState.sampleIndex = m_samples.size() - 1;
+				}
+			}
+			else if (opcode == "lokey")
+			{
+				currentSettingsState.lokey = keyStringToInt(value);
+			}
+			else if (opcode == "hikey")
+			{
+				currentSettingsState.hikey = keyStringToInt(value);
+			}
+			else if (opcode == "pitch_keycenter")
+			{
+				currentSettingsState.pitch_keycenter = keyStringToInt(value);
+			}
+			else if (opcode == "lovel")
+			{
+				currentSettingsState.lovel = value.toInt();
+			}
+			else if (opcode == "hivel")
+			{
+				currentSettingsState.hivel = value.toInt();
+			}
+			else if (opcode == "loop_mode")
+			{
+				if (value == "loop_continuous") { currentSettingsState.loop_mode = LoopMode::LoopContinuous; }
+				else { qDebug() << "Oops loop val"; }
+			}
+			else if (opcode == "ampeg_hold")
+			{
+				currentSettingsState.ampeg_hold = value.toFloat();
+			}
+			else if (opcode == "ampeg_sustain")
+			{
+				currentSettingsState.ampeg_sustain = value.toFloat();
+			}
+			else if (opcode == "ampeg_decay")
+			{
+				currentSettingsState.ampeg_decay = value.toFloat();
+			}
+			else if (opcode == "ampeg_release")
+			{
+				currentSettingsState.ampeg_release = value.toFloat();
+			}
+			else
+			{
+				qDebug() << "AAAAAAAA uknown opcode" << opcode << value;
+			}
+		}
+
+	}
+
+	qDebug() << "Yay!" << m_sfz.m_groups.size();
+	for (auto& group : m_sfz.m_groups)
+	{
+		qDebug() << "wooo" << group.m_regions.size();
+	}
+
+	auto iph = new InstrumentPlayHandle(this, instrumentTrack);
+	Engine::audioEngine()->addPlayHandle( iph );
+
 	emit dataChanged();
 }
 
-void SfzSampler::playNote(NotePlayHandle* handle, SampleFrame* workingBuffer)
+
+SfzSampler::~SfzSampler()
+{
+	delete[] m_tempBuffer;
+}
+
+
+
+bool SfzSampler::handleMidiEvent(const MidiEvent& event, const TimePos& time, f_cnt_t offset)
+{
+	if (event.type() == MidiNoteOn)
+	{
+		int key = event.key();
+		int velocity = event.velocity();
+		m_noteStates[key].pressed = true;
+		m_noteStates[key].velocity = velocity;
+		m_noteStates[key].frameCounter = -offset;
+		m_noteStates[key].playbackState = Sample::PlaybackState(AudioResampler::Mode::Linear);
+		qDebug() << "Note on!" << key << velocity;
+	}
+	if (event.type() == MidiNoteOff)
+	{
+		int key = event.key();
+		int velocity = event.velocity();
+		m_noteStates[key].pressed = false;
+		m_noteStates[key].velocity = velocity;
+		m_noteStates[key].frameReleased = m_noteStates[key].frameCounter;
+		qDebug() << "Note off!" << key << velocity;
+	}
+
+	float sampleRate = Engine::audioEngine()->outputSampleRate();
+
+	// Loop through all regions, and find ones which match the current state
+	// If they match, play them
+	if (event.type() == MidiNoteOn || event.type() == MidiNoteOff)
+	{
+		int key = event.key();
+		int velocity = event.velocity();
+		for (auto& group: m_sfz.m_groups)
+		{
+			for (auto& region: group.m_regions)
+			{
+				int lokey = region.m_settings.lokey.value_or(group.m_globalSettings.lokey.value_or(m_sfz.m_globalSettings.lokey.value_or(0)));
+				int hikey = region.m_settings.hikey.value_or(group.m_globalSettings.hikey.value_or(m_sfz.m_globalSettings.hikey.value_or(0)));
+				int pitch_keycenter = region.m_settings.pitch_keycenter.value_or(group.m_globalSettings.pitch_keycenter.value_or(m_sfz.m_globalSettings.pitch_keycenter.value_or(0)));
+				int lovel = region.m_settings.lovel.value_or(group.m_globalSettings.lovel.value_or(m_sfz.m_globalSettings.lovel.value_or(255)));
+				int hivel = region.m_settings.hivel.value_or(group.m_globalSettings.hivel.value_or(m_sfz.m_globalSettings.hivel.value_or(0)));
+				float ampeg_hold = region.m_settings.ampeg_hold.value_or(group.m_globalSettings.ampeg_hold.value_or(m_sfz.m_globalSettings.ampeg_hold.value_or(0)));
+				float ampeg_decay = region.m_settings.ampeg_decay.value_or(group.m_globalSettings.ampeg_decay.value_or(m_sfz.m_globalSettings.ampeg_decay.value_or(0)));
+				float ampeg_sustain = region.m_settings.ampeg_sustain.value_or(group.m_globalSettings.ampeg_sustain.value_or(m_sfz.m_globalSettings.ampeg_sustain.value_or(0)));
+				float ampeg_release = region.m_settings.ampeg_release.value_or(group.m_globalSettings.ampeg_release.value_or(m_sfz.m_globalSettings.ampeg_release.value_or(0)));
+				//qDebug() << "Checking!" << lokey << hikey << pitch_keycenter << lovel << hivel;
+				if (key >= lokey && key <= hikey && velocity >= lovel && velocity <= hivel)
+				{
+					qDebug() << "Found a match!!!" << region.m_settings.lokey.value_or(-1) << region.m_settings.hikey.value_or(-1) << region.m_settings.lovel.value_or(-1) << region.m_settings.hivel.value_or(-1) << region.m_settings.sampleFile.value_or("idk");
+					m_noteStates[key].sampleIndex = region.m_settings.sampleIndex;
+					int keydiff = key - pitch_keycenter;
+					double pitchRatio = std::exp2(-keydiff / 12.0);
+					m_noteStates[key].pitchRatio = pitchRatio;
+					qDebug() << "Pitch ratio:" << pitchRatio;
+					m_noteStates[key].holdFrames = ampeg_hold * sampleRate;
+					m_noteStates[key].decayFrames = ampeg_decay * sampleRate;
+					m_noteStates[key].sustainLevel = ampeg_sustain / 100.0f;
+					m_noteStates[key].releaseFrames = ampeg_release * sampleRate;
+				}
+			}
+		}
+	}
+
+	return true;
+}
+
+
+void SfzSampler::play(SampleFrame* workingBuffer)
+{
+	const fpp_t frames = Engine::audioEngine()->framesPerPeriod();
+	for (int key = 0; key < 128; ++key)
+	{
+		//m_noteStates[key].playbackState
+		f_cnt_t offsetFrames = 0;
+		if (m_noteStates[key].frameCounter < 0 && frames - m_noteStates[key].frameCounter > 0)
+		{
+			offsetFrames = frames - m_noteStates[key].frameCounter;
+		}
+		if (m_noteStates[key].sampleIndex != -1)
+		{
+			// don't play if past release
+			if (!m_noteStates[key].pressed && m_noteStates[key].frameCounter - m_noteStates[key].frameReleased > m_noteStates[key].releaseFrames) { continue; }
+			
+			m_samples.at(m_noteStates[key].sampleIndex).play(m_tempBuffer + offsetFrames, &m_noteStates[key].playbackState, frames - offsetFrames, Sample::Loop::Off, m_noteStates[key].pitchRatio);
+			
+			for (f_cnt_t f = 0; f < frames; ++f)
+			{
+				float ampeg_multiplier = 1.0f;
+				if (m_noteStates[key].pressed)
+				{
+					if (m_noteStates[key].frameCounter > 0 && m_noteStates[key].frameCounter < 0) // TODO attack
+					{
+						ampeg_multiplier = 1.0f; // TODO
+						//qDebug() << "Attack" << m_noteStates[key].frameCounter << m_noteStates[key].attackFrames << m_noteStates[key].holdFrames << m_noteStates[key].decayFrames << m_noteStates[key].sustainLevel << m_noteStates[key].releaseFrames;
+					}
+					else if (m_noteStates[key].frameCounter < m_noteStates[key].holdFrames)
+					{
+						ampeg_multiplier = 1.0f;
+						//qDebug() << "Hold" << m_noteStates[key].frameCounter << m_noteStates[key].attackFrames << m_noteStates[key].holdFrames << m_noteStates[key].decayFrames << m_noteStates[key].sustainLevel << m_noteStates[key].releaseFrames;
+					}
+					else if (m_noteStates[key].frameCounter < m_noteStates[key].holdFrames + m_noteStates[key].decayFrames)
+					{
+						ampeg_multiplier = 1.0f - (1.0f - m_noteStates[key].sustainLevel) * 1.0f * (m_noteStates[key].frameCounter - m_noteStates[key].holdFrames) / m_noteStates[key].decayFrames;
+						//qDebug() << 1.0f * (m_noteStates[key].frameCounter - m_noteStates[key].holdFrames) / m_noteStates[key].decayFrames << ampeg_multiplier;
+						//qDebug() << "Decay" << m_noteStates[key].frameCounter << m_noteStates[key].attackFrames << m_noteStates[key].holdFrames << m_noteStates[key].decayFrames << m_noteStates[key].sustainLevel << m_noteStates[key].releaseFrames;
+					}
+					else
+					{
+						ampeg_multiplier = m_noteStates[key].sustainLevel;
+						//qDebug() << "Sustain" << m_noteStates[key].frameCounter << m_noteStates[key].attackFrames << m_noteStates[key].holdFrames << m_noteStates[key].decayFrames << m_noteStates[key].sustainLevel << m_noteStates[key].releaseFrames;
+					}
+				}
+				else
+				{
+					if (m_noteStates[key].frameCounter - m_noteStates[key].frameReleased < m_noteStates[key].releaseFrames)
+					{
+						ampeg_multiplier = 1.0f - 1.0f * (m_noteStates[key].frameCounter - m_noteStates[key].frameReleased) / m_noteStates[key].releaseFrames;
+						//qDebug() << "Release" << m_noteStates[key].frameCounter << m_noteStates[key].attackFrames << m_noteStates[key].holdFrames << m_noteStates[key].decayFrames << m_noteStates[key].sustainLevel << m_noteStates[key].releaseFrames;
+					}
+					else
+					{
+						ampeg_multiplier = 0.0f;
+					}
+				}
+				workingBuffer[f] += m_tempBuffer[f] * ampeg_multiplier;
+				m_noteStates[key].frameCounter += 1;
+			}
+		}
+	}
+}
+
+
+
+/*void SfzSampler::playNote(NotePlayHandle* handle, SampleFrame* workingBuffer)
 {
 	//if (m_originalSample.sampleSize() <= 1) { return; }
 
@@ -91,6 +386,7 @@ void SfzSampler::playNote(NotePlayHandle* handle, SampleFrame* workingBuffer)
 	if (!handle->m_pluginData) { handle->m_pluginData = new Sample::PlaybackState(AudioResampler::Mode::Linear, startFrame); }
 
 	auto playbackState = static_cast<Sample::PlaybackState*>(handle->m_pluginData);
+
 
 	if (noteIndex % 2 == 0)
 	{
@@ -105,7 +401,7 @@ void SfzSampler::playNote(NotePlayHandle* handle, SampleFrame* workingBuffer)
 void SfzSampler::deleteNotePluginData(NotePlayHandle* handle)
 {
 	delete static_cast<Sample::PlaybackState*>(handle->m_pluginData);
-}
+}*/
 
 
 void SfzSampler::loadFile(const QString& file)
--- plugins/SfzSampler/SfzSampler.h
+++ plugins/SfzSampler/SfzSampler.h
@@ -31,6 +31,7 @@
 #include "Note.h"
 #include "Sample.h"
 #include "SfzSamplerView.h"
+#include "SfzFormat.h"
 
 namespace lmms {
 
@@ -40,9 +41,13 @@ class SfzSampler : public Instrument
 
 public:
 	SfzSampler(InstrumentTrack* instrumentTrack);
+	~SfzSampler();
 
-	void playNote(NotePlayHandle* handle, SampleFrame* workingBuffer) override;
-	void deleteNotePluginData(NotePlayHandle* handle) override;
+	//void playNote(NotePlayHandle* handle, SampleFrame* workingBuffer) override;
+	//void deleteNotePluginData(NotePlayHandle* handle) override;
+
+	void play(SampleFrame* workingBuffer) override;
+	bool handleMidiEvent(const MidiEvent& event, const TimePos& time = TimePos(), f_cnt_t offset = 0) override;
 
 	void saveSettings(QDomDocument& document, QDomElement& element) override;
 	void loadSettings(const QDomElement& element) override;
@@ -57,7 +62,31 @@ private:
 	Sample m_originalSample1;
 	Sample m_originalSample2;
 
+	SampleFrame* m_tempBuffer;
+
+	std::vector<Sample> m_samples;
+
+	SfzSettings m_sfz;
+
 	InstrumentTrack* m_parentTrack;
+
+	struct NoteState
+	{
+		bool pressed = false;
+		int velocity = 0;
+		int frameCounter = 0;
+		int frameReleased = 0;
+		int sampleIndex = -1;
+		double pitchRatio = 1.0;
+		int attackFrames = 0;
+		int holdFrames = 0;
+		int decayFrames = 0;
+		float sustainLevel = 1.0f;
+		int releaseFrames = 0;
+		Sample::PlaybackState playbackState;
+	};
+
+	std::array<NoteState, 128> m_noteStates;
 
 	friend class gui::SfzSamplerView;
 };
