ea8ef56b93a3fba3acfff6276f0c76b281463c12
Ewan Pettigrew <ewanpettigrew@outlook.com>
2025-12-16 22:38:05 +1030

    Update TransferFunctionControls.h

--- plugins/TransferFunction/TransferFunctionControls.h
+++ plugins/TransferFunction/TransferFunctionControls.h
@@ -1,11 +1,14 @@
 #ifndef LMMS_TRANSFERFUNCTION_CONTROLS_H
 #define LMMS_TRANSFERFUNCTION_CONTROLS_H
+
 #include "EffectControls.h"
 #include "Graph.h"
 #include "TransferFunctionControlDialog.h"
 #include "TransferFunctionLogic.h" 
 #include <QString>
 #include <vector>
+#include <complex>
+
 namespace lmms
 {
 class TransferFunctionEffect;
@@ -15,19 +18,31 @@ class TransferFunctionControls : public EffectControls
 public:
     TransferFunctionControls(TransferFunctionEffect* effect);
     ~TransferFunctionControls() override = default;
+
     void saveSettings(QDomDocument& doc, QDomElement& parent) override;
     void loadSettings(const QDomElement& parent) override;
     QString getFormula() const { return m_formulaString; }
-    
+
+    // Drawing (Preset 18)
     const std::vector<DrawPoint>& getDrawPoints() const { return m_drawPoints; }
-    
     void setDrawPoint(float freq, float mag);
-    // NEW: Function to reset the drawing
     void clearDrawing();
+
+    // Pole/Zero (Preset 20)
+    const std::vector<Complex>& getPoles() const { return m_poles; }
+    const std::vector<Complex>& getZeros() const { return m_zeros; }
+   
+    // Interaction
+    void addPole(Complex p);
+    void addZero(Complex z);
+    void updatePoleZero(int index, bool isPole, Complex newVal);
+    void removeClosestPoleZero(Complex pos);
+    void clearPoleZero();
     inline QString nodeName() const override
     {
         return "TransferFunctionControls";
     }
+
     gui::EffectControlDialog* createView() override
     {
         return new gui::TransferFunctionControlDialog(this);
@@ -40,12 +55,19 @@ private:
     TransferFunctionEffect* m_effect;
     FloatModel m_volumeModel; 
     QString m_formulaString;
-    
+ 
+    // Mode 18 Data
     std::vector<DrawPoint> m_drawPoints;
+    // Mode 20 Data
+    std::vector<Complex> m_poles;
+    std::vector<Complex> m_zeros;
     graphModel m_bodeMagModel;
     graphModel m_bodePhaseModel;
     friend class gui::TransferFunctionControlDialog;
     friend class TransferFunctionEffect;
 };
 } // namespace lmms
+
+
+
 #endif // LMMS_TRANSFERFUNCTION_CONTROLS_H
