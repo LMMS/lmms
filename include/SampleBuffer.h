c56249dc03ab07b88acd354dec9add99cf58a91f
Sotonye Atemie <sakertooth@gmail.com>
2025-12-20 15:46:54 -0500

    Reword docs a bit

--- include/SampleBuffer.h
+++ include/SampleBuffer.h
@@ -38,8 +38,8 @@ namespace lmms {
 //! @brief Represents a stereo, interleaved audio buffer.
 //! SampleBuffer can be loaded from an audio file on disk, a Base64 string, or raw audio frame data.
 //! @note This class implements the CoW (copy-on-write) technique. It stores a shared pointer to the raw audio data, so
-//! if copies are made, the data is shared automatically. When modifications to this buffer is made, a deep copy of the
-//! data is made if it is being shared, preventing modifications to the data being shared.
+//! if copies are made, the data is shared automatically. When modifications to this buffer are made, a deep copy of the
+//! data happens if being shared.
 //! @todo This class should support any number of channels, rather than being fixed at stereo.
 class LMMS_EXPORT SampleBuffer
 {
