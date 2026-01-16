43d79aeacd8188f6459bcd5de12607b85d46713a
regulus79 <117475203+regulus79@users.noreply.github.com>
2026-01-16 12:12:25 -0500

    Remove GUI. Maybe the initial version doesn't need a fancy GUI

--- plugins/SfzSampler/SfzSamplerView.cpp
+++ plugins/SfzSampler/SfzSamplerView.cpp
@@ -50,74 +50,14 @@ namespace gui {
 
 SfzSamplerView::SfzSamplerView(SfzSampler* instrument, QWidget* parent)
 	: InstrumentView(instrument, parent)
-	, m_logo(PLUGIN_NAME::getIconPixmap("logo"))
 	, m_instrument(instrument)
-	, m_hBoxLayout(new QHBoxLayout)
-	, m_vBoxLayout(new QVBoxLayout)
-	, m_sidebarWidget(new SfzSidebarWidget(this))
-	, m_controlsWidget(new SfzControlsWidget(this))
-	, m_pianoWidget(new SfzPianoWidget(this))
-	, m_controlsLayout(new QBoxLayout(QBoxLayout::TopToBottom, m_controlsWidget))
-	, m_sidebarLayout(new QVBoxLayout(m_sidebarWidget))
 {
 	// window settings
 	setAcceptDrops(true);
 	setAutoFillBackground(true);
 
 	setMaximumSize(QSize(10000, 10000));
-	setMinimumSize(QSize(s_sidebarMinWidth + s_controlsAreaWidth, s_controlsAreaHeight + s_keyboardAreaMinHeight));
-
-	setLayout(m_hBoxLayout);
-	m_hBoxLayout->addWidget(m_sidebarWidget);
-	m_hBoxLayout->addWidget(new SfzDividerLine(this, 3, 11, false));
-	m_hBoxLayout->addLayout(m_vBoxLayout);
-	m_vBoxLayout->addWidget(m_controlsWidget);
-	m_vBoxLayout->addWidget(new SfzDividerLine(this, 2, 8));
-	m_vBoxLayout->addWidget(m_pianoWidget);
-
-	m_hBoxLayout->setContentsMargins(0, 0, 0, 0);
-	m_hBoxLayout->setSpacing(0);
-	m_vBoxLayout->setContentsMargins(0, 0, 0, 0);
-	m_vBoxLayout->setSpacing(0);
-
-	m_controlsWidget->setFixedSize(QSize(s_controlsAreaWidth, s_controlsAreaHeight));
-	m_sidebarWidget->setMinimumWidth(s_sidebarMinWidth);
-	m_pianoWidget->setMinimumHeight(s_keyboardAreaMinHeight);
-
-	// Logo at top of sidebar
-	auto logoFrame = new SfzSidebarGroupBox(m_sidebarWidget);
-	auto logoLabel = new QLabel(logoFrame);
-	logoLabel->setPixmap(m_logo);
-	logoLabel->setAlignment(Qt::AlignCenter);
-	logoLabel->setFixedHeight(70);
-	logoFrame->layout()->setContentsMargins(0, 0, 0, 0);
-	logoFrame->layout()->addWidget(logoLabel);
-
-	m_sidebarLayout->addWidget(logoFrame);
-	m_sidebarLayout->addWidget(new SfzDividerLine(this, 2, 8));
-
-	// Setup info labels
-	auto frame1 = new SfzSidebarGroupBox(m_sidebarWidget);
-	m_sfzPathLabel = new QLabel(frame1);
-	frame1->layout()->addWidget(m_sfzPathLabel);
-	m_sampleCountLabel = new QLabel(frame1);
-	frame1->layout()->addWidget(m_sampleCountLabel);
-
-	auto frame2 = new SfzSidebarGroupBox(m_sidebarWidget);
-	m_activeVoiceCountLabel = new QLabel(frame2);
-	frame2->layout()->addWidget(m_activeVoiceCountLabel);
-	
-	m_sidebarLayout->addWidget(frame1);
-	m_sidebarLayout->addWidget(new SfzDividerLine(this, 2, 8));
-	m_sidebarLayout->addWidget(frame2);
-	m_sidebarLayout->addWidget(new SfzDividerLine(this, 2, 8));
-	m_sidebarLayout->addStretch();
-
-
-	m_emptyFileLabel = new QLabel(tr("Drag and drop or click to load SFZ file"), m_controlsWidget);
-	m_emptyFileLabel->setAlignment(Qt::AlignCenter);
-	m_emptyFileLabel->move(s_controlsAreaWidth / 2, s_controlsAreaHeight / 2);
-	m_controlsLayout->addWidget(m_emptyFileLabel, 0, Qt::AlignCenter);
+	setMinimumSize(QSize(250, 250));
 
 	update();
 }
@@ -143,6 +83,7 @@ void SfzSamplerView::dragEnterEvent(QDragEnterEvent* dee)
 	{
 		dee->accept();
 		m_instrument->loadFile(value);
+		return;
 	}
 	dee->ignore();
 }
@@ -165,116 +106,9 @@ void SfzSamplerView::resizeEvent(QResizeEvent* re)
 
 void SfzSamplerView::paintEvent(QPaintEvent* pe)
 {
-	QPainter brush(this);
-	brush.fillRect(rect(), QColor(19, 19, 20));
-
-	// Update info labels
-	m_sfzPathLabel->setText(tr("Loaded File: %1").arg("todo"));
-
-	m_sampleCountLabel->setText(tr("Sample Count: %1").arg("123"));
-
-	m_activeVoiceCountLabel->setText(tr("Active Voices: %1").arg("3"));
+	//QPainter brush(this);
+	//brush.fillRect(rect(), QColor(19, 19, 20));
 }
-
-
-void SfzSidebarWidget::paintEvent(QPaintEvent* pe)
-{
-	QPainter brush(this);
-	brush.fillRect(rect(), QColor(19, 19, 20));
-}
-
-
-void SfzControlsWidget::mousePressEvent(QMouseEvent* pe)
-{
-	static_cast<SfzSamplerView*>(parent())->openFile();
-}
-
-void SfzControlsWidget::paintEvent(QPaintEvent* pe)
-{
-	QPainter brush(this);
-	brush.fillRect(rect(), QColor(19, 19, 20));
-}
-
-
-void SfzPianoWidget::paintEvent(QPaintEvent* pe)
-{
-	QPainter brush(this);
-	brush.fillRect(rect(), QColor(72, 72, 75));
-
-	brush.setPen(QPen(QColor(47, 54, 94), 1));
-	for (int key = 0; key < 128; ++key)
-	{
-		bool isBlack = false;
-		if (key % 12 == 1 || key % 12 == 3 || key % 12 == 6 || key % 12 == 8 || key % 12 == 10) { isBlack = true; }
-		if (isBlack)
-		{
-			QRect noteRect(key * width() / 128.0f, 0, width() / 128.0f, height() / 2);
-			brush.drawRect(noteRect);
-			brush.fillRect(noteRect, QColor(22, 22, 25));
-			brush.drawLine((key + 0.5f) * width() / 128.0f, height() / 2, (key + 0.5f) * width() / 128.0f, height());
-		}
-		if (key % 12 == 0 || key % 12 == 5)
-		{
-			brush.drawLine(key * width() / 128.0f, 0, key * width() / 128.0f, height());
-		}
-	}
-}
-
-
-SfzSidebarGroupBox::SfzSidebarGroupBox(QWidget* parent)
-	: QFrame(parent)
-{
-	setLayout(new QVBoxLayout(this));
-}
-
-void SfzSidebarGroupBox::paintEvent(QPaintEvent* pe)
-{
-	QPainter brush(this);
-	brush.fillRect(rect(), QColor(34, 39, 39));
-}
-
-
-SfzDividerLine::SfzDividerLine(QWidget* parent, float lineWidth, float dividerWidth, bool horizontal)
-	: QWidget(parent)
-	, m_lineWidth(lineWidth)
-	, m_dividerWidth(dividerWidth)
-	, m_horizontal(horizontal)
-{
-	if (horizontal)
-	{
-		setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
-		setFixedHeight(m_dividerWidth);
-	}
-	else
-	{
-		setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
-		setFixedWidth(m_dividerWidth);
-	}
-}
-
-void SfzDividerLine::paintEvent(QPaintEvent* pe)
-{
-	QPainter brush(this);
-	const float padding = 0.5f * (m_dividerWidth);
-	const float x1 = padding, y1 = padding, x2 = width() - padding, y2 = height() - padding;
-
-	// Glow background of line
-	const int numPasses = 4;
-	for (int i = 1; i <= numPasses; ++i)
-	{
-		const float ratio = static_cast<float>(i) / numPasses;
-		brush.setPen(QPen(QColor(131, 149, 255), m_dividerWidth * (1.0f - ratio), Qt::SolidLine, Qt::RoundCap));
-		brush.setOpacity(ratio);
-		brush.drawLine(x1, y1, x2, y2);
-	}
-	// Actual line
-	brush.setPen(QPen(QColor(145, 232, 233), m_lineWidth, Qt::SolidLine, Qt::RoundCap));
-	brush.setOpacity(1.0f);
-	brush.drawLine(x1, y1, x2, y2);
-}
-
-
-
 
 } // namespace gui
 } // namespace lmms
--- plugins/SfzSampler/SfzSamplerView.h
+++ plugins/SfzSampler/SfzSamplerView.h
@@ -25,14 +25,7 @@
 #ifndef LMMS_GUI_SFZSAMPLER_VIEW_H
 #define LMMS_GUI_SFZSAMPLER_VIEW_H
 
-#include <QHBoxLayout>
-#include <QVBoxLayout>
-#include <QLabel>
-#include <QFrame>
-
 #include "InstrumentView.h"
-
-class QPushButton;
 
 namespace lmms {
 
@@ -40,70 +33,12 @@ class SfzSampler;
 
 namespace gui {
 
-class ComboBox;
-class Knob;
-class LcdSpinBox;
-class PixmapButton;
-
-class SfzControlsWidget : public QWidget
-{
-public:
-	using QWidget::QWidget;
-protected:
-	void mousePressEvent(QMouseEvent* pe) override;
-	void paintEvent(QPaintEvent* pe) override;
-};
-
-class SfzSidebarWidget : public QWidget
-{
-public:
-	using QWidget::QWidget;
-protected:
-	void paintEvent(QPaintEvent* pe) override;
-};
-
-class SfzPianoWidget : public QWidget
-{
-public:
-	using QWidget::QWidget;
-protected:
-	void paintEvent(QPaintEvent* pe) override;
-};
-
-class SfzSidebarGroupBox : public QFrame
-{
-public:
-	SfzSidebarGroupBox(QWidget* parent = nullptr);
-protected:
-	void paintEvent(QPaintEvent* pe) override;
-};
-
-class SfzDividerLine : public QWidget
-{
-public:
-	SfzDividerLine(QWidget* parent = nullptr, float lineWidth = 3, float dividerWidth = 11, bool horizontal = true);
-protected:
-	void paintEvent(QPaintEvent* pe) override;
-	float m_lineWidth = 3;
-	float m_dividerWidth = 11;
-	bool m_horizontal = true;
-};
-
-
-
 class SfzSamplerView : public InstrumentView
 {
 	Q_OBJECT
 
 public:
 	SfzSamplerView(SfzSampler* instrument, QWidget* parent);
-
-	static constexpr int s_controlsAreaWidth = 775; // Once parsing aria xml gui configurations is implemented, these hardcoded values will not be as needed
-	static constexpr int s_controlsAreaHeight = 335;
-
-	static constexpr int s_sidebarMinWidth = 200;
-
-	static constexpr int s_keyboardAreaMinHeight = 50;
 
 public slots:
 	void openFile();
@@ -118,27 +53,7 @@ protected:
 private:
 	bool isResizable() const override { return true; }
 
-	QPixmap m_logo;
-
 	SfzSampler* m_instrument;
-
-	QHBoxLayout* m_hBoxLayout;
-	QVBoxLayout* m_vBoxLayout;
-	SfzSidebarWidget* m_sidebarWidget;
-	SfzControlsWidget* m_controlsWidget;
-	SfzPianoWidget* m_pianoWidget;
-
-	QBoxLayout* m_controlsLayout;
-
-	QVBoxLayout* m_sidebarLayout;
-
-	QLabel* m_sfzPathLabel;
-	QLabel* m_sampleCountLabel;
-
-	QLabel* m_activeVoiceCountLabel;
-
-
-	QLabel* m_emptyFileLabel;
 };
 
 } // namespace gui
