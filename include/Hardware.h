6fccf9562d3f9fa5b5b557c6ae0795476594ad5b
Fawn <rubiefawn@gmail.com>
2026-02-03 13:01:39 -0700

    Fixes for Hardware.h
    - #include <new> *before* testing macro defined within it (lol)
    - Make #ifdef chain for hardware_destructive_interference_size more readable
    - Use LMMS_HOST_* macros from lmmsconfig.h where possible
    - Move #includes for busy_wait_hint() to top of file
    - Make busy_wait_hint() a function rather than a macro
    - Minor formatting changes

--- include/Hardware.h
+++ include/Hardware.h
@@ -24,49 +24,57 @@
 #ifndef LMMS_HARDWARE_H
 #define LMMS_HARDWARE_H
 
-//! @brief Platform-dependent minimum amount of padding between objects to prevent false cache sharing.
-// TODO: Use std::hardware_destructive_interference_size directly once our compilers are updated enough (GCC 12.1+, Clang 19+, MSVC 2017 15.3)
-// TODO: Add other platforms as needed (LMMS currently only supports 64-bit x86 and ARM)
-#if __cpp_lib_hardware_interference_size >= 201703L
 #include <new>
-namespace lmms
-{
-inline constexpr std::size_t hardware_destructive_interference_size = std::hardware_destructive_interference_size;
-}
-#elif defined(__aarch64__) || defined(_M_ARM64) // 64-bit ARM
-namespace lmms
-{
-inline constexpr std::size_t hardware_destructive_interference_size = 256;
-}
-#else // x86_64
-namespace lmms
-{
-inline constexpr std::size_t hardware_destructive_interference_size = 64;
-}
-#endif
-
-
-#if defined(__x86_64__) || defined(_M_AMD64) || defined(__i386__) || defined(_M_IX86)
+#include "lmmsconfig.h"
+// Includes for busy_wait_hint()
+#if defined(LMMS_HOST_X86_64) || defined(LMMS_HOST_X86)
 	#include <immintrin.h>
-	#define busy_wait_hint() _mm_pause()
+// TODO: Use LMMS_HOST_ARM64 instead of __aarch64__ and _M_ARM64 once MSVC quits
+// using its own non-standard intrinsics headers (which may be never)
 #elif defined(__aarch64__) // arm64
-	// isb 15 used instead of yield on arm64
-	// See https://github.com/rust-lang/rust/commit/c064b6560b7ce0adeb9bbf5d7dcf12b1acb0c807
 	#if defined(__ARM_ACLE)
 		#include <arm_acle.h>
-		#define busy_wait_hint() __isb(15)
 	#elif defined(__GNUG__)
-		// GCC for ARM lacks these intrinsics at the moment.
-		// TODO: Remove inline asm once GCC properly provides __ARM_ACLE
-		#define busy_wait_hint() asm volatile ("isb 15" ::: "memory")
+		// TODO: Remove this once GCC properly provides __ARM_ACLE
+		inline void __isb(unsigned int scope) { static_assert(scope == 15); asm volatile ("isb 15" ::: "memory"); }
 	#endif
 #elif defined(_M_ARM64) // arm64 msvc
 	#include <intrin.h>
-	// See https://github.com/rust-lang/rust/commit/c064b6560b7ce0adeb9bbf5d7dcf12b1acb0c807
-	#define busy_wait_hint() __isb(15)
+#endif
+
+namespace lmms
+{
+
+//! @brief Platform-dependent minimum amount of padding between objects to prevent false cache sharing.
+// TODO: Use std::hardware_destructive_interference_size directly once our compilers are updated enough (GCC 12.1+, Clang 19+, MSVC 2017 15.3)
+// TODO: Add other platforms as needed (LMMS currently only supports 64-bit x86 and ARM)
+inline constexpr std::size_t hardware_destructive_interference_size =
+#if __cpp_lib_hardware_interference_size >= 201703L
+	std::hardware_destructive_interference_size
+#elif defined(LMMS_HOST_ARM64)
+	256
+#elif defined(LMMS_HOST_X86_64) || defined(LMMS_HOST_X86)
+	64
+#else
+	#warning Defaulting to 64 for lmms::hardware_destructive_interference_size for this architecture. This may be incorrect.
+	64
+#endif
+	;
+
+// TODO: Add docs now that this isn't a macro
+inline void busy_wait_hint()
+{
+#if defined(LMMS_HOST_X86_64) || defined(LMMS_HOST_X86)
+	_mm_pause()
+#elif defined(LMMS_HOST_ARM64)
+	__isb(15)
 #else
 	// TODO: Add other platforms as needed (LMMS currently only supports 64-bit x86 and ARM)
 	#error No busy-wait hint intrinsic available for this platform!
 #endif
+	;
+}
 
-#endif
+} // namespace lmms
+
+#endif // LMMS_HARDWARE_H
