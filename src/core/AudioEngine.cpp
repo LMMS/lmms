ae14ed22a3d35b48c4d34f6e8df5334aa89a84cc
Carlo Bramini <carlo_bramini@users.sourceforge.net>
2026-02-04 19:57:16 +0100

    CMAKE: allow to use WINMM on CYGWIN.

--- src/core/AudioEngine.cpp
+++ src/core/AudioEngine.cpp
@@ -761,7 +761,7 @@ bool AudioEngine::isMidiDevNameValid(QString name)
 	}
 #endif
 
-#ifdef LMMS_BUILD_WIN32
+#ifdef LMMS_HAVE_WINMM
 	if (name == MidiWinMM::name())
 	{
 		return true;
@@ -1002,7 +1002,7 @@ MidiClient * AudioEngine::tryMidiClients()
 	}
 #endif
 
-#ifdef LMMS_BUILD_WIN32
+#ifdef LMMS_HAVE_WINMM
 	if( client_name == MidiWinMM::name() || client_name == "" )
 	{
 		MidiWinMM * mwmm = new MidiWinMM;
