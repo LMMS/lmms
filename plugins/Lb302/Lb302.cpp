LMMS/lmms

commit 1b563eda28f38aa914484bf29f3fb6f970ddc9d1

Inline some functions only used in one place

plugins/Lb302/Lb302.cpp
@@ -339,8 +339,38 @@ void Lb302Synth::process(SampleFrame* outbuf, const fpp_t size)
 	if (release_frame.load(std::memory_order_relaxed) == 0 || !m_playingNote) { vca_mode = VcaMode::Decay; }
 	if (new_freq)
 	{
-		initNote(phaseInc(true_freq), deadToggle.value());
 		new_freq = false;
+		const bool noteIsDead = deadToggle.value();
+		// catch_decay = 0;
+		vco_inc = phaseInc(true_freq);
+
+		// Always reset vca on non-dead notes, and only reset vca on decaying (decayed) and never-played
+		if (!noteIsDead || (vca_mode == VcaMode::Decay || vca_mode == VcaMode::NeverPlayed))
+		{
+			sample_cnt = 0;
+			vca_mode = VcaMode::Attack;
+		}
+		else { vca_mode = VcaMode::Idle; }
+
+		if (vco_slideinc != 0.f)
+		{
+			// Initiate Slide
+			vco_slide = vco_inc - vco_slideinc; // Slide amount
+			vco_slidebase = vco_inc; // The REAL frequency
+		}
+		else { vco_slide = 0.f; }
+
+		// Slide-from note, save inc for next note
+		// May need to equal vco_slidebase + vco_slide if last note slid
+		if (slideToggle.value()) { vco_slideinc = vco_inc; }
+
+		recalcFilter();
+		if (!noteIsDead)
+		{
+			// Swap next two blocks??
+			vcf().playNote();
+			vcf_envpos = ENVINC; // Ensure envelope is recalculated
+		}
 	}
 
 	// TODO: NORMAL RELEASE
@@ -490,48 +520,6 @@ void Lb302Synth::process(SampleFrame* outbuf, const fpp_t size)
 				vca_mode = VcaMode::NeverPlayed;
 			}
 		}
-	}
-}
-
-
-/*  Prepares the active LB302 note.  I separated this into a function because it
- *  needs to be called onplayNote() when a new note is started.  It also needs
- *  to be called from process() when a prior edge-to-edge note is done releasing.
- */
-
-void Lb302Synth::initNote(float noteVcoInc, bool noteIsDead)
-{
-	// catch_decay = 0;
-	vco_inc = noteVcoInc;
-
-	// Always reset vca on non-dead notes, and only reset vca on decaying (decayed) and never-played
-	if (!noteIsDead || (vca_mode == VcaMode::Decay || vca_mode == VcaMode::NeverPlayed))
-	{
-		sample_cnt = 0;
-		vca_mode = VcaMode::Attack;
-		// LB303:
-		//vca_a = 0;
-	}
-	else { vca_mode = VcaMode::Idle; }
-
-	initSlide();
-
-	// Slide-from note, save inc for next note
-	// May need to equal vco_slidebase + vco_slide if last note slid
-	if (slideToggle.value()) { vco_slideinc = vco_inc; }
-
-	recalcFilter();
-
-	if (!noteIsDead)
-	{
-		// Swap next two blocks??
-		vcf().playNote();
-
-		vcf_envpos = ENVINC; // Ensure envelope is recalculated
-
-		// Double Check
-		//vca_mode = VcaMode::Attack;
-		//vca_a = 0.f;
 	}
 }
 

commit 0fc7d88910de25987f13b9e6c4843b3cafb1893e

Fix bug introduced in 1b563eda28f38aa914484bf29f3fb6f970ddc9d1
- Restored a line that resets the slide-from note when a slide is done,
  the accidental removal of which was the cause of a weird bug
- Actually remove initSlide() and initNote() which were supposed to be
  fully inlined

plugins/Lb302/Lb302.cpp
@@ -356,6 +356,7 @@ void Lb302Synth::process(SampleFrame* outbuf, const fpp_t size)
 			// Initiate Slide
 			vco_slide = vco_inc - vco_slideinc; // Slide amount
 			vco_slidebase = vco_inc; // The REAL frequency
+			vco_slideinc = 0.f; // reset from-note
 		}
 		else { vco_slide = 0.f; }
 
@@ -522,20 +523,6 @@ void Lb302Synth::process(SampleFrame* outbuf, const fpp_t size)
 	}
 }
 
-
-void Lb302Synth::initSlide()
-{
-	if (vco_slideinc == 0.f)
-	{
-		vco_slide = 0.f;
-		return;
-	}
-
-	// Initiate Slide
-	vco_slide = vco_inc - vco_slideinc; // Slide amount
-	vco_slidebase = vco_inc; // The REAL frequency
-	vco_slideinc = 0.f; // reset from-note
-}
 
 
 void Lb302Synth::playNote(NotePlayHandle* nph, SampleFrame*)

plugins/Lb302/Lb302.h
@@ -161,8 +161,6 @@ public slots:
 private:
 	void processNote(NotePlayHandle* nph);
 	void process(SampleFrame* outbuf, const fpp_t size);
-	void initNote(float noteVcoInc, bool noteIsDead);
-	void initSlide();
 	void recalcFilter();
 
 	enum class VcoShape { Sawtooth, Square, Triangle, Moog, RoundSquare, Sine, Exponential, WhiteNoise,