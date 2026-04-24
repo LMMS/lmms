bdfe3eea2d09ab8bcfe3cf40f8b1a48a29e77a32
Fawn <rubiefawn@gmail.com>
2026-04-24 17:23:24 -0600

    Add 32-bit x86 and ARM support to Hardware.h

--- include/Hardware.h
+++ include/Hardware.h
@@ -24,23 +24,24 @@
 #ifndef LMMS_HARDWARE_H
 #define LMMS_HARDWARE_H
 
+#include <cstdint>
 #include <new>
 #include "lmmsconfig.h"
-// Includes for busyWaitHint()
 #if defined(LMMS_HOST_X86_64) || defined(LMMS_HOST_X86)
 	#include <immintrin.h>
-#elif defined(LMMS_HOST_ARM64)
+#elif defined(LMMS_HOST_ARM64) || defined(LMMS_HOST_ARM32)
 	#if defined(__ARM_ACLE)
 		#include <arm_acle.h>
-	#elif defined(__GNUG__) // HACK: Remove this once GCC properly provides __ARM_ACLE
-		// HACK: Use the actual __isb() ACLE once GCC provides it
-		// 15 is the only allowed value for the parameter, so just ignore it and use 15 lol
-		inline void __isb(unsigned int scope = 15)
-		{
-			asm volatile ("isb 15" ::: "memory");
-		}
-	#elif defined(_M_ARM64) // arm64 msvc
+	#elif defined(_MSC_VER)
 		#include <intrin.h>
+	#elif defined(__GNUG__) // HACK: Remove this once GCC properly provides __ARM_ACLE
+		#if defined(LMMS_HOST_ARM64)
+			// https://developer.arm.com/documentation/ddi0602/2026-03/Base-Instructions/ISB--Instruction-synchronization-barrier-
+			// 15 is the only allowed value for the parameter, so just ignore it and use 15 lol
+			inline void __isb(unsigned int scope = 15) { asm volatile ("isb 15" ::: "memory"); }
+		#elif defined(LMMS_HOST_ARM32)
+			inline void __yield() { asm volatile ("yield"); }
+		#endif
 	#endif
 #endif
 
@@ -48,7 +49,6 @@ namespace lmms
 {
 
 //! @brief Platform-dependent minimum amount of padding between objects to prevent false cache sharing.
-// TODO: Use std::hardware_destructive_interference_size directly once our compilers are updated enough (GCC 12.1+, Clang 19+, MSVC 2017 15.3)
 // TODO: Add other platforms as needed (LMMS currently only supports 64-bit x86 and ARM)
 inline constexpr std::size_t hardware_destructive_interference_size =
 #if __cpp_lib_hardware_interference_size >= 201703L
@@ -67,50 +67,69 @@ inline constexpr std::size_t hardware_destructive_interference_size =
 
 //! @brief Platform-dependent hint to the processor that it is in a busy-wait loop.
 //! This helps optimize spinlocks by slowing down the processor a bit, which helps reduce contention on atomics.
+// TODO: Add other platforms as needed (LMMS_HOST_*)
 inline void busyWaitHint()
 {
 #if defined(LMMS_HOST_X86_64) || defined(LMMS_HOST_X86)
 	_mm_pause()
 #elif defined(LMMS_HOST_ARM64)
 	__isb(15)
+#elif defined(LMMS_HOST_ARM32)
+	__yield()
 #else
-	// TODO: Add other platforms as needed (LMMS currently only supports 64-bit x86 and ARM)
-	#error No busy-wait hint intrinsic available for this platform!
+	#warning lmms::busyWaitHint() is not implemented on this architecture and will have no effect. Performance may suffer.
+	(void)
 #endif
 	;
 }
 
 
 
-// Set denormal protection for this thread.
+//! @brief Disable IEEE 754 denormals on the current thread.
+//!
+//! This is desirable for audio calculations as processing denormal
+//! values is significantly slower than normal values, and in the
+//! context of audio signal processing, denormal values are typically
+//! near silence and contain no useful information anyways.
 inline void disableDenormals()
 {
 #if defined(LMMS_HOST_X86_64) || defined(LMMS_HOST_X86)
-	_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON); // FTZ flag
-	// Setting DAZ might freeze systems not supporting it:
-	// Intel® 64 and IA-32 Architectures Software Developer’s Manual,
-	// Volume 1: Basic Architecture,
+	// https://www.intel.com/content/www/us/en/developer/articles/technical/intel-sdm.html
+	// Intel® 64 and IA-32 Architectures Software Developer's Manual Volume 1: Basic Architecture
 	// 11.6.3 Checking for the DAZ Flag in the MXCSR Register
-	alignas(16) unsigned char buffer[512] = {0};
-	#if defined(LMMS_HOST_X86)
-		_fxsave(buffer);
-	#elif defined(LMMS_HOST_X86_64)
-		_fxsave64(buffer);
+	unsigned int flags = 0x8000; // FTZ
+	std::uint8_t buf[512] = {0};
+	#if defined(LMMS_HOST_X86_64)
+		_fxsave64(buf);
+	#elif defined(LMMS_HOST_X86)
+		_fxsave(buf);
 	#endif
-	// Bit 6 of the MXCSR_MASK, i.e. in the lowest byte, tells if we
-	// can use the DAZ flag.
-	if (buffer[28] & (1 << 6))
-	{
-		_MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
-	}
-#elif defined(LMMS_HOST_ARM64)
-	// https://developer.arm.com/documentation/ddi0601/2025-12/AArch64-Registers/FPCR--Floating-point-Control-Register
-	constexpr std::intptr_t fz = 1 << 24; // Flushing denormalized numbers to zero control bit
-	std::intptr_t fpcr;
-	asm volatile ("mrs %0, fpcr" : "=r" (fpcr));
-	asm volatile ("msr fpcr, %0" :: "ri" (fpcr | fz));
+	flags |= buf[28] & 0x0040; // DAZ if supported
+	_mm_setcsr(_mm_getcsr() | flags);
+#elif defined(LMMS_HOST_ARM64) || defined(LMMS_HOST_ARM32)
+	constexpr auto FZ = 1 << 24; // Flushing denormalized numbers to zero control bit
+	#if defined(_MSC_VER) && defined(LMMS_HOST_ARM64)
+		// https://learn.microsoft.com/en-us/cpp/intrinsics/arm64-intrinsics
+		// 0x5a20 == ARM64_SYSREG(0b11, 0b011, 0b0100, 0b0100, 0b000)
+		_WriteStatusReg(0x5a20, _ReadStatusReg(0x5a20) | FZ);
+	#elif defined(_MSC_VER) && defined(LMMS_HOST_ARM32)
+		// MSVC for ARM32 is discontinued, so no point in implementing
+		// this. Attempting to build using MSVC for ARM32 anyways will
+		// result in fatal error C1189. Thanks Microsoft!
+		#warning lmms::disableDenormals() is not implemented on this compiler and will have no effect. Performance may suffer.
+	#elif defined(LMMS_HOST_ARM64)
+		// https://developer.arm.com/documentation/ddi0601/2026-03/AArch64-Registers/FPCR--Floating-point-Control-Register
+		std::uint64_t fpcr;
+		asm volatile ("mrs %0, fpcr" : "=r" (fpcr));
+		asm volatile ("msr fpcr, %0" :: "ri" (fpcr | FZ));
+	#elif defined(LMMS_HOST_ARM32)
+		// https://developer.arm.com/documentation/ddi0601/2026-03/AArch32-Registers/FPSCR--Floating-Point-Status-and-Control-Register
+		std::uint32_t fpscr;
+		asm volatile ("vmrs %0, fpscr" : "=r" (fpscr));
+		asm volatile ("vmsr fpscr, %0" :: "ri" (fpscr | FZ));
+	#endif
 #else
-	#warning Cannot disable floating-point denormals or enable flush-to-zero mode on this platform! Performance may suffer.
+	#warning lmms::disableDenormals() is not implemented on this architecture and will have no effect. Performance may suffer.
 #endif
 }
 
