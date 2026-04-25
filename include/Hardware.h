fd06d0e7bdcf4fdee32c9c078013ad00e3c44e03
Fawn <rubiefawn@gmail.com>
2026-04-24 21:03:22 -0600

    Add RISC-V support to lmms::busyWaitHint()
    Technically it's an extension ("Zihintpause"), but it is encoded as a
    FENCE instruction so it should be ineffective but harmless on
    implementations without the extension

--- include/Hardware.h
+++ include/Hardware.h
@@ -67,20 +67,20 @@ inline constexpr std::size_t hardware_destructive_interference_size =
 
 //! @brief Platform-dependent hint to the processor that it is in a busy-wait loop.
 //! This helps optimize spinlocks by slowing down the processor a bit, which helps reduce contention on atomics.
-// TODO: Add other platforms as needed (LMMS_HOST_*)
 inline void busyWaitHint()
 {
 #if defined(LMMS_HOST_X86_64) || defined(LMMS_HOST_X86)
-	_mm_pause()
+	_mm_pause();
 #elif defined(LMMS_HOST_ARM64)
-	__isb(15)
+	__isb(15);
 #elif defined(LMMS_HOST_ARM32)
-	__yield()
+	__yield();
+#elif defined(LMMS_HOST_RISCV64) || defined(LMMS_HOST_RISCV32)
+	asm volatile ("pause");
 #else
+	// TODO LMMS_HOST_PPC*
 	#warning lmms::busyWaitHint() is not implemented on this architecture and will have no effect. Performance may suffer.
-	(void)
 #endif
-	;
 }
 
 
@@ -113,10 +113,9 @@ inline void disableDenormals()
 		// 0x5a20 == ARM64_SYSREG(0b11, 0b011, 0b0100, 0b0100, 0b000)
 		_WriteStatusReg(0x5a20, _ReadStatusReg(0x5a20) | FZ);
 	#elif defined(_MSC_VER) && defined(LMMS_HOST_ARM32)
-		// MSVC for ARM32 is discontinued, so no point in implementing
-		// this. Attempting to build using MSVC for ARM32 anyways will
-		// result in fatal error C1189. Thanks Microsoft!
-		#warning lmms::disableDenormals() is not implemented on this compiler and will have no effect. Performance may suffer.
+		// https://learn.microsoft.com/en-us/windows/arm/arm32-to-arm64
+		// https://learn.microsoft.com/en-us/windows/whats-new/deprecated-features
+		#warning MSVC for ARM32 is deprecated. lmms::disableDenormals() is not implemented and will have no effect.
 	#elif defined(LMMS_HOST_ARM64)
 		// https://developer.arm.com/documentation/ddi0601/2026-03/AArch64-Registers/FPCR--Floating-point-Control-Register
 		std::uint64_t fpcr;
@@ -129,6 +128,13 @@ inline void disableDenormals()
 		asm volatile ("vmsr fpscr, %0" :: "ri" (fpscr | FZ));
 	#endif
 #else
+	// TODO LMMS_HOST_RISCV*: https://docs.riscv.org/reference/isa/unpriv/f-st-ext.html
+	// As of v2.2 of both the F and D extensions, there appears to be no
+	// way to flush denormals to zero. This may change in the future.
+	// TODO LMMS_HOST_PPC*: https://openpowerfoundation.org/specifications/isa
+	// This is possible for SIMD instructions by setting bit 111 of the
+	// VSCR register (a.k.a. the "NJ" flag), but there doesn't appear to
+	// be an equivalent for scalar instructions?
 	#warning lmms::disableDenormals() is not implemented on this architecture and will have no effect. Performance may suffer.
 #endif
 }
