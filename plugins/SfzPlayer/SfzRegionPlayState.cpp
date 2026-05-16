97ab20c92cb35bcfaa10d514e4f70c7b11863bc4
regulus79 <117475203+regulus79@users.noreply.github.com>
2026-05-16 15:45:01 -0400

    Fix style and remove old profiling code

--- plugins/SfzPlayer/SfzRegionPlayState.cpp
+++ plugins/SfzPlayer/SfzRegionPlayState.cpp
@@ -211,15 +211,6 @@ bool SfzRegionPlayState::play(SampleFrame* buffer, const f_cnt_t frames)
 	// If the initial m_frameCount is negative, that means the note hasn't started yet
 	if (m_frameCount < -static_cast<int>(frames)) { m_frameCount += frames; return false; } // If the note doesn't start in this buffer, don't play anything
 
-	/*
-	static int totalMicroseconds = 0;
-	static int totalSquaredMicroseconds = 0;
-	static int totalCalls = 0;
-	static int minElapsed = 10000000;
-	static int maxElapsed = 0;
-	MicroTimer profiler;
-	*/
-
 	// Helper variable
 	const float normalizedVelocity = m_trigger.velocity().value() / 127.0f;
 
@@ -332,10 +323,6 @@ bool SfzRegionPlayState::play(SampleFrame* buffer, const f_cnt_t frames)
 	{
 		m_active = false; // TODO should this forcefully decative or just release?
 	}
-
-	//int elapsed = profiler.elapsed(); totalMicroseconds += elapsed; totalSquaredMicroseconds += elapsed * elapsed; totalCalls++; minElapsed = std::min(minElapsed, elapsed); maxElapsed = std::max(maxElapsed, elapsed);
-	//float mean = 1.0f * totalMicroseconds / totalCalls, variance = (1.0f * totalSquaredMicroseconds - 1.0f * totalMicroseconds * totalMicroseconds / totalCalls / totalCalls) / totalCalls;
-	//qDebug() << "SfzRegionPlayState::play profiler:" << elapsed << "Min:" << minElapsed << "Max:" << maxElapsed << "Total calls" << totalCalls << "Mean:" << mean << "Stdev:" << std::sqrt(variance) << "Stdev of mean:" << sqrt(variance / totalCalls);
 
 	return true;
 }
