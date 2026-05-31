0b2b9e3a1a20a0338ec0a9463a223e312ea6c44a
Sotonye Atemie <sakertooth@gmail.com>
2026-05-31 11:04:49 -0400

    Undo changes in Mixer.h

--- include/Mixer.h
+++ include/Mixer.h
@@ -25,15 +25,15 @@
 #ifndef LMMS_MIXER_H
 #define LMMS_MIXER_H
 
-#include <QColor>
-#include <atomic>
-#include <optional>
-
 #include "AudioBuffer.h"
 #include "EffectChain.h"
 #include "JournallingObject.h"
 #include "Model.h"
 #include "ThreadableJob.h"
+
+#include <atomic>
+#include <optional>
+#include <QColor>
 
 namespace lmms
 {
