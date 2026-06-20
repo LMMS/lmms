c60e855c9344cb3b96adca8229c5fb9a97f53672
Sotonye Atemie <sakertooth@gmail.com>
2026-06-19 23:07:34 -0400

    Remove use of whats this, use tooltip instead, make description more succinct

--- src/gui/modals/SetupDialog.cpp
+++ src/gui/modals/SetupDialog.cpp
@@ -659,11 +659,8 @@ SetupDialog::SetupDialog(ConfigTab tab_to_open) :
 
 	const auto enableMixSanitizationCheckbox = addCheckBox(tr("Enable mix sanitization"), otherBox, otherBoxLayout,
 		m_mixSanitization, SLOT(toggleMixSanitization(bool)), false);
-	enableMixSanitizationCheckbox->setWhatsThis(
-		tr("Automatically detects and silences audio channels containing corrupted audio (i.e., NaNs and Infinities) "
-		   "caused by unstable plugins or tracks. Turning this on can prevent sudden loud bursts or other strange "
-		   "behavior, but may increase CPU usage. When corrupted audio is detected from an effect or track, the "
-		   "bypass button or activity indicator will flash red."));
+	enableMixSanitizationCheckbox->setToolTip(tr("Provides protection from any plugins or tracks that generate "
+												 "corrupted audio, but may negatively impact performance."));
 
 	// Audio layout ordering.
 	audio_layout->addWidget(audioInterfaceBox);
