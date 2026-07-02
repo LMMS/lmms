f4d5957b2a6bfe76817c7b3a0baab1bbf67ab017
regulus79 <117475203+regulus79@users.noreply.github.com>
2026-07-02 07:55:42 -0400

    Fix sensitivity of tangent editing in Automation Editor (#7946)
    Previously, the sensitivity of the tangent editing in the automation editor was hardcoded so that it would change the internal value by the number of pixels the mouse moved up (with a bit of scaling based on the x position too). This is not correct, since it does not respect the zoom of the editor or the min/max values relative to the window size.
    
    This PR adds the correct scaling constant to the tanget value to convert pixels to levels. The automation editor already had the right constant as a member variable, m_y_delta, but it would only update when using the hardcoded zoom levels, so I reworked it to also work for auto zoom.
    
    ---------
    
    Co-authored-by: Dalton Messmer <messmer.dalton@gmail.com>
    Co-authored-by: Fawn <rubiefawn@gmail.com>

--- src/gui/editors/AutomationEditor.cpp
+++ src/gui/editors/AutomationEditor.cpp
@@ -877,8 +877,8 @@ void AutomationEditor::mouseMoveEvent(QMouseEvent * mouseEvent )
 						? yCoordOfLevel(OUTVAL(it))
 						: yCoordOfLevel(INVAL(it));
 					float dy = m_draggedOutTangent
-						? y - pos.y()
-						: pos.y() - y;
+						? (y - pos.y()) / m_y_delta
+						: (pos.y() - y) / m_y_delta;
 					float dx = std::abs(posTicks - POS(it));
 					float newTangent = dy / std::max(dx, 1.0f);
 
@@ -1078,7 +1078,7 @@ void AutomationEditor::paintEvent(QPaintEvent * pe )
 	else
 	{
 		int level = (int)m_bottomLevel;
-		int printable = qMax(1, 5 * DEFAULT_Y_DELTA / m_y_delta);
+		auto printable = static_cast<int>(std::max(1.0f, 5 * DEFAULT_Y_DELTA / m_y_delta));
 		int module = level % printable;
 		if (module)
 		{
@@ -1531,6 +1531,7 @@ void AutomationEditor::resizeEvent(QResizeEvent * re)
 	m_timeLine->setFixedWidth(width());
 
 	updateTopBottomLevels();
+	updateYDelta();
 	update();
 }
 
@@ -1822,6 +1823,12 @@ void AutomationEditor::zoomingXChanged()
 
 void AutomationEditor::zoomingYChanged()
 {
+	updateYDelta();
+	resizeEvent(nullptr);
+}
+
+void AutomationEditor::updateYDelta()
+{
 	const QString & zfac = m_zoomingYModel.currentText();
 	m_y_auto = zfac == "Auto";
 	if( !m_y_auto )
@@ -1829,10 +1836,11 @@ void AutomationEditor::zoomingYChanged()
 		m_y_delta = zfac.left( zfac.length() - 1 ).toInt()
 							* DEFAULT_Y_DELTA / 100;
 	}
-#ifdef LMMS_DEBUG
-	assert( m_y_delta > 0 );
-#endif
-	resizeEvent(nullptr);
+	else
+	{
+		int gridBottom = height() - SCROLLBAR_SIZE - 1;
+		m_y_delta = static_cast<float>(gridBottom - TOP_MARGIN) / (m_maxLevel - m_minLevel);
+	}
 }
 
 
@@ -1866,22 +1874,22 @@ void AutomationEditor::updateTopBottomLevels()
 		int centralLevel = (int)( m_minLevel + m_maxLevel - m_scrollLevel );
 
 		m_bottomLevel = centralLevel - ( half_grid
-							/ (float)m_y_delta );
+							/ m_y_delta );
 		if( m_bottomLevel < m_minLevel )
 		{
 			m_bottomLevel = m_minLevel;
 			m_topLevel = m_minLevel + (int)floorf( grid_height
-							/ (float)m_y_delta );
+							/ m_y_delta );
 		}
 		else
 		{
 			m_topLevel = m_bottomLevel + (int)floorf( grid_height
-							/ (float)m_y_delta );
+							/ m_y_delta );
 			if( m_topLevel > m_maxLevel )
 			{
 				m_topLevel = m_maxLevel;
 				m_bottomLevel = m_maxLevel - (int)floorf(
-					grid_height / (float)m_y_delta );
+					grid_height / m_y_delta );
 			}
 		}
 	}
