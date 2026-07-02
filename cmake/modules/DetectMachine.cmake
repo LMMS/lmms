LMMS/lmms

commit 6f50f90b9cc424df0ebdcae6fe53e894033ad24f

Remove the FIFO thread (#7568)
Removes the FIFO writer thread in the audio engine to simplify inter-thread communication and avoid deadlock bugs.

---------

Co-authored-by: szeli1 <143485814+szeli1@users.noreply.github.com>
Co-authored-by: Dalton Messmer <messmer.dalton@gmail.com>

src/core/audio/AudioSndio.cpp
@@ -41,12 +41,10 @@
 namespace lmms
 {
 
-AudioSndio::AudioSndio(bool & _success_ful, AudioEngine * _audioEngine) :
-	AudioDevice(std::clamp<ch_cnt_t>(
-		ConfigManager::inst()->value("audiosndio", "channels").toInt(),
-		DEFAULT_CHANNELS,
-		DEFAULT_CHANNELS), _audioEngine),
-	m_convertEndian ( false )
+AudioSndio::AudioSndio(bool& _success_ful, AudioEngine* _audioEngine)
+	: AudioDevice(std::clamp<ch_cnt_t>(ConfigManager::inst()->value("audiosndio", "channels").toInt(), DEFAULT_CHANNELS,
+					  DEFAULT_CHANNELS),
+		  _audioEngine)
 {
 	_success_ful = false;
 
@@ -73,13 +71,8 @@ AudioSndio::AudioSndio(bool & _success_ful, AudioEngine * _audioEngine) :
 	m_par.bits = 16;
 	m_par.le = SIO_LE_NATIVE;
 	m_par.rate = sampleRate();
-	m_par.round = audioEngine()->framesPerPeriod();
+	m_par.round = audioEngine()->framesPerAudioBuffer();
 	m_par.appbufsz = m_par.round * 2;
-
-	if ( (isLittleEndian() && (m_par.le == 0)) ||
-	     (!isLittleEndian() && (m_par.le == 1))) {
-		m_convertEndian = true;
-	}
 
 	struct sio_par reqpar = m_par;
 
@@ -115,7 +108,6 @@ AudioSndio::AudioSndio(bool & _success_ful, AudioEngine * _audioEngine) :
 
 AudioSndio::~AudioSndio()
 {
-	stopProcessing();
 	if (m_hdl != nullptr)
 	{
 		sio_close( m_hdl );
@@ -124,42 +116,27 @@ AudioSndio::~AudioSndio()
 }
 
 
-void AudioSndio::startProcessing()
+void AudioSndio::startProcessingImpl()
 {
-	if( !isRunning() )
-	{
-		start( QThread::HighPriority );
-	}
+	start(QThread::HighPriority);
 }
 
 
-void AudioSndio::stopProcessing()
+void AudioSndio::stopProcessingImpl()
 {
 	stopProcessingThread( this );
 }
 
 void AudioSndio::run()
 {
-	SampleFrame* temp = new SampleFrame[audioEngine()->framesPerPeriod()];
-	int_sample_t * outbuf = new int_sample_t[audioEngine()->framesPerPeriod() * channels()];
+	const auto framesPerAudioBuffer = audioEngine()->framesPerAudioBuffer();
+	auto buf = std::vector<float>(framesPerAudioBuffer * channels());
 
-	while( true )
+	while (AudioDevice::isRunning())
 	{
-		const f_cnt_t frames = getNextBuffer( temp );
-		if( !frames )
-		{
-			break;
-		}
-
-		uint bytes = convertToS16(temp, frames, outbuf, m_convertEndian);
-		if( sio_write( m_hdl, outbuf, bytes ) != bytes )
-		{
-			break;
-		}
+		audioEngine()->renderNextBuffer({buf.data(), channels(), framesPerAudioBuffer});
+		sio_write(m_hdl, buf.data(), buf.size() * sizeof(float));
 	}
-
-	delete[] temp;
-	delete[] outbuf;
 }
 
 

include/AudioAlsa.h
@@ -82,8 +82,8 @@ public:
 	static DeviceInfoCollection getAvailableDevices();
 
 private:
-	void startProcessing() override;
-	void stopProcessing() override;
+	void startProcessingImpl() override;
+	void stopProcessingImpl() override;
 	void run() override;
 
 	int setHWParams( const ch_cnt_t _channels, snd_pcm_access_t _access );
@@ -98,9 +98,6 @@ private:
 
 	snd_pcm_hw_params_t * m_hwParams;
 	snd_pcm_sw_params_t * m_swParams;
-
-	bool m_convertEndian;
-
 } ;
 
 } // namespace lmms

include/AudioBufferView.h
@@ -348,6 +348,12 @@ public:
 		return std::span<T>{this->m_data, this->m_frames * Base::channels()};
 	}
 
+	//! @return the sample at the given channel and frame indicies
+	constexpr auto sample(ch_cnt_t channel, f_cnt_t frame) const noexcept -> T&
+	{
+		return framePtr(frame)[channel];
+	}
+
 	//! @return the frame at the given index
 	constexpr auto frame(f_cnt_t index) const noexcept
 	{
@@ -503,6 +509,12 @@ public:
 	constexpr auto empty() const noexcept -> bool
 	{
 		return !this->m_data || Base::channels() == 0 || this->m_frames == 0;
+	}
+
+	//! @return the sample at the given channel and frame indicies
+	constexpr auto sample(ch_cnt_t channel, f_cnt_t frame) const noexcept -> T&
+	{
+		return bufferPtr(channel)[frame];
 	}
 
 	//! @return the buffer of the given channel

include/AudioDevice.h
@@ -46,17 +46,6 @@ public:
 	AudioDevice( const ch_cnt_t _channels, AudioEngine* audioEngine );
 	virtual ~AudioDevice();
 
-	inline void lock()
-	{
-		m_devMutex.lock();
-	}
-
-	inline void unlock()
-	{
-		m_devMutex.unlock();
-	}
-
-
 	// if audio-driver supports ports, classes inheriting AudioBusHandle
 	// (e.g. channel-tracks) can register themselves for making
 	// audio-driver able to collect their individual output and provide
@@ -75,71 +64,40 @@ public:
 		return m_sampleRate;
 	}
 
-	void processNextBuffer();
+	void startProcessing();
 
-	virtual void startProcessing()
-	{
-		m_inProcess = true;
-	}
+	void stopProcessing();
 
-	virtual void stopProcessing();
+	bool isRunning() const { return m_running.test(std::memory_order_acquire); }
 
 protected:
-	// subclasses can re-implement this for being used in conjunction with
-	// processNextBuffer()
-	virtual void writeBuffer(const SampleFrame* /* _buf*/, const f_cnt_t /*_frames*/) {}
-
-	// called by according driver for fetching new sound-data
-	f_cnt_t getNextBuffer(SampleFrame* _ab);
-
 	// convert a given audio-buffer to a buffer in signed 16-bit samples
 	// returns num of bytes in outbuf
-	int convertToS16(const SampleFrame* _ab,
-						const f_cnt_t _frames,
-						int_sample_t * _output_buffer,
-						const bool _convert_endian = false );
+	int convertToS16(const SampleFrame* _ab, const f_cnt_t _frames, int_sample_t* _output_buffer,
+		const bool _convert_endian = false);
 
 	// clear given signed-int-16-buffer
-	void clearS16Buffer( int_sample_t * _outbuf,
-							const f_cnt_t _frames );
+	void clearS16Buffer(int_sample_t* _outbuf, const f_cnt_t _frames);
 
-	ch_cnt_t channels() const
-	{
-		return m_channels;
-	}
+	ch_cnt_t channels() const { return m_channels; }
 
-	inline void setSampleRate( const sample_rate_t _new_sr )
-	{
-		m_sampleRate = _new_sr;
-	}
+	AudioEngine* audioEngine() { return m_audioEngine; }
 
-	void setChannels(const ch_cnt_t channels)
-	{
-		m_channels = channels;
-	}
-
-	AudioEngine* audioEngine()
-	{
-		return m_audioEngine;
-	}
+	void setSampleRate(const sample_rate_t _new_sr) { m_sampleRate = _new_sr; }
+	void setChannels(const ch_cnt_t channels) { m_channels = channels; }
 
 	static void stopProcessingThread( QThread * thread );
-
-
 protected:
 	bool m_supportsCapture;
 
-
 private:
+	virtual void startProcessingImpl() = 0;
+	virtual void stopProcessingImpl() = 0;
+
 	sample_rate_t m_sampleRate;
 	ch_cnt_t m_channels;
-	AudioEngine* m_audioEngine;
-	bool m_inProcess;
-
-	QMutex m_devMutex;
-
-	SampleFrame* m_buffer;
-
+	AudioEngine* m_audioEngine = nullptr;
+	std::atomic_flag m_running = ATOMIC_FLAG_INIT;
 };
 
 } // namespace lmms

include/AudioDummy.h
@@ -78,12 +78,12 @@ public:
 
 
 private:
-	void startProcessing() override
+	void startProcessingImpl() override
 	{
 		start();
 	}
 
-	void stopProcessing() override
+	void stopProcessingImpl() override
 	{
 		stopProcessingThread( this );
 	}
@@ -91,18 +91,10 @@ private:
 	void run() override
 	{
 		MicroTimer timer;
-		while( true )
+		while (AudioDevice::isRunning())
 		{
 			timer.reset();
-			const SampleFrame* b = audioEngine()->nextBuffer();
-			if( !b )
-			{
-				break;
-			}
-			if( audioEngine()->hasFifoWriter() )
-			{
-				delete[] b;
-			}
+			audioEngine()->renderNextPeriod();
 
 			const int microseconds = static_cast<int>( audioEngine()->framesPerPeriod() * 1000000.0f / audioEngine()->outputSampleRate() - timer.elapsed() );
 			if( microseconds > 0 )

include/AudioEngine.h
@@ -33,11 +33,11 @@
 #include <memory>
 #include <vector>
 
+#include "AudioBufferView.h"
 #include "AudioDevice.h"
 #include "LmmsTypes.h"
 #include "SampleFrame.h"
 #include "LocklessList.h"
-#include "FifoBuffer.h"
 #include "AudioEngineProfiler.h"
 #include "PlayHandle.h"
 
@@ -127,7 +127,7 @@ public:
 
 	//! Set new audio device. Old device will be deleted,
 	//! unless it's stored using storeAudioDevice
-	void setAudioDevice(AudioDevice* _dev, bool _needs_fifo, bool startNow);
+	void setAudioDevice(AudioDevice* _dev, bool startNow);
 	void storeAudioDevice();
 	void restoreAudioDevice();
 	inline AudioDevice * audioDev()
@@ -170,14 +170,6 @@ public:
 	}
 
 	void removePlayHandlesOfTypes(Track * track, PlayHandle::Types types);
-
-
-	// methods providing information for other classes
-	inline f_cnt_t framesPerPeriod() const
-	{
-		return m_framesPerPeriod;
-	}
-
 
 	AudioEngineProfiler& profiler()
 	{
@@ -236,11 +228,6 @@ public:
 
 	bool criticalXRuns() const;
 
-	inline bool hasFifoWriter() const
-	{
-		return m_fifoWriter != nullptr;
-	}
-
 	void pushInputFrames( SampleFrame* _ab, const f_cnt_t _frames );
 
 	inline const SampleFrame* inputBuffer()
@@ -253,10 +240,54 @@ public:
 		return m_inputBufferFrames[ m_inputBufferRead ];
 	}
 
-	inline const SampleFrame* nextBuffer()
-	{
-		return hasFifoWriter() ? m_fifo->read() : renderNextBuffer();
-	}
+	/**
+	 * @returns The internal buffer size used by audio plugins and other processing done within the audio engine.
+	 * Its value is @ref DEFAULT_BUFFER_SIZE or @ref framesPerAudioBuffer(), whichever is lower.
+	 * @see renderNextPeriod()
+	 */
+	f_cnt_t framesPerPeriod() const { return m_framesPerPeriod; }
+
+	/**
+	 * @returns The buffer size used by the configured @ref AudioDevice "output audio device". It is in the range
+	 * of @ref MINIMUM_BUFFER_SIZE and @ref MAXIMUM_BUFFER_SIZE.
+	 * @note This should not be needed for a majority of cases. Currently, it's only being used to set up the
+	 * audio devices. This member function may be removed in a later refactor.
+	 */
+	f_cnt_t framesPerAudioBuffer() const { return m_framesPerAudioBuffer; }
+
+	/**
+	 * @brief Renders the next audio period.
+	 *
+	 * An audio period is a fixed-size chunk of audio the engine generates, and represents a single cycle of the
+	 * engine's output. The rendering is chunked into smaller periods to timely handle per-buffer updates like
+	 * non-sample-accurate automation, as well as to improve memory cache performance.
+	 *
+	 * The audio period generated is interleaved and stereo.
+	 *
+	 * @note The audio period returned is non-owning and will be changed on subsequent calls to @ref renderNextPeriod()
+	 * and @ref renderNextBuffer(). Callers must copy the data into their own local buffers if they need it to persist.
+	 *
+	 * @returns A non-owning buffer to the next audio period.
+	 */
+	std::span<const SampleFrame> renderNextPeriod();
+
+	/**
+	 * @brief Renders an audio buffer into @a dst.
+	 *
+	 * Renders @ref renderNextAudioPeriod() "audio periods" into @a dst. If @a dst is not a multiple of the period
+	 * size, the remaining frames are partially rendered (an extra period may be rendered in such cases, which can
+	 * degrade performance).
+	 *
+	 * If @a dst has 1 channel, the channels are averaged to mono.
+	 * If @a dst has 2 channels, the channels are directly copied.
+	 * If @a dst has more than 2 channels, the stereo channels are copied and the rest are zero-filled.
+	 *
+	 * @param dst An audio buffer view to write into. Both interleaved and planar overloads are provided.
+	 */
+	void renderNextBuffer(InterleavedBufferView<float> dst) { renderNextBuffer<InterleavedBufferView<float>>(dst); }
+
+	//! @copydoc renderNextBuffer(InterleavedBufferView<float>)
+	void renderNextBuffer(PlanarBufferView<float> dst) { renderNextBuffer<PlanarBufferView<float>>(dst); }
 
 	//! Block until a change in model can be done (i.e. wait for audio thread)
 	void requestChangeInModel();
@@ -278,32 +309,44 @@ signals:
 
 
 private:
-	using Fifo = FifoBuffer<SampleFrame*>;
-
-	class fifoWriter : public QThread
+	void renderNextBuffer(AudioBufferView<float> auto dst)
 	{
-	public:
-		fifoWriter( AudioEngine * audioEngine, Fifo * fifo );
+		for (auto frame = f_cnt_t{0}; frame < dst.frames(); ++frame)
+		{
+			if (m_outputBufferReadIndex == m_framesPerPeriod) { m_outputBufferReadIndex = 0; }
+			if (m_outputBufferReadIndex == 0) { renderNextPeriod(); }
 
-		void finish();
+			switch (dst.channels())
+			{
+			case 0:
+				assert(false);
+				break;
+			case 1:
+				dst.sample(0, frame) = m_outputBufferRead[m_outputBufferReadIndex].average();
+				break;
+			case 2:
+				dst.sample(0, frame) = m_outputBufferRead[m_outputBufferReadIndex][0];
+				dst.sample(1, frame) = m_outputBufferRead[m_outputBufferReadIndex][1];
+				break;
+			default:
+				dst.sample(0, frame) = m_outputBufferRead[m_outputBufferReadIndex][0];
+				dst.sample(1, frame) = m_outputBufferRead[m_outputBufferReadIndex][1];
+				for (auto channel = 2; channel < dst.channels(); ++channel)
+				{
+					dst.sample(channel, frame) = 0.f;
+				}
+				break;
+			}
 
-
-	private:
-		AudioEngine * m_audioEngine;
-		Fifo * m_fifo;
-		volatile bool m_writing;
-
-		void run() override;
-
-		void write(SampleFrame* buffer);
-	} ;
-
+			++m_outputBufferReadIndex;
+		}
+	}
 
 	AudioEngine( bool renderOnly );
 	~AudioEngine() override;
 
-	void startProcessing(bool needsFifo = true);
-	void stopProcessing();
+	void startProcessing() { m_audioDev->startProcessing(); }
+	void stopProcessing() { m_audioDev->stopProcessing(); }
 
 
 	AudioDevice * tryAudioDevices();
@@ -314,7 +357,6 @@ private:
 	void renderStageEffects();
 	void renderStageMix();
 
-	const SampleFrame* renderNextBuffer();
 
 	void swapBuffers();
 
@@ -324,17 +366,19 @@ private:
 
 	std::vector<AudioBusHandle*> m_audioBusHandles;
 
+	f_cnt_t m_framesPerAudioBuffer;
 	f_cnt_t m_framesPerPeriod;
+	sample_rate_t m_baseSampleRate;
 
 	SampleFrame* m_inputBuffer[2];
 	f_cnt_t m_inputBufferFrames[2];
 	f_cnt_t m_inputBufferSize[2];
-	sample_rate_t m_baseSampleRate;
 	int m_inputBufferRead;
 	int m_inputBufferWrite;
 
 	std::unique_ptr<SampleFrame[]> m_outputBufferRead;
 	std::unique_ptr<SampleFrame[]> m_outputBufferWrite;
+	f_cnt_t m_outputBufferReadIndex;
 
 	// worker thread stuff
 	std::vector<AudioEngineWorkerThread *> m_workers;
@@ -358,10 +402,6 @@ private:
 	// MIDI device stuff
 	MidiClient * m_midiClient;
 	QString m_midiClientName;
-
-	// FIFO stuff
-	Fifo * m_fifo;
-	fifoWriter * m_fifoWriter;
 
 	AudioEngineProfiler m_profiler;
 

include/AudioFileDevice.h
@@ -49,6 +49,8 @@ public:
 
 	OutputSettings const & getOutputSettings() const { return m_outputSettings; }
 
+	//! Write `size` sample frames from `buf` into the output file.
+	virtual void writeBuffer(const SampleFrame* buf, const f_cnt_t frames) = 0;
 
 protected:
 	int writeData( const void* data, int len );
@@ -64,6 +66,9 @@ protected:
 	}
 
 private:
+	void startProcessingImpl() override {}
+	void stopProcessingImpl() override {}
+
 	QFile m_outputFile;
 	OutputSettings m_outputSettings;
 } ;

include/AudioJack.h
@@ -103,12 +103,12 @@ private:
 	bool initJackClient();
 	void resizeInputBuffer(jack_nframes_t nframes);
 
+	void startProcessingImpl() override;
+	void stopProcessingImpl() override;
+
 	void attemptToConnect(size_t index, const char *lmms_port_type, const char *source_port, const char *destination_port);
 	void attemptToReconnectOutput(size_t outputIndex, const QString& targetPort);
 	void attemptToReconnectInput(size_t inputIndex, const QString& sourcePort);
-
-	void startProcessing() override;
-	void stopProcessing() override;
 
 	void registerPort(AudioBusHandle* port) override;
 	void unregisterPort(AudioBusHandle* port) override;
@@ -122,17 +122,12 @@ private:
 	jack_client_t* m_client;
 
 	bool m_active;
-	std::atomic<bool> m_stopped;
 
 	std::atomic<MidiJack*> m_midiClient;
 	std::vector<jack_port_t*> m_outputPorts;
 	std::vector<jack_port_t*> m_inputPorts;
 	jack_default_audio_sample_t** m_tempOutBufs;
 	std::vector<SampleFrame> m_inputFrameBuffer;
-	SampleFrame* m_outBuf;
-
-	f_cnt_t m_framesDoneInCurBuf;
-	f_cnt_t m_framesToDoInCurBuf;
 
 #ifdef AUDIO_BUS_HANDLE_SUPPORT
 	struct StereoPort

commit 1d04b4e2a872739c5f07be04b1ca78aed3a9314b

Fix Sndio backend & cleanup compilation on OpenBSD (#8407)
- Resolves #8406 by fixing a sndio backend regression introduced in #7568
- Sets `Qt[5-6]_DIR` and `Qt[5-6]Test_DIR` automatically for OpenBSD, similar to how this is handled for macOS
- Add `$LOCALBASE` and `$X11BASE` to include and link paths when compiling for OpenBSD
- Explicitly disable several features by default when compiling for OpenBSD, as is done in the ports tree

CMakeLists.txt
@@ -150,6 +150,32 @@ ELSE(LMMS_BUILD_WIN32)
 	SET(STATUS_WINMM "<not supported on this platform>")
 ENDIF(LMMS_BUILD_WIN32)
 
+IF(LMMS_BUILD_OPENBSD)
+	# “don't link deprecated ossaudio on OpenBSD”
+	# https://cvsweb.openbsd.org/checkout/ports/audio/lmms/patches/patch-CMakeLists_txt?rev=1.3
+	SET(WANT_OSS OFF)
+	SET(STATUS_OSS "<not supported on this platform>")
+	# LMMS finds SDL and compiles fine with it enabled, but it doesn't
+	# appear as an option when choosing an audio backend in settings for
+	# some reason. This might be fixable, but the OpenBSD ports
+	# maintainers have also explicitly disabled SDL since 2010-10-28.
+	# https://cvsweb.openbsd.org/checkout/ports/audio/lmms/Makefile?rev=1.1
+	SET(WANT_SDL OFF)
+	SET(STATUS_SDL "<not supported on this platform>")
+	# “explicitly disable pulseaudio in these ports so they won't pick
+	# it up when it becomes available.”
+	# https://cvsweb.openbsd.org/checkout/ports/audio/lmms/Makefile?rev=1.5
+	SET(WANT_PULSEAUDIO OFF)
+	SET(STATUS_PULSEAUDIO "<not supported on this platform>")
+	# OpenBSD does not and never will have WINE, which is necessary for
+	# Windows VST support.
+	# TODO: Is Linux VST support on OpenBSD possible?
+	SET(WANT_VST OFF)
+	SET(STATUS_VST "<not supported on this platform>")
+	SET(STATUS_VST_32 "<not supported on this platform>")
+	SET(STATUS_VST_64 "<not supported on this platform>")
+ENDIF()
+
 SET(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 CHECK_INCLUDE_FILES(pthread.h LMMS_HAVE_PTHREAD_H)
@@ -165,8 +191,6 @@ CHECK_INCLUDE_FILES(sys/soundcard.h LMMS_HAVE_SYS_SOUNDCARD_H)
 CHECK_INCLUDE_FILES(soundcard.h LMMS_HAVE_SOUNDCARD_H)
 CHECK_INCLUDE_FILES(fcntl.h LMMS_HAVE_FCNTL_H)
 CHECK_INCLUDE_FILES(sys/ioctl.h LMMS_HAVE_SYS_IOCTL_H)
-CHECK_INCLUDE_FILES(ctype.h LMMS_HAVE_CTYPE_H)
-CHECK_INCLUDE_FILES(string.h LMMS_HAVE_STRING_H)
 CHECK_INCLUDE_FILES(process.h LMMS_HAVE_PROCESS_H)
 CHECK_INCLUDE_FILES(locale.h LMMS_HAVE_LOCALE_H)
 

cmake/modules/DetectMachine.cmake
@@ -192,3 +192,12 @@ IF(LMMS_BUILD_APPLE)
 	STRING(REGEX REPLACE "\\.[0-9]*$" "" APPLE_OS_VER "${APPLE_OS_VER}")
 	SET(CMAKE_MACOSX_RPATH 1)	
 ENDIF()
+
+IF(LMMS_BUILD_OPENBSD)
+	SET(Qt5_DIR "${OPENBSD_LOCALBASE}/lib/qt5/cmake/Qt5/")
+	SET(Qt5Test_DIR "${OPENBSD_LOCALBASE}/lib/qt5/cmake/Qt5Test/")
+	SET(Qt6_DIR "${OPENBSD_LOCALBASE}/lib/qt6/cmake/Qt6/")
+	SET(Qt6Test_DIR "${OPENBSD_LOCALBASE}/lib/qt6/cmake/Qt6Test/")
+	INCLUDE_DIRECTORIES("${OPENBSD_LOCALBASE}/include/" "${OPENBSD_X11BASE}/include/")
+	LINK_DIRECTORIES("${OPENBSD_LOCALBASE}/lib/" "${OPENBSD_X11BASE}/lib/")
+ENDIF()

doc/Doxyfile.in
@@ -1981,8 +1981,6 @@ PREDEFINED             = "LMMS_BUILD_LINUX=" \
                          "LMMS_HAVE_SOUNDCARD_H=" \
                          "LMMS_HAVE_FCNTL_H=" \
                          "LMMS_HAVE_SYS_IOCTL_H=" \
-                         "LMMS_HAVE_CTYPE_H=" \
-                         "LMMS_HAVE_STRING_H=" \
                          "LMMS_HAVE_PROCESS_H=" \
                          "LMMS_HAVE_LOCALE_H=" \
 

src/core/audio/AudioSndio.cpp
@@ -68,7 +68,8 @@ AudioSndio::AudioSndio(bool& _success_ful, AudioEngine* _audioEngine)
 	sio_initpar(&m_par);
 
 	m_par.pchan = channels();
-	m_par.bits = 16;
+	m_par.bits = sizeof(int_sample_t) * 8;
+	m_par.sig = 1; // int_sample_t must be signed
 	m_par.le = SIO_LE_NATIVE;
 	m_par.rate = sampleRate();
 	m_par.round = audioEngine()->framesPerAudioBuffer();
@@ -130,12 +131,25 @@ void AudioSndio::stopProcessingImpl()
 void AudioSndio::run()
 {
 	const auto framesPerAudioBuffer = audioEngine()->framesPerAudioBuffer();
-	auto buf = std::vector<float>(framesPerAudioBuffer * channels());
+	const auto samplesPerAudioBuffer = framesPerAudioBuffer * channels();
+	auto fbuf = std::vector<sample_t>(samplesPerAudioBuffer);
+	auto ibuf = std::vector<int_sample_t>(samplesPerAudioBuffer);
 
 	while (AudioDevice::isRunning())
 	{
-		audioEngine()->renderNextBuffer({buf.data(), channels(), framesPerAudioBuffer});
-		sio_write(m_hdl, buf.data(), buf.size() * sizeof(float));
+		audioEngine()->renderNextBuffer({fbuf.data(), channels(), framesPerAudioBuffer});
+
+		// Sndio doesn't speak float, so convert samples to signed int.
+		// While convertToS16() exists, it is intentionally not used
+		// here. There is no need to convert endian-ness since sndio was
+		// initialized with SIO_LE_NATIVE, and there's no reason to
+		// also convert to SampleFrame.
+		for (auto i = 0u; i < samplesPerAudioBuffer; ++i)
+		{
+			ibuf[i] = static_cast<int_sample_t>(AudioEngine::clip(fbuf[i]) * OUTPUT_SAMPLE_MULTIPLIER);
+		}
+
+		sio_write(m_hdl, ibuf.data(), ibuf.size() * sizeof(int_sample_t));
 	}
 }
 

src/lmmsconfig.h.in
@@ -52,7 +52,5 @@
 #cmakedefine LMMS_HAVE_SOUNDCARD_H
 #cmakedefine LMMS_HAVE_FCNTL_H
 #cmakedefine LMMS_HAVE_SYS_IOCTL_H
-#cmakedefine LMMS_HAVE_CTYPE_H
-#cmakedefine LMMS_HAVE_STRING_H
 #cmakedefine LMMS_HAVE_PROCESS_H
 #cmakedefine LMMS_HAVE_LOCALE_H