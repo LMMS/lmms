f6a308536b8ecb1739846655774ea914f71c191e
Dalton Messmer <messmer.dalton@gmail.com>
2026-07-11 00:36:45 -0400

    Add CPU feature detection for MSVC ARM64 builds

--- cmake/modules/CheckCPUFeatures.cmake
+++ cmake/modules/CheckCPUFeatures.cmake
@@ -26,19 +26,26 @@ function(determine_msvc_native_arch _arch_option_out)
 		message(FATAL_ERROR "Cross-compiling is not supported")
 	endif()
 
-	if(NOT LMMS_HOST_X86_64)
-		message(WARNING "CPU feature detection is only implemented for x86_64 - using default /arch option.")
+	if(NOT LMMS_HOST_X86_64 AND NOT LMMS_HOST_ARM64)
+		message(WARNING "CPU feature detection is only implemented for x86_64 and arm64 - using default /arch option.")
 		set("${_arch_option_out}" "" CACHE INTERNAL "Native /arch option")
 		return()
 	endif()
 
 	set(_source [[
 		#include <iostream>
-		#include <immintrin.h>
-		#include <isa_availability.h>
+		#if defined(_M_X64)
+		#	include <immintrin.h>
+		#	include <isa_availability.h>
+		#elif defined(_M_ARM64)
+		#	include <Windows.h>
+		#else
+		#	error Unsupported platform
+		#endif
 
 		auto main() -> int
 		{
+		#if defined(_M_X64)
 			if (__check_isa_support(__IA_SUPPORT_AVX10_2, 0)) {
 				std::cout << "/arch:AVX10.2";
 			} else if (__check_isa_support(__IA_SUPPORT_AVX10, 0)) {
@@ -54,6 +61,62 @@ function(determine_msvc_native_arch _arch_option_out)
 			} else {
 				std::cout << "";
 			}
+		#else
+		#	if defined(PF_ARM_SME2_INSTRUCTIONS_AVAILABLE)
+			if (IsProcessorFeaturePresent(PF_ARM_SME2_INSTRUCTIONS_AVAILABLE) != FALSE) {
+				std::cout << "/arch:armv9.4";
+			} else
+		#	endif
+		#	if defined(PF_ARM_SME_INSTRUCTIONS_AVAILABLE)
+			if (IsProcessorFeaturePresent(PF_ARM_SME_INSTRUCTIONS_AVAILABLE) != FALSE) {
+				std::cout << "/arch:armv9.2";
+			} else
+		#	endif
+		#	if defined(PF_ARM_SVE2_INSTRUCTIONS_AVAILABLE)
+			if (IsProcessorFeaturePresent(PF_ARM_SVE2_INSTRUCTIONS_AVAILABLE) != FALSE) {
+				std::cout << "/arch:armv9.0";
+			} else
+		#	endif
+		#	if defined(PF_ARM_V83_JSCVT_INSTRUCTIONS_AVAILABLE) && defined(PF_ARM_V83_LRCPC_INSTRUCTIONS_AVAILABLE)
+			if (IsProcessorFeaturePresent(PF_ARM_V83_JSCVT_INSTRUCTIONS_AVAILABLE) != FALSE
+				&& IsProcessorFeaturePresent(PF_ARM_V83_LRCPC_INSTRUCTIONS_AVAILABLE) != FALSE) {
+				std::cout << "/arch:armv8.3";
+			} else
+		#	endif
+		#	if defined(PF_ARM_V82_DP_INSTRUCTIONS_AVAILABLE)
+			if (IsProcessorFeaturePresent(PF_ARM_V82_DP_INSTRUCTIONS_AVAILABLE) != FALSE) {
+		#		if defined(PF_ARM_V83_LRCPC_INSTRUCTIONS_AVAILABLE)
+				if (IsProcessorFeaturePresent(PF_ARM_V83_LRCPC_INSTRUCTIONS_AVAILABLE) != FALSE) {
+					std::cout << "/arch:armv8.2+rcpc";
+				} else
+		#		endif
+				{
+					std::cout << "/arch:armv8.2";
+				}
+			} else
+		#	endif
+		#	if defined(PF_ARM_SVE_INSTRUCTIONS_AVAILABLE)
+			if (IsProcessorFeaturePresent(PF_ARM_SVE_INSTRUCTIONS_AVAILABLE) != FALSE) {
+		#		if defined(PF_ARM_V83_LRCPC_INSTRUCTIONS_AVAILABLE)
+				if (IsProcessorFeaturePresent(PF_ARM_V83_LRCPC_INSTRUCTIONS_AVAILABLE) != FALSE) {
+					std::cout << "/arch:armv8.2+rcpc";
+				} else
+		#		endif
+				{
+					std::cout << "/arch:armv8.2";
+				}
+			} else
+		#	endif
+			if (IsProcessorFeaturePresent(PF_ARM_V8_INSTRUCTIONS_AVAILABLE) != FALSE) {
+				if (IsProcessorFeaturePresent(PF_ARM_V81_ATOMIC_INSTRUCTIONS_AVAILABLE) != FALSE) {
+					std::cout << "/arch:armv8.0+lse";
+				} else {
+					std::cout << "/arch:armv8.0";
+				}
+			} else {
+				std::cout << "";
+			}
+		#endif
 		}
 	]])
 
