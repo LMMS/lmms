680afeb4986a6661396dae3e6bf9cf39d6dc8075
Sotonye Atemie <sakertooth@gmail.com>
2026-07-13 10:29:34 -0400

    Do not play the sample when there is no buffer, or the buffer is empty

--- src/core/Sample.cpp
+++ src/core/Sample.cpp
@@ -98,6 +98,8 @@ auto Sample::operator=(Sample&& other) noexcept -> Sample&
 
 bool Sample::play(SampleFrame* dst, PlaybackState* state, size_t numFrames, Loop loop, double ratio) const
 {
+	if (!m_buffer || m_buffer->size() == 0) { return false; }
+
 	state->m_frameIndex = std::max<int>(m_startFrame, state->m_frameIndex);
 
 	const auto sampleRateRatio = static_cast<double>(Engine::audioEngine()->outputSampleRate()) / m_buffer->sampleRate();
