71d73447bed8bc25e9472ac711c3765a7002d9bc
Hyunjin Song <tteu.ingog@gmail.com>
2026-08-29 22:49:11 +0900

    Fix a wrong merge conflict resolution

--- plugins/GigPlayer/GigPlayer.h
+++ plugins/GigPlayer/GigPlayer.h
@@ -218,7 +218,7 @@ public:
 	bool isRelease; // Whether this is a release sample, changes when we delete it
 	GigState state;
 	float frequency;
-	std::vector<GigSample> samples;
+	std::list<GigSample> samples;
 
 	// Used to determine which note should be released on key up
 	//
